ea0bee55f75a1847f26666ccad13fe902aeda6c9
William Hart <hart_wb@yahoo.com>
2009-09-02 18:26:38 +0100

    Added code, tests and docs for is_probabprime_fermat.

---
+++ ulong_extras/is_probabprime_fermat.c
@@ -0,0 +1,45 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2008 Peter Shrimpton
+   Copyright (C) 2009 William Hart
+   
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int n_is_probabprime_fermat(mp_limb_t n, mp_limb_t i)
+{
+	if (FLINT_BIT_COUNT(n) <= 53) return (n_powmod(i, n - 1, n) == 1UL);
+   else 
+   {
+      if ((mp_limb_signed_t) (n - 1) < 0L)
+      {
+         mp_limb_t temp = n_powmod2(i, (n - 1)/2, n);
+         return (n_powmod2(temp, 2, n) == 1UL);
+      } else
+      {
+         return (n_powmod2(i, n - 1, n) == 1UL);
+      }
+   }
+}
---
+++ ulong_extras/test/t-is_probabprime_fermat.c
@@ -0,0 +1,108 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   ulong count = 0UL;
+   printf("is_probabprime_fermat....");
+   fflush(stdout);
+   
+   for (ulong i = 0; i < 100000UL; i++) /* Test that primes pass the test */
+   {
+      mp_limb_t d, j, g;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randtest_not_zero();
+         if (d == 1UL) d++;
+         mpz_set_ui(d_m, d);
+         mpz_nextprime(d_m, d_m);
+         d = mpz_get_ui(d_m);
+      } while (mpz_size(d_m) > 1);
+
+      do
+      {
+         j = n_randint(d);
+         if ((j == 1L) && (d != 2UL)) j++;
+      } while (n_gcd(d, j) != 1UL);
+
+      result = n_is_probabprime_fermat(d, j);
+      
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("d = %lu is declared composite\n", d); 
+         abort();
+      }
+
+      mpz_clear(d_m);
+   }
+         
+   for (ulong i = 0; i < 100000UL; i++) /* Test that not too many composites pass */
+   {
+      mp_limb_t d, j, g;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randtest_not_zero();
+         if (d == 1UL) d++;
+         mpz_set_ui(d_m, d);
+      } while (mpz_probab_prime_p(d_m, 12));
+
+      do
+      {
+         j = n_randint(d);
+         if ((j == 1L) && (d != 2UL)) j++;
+      } while (n_gcd(d, j) != 1UL);
+
+      if (n_is_probabprime_fermat(d, j)) count++;
+      
+      mpz_clear(d_m);
+   }
+
+   result = (count < 1000UL);
+   if (!result)
+   {
+      printf("FAIL\n");
+      printf("%lu composites declared prime\n", count); 
+      abort();
+   }
+   
+   printf("PASS\n");
+   return 0;
+}
