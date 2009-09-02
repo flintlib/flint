87ae0dd2a87d1cb33a8fa11cf85e5aec56ad3343
William Hart <hart_wb@yahoo.com>
2009-09-02 18:37:20 +0100

    Added code, tests and docs for factor.

---
+++ ulong_extras/factor.c
@@ -0,0 +1,98 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int is_prime(mp_limb_t n, int proved)
+{
+   if (proved) return n_is_prime(n);
+   else return n_is_probabprime(n);
+}
+
+void n_factor(n_factor_t * factors, mp_limb_t n, int proved)
+{
+   ulong factor_arr[FLINT_MAX_FACTORS_IN_LIMB];
+   ulong exp_arr[FLINT_MAX_FACTORS_IN_LIMB];
+   ulong exp;
+   ulong factors_left, k;
+   mp_limb_t cofactor, factor, cutoff;
+
+   cofactor = n_factor_trial(factors, n, FLINT_FACTOR_TRIAL_PRIMES);
+   if (cofactor == 1UL) return;
+   if (is_prime(cofactor, proved)) 
+   {
+      n_factor_insert(factors, cofactor, 1UL);
+      return;
+   }
+
+   factor_arr[0] = cofactor;
+   factors_left = 1;
+   exp_arr[0] = 1;
+
+   cutoff = flint_primes[FLINT_FACTOR_TRIAL_PRIMES - 1]*flint_primes[FLINT_FACTOR_TRIAL_PRIMES - 1];
+
+   while (factors_left > 0)
+   {
+      factor = factor_arr[factors_left - 1];
+
+      if (factor >= cutoff)
+		{
+	      if (cofactor = n_factor_power235(&exp, factor)) 
+         {
+            exp_arr[factors_left - 1] *= exp;
+            factor_arr[factors_left - 1] = factor = cofactor;
+         }
+           
+         if ((factor >= cutoff) && !is_prime(factor, proved))
+		   {
+		      if (((factor < FLINT_FACTOR_ONE_LINE_MAX) &&
+                 (cofactor = n_factor_one_line(factor, FLINT_FACTOR_ONE_LINE_ITERS))) 
+              || (cofactor = n_factor_SQUFOF(factor, FLINT_FACTOR_SQUFOF_ITERS)))
+				{
+					exp_arr[factors_left] = exp_arr[factors_left - 1];
+               factor_arr[factors_left] = cofactor;
+               factor_arr[factors_left - 1] /= cofactor;
+               factors_left++;
+				} else
+				{
+               printf("Error : failed to factor %ld\n", n);
+               abort();
+				}
+         } else
+			{
+				n_factor_insert(factors, factor, exp_arr[factors_left - 1]);
+            factors_left--;
+			}
+		} else
+		{
+			n_factor_insert(factors, factor, exp_arr[factors_left - 1]);
+         factors_left--;
+		}
+   } 
+}
---
+++ ulong_extras/test/t-factor.c
@@ -0,0 +1,67 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("factor....");
+   fflush(stdout);
+ 
+   for (ulong i = 0; i < 10000UL; i++) /* Test random numbers */
+   {
+      mp_limb_t n1, n2;
+      n_factor_t factors;
+
+      n_factor_init(&factors);
+
+      n1 = n_randtest_not_zero();
+      n_factor(&factors, n1, 0);
+      
+      n2 = 1UL;
+
+      for (ulong i = 0; i < factors.num; i++)
+      {
+         n2 *= n_pow(factors.p[i], factors.exp[i]);
+      }
+
+      result = (n1 == n2);
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("n1 = %lu, n2 = %lu\n", n1, n2); 
+         abort();
+      }
+   }
+   
+   printf("PASS\n");
+   return 0;
+}
