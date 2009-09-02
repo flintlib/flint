405f6c1ad88a9d61bdc868f09e276ff7907da898
William Hart <hart_wb@yahoo.com>
2009-09-02 18:34:34 +0100

    Added code, tests and docs for is_prime_pocklington.

---
+++ ulong_extras/is_prime_pocklington.c
@@ -0,0 +1,85 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int n_is_prime_pocklington(mp_limb_t n, ulong iterations)
+{
+	int i, j, k, pass, exp;
+	mp_limb_t n1, cofactor, b, c, ninv;
+	n_factor_t factors;
+
+   if (n % 2 == 0)
+   {
+	   if (n == 2UL) return 1;
+	   else return 0;
+	}
+
+   n1 = n - 1;
+	
+   n_factor_init(&factors);
+
+   cofactor = n_factor_partial1(&factors, n1);
+
+   if (cofactor*cofactor >= n1) return -1;
+
+   ninv = n_preinvert_limb(n);
+
+   for (i = factors.num - 1; i >= 0 ; i--)
+	{		
+		pass = 0;
+		c = 1;
+		mp_limb_t exp = n1/factors.p[i];
+		
+		for (j = 2; j < iterations && pass == 0; j++)
+		{
+			b = n_powmod2_preinv(j, exp, n, ninv);
+         if (n_powmod2_preinv(b, factors.p[i], n, ninv) != 1UL) return 0;
+
+         b = n_submod(b, 1UL, n);
+		   if (b != 0UL)
+			{
+			   c = n_mulmod2_preinv(c, b, n, ninv);	
+				pass = 1;
+			}
+			
+			if (c == 0)
+			{
+				return 0;
+			}
+		}
+
+		if (j == iterations)
+      {
+			return -1;
+		}
+	}
+
+	if (n_gcd(n, c) != 1UL) return 0;
+
+	return 1;
+}
