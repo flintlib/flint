59762d5007a81d7c53eebc6e734fa9676e1900fd
William Hart <hart_wb@yahoo.com>
2009-09-02 17:47:20 +0100

    Added code for basic profiler.

---
+++ profiler.c
@@ -0,0 +1,106 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/******************************************************************************
+
+ (C) 2007 William Hart and David Harvey
+
+******************************************************************************/
+
+#include "profiler.h"
+#include <math.h>
+
+
+/*
+   clock_last[i] is the last read clock value for clock #i.
+   clock_accum[i] is the total time attributed to clock #i so far.
+   These should not be read directly; use get_clock(i) instead.
+ */
+double clock_last[FLINT_NUM_CLOCKS];
+double clock_accum[FLINT_NUM_CLOCKS];
+
+
+void prof_repeat(double* min, double* max, profile_target_t target, void* arg)
+{
+   // the number of timings that were at least DURATION_THRESHOLD microseconds:
+   unsigned long good_count = 0;
+   double max_time, min_time;
+
+   // first try one loop
+   unsigned long num_trials = 1;
+   init_clock(0);
+   target(arg, num_trials);
+   double last_time = get_clock(0);
+
+   // loop until we have enough good times
+   while (1)
+   {
+      double per_trial = last_time / num_trials;
+      
+      // if the last recorded time was long enough, record it
+      if (last_time > DURATION_THRESHOLD)
+      {
+         if (good_count)
+         {
+            if (per_trial > max_time)
+               max_time = per_trial;
+            if (per_trial < min_time)
+               min_time = per_trial;
+         }
+         else
+            max_time = min_time = per_trial;
+
+         if (++good_count == 5)
+         {
+            // we've got enough data
+            break;
+         }
+      }
+
+      // adjust num_trials so that the elapsed time gravitates towards
+      // DURATION_TARGET; num_trials can be changed by a factor of
+      // at most 25%, and must be at least 1
+      if (last_time < 0.0001)
+         last_time = 0.0001;
+      double adjust_ratio = DURATION_TARGET / last_time;
+      if (adjust_ratio > 1.25)
+         adjust_ratio = 1.25;
+      if (adjust_ratio < 0.75)
+         adjust_ratio = 0.75;
+      num_trials = (unsigned long) ceil(adjust_ratio * num_trials);
+      // just to be safe:
+      if (num_trials == 0)
+         num_trials = 1;
+
+      // run another trial
+      init_clock(0);
+      target(arg, num_trials);
+      last_time = get_clock(0);
+   }
+   
+   // store results
+   if (min)
+      *min = min_time;
+   if (max)
+      *max = max_time;
+}
+
+
+
+// end of file ****************************************************************
---
+++ profiler.h
@@ -0,0 +1,144 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/******************************************************************************
+
+ (C) 2007 William Hart and David Harvey
+
+******************************************************************************/
+
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#ifndef FLINT_PROFILER_H
+#define FLINT_PROFILER_H
+
+typedef struct
+{
+   long cpu;
+   long wall;
+} timeit_t[1];
+
+static inline
+void timeit_start(timeit_t t)
+{
+   struct timeval tv;
+   gettimeofday(&tv, 0);
+   t->wall = - tv.tv_sec * 1000 - tv.tv_usec / 1000;
+   t->cpu = - clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+static inline
+void timeit_stop(timeit_t t)
+{
+   struct timeval tv;
+   gettimeofday(&tv, 0);
+   t->wall += tv.tv_sec * 1000 + tv.tv_usec / 1000;
+   t->cpu += clock() * 1000 / CLOCKS_PER_SEC;
+}
+
+/******************************************************************************
+
+   Timer based on the cycle counter
+   
+******************************************************************************/
+
+#define FLINT_NUM_CLOCKS 20
+
+#define FLINT_CLOCKSPEED 2800000000.0
+
+extern double clock_last[FLINT_NUM_CLOCKS];
+extern double clock_accum[FLINT_NUM_CLOCKS];
+
+static inline 
+double get_cycle_counter()
+{
+   unsigned int hi;
+   unsigned int lo;
+
+   __asm("rdtsc; movl %%edx,%0; movl %%eax,%1" 
+       : "=r" (hi), "=r" (lo)
+       : 
+       : "%edx", "%eax");
+
+   return (double) hi * (1 << 30) * 4 + lo;
+}
+
+#define FLINT_CLOCK_SCALE_FACTOR (1000000.0 / FLINT_CLOCKSPEED)
+
+static inline 
+void init_clock(unsigned long n)
+{
+   clock_accum[n] = 0.0;
+}
+
+static inline 
+void init_all_clocks()
+{
+   for (unsigned long i = 0; i < FLINT_NUM_CLOCKS; i++)
+      clock_accum[i] = 0.0;
+}
+
+static inline 
+double get_clock(unsigned long n)
+{
+   return clock_accum[n] * FLINT_CLOCK_SCALE_FACTOR;
+}
+
+static inline 
+void start_clock(unsigned long n)
+{
+   clock_last[n] = get_cycle_counter();
+}
+
+static inline 
+void stop_clock(unsigned long n)
+{
+   double now = get_cycle_counter();
+   clock_accum[n] += (now - clock_last[n]);
+}
+
+/******************************************************************************
+
+   Framework for repeatedly sampling a single target
+   
+******************************************************************************/
+
+static inline
+void prof_start()
+{
+   start_clock(0);
+}
+
+static inline
+void prof_stop()
+{
+   stop_clock(0);
+}
+
+typedef void (*profile_target_t)(void* arg, unsigned long count);
+
+void prof_repeat(double* min, double* max, profile_target_t target, void* arg);
+
+#define DURATION_THRESHOLD 500
+
+#define DURATION_TARGET 1000
+
+#endif // #ifndef FLINT_PROFILER_H
