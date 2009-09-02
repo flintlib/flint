2a3dd6762a297c07fc7e65f6f3a7d587d9db9e5b
William Hart <hart_wb@yahoo.com>
2009-09-02 18:24:04 +0100

    Inserted docs and code for factor_insert and docs, test and code for factor_trial and factor_trial_range.

---
+++ ulong_extras/factor_insert.c
@@ -0,0 +1,48 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+void n_factor_insert(n_factor_t * factors, mp_limb_t p, ulong exp)
+{
+   ulong i;
+   
+   for (i = 0; i < factors->num; i++)
+   {
+      if (factors->p[i] == p) break;
+   }
+   
+   if (i != factors->num) 
+   {
+      factors->exp[i] += exp;
+   } else
+   {
+      factors->p[i] = p;
+      factors->exp[i] = exp;
+      factors->num++;
+   }
+}
---
+++ ulong_extras/factor_trial_range.c
@@ -0,0 +1,48 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+mp_limb_t n_factor_trial_range(n_factor_t * factors, mp_limb_t n, ulong start, ulong num_primes)
+{
+   unsigned int exp;
+   mp_limb_t p;
+   double ppre;
+
+   n_compute_primes(num_primes);
+
+   for (ulong i = start; i < num_primes; i++)
+   {
+      p = flint_primes[i];
+      if (p*p > n) break;
+      ppre = flint_prime_inverses[i];
+      exp = n_remove2_precomp(&n, p, ppre);
+      if (exp) n_factor_insert(factors, p, exp);
+   }
+       
+   return n;
+}
