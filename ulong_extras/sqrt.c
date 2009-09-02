77f3c2ef678c96ca48331beecf86369942550a0a
William Hart <hart_wb@yahoo.com>
2009-09-02 18:13:45 +0100

    Added code, tests and docs for sqrt and sqrtrem.c.

---
+++ ulong_extras/sqrt.c
@@ -0,0 +1,43 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include <math.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+mp_limb_t n_sqrt(mp_limb_t a)
+{
+   mp_limb_t is, res;
+
+   if (a >= 281474976710656UL)
+      is = (mp_limb_t) sqrt((double) a);
+   else
+      is = (mp_limb_t) sqrtf((float) a);
+
+   is -= (is*is > a);
+   if (is == 4294967296UL) is--;
+   return is;
+}
---
+++ ulong_extras/test/t-sqrtrem.c
@@ -0,0 +1,108 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("sqrtrem....");
+   fflush(stdout);
+
+   for (ulong i = 0; i < 100000UL; i++)
+   {
+      mp_limb_t a, r1, r2, s1, s2;
+      mpz_t a_m, r2_m, s2_m;
+      mp_limb_signed_t exp;
+
+      mpz_init(a_m);
+      mpz_init(r2_m);
+      mpz_init(s2_m);
+      
+      a = n_randtest();
+      
+      s1 = n_sqrtrem(&r1, a);
+
+      mpz_set_ui(a_m, a);
+      mpz_sqrtrem(s2_m, r2_m, a_m);
+      r2 = mpz_get_ui(r2_m);
+      s2 = mpz_get_ui(s2_m);
+      
+      result = ((r1 == r2) && (s1 == s2));
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("a = %lu, r1 = %ld, r2 = %lu, s1 = %ld, s2 = %lu\n", a, r1, r2, s1, s2); 
+         abort();
+      }
+
+      mpz_clear(a_m);
+      mpz_clear(r2_m);
+      mpz_clear(s2_m);
+   }
+
+   for (ulong i = 0; i < 100000UL; i++)
+   {
+      mp_limb_t a, r1, r2, s1, s2, bits;
+      mpz_t a_m, r2_m, s2_m;
+      mp_limb_signed_t exp;
+
+      mpz_init(a_m);
+      mpz_init(r2_m);
+      mpz_init(s2_m);
+      
+      bits = n_randint(33);
+      a = n_randbits(bits);
+      a = a*a;
+      a += (n_randint(100) - 50);
+      s1 = n_sqrtrem(&r1, a);
+
+      mpz_set_ui(a_m, a);
+      mpz_sqrtrem(s2_m, r2_m, a_m);
+      r2 = mpz_get_ui(r2_m);
+      s2 = mpz_get_ui(s2_m);
+      
+      result = ((r1 == r2) && (s1 == s2));
+
+      if (!result)
+      {
+         printf("FAIL2\n");
+         printf("a = %lu, r1 = %ld, r2 = %lu, s1 = %ld, s2 = %lu\n", a, r1, r2, s1, s2); 
+         abort();
+      }
+
+      mpz_clear(a_m);
+      mpz_clear(r2_m);
+      mpz_clear(s2_m);
+   }
+
+   printf("PASS\n");
+   return 0;
+}
