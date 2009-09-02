a3275c7a60dfdc3b99d7181739b2240aeaf6a27f
William Hart <hart_wb@yahoo.com>
2009-09-02 17:46:15 +0100

    Added test code for udiv_qrnnd, t-udiv_qrnnd_preinv and t-sdiv_qrnnd.

---
+++ test/t-udiv_qrnnd.c
@@ -0,0 +1,66 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("udiv_qrnnd....");
+   fflush(stdout);
+
+   for (ulong i = 0; i < 1000000UL; i++)
+   {
+      mp_limb_t d, nh, nl, q, r, ph, pl;
+
+      do 
+      {
+         d = n_randtest_not_zero();
+         nh = n_randtest();
+      } while (nh >= d);
+      nl = n_randtest();
+
+      udiv_qrnnd(q, r, nh, nl, d);
+      umul_ppmm(ph, pl, d, q);
+      add_ssaaaa(ph, pl, ph, pl, 0UL, r);
+
+      result = ((ph == nh) && (pl == nl));
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("nh = %lu, nl = %lu, d = %lu\n", nh, nl, d); 
+         printf("ph = %lu, pl = %lu\n", ph, pl);
+         abort();
+      }
+   }
+
+   printf("PASS\n");
+   return 0;
+}
---
+++ test/t-udiv_qrnnd_preinv.c
@@ -0,0 +1,69 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("udiv_qrnnd_preinv....");
+   fflush(stdout);
+
+   for (ulong i = 0; i < 1000000UL; i++)
+   {
+      mp_limb_t d, dinv, nh, nl, q1, r1, q2, r2, norm;
+
+      do 
+      {
+         d = n_randtest_not_zero();
+         nh = n_randtest();
+         count_leading_zeros(norm, d);
+         d <<= norm;
+      } while (nh >= d);
+      nl = n_randtest();
+
+      invert_limb(dinv, d);
+
+      udiv_qrnnd_preinv(q1, r1, nh, nl, d, dinv);
+      udiv_qrnnd(q2, r2, nh, nl, d);
+
+      result = ((q1 == q2) && (r1 == r2));
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("nh = %lu, nl = %lu, d = %lu, dinv = %lu\n", nh, nl, d, dinv); 
+         printf("q1 = %lu, q2 = %lu, r1 = %lu, r2 = %lu\n", q1, q2, r1, r2);
+         abort();
+      }
+   }
+
+   printf("PASS\n");
+   return 0;
+}
