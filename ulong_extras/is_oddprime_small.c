b2f24802d2b692909399fd6a9ece0f96811c0315
William Hart <hart_wb@yahoo.com>
2009-09-02 18:21:49 +0100

    Added code, tests and docs for is_oddprime_small.

---
+++ ulong_extras/is_oddprime_small.c
@@ -0,0 +1,51 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 Tom Boothby
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+mp_limb_t FLINT_ODD_PRIME_LOOKUP[] =
+{
+    0x816d129a64b4cb6eUL, 0x2196820d864a4c32UL, 0xa48961205a0434c9UL,
+    0x4a2882d129861144UL, 0x834992132424030UL,  0x148a48844225064bUL,
+    0xb40b4086c304205UL,  0x65048928125108a0UL, 0x80124496804c3098UL,
+    0xc02104c941124221UL, 0x804490000982d32UL,  0x220825b082689681UL,
+    0x9004265940a28948UL, 0x6900924430434006UL, 0x12410da408088210UL,
+    0x86122d22400c060UL,  0x110d301821b0484UL,  0x14916022c044a002UL,
+    0x92094d204a6400cUL,  0x4ca2100800522094UL, 0xa48b081051018200UL,
+    0x34c108144309a25UL,  0x2084490880522502UL, 0x241140a218003250UL,
+    0xa41a00101840128UL,  0x2926000836004512UL, 0x10100480c0618283UL,
+    0xc20c26584822006dUL, 0x4520582024894810UL, 0x10c0250219002488UL,
+    0x802832ca01140868UL, 0x60901300264b0400UL
+};
+    
+int n_is_oddprime_small(mp_limb_t n) 
+{
+    mp_limb_t q = n/2;
+    mp_limb_t x = (q & 63UL);
+    return (FLINT_ODD_PRIME_LOOKUP[q/64] & (1UL<<x))>>x;
+}
---
+++ ulong_extras/test/t-is_oddprime_small.c
@@ -0,0 +1,92 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("is_oddprime_small....");
+   fflush(stdout);
+   
+   for (ulong i = 0; i < 100000UL; i++) /* Test that primes pass the test */
+   {
+      mp_limb_t d;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randint(FLINT_ODDPRIME_SMALL_CUTOFF) | 1;
+         mpz_set_ui(d_m, d);
+         mpz_nextprime(d_m, d_m);
+         d = mpz_get_ui(d_m);
+      } while (d > FLINT_ODDPRIME_SMALL_CUTOFF);
+
+      result = n_is_oddprime_small(d);
+      
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("d = %lu is declared composite\n", d); 
+         abort();
+      }
+
+      mpz_clear(d_m);
+   }
+         
+   for (ulong i = 0; i < 100000UL; i++) /* Test that not too many composites pass */
+   {
+      mp_limb_t d;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randint(FLINT_ODDPRIME_SMALL_CUTOFF) | 1;
+         mpz_set_ui(d_m, d);
+      } while ((mpz_probab_prime_p(d_m, 12)) || (d > FLINT_ODDPRIME_SMALL_CUTOFF));
+
+      result = !n_is_oddprime_small(d);
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("d = %lu is declared prime\n", d); 
+         abort();
+      }
+
+      mpz_clear(d_m);
+   }
+
+   printf("PASS\n");
+   return 0;
+}
