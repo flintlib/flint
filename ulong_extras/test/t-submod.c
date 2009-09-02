53c19922535a2390df4aff685333066548d5e942
William Hart <hart_wb@yahoo.com>
2009-09-02 18:04:27 +0100

    Added documentation and test code for addmod and submof which are defined in ulong_extras.h.

---
+++ ulong_extras/test/t-submod.c
@@ -0,0 +1,65 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("submod....");
+   fflush(stdout);
+
+   for (ulong i = 0; i < 1000000UL; i++)
+   {
+      mp_limb_t a, b, d, r1, r2, s1;
+      
+      d = n_randtest();
+      a = n_randint(d);
+      b = n_randint(d);
+      
+      r1 = n_submod(a, b, d);
+
+      add_ssaaaa(s1, r2, 0UL, a, 0UL, d);
+      sub_ddmmss(s1, r2, s1, r2, 0UL, b);
+      if ((s1) || (r2 >= d)) r2 -= d;
+      
+      result = (r1 == r2);
+
+      if (!result)
+      {
+         printf("FAIL\n");
+         printf("a = %lu, b = %lu, d = %lu\n", a, b, d); 
+         printf("r1 = %lu, r2 = %lu\n", r1, r2);
+         abort();
+      }
+   }
+
+   printf("PASS\n");
+   return 0;
+}
