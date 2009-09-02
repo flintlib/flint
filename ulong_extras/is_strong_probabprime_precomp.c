f481ab78c80bb7e30008cb9d60cedfa8387d1ded
William Hart <hart_wb@yahoo.com>
2009-09-02 18:48:11 +0100

    Added code test and documentation for is_strong_probabprime_precomp and is_strong_probabprime2_preinv.

---
+++ ulong_extras/is_strong_probabprime_precomp.c
@@ -0,0 +1,48 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int n_is_strong_probabprime_precomp(mp_limb_t n, double npre, mp_limb_t a, mp_limb_t d)
+{
+   mp_limb_t t = d;
+   mp_limb_t y;
+      
+   y = n_powmod_precomp(a, t, n, npre);
+
+   if (y == 1UL) return 1;
+   t <<= 1;
+
+   while ((t != n - 1) && (y != n - 1))
+   {
+      y = n_mulmod_precomp(y, y, n, npre);
+      t <<= 1;
+   }
+   
+   return (y == n - 1);
+}
+
---
+++ ulong_extras/test/t-is_strong_probabprime2_preinv.c
@@ -0,0 +1,113 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("is_strong_probabprime2_preinv....");
+   fflush(stdout);
+   ulong count = 0UL;
+
+   for (ulong i = 0; i < 1000UL; i++) /* Test that primes pass the test */
+   {
+      mp_limb_t a, d, dinv, norm;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randtest() | 1;
+         mpz_set_ui(d_m, d);
+         mpz_nextprime(d_m, d_m);
+         d = mpz_get_ui(d_m);
+      } while (mpz_size(d_m) > 1);
+      if (d == 2UL) d++;
+         
+      for (ulong j = 0; j < 100; j++)
+      {
+         do a = n_randint(d);
+         while (a == 0UL);
+      
+         dinv = n_preinvert_limb(d);
+         count_trailing_zeros(norm, d - 1);
+         result = n_is_strong_probabprime2_preinv(d, dinv, a, (d - 1)>>norm);
+
+         if (!result)
+         {
+            printf("FAIL\n");
+            printf("a = %lu, d = %lu\n", a, d); 
+            abort();
+         }
+      }
+
+      mpz_clear(d_m);
+   }
+         
+   for (ulong i = 0; i < 1000UL; i++) /* Test that not too many composites pass */
+   {
+      mp_limb_t a, d, dinv, norm;
+      mpz_t d_m;
+      
+      mpz_init(d_m);
+
+      do
+      {
+         d = n_randtest() | 1;
+         if (d == 1UL) d++;
+         mpz_set_ui(d_m, d);
+      } while (mpz_probab_prime_p(d_m, 12));
+
+      for (ulong j = 0; j < 100; j++)
+      {
+         do a = n_randint(d);
+         while (a == 0UL);
+      
+         dinv = n_preinvert_limb(d);
+         count_trailing_zeros(norm, d - 1);
+         result = !n_is_strong_probabprime2_preinv(d, dinv, a, (d - 1)>>norm);
+
+         if (!result) count++;
+      }
+
+      mpz_clear(d_m);
+   }
+
+   if (count > 1000) 
+   {
+      printf("FAIL\n");
+      printf("count = %lu\n", count);
+      abort();
+   }
+
+   printf("PASS\n");
+   return 0;
+}
