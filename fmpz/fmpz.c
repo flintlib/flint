88afcb9819f6ca4542eec30ccce8f3c09fd770ab
William Hart <hart_wb@yahoo.com>
2009-09-04 01:04:49 +0100

    Added the basis of an fmpz module, including basic type and promotion/demotion.

---
+++ fmpz/fmpz.c
@@ -0,0 +1,1 @@
+fmpz/fmpz_reentrant.c
---
+++ fmpz/test/t-fmpz.c
@@ -0,0 +1,63 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+
+int main(void)
+{
+   int result;
+   printf("fmpz....");
+   fflush(stdout);
+   
+   for (ulong i = 0; i < 100000UL; i++) 
+   {
+      fmpz_t a, b;
+
+      mp_limb_t bits = n_randint(63);
+      *a = n_randbits(bits);
+      if (n_randint(2)) *a = -*a;
+      
+      *b = *a;
+
+      _fmpz_promote_val(a);
+      _fmpz_demote_val(a);
+
+      result = (*b == *a);
+      
+      if (!result)
+      {
+         printf("FAIL\n");
+         abort();
+      }
+   }
+
+   printf("PASS\n");
+   return 0;
+}
