56cdfb7ac1e5c5f2349c896d0857c866b4352f21
unknown <wbhart@.(none)>
2009-10-30 05:56:27 +0000

    Fixed t-xgcd to work with 32 bits.

--- ulong_extras/test/t-xgcd.c
+++ ulong_extras/test/t-xgcd.c
@@ -40,9 +40,9 @@ int main(void)
       mp_limb_t a, b, c, g, bits1, bits2, bits3, ph, pl, qh, ql;
       mp_limb_signed_t s, t;
       
-      bits1 = n_randint(63) + 1;
+      bits1 = n_randint(FLINT_BITS-1) + 1;
       bits2 = n_randint(bits1) + 1;
-      bits3 = n_randint(64 - bits1) + 1;
+      bits3 = n_randint(FLINT_BITS - bits1) + 1;
 
       do
       {
