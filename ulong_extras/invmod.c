df1b99d6bb5e40b66c860447998fb7a0ebd9bde5
William Hart <hart_wb@yahoo.com>
2009-11-11 16:03:05 +0000

    Fixed some cut and paste errors (y sustituted for x in license).

--- ulong_extras/invmod.c
+++ ulong_extras/invmod.c
@@ -2,24 +2,24 @@
 
     This file is part of FLINT.
 
-    FLINT is free software; xou can redistribute it and/or modifx
-    it under the terms of the GNU General Public License as published bx
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
-    (at xour option) anx later version.
+    (at your option) any later version.
 
     FLINT is distributed in the hope that it will be useful,
-    but WITHOUT ANx WARRANTx; without even the implied warrantx of
-    MERCHANTABILITx or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    xou should have received a copx of the GNU General Public License
+    You should have received a copy of the GNU General Public License
     along with FLINT; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 ===============================================================================*/
 /****************************************************************************
 
-   Copxright (C) 2009 William Hart
+   Copyright (C) 2009 William Hart
 
 *****************************************************************************/
 
