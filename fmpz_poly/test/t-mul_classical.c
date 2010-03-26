fc798e149eedd40b426f42ade5aefb9acc8bf8a5
William Hart <hart_wb@yahoo.com>
2010-03-26 20:22:32 +0000

    Added test code for mul_classical.

---
+++ fmpz_poly/test/t-mul_classical.c
@@ -0,0 +1,106 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("mul_classical....");
+   fflush(stdout);
+   
+   fmpz_poly_randinit();
+   
+   // check aliasing of a and b
+   for (ulong i = 0; i < 10000UL; i++) 
+   {
+      fmpz_poly_t a, b, c;
+      
+      fmpz_poly_init(a);
+      fmpz_poly_init(b);
+      fmpz_poly_init(c);
+      fmpz_poly_randtest(b, n_randint(100), n_randint(200));
+      fmpz_poly_randtest(c, n_randint(100), n_randint(200));
+   
+	  fmpz_poly_mul_classical(a, b, c);
+      fmpz_poly_mul_classical(b, b, c);
+      
+      result = (fmpz_poly_equal(a, b));
+      if (!result)
+      {
+         printf("Error:\n");
+         fmpz_poly_print(a); printf("\n\n");
+         fmpz_poly_print(b); printf("\n\n");
+         abort();
+      }
+
+      fmpz_poly_clear(a);
+      fmpz_poly_clear(b);
+      fmpz_poly_clear(c);
+   }
+
+   // check (a*n1)*n2 = a*(n1*n2)
+   /*for (ulong i = 0; i < 10000UL; i++) 
+   {
+      fmpz_poly_t a, b, c;
+      ulong n1 = n_randbits(FLINT_BITS/2);
+      ulong n2 = n_randbits(FLINT_BITS/2);
+
+      fmpz_poly_init(a);
+      fmpz_poly_init(b);
+      fmpz_poly_init(c);
+      fmpz_poly_randtest(a, n_randint(100), n_randint(200));
+      
+      fmpz_poly_scalar_mul_ui(b, a, n1);
+      fmpz_poly_scalar_mul_ui(c, b, n2);
+      fmpz_poly_scalar_mul_ui(b, a, n1*n2);
+
+      result = (fmpz_poly_equal(b, c));
+      if (!result)
+      {
+         printf("Error n1 = %lu, n2 = %lu:\n", n1, n2);
+         fmpz_poly_print(a); printf("\n\n");
+         fmpz_poly_print(b); printf("\n\n");
+         fmpz_poly_print(c); printf("\n\n");
+         abort();
+      }
+
+      fmpz_poly_clear(a);
+      fmpz_poly_clear(b);
+      fmpz_poly_clear(c);
+   }*/
+
+   fmpz_poly_randclear();
+      
+   _fmpz_cleanup();
+   printf("PASS\n");
+   return 0;
+}
