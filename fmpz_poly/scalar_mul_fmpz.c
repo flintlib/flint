16e076dfa3634c17a9f2900ac06a08c8b3b418e6
William Hart <hart_wb@yahoo.com>
2010-03-26 21:26:51 +0000

    Added _fmpz_scalar_mul_fmpz.

--- fmpz_poly/scalar_mul_fmpz.c
+++ fmpz_poly/scalar_mul_fmpz.c
@@ -28,34 +28,48 @@
 #include "fmpz.h"
 #include "fmpz_poly.h"
 
+void _fmpz_poly_scalar_mul_fmpz(fmpz * poly1, const fmpz * poly2, ulong len2, const fmpz_t x)
+{
+	long i;
+	fmpz c = *x;
+		 
+	if (!COEFF_IS_MPZ(c))
+	{
+        for (i = 0; i < len2; i++)
+			fmpz_mul_si(poly1 + i, poly2 + i, c);
+	} else
+	{
+        for (i = 0; i < len2; i++)
+			fmpz_mul(poly1 + i, poly2 + i, x);
+	}
+}
+
 void fmpz_poly_scalar_mul_fmpz(fmpz_poly_t poly1, const fmpz_poly_t poly2, const fmpz_t x)
 {
 	// either scalar or input poly is zero
 	if ((*x == 0) || (poly2->length == 0)) 
 	{
-	   fmpz_poly_zero(poly1);
+	    fmpz_poly_zero(poly1);
 		return;
 	}
 	
 	// special case, muliply by 1
 	if (*x == 1L)
 	{
-	   fmpz_poly_set(poly1, poly2);
+	    fmpz_poly_set(poly1, poly2);
 		return;
 	}
 	
 	// special case, muliply by -1
 	if (*x == -1L)
 	{
-	   fmpz_poly_neg(poly1, poly2);
+	    fmpz_poly_neg(poly1, poly2);
 		return;
 	}
 	
 	fmpz_poly_fit_length(poly1, poly2->length);
 	
-	ulong i;
-	for (i = 0; i < poly2->length; i++) 
-		fmpz_mul(poly1->coeffs + i, poly2->coeffs + i, x);
+	_fmpz_poly_scalar_mul_fmpz(poly1->coeffs, poly2->coeffs, poly2->length, x);
 
 	_fmpz_poly_set_length(poly1, poly2->length);
 }
