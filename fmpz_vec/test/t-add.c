f45bc45286e363b1d02a60016746ad4ef92e8ff2
William Hart <hart_wb@yahoo.com>
2010-04-14 21:15:58 +0100

    Added test code for _fmpz_vec_add/sub.

---
+++ fmpz_vec/test/t-add.c
@@ -0,0 +1,106 @@
+/*============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+===============================================================================*/
+/****************************************************************************
+
+   Copyright (C) 2009, 2010 William Hart
+
+*****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_vec.h"
+#include "ulong_extras.h"
+
+int main(void)
+{
+   int result;
+   printf("add....");
+   fflush(stdout);
+   
+   _fmpz_vec_randinit();
+   
+   // check aliasing of a and c
+   for (ulong i = 0; i < 10000UL; i++) 
+   {
+      fmpz * a, * b, * c;
+      ulong length = n_randint(100);
+      
+      a = _fmpz_vec_init(length);
+      b = _fmpz_vec_init(length);
+      c = _fmpz_vec_init(length);
+      _fmpz_vec_randtest(a, length, n_randint(200));
+      _fmpz_vec_randtest(b, length, n_randint(200));
+      
+      _fmpz_vec_add(c, a, b, length);
+      _fmpz_vec_add(a, a, b, length);
+      
+      result = (_fmpz_vec_equal(a, c, length));
+      if (!result)
+      {
+         printf("Error:\n");
+         _fmpz_vec_print(a, length); printf("\n\n");
+         _fmpz_vec_print(c, length); printf("\n\n");
+         abort();
+      }
+
+      _fmpz_vec_clear(a, length);
+      _fmpz_vec_clear(b, length);
+      _fmpz_vec_clear(c, length);
+   }
+
+   // check aliasing of b and c
+   for (ulong i = 0; i < 10000UL; i++) 
+   {
+      fmpz * a, * b, * c;
+      ulong length = n_randint(100);
+      
+      a = _fmpz_vec_init(length);
+      b = _fmpz_vec_init(length);
+      c = _fmpz_vec_init(length);
+      _fmpz_vec_randtest(a, length, n_randint(200));
+      _fmpz_vec_randtest(b, length, n_randint(200));
+      
+      _fmpz_vec_add(c, a, b, length);
+      _fmpz_vec_add(b, a, b, length);
+      
+      result = (_fmpz_vec_equal(b, c, length));
+      if (!result)
+      {
+         printf("Error:\n");
+         _fmpz_vec_print(b, length); printf("\n\n");
+         _fmpz_vec_print(c, length); printf("\n\n");
+         abort();
+      }
+
+      _fmpz_vec_clear(a, length);
+      _fmpz_vec_clear(b, length);
+      _fmpz_vec_clear(c, length);
+   }
+
+
+   _fmpz_vec_randclear();
+      
+   _fmpz_cleanup();
+   printf("PASS\n");
+   return 0;
+}
