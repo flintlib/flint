de209e2efe6a5082d666ecd27b6e84b2574e9b00
Sebastian Pancratz <sfp@pancratz.org>
2010-07-06 11:37:17 +0200

    Added methods set_mpz, set_mpq to fmpq_poly

--- fmpq_poly.h
+++ fmpq_poly.h
@@ -74,9 +74,6 @@ ulong fmpq_poly_length(fmpq_poly_t poly)
 
 #define fmpq_poly_denref(poly)  ((poly)->den)
 
-static inline 
-void fmpq_poly_get_num(
-
 void fmpq_poly_set(fmpq_poly_t poly1, const fmpq_poly_t poly2);
 
 void fmpq_poly_set_si(fmpq_poly_t poly, const long x);
