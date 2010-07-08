2b3f528a60d6ca995a98c01cc2041ca7d593d4c5
Sebastian Pancratz <sfp@pancratz.org>
2010-07-08 17:30:37 +0200

    Added stdlib to fmpq_poly_scalar_div_mpq

--- fmpq_poly/scalar_div_mpq.c
+++ fmpq_poly/scalar_div_mpq.c
@@ -30,63 +30,102 @@
 #include "fmpz_vec.h"
 #include "fmpq_poly.h"
 
+void _fmpq_poly_scalar_div_mpq(fmpz * rpoly, fmpz_t rden, 
+                               const fmpz * poly, const fmpz_t den, ulong len, 
+                               const fmpz_t r, const fmpz_t s)
+{
+    fmpz_t gcd1;  /* GCD( poly, s ) */
+    fmpz_t gcd2;  /* GCD( r, den )  */
+    fmpz_init(gcd1);
+    fmpz_init(gcd2);
+    fmpz_set_ui(gcd1, 1UL);
+    fmpz_set_ui(gcd2, 1UL);
+    if (*s != 1L)
+    {
+        _fmpz_vec_content(gcd1, poly, len);
+        if (*gcd1 != 1L)
+            fmpz_gcd(gcd1, gcd1, s);
+    }
+    if (*den != 1L && *r != 1L)
+        fmpz_gcd(gcd2, gcd2, den);
+    
+    if (*gcd1 == 1L)
+    {
+        if (*gcd2 == 1L)
+        {
+            _fmpz_vec_scalar_mul_fmpz(rpoly, poly, len, r);
+            fmpz_mul(rden, den, s);
+        }
+        else
+        {
+            fmpz_t r2;
+            fmpz_init(r2);
+            fmpz_divexact(r2, r, gcd2);
+            _fmpz_vec_scalar_mul_fmpz(rpoly, poly, len, r2);
+            fmpz_divexact(rden, den, gcd2);
+            fmpz_mul(rden, rden, s);
+            fmpz_clear(r2);
+        }
+    }
+    else
+    {
+        fmpz_t s2;
+        fmpz_init(s2);
+        fmpz_divexact(s2, s, gcd1);
+        if (*gcd2 == 1L)
+        {
+            _fmpz_vec_scalar_divexact(rpoly, poly, len, gcd1);
+            _fmpz_vec_scalar_mul_fmpz(rpoly, rpoly, len, r);
+            fmpz_mul(rden, den, s2);
+        }
+        else
+        {
+            fmpz_t r2;
+            fmpz_init(r2);
+            fmpz_divexact(r2, r, gcd2);
+            _fmpz_vec_scalar_divexact(rpoly, poly, len, gcd1);
+            _fmpz_vec_scalar_mul_fmpz(rpoly, rpoly, len, r2);
+            fmpz_divexact(rden, den, gcd2);
+            fmpz_mul(rden, rden, s2);
+            fmpz_clear(r2);
+        }
+        fmpz_clear(s2);
+    }
+    
+    if (_fmpz_vec_is_zero(rpoly, len))
+        fmpz_set_ui(rden, 1UL);
+    
+    fmpz_clear(gcd1);
+    fmpz_clear(gcd2);
+}
+
 void fmpq_poly_scalar_div_mpq(fmpq_poly_t rop, const fmpq_poly_t op, const mpq_t c)
 {
     if (mpq_sgn(c) == 0)
     {
-        printf("ERROR (fmpq_poly_scalar_div_mpq).  Division by 0.\n");
+        printf("Exception: division by zero in fmpq_poly_scalar_div_mpq\n");
         abort();
     }
     
-    if (mpz_cmpabs_ui(mpq_numref(c), 1UL) == 0)
+    if (fmpq_poly_is_zero(op))
     {
-        fmpq_poly_scalar_mul_mpz(rop, op, mpq_denref(c));
-        if (mpz_cmp_si(mpq_numref(c), -1) == 0)
-            fmpq_poly_neg(rop, rop);
+        fmpq_poly_zero(rop);
         return;
     }
     
     fmpq_poly_fit_length(rop, op->length);
-    
-    /* First set {num,den} = {denref(c),numref(c)} with the right sign. */
-    /* Then proceed as in the multiplication code.                      */
-    fmpz_t d, num, den;
-    fmpz_init(d);
-    fmpz_init(num);
-    fmpz_init(den);
-    fmpz_set_mpz(num, mpq_denref(c));
-    fmpz_set_mpz(den, mpq_numref(c));
-    if (fmpz_sgn(den) < 0)
-    {
-        fmpz_neg(num, num);
-        fmpz_neg(den, den);
-    }
-    
-    _fmpz_vec_content(d, op->coeffs, op->length);
-    fmpz_gcd(d, d, den);
-    if (*d != 1)
-    {
-        _fmpz_vec_scalar_divexact(rop->coeffs, op->coeffs, op->length, d);
-        fmpz_divexact(den, den, d);
-    }
-    fmpz_gcd(d, op->den, num);
-    if (*d != 1)
-    {
-        _fmpz_vec_scalar_mul_fmpz(rop->coeffs, op->coeffs, op->length, num);
-        fmpz_mul(rop->den, op->den, den);
-    }
-    else
-    {
-        fmpz_divexact(num, num, d);
-        _fmpz_vec_scalar_mul_fmpz(rop->coeffs, op->coeffs, op->length, num);
-        fmpz_divexact(rop->den, op->den, den);
-        fmpz_mul(rop->den, rop->den, den);
-    }
-    fmpz_clear(d);
-    fmpz_clear(num);
-    fmpz_clear(den);
-    
     _fmpq_poly_set_length(rop, op->length);
-    _fmpq_poly_normalise(rop);
+    
+    fmpz_t r, s;
+    fmpz_init(r);
+    fmpz_init(s);
+    fmpz_set_mpz(r, mpq_numref(c));
+    fmpz_set_mpz(s, mpq_denref(c));
+    
+    _fmpq_poly_scalar_div_mpq(rop->coeffs, rop->den, 
+                              op->coeffs, op->den, op->length, r, s);
+    
+    fmpz_clear(r);
+    fmpz_clear(s);
 }
 
