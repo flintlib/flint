e51e2dc8cc57e57f997a16ce8e45ca9cd3d1cc6a
SPancratz <spancratz@github.com>
2010-08-04 22:56:45 +0100

    Updating fmpz_mat/doc

--- fmpz_mat/randtest.c
+++ fmpz_mat/randtest.c
@@ -30,7 +30,7 @@
 #include "fmpz_mat.h"
 
 void
-fmpz_mat_randtest(fmpz_mat_t mat, mp_bitcnt_t bits_in)
+fmpz_mat_randtest(fmpz_mat_t mat, mp_bitcnt_t bits)
 {
     long r, c, i, j;
 
@@ -39,5 +39,5 @@ fmpz_mat_randtest(fmpz_mat_t mat, mp_bitcnt_t bits_in)
 
     for (i = 0; i < r; i++)
         for (j = 0; j < c; j++)
-            fmpz_randtest(mat->rows[i] + j, bits_in);
+            fmpz_randtest(mat->rows[i] + j, bits);
 }
