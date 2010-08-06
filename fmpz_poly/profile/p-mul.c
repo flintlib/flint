32af8f1e5a279f014a98c27c3db516c3e54fffd5
SPancratz <spancratz@github.com>
2010-08-06 14:49:03 +0100

    Better choice of randomisation in fmpz_poly/profile/p-mul.c

--- fmpz_poly/profile/p-mul.c
+++ fmpz_poly/profile/p-mul.c
@@ -32,83 +32,106 @@
 #include "ulong_extras.h"
 #include "profiler.h"
 
+#define lenlo   1
+#define lenhi   60
+#define lenh    1
+#define bitslo  16
+#define bitshi  1024
+#define bitsh   16
+#define cols    ((lenhi + 1 - lenlo + (lenh - 1)) / lenh)
+#define rows    ((bitshi + 1 - bitslo + (bitsh - 1)) / bitsh)
+#define cpumin  10
+#define N       10
+
 int
 main(void)
 {
-    long len, lenlo = 1, lenhi = 100, lenh = 1;
-    long bits, bitslo = 16, bitshi = 2048, bitsh = 16;
-    
-    long cols = (lenhi + 1 - lenlo + (lenh - 1)) / lenh;     /* ceil((lenhi + 1 - lenlo) / lenh) */
-    long rows = (bitshi + 1 - bitslo + (bitsh - 1)) / bitsh; /* ceil((bitshi + 1 - bitslo) / bitsh) */
-    
+    int len, bits;
     int i, j, x[rows][cols];
-    long cpumin = 10;
-    timeit_t tottime;
-    
-    timeit_start(tottime);
     
     fmpz_poly_randinit();
     
     for (len = lenlo, j = 0; len <= lenhi; len += lenh, j++)
     {
+        fmpz_poly_t f, g, h;
+        fmpz_poly_init2(f, len);
+        fmpz_poly_init2(g, len);
+        fmpz_poly_init2(h, 2*len - 1);
+        
         for (bits = bitslo, i = 0; bits <= bitshi; bits += bitsh, i++)
         {
-            /*
-               Run the three methods and find out which is fastest.
-             */
-            fmpz_poly_t f, g, h;
-            timeit_t t[3];
-            long l, loops = 1;
+            int n;
+            long s[3] = {0, 0, 0};
+            int reps = 0;
             
-            fmpz_poly_init2(f, len);
-            fmpz_poly_init2(g, len);
-            fmpz_poly_init2(h, 2*len - 1);
-            
-            fmpz_poly_randtest_not_zero(f, len, bits);
-            fmpz_poly_randtest_not_zero(g, len, bits);
-
-          loop:
-
-            timeit_start(t[0]);
-            for (l = 0; l < loops; l++)
-                fmpz_poly_mul_classical(h, f, g);
-            timeit_stop(t[0]);
-            
-            timeit_start(t[1]);
-            for (l = 0; l < loops; l++)
-                fmpz_poly_mul_karatsuba(h, f, g);
-            timeit_stop(t[1]);
-
-            timeit_start(t[2]);
-            for (l = 0; l < loops; l++)
-                fmpz_poly_mul_KS(h, f, g);
-            timeit_stop(t[2]);
-            
-            if (t[0]->cpu <= 10 || t[1]->cpu <= 10 || t[2]->cpu <= 10)
+            for (n = 0; n < N; n++)
             {
-                loops *= 10;
-                goto loop;
+                timeit_t t[3];
+                int l, loops = 1;
+                
+                /*
+                   Construct random polynomials f and g
+                 */
+                {
+                    long k;
+                    for (k = 0; k < len; k++)
+                    {
+                        fmpz_randbits(f->coeffs + k, bits);
+                        fmpz_randbits(g->coeffs + k, bits);
+                    }
+                    k = len;
+                    while (k && !f->coeffs[k - 1])
+                        k--;
+                    f->length = k;
+                    k = len;
+                    while (k && !g->coeffs[k - 1])
+                        k--;
+                    g->length = k;
+                }
+                
+              loop:
+
+                timeit_start(t[0]);
+                for (l = 0; l < loops; l++)
+                    fmpz_poly_mul_classical(h, f, g);
+                timeit_stop(t[0]);
+                
+                timeit_start(t[1]);
+                for (l = 0; l < loops; l++)
+                    fmpz_poly_mul_karatsuba(h, f, g);
+                timeit_stop(t[1]);
+
+                timeit_start(t[2]);
+                for (l = 0; l < loops; l++)
+                    fmpz_poly_mul_KS(h, f, g);
+                timeit_stop(t[2]);
+                
+                if (t[0]->cpu <= cpumin || t[1]->cpu <= cpumin || t[2]->cpu <= cpumin)
+                {
+                    loops *= 10;
+                    goto loop;
+                }
+                
+                s[0] += t[0]->cpu;
+                s[1] += t[1]->cpu;
+                s[2] += t[2]->cpu;
+                reps += loops;
             }
             
-            printf("%ld %ld %lf.3 %lf.3 %lf.3\n", len, bits, t[0]->cpu / (double) loops, t[1]->cpu / (double) loops, t[2]->cpu / (double) loops);
-
-            if (t[0]->cpu <= t[1]->cpu && t[0]->cpu <= t[2]->cpu)
+            printf("%d %d %lf %lf %lf\n", len, bits, s[0] / (double) reps, s[1] / (double) reps, s[2] / (double) reps);
+            
+            if (s[0] <= s[1] && s[0] <= s[2])
                 x[i][j] = 0;
-            else if (t[1]->cpu <= t[2]->cpu)
+            else if (s[1] <= s[2])
                 x[i][j] = 1;
             else
                 x[i][j] = 2;
-            
-            fmpz_poly_clear(f);
-            fmpz_poly_clear(g);
-            fmpz_poly_clear(h);
         }
+        fmpz_poly_clear(f);
+        fmpz_poly_clear(g);
+        fmpz_poly_clear(h);
     }
     
-    timeit_stop(tottime);
-
-    printf("Total time: %ldms\n", tottime->cpu);
-
     for (i = 0; i < rows; i++)
     {
         for (j = 0; j < cols; j++)
