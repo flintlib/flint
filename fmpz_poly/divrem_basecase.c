80fa06f71acd2eedfc49d424d44462c1e0967e1b
SPancratz <spancratz@github.com>
2010-09-10 14:21:29 +0100

    Changed divrem_basecase to always be a full division, update documentation

--- fmpz_poly/divrem_basecase.c
+++ fmpz_poly/divrem_basecase.c
@@ -35,52 +35,30 @@ void
 _fmpz_poly_divrem_basecase(fmpz * Q, fmpz * R, const fmpz * A, long lenA,
                            const fmpz * B, long lenB)
 {
-    const fmpz * leadB = B + lenB - 1;
-    long B1, B2, iQ = lenA - lenB;
-    int want_rem;
+    const fmpz * leadB = B + (lenB - 1);
+    long iQ = lenA - lenB, iR = lenA - 1;
 
-    want_rem = (R != NULL);
-    if (want_rem)
+    if (R != A)
         _fmpz_vec_copy(R, A, lenA);
 
-    while (lenA >= lenB && fmpz_cmpabs(A + lenA - 1, leadB) < 0)
+    while (iQ >= 0 && fmpz_cmpabs(R + iR, leadB) < 0)
     {
         fmpz_zero(Q + iQ);
         iQ--;
-        lenA--;
+        iR--;
     }
 
-    if (lenA < lenB)
-        return;
-
-    if (!want_rem)
+    while (iQ >= 0)
     {
-        R = _fmpz_vec_init(lenA);
-        _fmpz_vec_copy(R + lenB - 1, A + lenB - 1, lenA - lenB + 1);
-    }
-
-    B1 = lenB - (!want_rem);
-    B2 = lenB;
-    
-    while (lenA >= lenB)
-    {
-        if (fmpz_cmpabs(R + lenA - 1, leadB) < 0)
+        if (fmpz_cmpabs(R + iR, leadB) < 0)
             fmpz_zero(Q + iQ);
         else
         {
-            fmpz_fdiv_q(Q + iQ, R + lenA - 1, leadB);
-            _fmpz_vec_scalar_submul_fmpz(R + lenA - B2, B, B1, Q + iQ);
+            fmpz_fdiv_q(Q + iQ, R + iR, leadB);
+            _fmpz_vec_scalar_submul_fmpz(R + iQ, B, lenB, Q + iQ);
         }
-
-        if ((!want_rem) && (B1 >= lenA - lenB + 1))
-        {
-            B++;
-            B1--;
-            B2--;
-        }
-
-        lenA--;
         iQ--;
+        iR--;
     }
 }
 
@@ -110,14 +88,14 @@ fmpz_poly_divrem_basecase(fmpz_poly_t Q, fmpz_poly_t R,
 
     lenq = A->length - B->length + 1;
     lenr = A->length;
-    if ((Q == A) || (Q == B))
+    if (Q == A || Q == B)
         q = _fmpz_vec_init(lenq);
     else
     {
         fmpz_poly_fit_length(Q, lenq);
         q = Q->coeffs;
     }
-    if ((R == A) || (R == B))
+    if (R == B)
         r = _fmpz_vec_init(lenr);
     else
     {
@@ -128,7 +106,7 @@ fmpz_poly_divrem_basecase(fmpz_poly_t Q, fmpz_poly_t R,
     _fmpz_poly_divrem_basecase(q, r, A->coeffs, A->length,
                                      B->coeffs, B->length);
 
-    if ((Q == A) || (Q == B))
+    if (Q == A || Q == B)
     {
         _fmpz_vec_clear(Q->coeffs, Q->alloc);
         Q->coeffs = q;
@@ -137,7 +115,7 @@ fmpz_poly_divrem_basecase(fmpz_poly_t Q, fmpz_poly_t R,
     }
     else
         _fmpz_poly_set_length(Q, lenq);
-    if ((R == A) || (R == B))
+    if (R == B)
     {
         _fmpz_vec_clear(R->coeffs, R->alloc);
         R->coeffs = r;
