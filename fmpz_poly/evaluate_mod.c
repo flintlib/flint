a6d9d1d37e975581abacf354d9ca0bfe53b65a27
SPancratz <spancratz@github.com>
2010-12-17 18:34:44 +0000

    Add function "fmpz_poly_evaluate_mod"

---
+++ fmpz_poly/evaluate_mod.c
@@ -0,0 +1,72 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2008, 2009 William Hart
+    Copyright (C) 2010 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+
+mp_limb_t _fmpz_poly_evaluate_mod(const fmpz * poly, long len, mp_limb_t a, 
+                                  mp_limb_t p, mp_limb_t pinv)
+{
+    mp_limb_t c, res = 0;
+    fmpz_t f;
+
+    fmpz_init(f);
+
+    while (len--)
+    {
+        c = fmpz_mod_ui(f, poly + len, p);
+        res = n_addmod(n_mulmod2_preinv(res, a, p, pinv), c, p);
+    }
+
+    fmpz_clear(f);
+
+    return res;
+}
+
+mp_limb_t fmpz_poly_evaluate_mod(const fmpz_poly_t poly, mp_limb_t a, 
+                                 mp_limb_t p, mp_limb_t pinv)
+{
+    if (poly->length == 0)
+        return 0;
+
+    if (a == 0)
+    {
+        mp_limb_t res;
+        fmpz_t f;
+
+        fmpz_init(f);
+        res = fmpz_mod_ui(f, poly->coeffs, p);
+        fmpz_clear(f);
+
+        return res;
+    }
+
+    return _fmpz_poly_evaluate_mod(poly->coeffs, poly->length, a, p, pinv);
+}
+
---
+++ fmpz_poly/test/t-evaluate_mod.c
@@ -0,0 +1,87 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2010 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+#include "ulong_extras.h"
+
+int
+main(void)
+{
+    int i, result;
+    fmpz_randstate_t state;
+
+    printf("evaluate_mod....");
+    fflush(stdout);
+
+    fmpz_poly_randinit(state);
+
+    /* Compare with evaluation over the integers */
+    for (i = 0; i < 10000; i++)
+    {
+        fmpz_t b, s;
+        fmpz_poly_t f;
+        mp_limb_t a, n, ninv, r;
+
+        fmpz_poly_init(f);
+        fmpz_poly_randtest(f, state, n_randint(10), 20);
+
+        n = n_randtest_not_zero();
+        a = n_randint(n);
+        ninv = n_preinvert_limb(n);
+
+        fmpz_init(b);
+        fmpz_set_ui(b, a);
+
+        r = fmpz_poly_evaluate_mod(f, a, n, ninv);
+        fmpz_poly_evaluate(s, f, b);
+
+        result = (r == fmpz_mod_ui(s, s, n));
+        if (!result)
+        {
+            printf("FAIL:\n");
+            fmpz_poly_print(f), printf("\n\n");
+            gmp_printf("a = %Mu\n\n", a);
+            gmp_printf("n = %Mu\n\n", n);
+            gmp_printf("r = %Mu\n\n", r);
+            printf("s = "), fmpz_print(s), printf("\n\n");
+            abort();
+        }
+
+        fmpz_poly_clear(f);
+        fmpz_clear(b);
+        fmpz_clear(s);
+    }
+
+    fmpz_poly_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return EXIT_SUCCESS;
+}
