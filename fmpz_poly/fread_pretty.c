927afe85f78e420a8a5ea530e089b224a3290493
SPancratz <spancratz@github.com>
2010-12-30 15:55:52 +0100

    Add "fmpz_poly_fread_pretty" with documentation and test code

---
+++ fmpz_poly/fread_pretty.c
@@ -0,0 +1,310 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2010 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <mpir.h>
+
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+
+static __inline__
+int is_varsymbol0(char c)
+{
+    return isalpha(c);
+}
+
+static __inline__
+int is_varsymbol1(char c)
+{
+    return isalnum(c) || (c == '_');
+}
+
+#define next_event()                                                      \
+do {                                                                      \
+    c = fgetc(file);                                                      \
+    if (c == EOF && !feof(file))                                          \
+        goto s_ioe;                                                       \
+    ++r;                                                                  \
+    if (i == N)                                                           \
+    {                                                                     \
+        buf = realloc(buf, N = 2*N);                                      \
+        if (buf == NULL)                                                  \
+        {                                                                 \
+            printf("ERROR (fmpz_poly_fread_pretty).  realloc failed.\n"); \
+            abort();                                                      \
+        }                                                                 \
+    }                                                                     \
+    buf[i++] = c;                                                         \
+} while (0)
+
+#define add_coeff()                                            \
+do {                                                           \
+    fmpz_set_mpz(f_coeff, z_coeff);                            \
+    fmpz_poly_fit_length(poly, exp + 1);                       \
+    fmpz_add(poly->coeffs + exp, poly->coeffs + exp, f_coeff); \
+    if (poly->length < exp + 1)                                \
+        poly->length = exp + 1;                                \
+} while (0)
+
+int fmpz_poly_fread_pretty(FILE *file, fmpz_poly_t poly, char **x)
+{
+    /*
+        var     - variable name
+        buf     - buffer of size N, at write position i, with c == buf[i-1]
+        z_coeff - mpz_t for the coefficient, f_coeff is the fmpz_t version
+        z_exp   - mpz_t for the exponent, exp is the long version
+        r       - return value
+     */
+
+    char *var = NULL;
+
+    char c, *buf;
+    int i, N;
+
+    fmpz_t f_coeff;
+    mpz_t z_coeff, z_exp;
+    long exp;
+
+    int r = 0;
+
+    fmpz_poly_zero(poly);
+    if (poly->alloc)
+        mpn_zero((mp_ptr) poly->coeffs, poly->alloc);
+
+    i = 0;
+    N = 80;
+    buf = malloc(N);
+    if (buf == NULL)
+    {
+        printf("ERROR (fmpz_poly_fread_pretty).  malloc failed.\n");
+        abort();
+    }
+
+    fmpz_init(f_coeff);
+    mpz_init(z_coeff);
+    mpz_init(z_exp);
+
+  s_0 :
+
+    next_event();
+
+    if (c == '-')
+        goto s_1;
+    if (isdigit(c))
+        goto s_2;
+    if (is_varsymbol0(c))
+    {
+        mpz_set_si(z_coeff, 1);
+        goto s_3;
+    }
+
+    goto s_parse_error;
+
+  s_1 :
+
+    next_event();
+
+    if (isdigit(c))
+        goto s_2;
+    if (is_varsymbol0(c))
+    {
+        if (i == 1)
+            mpz_set_si(z_coeff, 1);
+        else  /* i == 2 */
+        {
+            mpz_set_si(z_coeff, -1);
+            buf[0] = c;
+            i = 1;
+        }
+        goto s_3;
+    }
+
+    goto s_parse_error;
+
+  s_2 :
+
+    next_event();
+
+    if (isdigit(c))
+        goto s_2;
+    if (c == '*')
+    {
+        buf[i-1] = '\0';
+        mpz_set_str(z_coeff, buf, 10);
+        i = 0;
+        goto s_4;
+    }
+    {
+        buf[i-1] = '\0';
+        mpz_set_str(z_coeff, buf, 10);
+        exp = 0;
+        add_coeff();
+        if (c == '+' || c == '-')
+        {
+            i = 0;
+            if (c == '-')
+                buf[i++] = '-';
+            goto s_1;
+        }
+        else
+            goto s_end;
+    }
+
+  s_3 :
+
+    next_event();
+
+    if (is_varsymbol1(c))
+        goto s_3;
+
+    {
+        buf[i-1] = '\0';
+        if (var)
+        {
+            if (strcmp(buf, var))  /* Parse error */
+                goto s_parse_error;
+        }
+        else
+        {
+            var = malloc(i);
+            if (var == NULL)
+            {
+                printf("ERROR (fmpz_poly_fread_pretty).  malloc failed.\n");
+                abort();
+            }
+            strcpy(var, buf);
+        }
+
+        if (c == '^')
+        {
+            i = 0;
+            goto s_5;
+        }
+        else if (c == '+' || c == '-')
+        {
+            exp = 1;
+            add_coeff();
+
+            i = 0;
+            if (c == '-')
+                buf[i++] = '-';
+            goto s_1;
+        }
+        else
+        {
+            exp = 1;
+            add_coeff();
+            goto s_end;
+        }
+    }
+
+  s_4 :
+
+    next_event();
+
+    if (is_varsymbol0(c))
+        goto s_3;
+
+    goto s_parse_error;
+
+  s_5 :
+
+    next_event();
+
+    if (isdigit(c))
+        goto s_6;
+
+    goto s_parse_error;
+
+  s_6 :
+
+    next_event();
+
+    if (isdigit(c))
+        goto s_6;
+
+    {
+        buf[i-1] = '\0';
+        mpz_set_str(z_exp, buf, 10);
+        if (!mpz_fits_slong_p(z_exp))
+        {
+            goto s_parse_error;
+        }
+        exp = mpz_get_si(z_exp);
+        add_coeff();
+
+        if (c == '+' || c == '-')
+        {
+            i = 0;
+            if (c == '-')
+                buf[i++] = '-';
+            goto s_1;
+        }
+        else
+            goto s_end;
+    }
+
+  s_parse_error :
+
+    r = -2;
+    goto s_end;
+
+  s_ioe :
+
+    r = -1;
+    goto s_end;
+
+  s_end :
+
+    _fmpz_poly_normalise(poly);
+
+    if (var)
+        *x = var;
+    else
+    {
+        *x = malloc(1);
+        if (*x == NULL)
+        {
+            printf("ERROR (fmpz_poly_fread_pretty).  malloc failed.\n");
+            abort();
+        }
+        **x = '\0';
+    }
+
+    fmpz_clear(f_coeff);
+    mpz_clear(z_coeff);
+    mpz_clear(z_exp);
+    free(buf);
+
+    return r;
+}
+
+#undef next_event
+#undef add_coeff
+
---
+++ fmpz_poly/test/t-print_read_pretty.c
@@ -0,0 +1,259 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2010 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mpir.h>
+
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+
+/*
+    The function fdopen is declared in stdio.h.  It is POSIX.1 compliant, 
+    but not ANSI compliant.  The following line enables compilation with 
+    the "-ansi" flag.
+ */
+extern FILE * fdopen(int fildes, const char *mode);
+
+int main(void)
+{
+    int i, j, n = 1000, result;
+    fmpz_randstate_t state;
+
+    FILE *in, *out;
+    int fd[2];
+    pid_t childpid;
+
+    printf("print/ read_pretty....");
+    fflush(stdout);
+
+    fmpz_poly_randinit(state);
+
+    /* Randomise n polynomials, write to and read from a pipe */
+    {
+        fmpz_poly_t *a;
+        char *var = "x";
+
+        a = malloc(n * sizeof(fmpz_poly_t));
+        for (i = 0; i < n; i++)
+        {
+            fmpz_poly_init(a[i]);
+            fmpz_poly_randtest(a[i], state, 100, 100);
+        }
+
+        if (pipe(fd))
+        {
+            printf("FAIL:\n");
+            printf("Failed to set-up the pipe.\n");
+            abort();
+        }
+
+        if((childpid = fork()) == -1)
+        {
+            printf("FAIL:\n");
+            printf("Failed to fork the process.\n");
+            abort();
+        }
+
+        if(childpid == 0)  /* Child process */
+        {
+            int r;
+
+            close(fd[0]);
+            out = fdopen(fd[1], "w");
+            if (out == NULL)
+            {
+                printf("FAIL:\n");
+                printf("Could not open output file at the pipe.\n");
+                abort();
+            }
+
+            for (j = 0; j < n; j++)
+            {
+                r = fmpz_poly_fprint_pretty(out, a[j], var);
+                if ((j < n - 1) && (r > 0))
+                    r = fprintf(out, "\n");
+
+                if (r <= 0)
+                {
+                    printf("FAIL:\n");
+                    printf("Write error.\n");
+                    abort();
+                }
+            }
+
+            fclose(out);
+            exit(0);
+        }
+        else  /* Parent process */
+        {
+            int r;
+            fmpz_poly_t t;
+            char *rvar;
+
+            close(fd[1]);
+            in = fdopen(fd[0], "r");
+            if (in == NULL)
+            {
+                printf("FAIL:\n");
+                printf("Could not open input file at the pipe.\n");
+                abort();
+            }
+
+            fmpz_poly_init(t);
+
+            i = 0;
+            while (!feof(in))
+            {
+                r = fmpz_poly_fread_pretty(in, t, &rvar);
+                if (r <= 0)
+                {
+                    printf("FAIL:\n");
+                    printf("Read error.\n");
+                    abort();
+                }
+
+                result = fmpz_poly_equal(t, a[i]) && (strcmp(var, rvar) == 0);
+                if (!result)
+                {
+                    printf("FAIL:\n");
+                    printf("a[i] = "), fmpz_poly_print_pretty(a[i], var), printf("\n");
+                    printf("t    = "), fmpz_poly_print_pretty(t, rvar), printf("\n");
+                    printf("rvar = %s\n", rvar);
+                    abort();
+                }
+                free(rvar);
+
+                ++i;
+            }
+
+            fmpz_poly_clear(t);
+            fclose(in);
+        }
+
+        if (i != n)
+        {
+            printf("FAIL:\n");
+            printf("Only %d out of %d objects were processed.\n", i, n);
+            abort();
+        }
+
+        for (i = 0; i < n; i++)
+            fmpz_poly_clear(a[i]);
+        free(a);
+    }
+
+    /* Write "blah" to the pipe and see it read as a variable */
+    {
+        char str[5] = {'b', 'l', 'a', 'h', '\0'};
+
+        if (pipe(fd))
+        {
+            printf("FAIL:\n");
+            printf("Failed to set-up the pipe.\n");
+            abort();
+        }
+
+        if((childpid = fork()) == -1)
+        {
+            printf("FAIL:\n");
+            printf("Failed to fork the process.\n");
+            abort();
+        }
+
+        if(childpid == 0)  /* Child process */
+        {
+            int r;
+
+            close(fd[0]);
+            out = fdopen(fd[1], "w");
+            if (out == NULL)
+            {
+                printf("FAIL:\n");
+                printf("Could not open output file at the pipe.\n");
+                abort();
+            }
+
+            r = fputs(str, out);
+            if (r == EOF)
+            {
+                printf("FAIL:\n");
+                printf("Write error.\n");
+                abort();
+            }
+
+            fclose(out);
+            exit(0);
+        }
+        else  /* Parent process */
+        {
+            char *rvar = NULL;
+            int r;
+            fmpz_poly_t t;
+
+            close(fd[1]);
+            in = fdopen(fd[0], "r");
+            if (in == NULL)
+            {
+                printf("FAIL:\n");
+                printf("Could not open input file at the pipe.\n");
+                abort();
+            }
+
+            fmpz_poly_init(t);
+
+            while (!feof(in))
+            {
+                r = fmpz_poly_fread_pretty(in, t, &rvar);
+                result = (r > 0) && rvar && (strcmp(str, rvar) == 0) && 
+                         (t->length == 2) && (t->coeffs[0] == 0L) && 
+                         (t->coeffs[1] == 1L);
+                if (!result)
+                {
+                    printf("FAIL:\n");
+                    printf("r    = %d\n", r);
+                    printf("str  = {%s}\n", str);
+                    printf("rvar = {%s}\n", rvar);
+                    printf("t    = "), fmpz_poly_print(t), printf("\n");
+                    abort();
+                }
+                if (rvar)
+                    free(rvar);
+            }
+
+            fmpz_poly_clear(t);
+            fclose(in);
+        }
+    }
+
+    fmpz_poly_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return EXIT_SUCCESS;
+}
