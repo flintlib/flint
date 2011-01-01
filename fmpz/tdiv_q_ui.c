0be8ad6530e1927a74e24a032b53b9361c64e7fe
SPancratz <spancratz@github.com>
2011-01-01 18:15:50 +0100

    Add the functions "tdiv_q", "t_div_q_si" and "t_div_q_ui" to fmpz

---
+++ fmpz/tdiv_q_ui.c
@@ -0,0 +1,66 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2010 Sebastian Pancratz
+    
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+
+void
+fmpz_tdiv_q_ui(fmpz_t f, const fmpz_t g, ulong h)
+{
+    fmpz c1 = *g;
+    ulong c2 = h;
+
+    if (h == 0)
+    {
+        printf("Exception: division by zero in fmpz_tdiv_q_ui\n");
+        abort();
+    }
+
+    if (!COEFF_IS_MPZ(c1))      /* g is small */
+    {
+        if (c1 > 0)
+        {
+            fmpz_set_ui(f, c1 / c2);
+        }
+        else
+        {
+            ulong q = ((ulong) -c1) / c2;
+
+            fmpz_set_si(f, - (long) q);
+        }
+    }
+    else                        /* g is large */
+    {
+        __mpz_struct *mpz_ptr = _fmpz_promote(f);
+
+        mpz_tdiv_q_ui(mpz_ptr, COEFF_TO_PTR(c1), c2);
+        _fmpz_demote_val(f);    /* division by h may result in small value */
+    }
+}
