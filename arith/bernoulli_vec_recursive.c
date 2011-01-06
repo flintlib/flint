8db9a5d7891637940e67afbdee528a63a3135688
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-01-06 19:09:24 +0100

    fix missing include

--- arith/bernoulli_vec_recursive.c
+++ arith/bernoulli_vec_recursive.c
@@ -24,6 +24,7 @@
 ******************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpir.h>
 #include "flint.h"
 #include "fmpz.h"
