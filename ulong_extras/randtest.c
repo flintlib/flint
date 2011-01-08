996e53fb543492faff434f28521936161dacfac2
SPancratz <spancratz@github.com>
2011-01-07 18:05:26 -0800

    Update n_randtest()

--- ulong_extras/randtest.c
+++ ulong_extras/randtest.c
@@ -1,4 +1,4 @@
-/*============================================================================
+/*=============================================================================
 
     This file is part of FLINT.
 
@@ -16,23 +16,46 @@
     along with FLINT; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-===============================================================================*/
-/****************************************************************************
+=============================================================================*/
+/******************************************************************************
 
-   Copyright (C) 2009 William Hart
- 
-*****************************************************************************/
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2010 Sebastian Pancratz
 
+******************************************************************************/
+
+#include <limits.h>
 #include <mpir.h>
+
 #include "flint.h"
+#include "fmpz.h"
 #include "ulong_extras.h"
 
 mp_limb_t n_randtest(void)
 {
-   return n_randbits(n_randint(FLINT_BITS + 1));
+    ulong m;
+
+    m = n_randlimb();
+
+    if (m & 7UL)
+    {
+        return n_randbits(n_randint(FLINT_BITS + 1));
+    }
+    else
+    {
+        m >>= 3;
+
+        if      (m % 4 == 0) return 0;
+        else if (m % 4 == 1) return 1;
+        else if (m % 4 == 2) return COEFF_MAX;
+        else                 return LONG_MAX;
+    }
 }
 
 mp_limb_t n_randtest_not_zero(void)
 {
-   return n_randbits(n_randint(FLINT_BITS) + 1);
+    mp_limb_t n;
+
+    while ((n = n_randtest()) == 0) ;
+    return n;
 }
