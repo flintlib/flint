11bb46d426470927b6363d2cafe56f6e33e0b1bb
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-02-22 15:55:16 +0100

    Include missing test file for nmod_poly_integral

---
+++ nmod_poly/test/t-integral.c
@@ -0,0 +1,114 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2010 William Hart
+    Copyright (C) 2011 Fredrik Johansson
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "nmod_poly.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+
+int
+main(void)
+{
+    int i, result = 1;
+    flint_rand_t state;
+    flint_randinit(state);
+    
+    printf("integral....");
+    fflush(stdout);
+
+    /* Check with derivative */
+    for (i = 0; i < 10000; i++)
+    {
+        nmod_poly_t a, b, c;
+        ulong len;
+        mp_limb_t c0;
+        mp_limb_t n = n_randtest_prime(state, 0);
+
+        len = n_randint(state, 100);
+        len = FLINT_MIN(len, n - 1);
+
+        nmod_poly_init(a, n);
+        nmod_poly_init(b, n);
+        nmod_poly_init(c, n);
+
+        nmod_poly_randtest(a, state, len);
+
+        nmod_poly_integral(b, a);
+        c0 = nmod_poly_get_coeff_ui(b, 0);
+        nmod_poly_derivative(c, b);
+
+        result = (c0 == 0UL) && nmod_poly_equal(a, c);
+
+        if (!result)
+        {
+            printf("FAIL:\n");
+            printf("a->length = %ld, n = %lu\n", a->length, a->mod.n);
+            nmod_poly_print(a), printf("\n\n");
+            nmod_poly_print(c), printf("\n\n");
+            nmod_poly_print(b), printf("\n\n");
+            abort();
+        }
+
+        nmod_poly_clear(a);
+        nmod_poly_clear(b);
+        nmod_poly_clear(c);
+    }
+
+    /* Check aliasing */
+    for (i = 0; i < 10000; i++)
+    {
+        nmod_poly_t a, b;
+        mp_limb_t n = n_randtest_prime(state, 0);
+
+        nmod_poly_init(a, n);
+        nmod_poly_init(b, n);
+        nmod_poly_randtest(a, state, n_randint(state, 100));
+
+        nmod_poly_integral(b, a);
+        nmod_poly_integral(a, a);
+        
+        result = nmod_poly_equal(a, b);
+        if (!result)
+        {
+            printf("FAIL:\n");
+            printf("a->length = %ld, n = %lu\n", a->length, a->mod.n);
+            nmod_poly_print(a), printf("\n\n");
+            nmod_poly_print(b), printf("\n\n");
+            abort();
+        }
+
+        nmod_poly_clear(a);
+        nmod_poly_clear(b);
+    }
+
+    flint_randclear(state);
+
+    printf("PASS\n");
+    return 0;
+}
