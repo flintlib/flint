90efd3786fa91c8fc9e59b69c4479be615d64de4
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-02-26 20:39:54 +0100

    speedup n_factorial_mod2_preinv for special cases

--- ulong_extras/factorial_mod2_preinv.c
+++ ulong_extras/factorial_mod2_preinv.c
@@ -28,15 +28,40 @@
 #include "ulong_extras.h"
 
 
+static const mp_limb_t small_factorials[] =
+{
+    1UL, 1UL, 2UL, 6UL, 24UL, 120UL, 720UL, 5040UL, 40320UL, 362880UL,
+    3628800UL, 39916800UL, 479001600UL,
+#if FLINT64
+    6227020800UL, 87178291200UL,
+    1307674368000UL, 20922789888000UL, 355687428096000UL, 6402373705728000UL,
+    121645100408832000UL, 2432902008176640000UL,
+#endif
+};
+
+#if FLINT64
+#define MAX_SMALL_FACTORIAL 20
+#else
+#define MAX_SMALL_FACTORIAL 12
+#endif
+
+
 mp_limb_t n_factorial_mod2_preinv(ulong n, mp_limb_t p, mp_limb_t pinv)
 {
     mp_limb_t prod, hi, lo;
 
-    prod = 1UL;
-    lo = 1UL;
+    if (n <= MAX_SMALL_FACTORIAL)
+        return n_mod2_preinv(small_factorials[n], p, pinv);
+
+    if (n >= p)
+        return 0UL;
+
+    prod = small_factorials[MAX_SMALL_FACTORIAL];
+    lo = n;
+    n--;
 
     /* TODO: speedup for n in the range of sqrt(ULONG_MAX) */
-    while (n)
+    while (n > MAX_SMALL_FACTORIAL)
     {
         umul_ppmm(hi, lo, lo, n);
 
