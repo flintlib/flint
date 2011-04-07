4514a578e5369087a26138454f894cedcb361967
SPancratz <spancratz@github.com>
2011-04-07 12:34:03 +0100

    Bugfix in "padic_normalise"

--- padic/normalise.c
+++ padic/normalise.c
@@ -2,16 +2,16 @@
 
 void padic_normalise(padic_t rop, const padic_ctx_t ctx)
 {
-    if (!fmpz_is_zero(rop) || rop[1] < ctx->N)
+    if (!fmpz_is_zero(rop) && (rop[1] < ctx->N))
     {
         rop[1] += fmpz_remove(rop, rop, ctx->p);
 
         if (rop[1] < ctx->N)
             _padic_reduce_unit(rop, ctx);
         else
-            padic_zero(rop);
+            padic_zero(rop, ctx);
     }
     else
-        padic_zero(rop);
+        padic_zero(rop, ctx);
 }
 
