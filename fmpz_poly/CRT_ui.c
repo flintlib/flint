e996141838a346c3812a0602100e3a24e8ffe97f
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-04-18 14:11:54 +0200

    implement CRT_ui for fmpz_polys

---
+++ fmpz_poly/CRT_ui.c
@@ -0,0 +1,129 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2011 Fredrik Johansson
+
+******************************************************************************/
+
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_vec.h"
+#include "fmpz_poly.h"
+#include "nmod_poly.h"
+
+
+void
+__fmpz_poly_CRT_ui(fmpz * res, const fmpz * poly1, long len1,
+               const fmpz_t m1, mp_srcptr poly2, long len2, mp_limb_t m2,
+                mp_limb_t m2inv, int sign)
+{
+    long i;
+
+    mp_limb_t c;
+    fmpz_t m1m2;
+
+    c = fmpz_fdiv_ui(m1, m2);
+    c = n_invmod(c, m2);
+
+    if (c == 0)
+    {
+        printf("Exception in fmpz_poly_CRT_ui: m1 not invertible modulo m2!\n");
+        abort();
+    }
+
+    fmpz_init(m1m2);
+    fmpz_mul_ui(m1m2, m1, m2);
+
+    for (i = 0; i < FLINT_MIN(len1, len2); i++)
+    {
+        _fmpz_CRT_ui_precomp(res + i, poly1 + i, m1,
+                                poly2[i], m2, m2inv, m1m2, c, sign);
+    }
+
+    /* TODO: do this efficiently; write an fmpz or fmpz_vec function for it */
+    if (len2 > len1)
+    {
+        fmpz_t zero;
+        fmpz_init(zero);
+        for (i = len1; i < len2; i++)
+        {
+            _fmpz_CRT_ui_precomp(res + i, zero, m1,
+                                    poly2[i], m2, m2inv, m1m2, c, sign);
+        }
+        fmpz_clear(zero);
+    }
+
+    /* TODO: do this efficiently; write an fmpz or fmpz_vec function for it */
+    for (i = len2; i < len1; i++)
+    {
+        _fmpz_CRT_ui_precomp(res + i, res + i, m1,
+                            0, m2, m2inv, m1m2, c, sign);
+    }
+
+    fmpz_clear(m1m2);
+}
+
+void
+fmpz_poly_CRT_ui(fmpz_poly_t res, const fmpz_poly_t poly1,
+                        const fmpz_t m1, const nmod_poly_t poly2)
+{
+    long len1 = poly1->length;
+    long len2 = poly2->length;
+    long len = FLINT_MAX(len1, len2);
+
+    if (len == 0)
+    {
+        fmpz_poly_zero(res);
+        return;
+    }
+
+    fmpz_poly_fit_length(res, len);
+
+    __fmpz_poly_CRT_ui(res->coeffs, poly1->coeffs, poly1->length, m1,
+        poly2->coeffs, poly2->length, poly2->mod.n, poly2->mod.ninv, 1);
+
+    _fmpz_poly_set_length(res, len);
+    _fmpz_poly_normalise(res);
+}
+
+void
+fmpz_poly_CRT_ui_unsigned(fmpz_poly_t res, const fmpz_poly_t poly1,
+                            const fmpz_t m1, const nmod_poly_t poly2)
+{
+    long len1 = poly1->length;
+    long len2 = poly2->length;
+    long len = FLINT_MAX(len1, len2);
+
+    if (len == 0)
+    {
+        fmpz_poly_zero(res);
+        return;
+    }
+
+    fmpz_poly_fit_length(res, len);
+
+    __fmpz_poly_CRT_ui(res->coeffs, poly1->coeffs, poly1->length, m1,
+        poly2->coeffs, poly2->length, poly2->mod.n, poly2->mod.ninv, 0);
+
+    _fmpz_poly_set_length(res, len);
+    _fmpz_poly_normalise(res);
+}
---
+++ fmpz_poly/test/t-CRT_ui.c
@@ -0,0 +1,118 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2007 William Hart and David Harvey
+    Copyright (C) 2011 Fredrik Johansson
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_poly.h"
+#include "nmod_poly.h"
+
+int
+main(void)
+{
+    int i;
+    flint_rand_t state;
+
+    printf("CRT_ui....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    for (i = 0; i < 10000; i++)
+    {
+        long bits, prime_bits, length, num_primes, j;
+        fmpz_t mod;
+        fmpz_poly_t A, B, C;
+        nmod_poly_t Amod;
+        mp_limb_t primes[1000];
+
+        bits = n_randint(state, 500) + 1;
+        length = n_randint(state, 30) + 1;
+        prime_bits = 1 + n_randint(state, FLINT_BITS - 1);
+
+        fmpz_poly_init(A);
+        fmpz_poly_init(B);
+        fmpz_poly_init(C);
+
+        fmpz_poly_randtest(A, state, length, bits);
+
+        fmpz_init(mod);
+        num_primes = 0;
+        primes[0] = n_nextprime(1UL << prime_bits, 0);
+        fmpz_set_ui(mod, primes[0]);
+
+        /* + 1 for sign */
+        while (fmpz_bits(mod) <= bits + 1)
+        {
+            primes[num_primes + 1] = n_nextprime(primes[num_primes], 0);
+            fmpz_mul_ui(mod, mod, primes[num_primes + 1]);
+            num_primes++;
+        }
+
+        num_primes++;
+
+        nmod_poly_init(Amod, primes[0]);
+        fmpz_poly_get_nmod_poly(Amod, A);
+        fmpz_poly_set_nmod_poly(B, Amod);
+        fmpz_set_ui(mod, primes[0]);
+
+        for (j = 1; j < num_primes; j++)
+        {
+            nmod_poly_clear(Amod);
+            nmod_poly_init(Amod, primes[j]);
+            fmpz_poly_get_nmod_poly(Amod, A);
+            fmpz_poly_CRT_ui(B, B, mod, Amod);
+            fmpz_mul_ui(mod, mod, primes[j]);
+        }
+
+        if (!fmpz_poly_equal(B, A))
+        {
+            printf("FAIL!\n");
+            printf("primes: ");
+            for (j = 0; j < num_primes; j++)
+                printf("%lu ", primes[j]);
+            printf("\nA: \n");
+            fmpz_poly_print(A);
+            printf("\nB: \n");
+            fmpz_poly_print(B);
+            printf("\n");
+            abort();
+        }
+
+        nmod_poly_clear(Amod);
+        fmpz_poly_clear(A);
+        fmpz_poly_clear(B);
+        fmpz_poly_clear(C);
+        fmpz_clear(mod);
+    }
+
+    flint_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return 0;
+}
