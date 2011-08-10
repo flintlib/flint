831634f9f86aeacbebe38aaea135c17a1342c564
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-08-10 10:05:59 +0200

    fix whitespace and a compiler warning in factor_lehman.c

--- ulong_extras/factor_lehman.c
+++ ulong_extras/factor_lehman.c
@@ -30,47 +30,47 @@
 
 mp_limb_t n_factor_lehman(mp_limb_t n)
 {
-	double limit;
-	mp_limb_t cuberoot, k;
-   n_factor_t factors;
-   long bound;
+    double limit;
+    mp_limb_t cuberoot, k;
+    n_factor_t factors;
+    long bound;
 
 #if FLINT64 /* cannot compute enough primes */
-   if (n > 10000000000000000UL) return n;
+    if (n > 10000000000000000UL) return n;
 #endif
 
-   if ((n & 1) == 0) return 2;
+    if ((n & 1) == 0) return 2;
 
-   limit = pow(n, 1.0/3.0);
-   cuberoot = (mp_limb_t) ceil(limit);
-   bound = n_prime_pi(cuberoot);
-    
-   n_factor_init(&factors);
-   if (n_factor_trial_range(&factors, n, 0, bound) != n)
-      return factors.p[0];
-   
-   if (factors.p[0] = n_factor_one_line(n, FLINT_FACTOR_ONE_LINE_ITERS))
-       if (factors.p[0] != n)
-           return factors.p[0];
+    limit = pow(n, 1.0/3.0);
+    cuberoot = (mp_limb_t) ceil(limit);
+    bound = n_prime_pi(cuberoot);
 
-	for (k = 1; k <= cuberoot + 1; k++)
-   {
-		mp_limb_t x = (mp_limb_t) ceil(2.0*sqrt((double) k)*sqrt((double) n));
-		mp_limb_t end = x + (mp_limb_t) floor(1.0 + pow(n, 1.0/6.0)/((double) 4.0*sqrt((double) k)));
-		mp_limb_t sub = k*n*4;
-   
-      for ( ; x <= end; x++)
-      {
-			mp_limb_t p, sq = x*x - sub;
-			if (n_is_square(sq))
-         {
-				sq = sqrt((double) sq);
-				p = n_gcd(n, x - sq);
-				if (p != 1)
-               return p;
-			}
-		}
-	}
+    n_factor_init(&factors);
+    if (n_factor_trial_range(&factors, n, 0, bound) != n)
+        return factors.p[0];
 
-   return n;
+    if ((factors.p[0] = n_factor_one_line(n, FLINT_FACTOR_ONE_LINE_ITERS)))
+        if (factors.p[0] != n)
+            return factors.p[0];
+
+    for (k = 1; k <= cuberoot + 1; k++)
+    {
+        mp_limb_t x = (mp_limb_t) ceil(2.0*sqrt((double) k)*sqrt((double) n));
+        mp_limb_t end = x + (mp_limb_t) floor(1.0 + pow(n, 1.0/6.0)/((double) 4.0*sqrt((double) k)));
+        mp_limb_t sub = k*n*4;
+
+        for ( ; x <= end; x++)
+        {
+            mp_limb_t p, sq = x*x - sub;
+            if (n_is_square(sq))
+            {
+                sq = sqrt((double) sq);
+                p = n_gcd(n, x - sq);
+                if (p != 1)
+                    return p;
+            }
+        }
+    }
+
+    return n;
 }
