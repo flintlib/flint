c406b3f82ecada645595fef454d2b97b33b5bde3
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-08-23 16:30:56 +0200

    add _fmpz_vec_height and fmpz_poly_height

--- fmpz_poly.h
+++ fmpz_poly.h
@@ -90,18 +90,6 @@ long fmpz_poly_degree(const fmpz_poly_t poly)
     return poly->length - 1;
 }
 
-static __inline__ 
-ulong fmpz_poly_max_limbs(const fmpz_poly_t poly)
-{
-    return _fmpz_vec_max_limbs(poly->coeffs, poly->length);
-}
-
-static __inline__ 
-long fmpz_poly_max_bits(const fmpz_poly_t poly)
-{
-    return _fmpz_vec_max_bits(poly->coeffs, poly->length);
-}
-
 /*  Assignment and basic manipulation  ***************************************/
 
 void fmpz_poly_set(fmpz_poly_t poly1, const fmpz_poly_t poly2);
@@ -444,6 +432,24 @@ void _fmpz_poly_2norm(fmpz_t res, const fmpz * poly, long len);
 void fmpz_poly_2norm(fmpz_t res, const fmpz_poly_t poly);
 
 mp_bitcnt_t _fmpz_poly_2norm_normalised_bits(const fmpz * poly, long len);
+
+static __inline__ 
+ulong fmpz_poly_max_limbs(const fmpz_poly_t poly)
+{
+    return _fmpz_vec_max_limbs(poly->coeffs, poly->length);
+}
+
+static __inline__ 
+long fmpz_poly_max_bits(const fmpz_poly_t poly)
+{
+    return _fmpz_vec_max_bits(poly->coeffs, poly->length);
+}
+
+static __inline__ void
+fmpz_poly_height(fmpz_t res, const fmpz_poly_t poly)
+{
+    _fmpz_vec_height(res, poly->coeffs, poly->length);
+}
 
 /*  Greatest common divisor  *************************************************/
 
