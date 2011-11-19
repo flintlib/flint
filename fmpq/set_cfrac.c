bbb2497dfc1f765ae931d64b9b458ccc3670ebe1
Fredrik Johansson <fredrik.johansson@gmail.com>
2011-11-19 21:24:17 +0100

    doc update

--- fmpq/set_cfrac.c
+++ fmpq/set_cfrac.c
@@ -27,19 +27,18 @@
 #include "flint.h"
 #include "fmpz.h"
 #include "fmpq.h"
+#include "fmpz_mat.h"
 
 void
-_fmpq_set_cfrac(fmpz_t p, fmpz_t q, const fmpz * c, long n)
+_fmpq_set_cfrac_basecase(fmpz_t p, fmpz_t t, fmpz_t q, fmpz_t u,
+    const fmpz * c, long n)
 {
-    fmpz_t t, u;
     long i;
 
     fmpz_set(p, c);
     fmpz_set_ui(q, 1);
-
-    fmpz_init(t);
-    fmpz_init(u);
     fmpz_set_ui(t, 1);
+    fmpz_zero(u);
 
     for (i = 1; i < n; i++)
     {
@@ -48,14 +47,53 @@ _fmpq_set_cfrac(fmpz_t p, fmpz_t q, const fmpz * c, long n)
         fmpz_swap(t, p);
         fmpz_swap(u, q);
     }
+}
 
-    fmpz_clear(t);
-    fmpz_clear(u);
+void
+_fmpq_set_cfrac_divconquer(fmpz_mat_t P, const fmpz * c, long n)
+{
+    if (n < 32)
+    {
+        _fmpq_set_cfrac_basecase(
+            fmpz_mat_entry(P, 0, 0), fmpz_mat_entry(P, 0, 1),
+            fmpz_mat_entry(P, 1, 0), fmpz_mat_entry(P, 1, 1), c, n);
+    }
+    else
+    {
+        fmpz_mat_t L, R;
+        long m = n / 2;
+
+        fmpz_mat_init(L, 2, 2);
+        fmpz_mat_init(R, 2, 2);
+
+        _fmpq_set_cfrac_divconquer(L, c, m);
+        _fmpq_set_cfrac_divconquer(R, c + m, n - m);
+        fmpz_mat_mul_classical(P, L, R);  /* Should be Strassen */
+
+        fmpz_mat_clear(L);
+        fmpz_mat_clear(R);
+    }
 }
 
 void
 fmpq_set_cfrac(fmpq_t x, const fmpz * c, long n)
 {
-    _fmpq_set_cfrac(fmpq_numref(x), fmpq_denref(x), c, n);
+    if (n <= 64)
+    {
+        fmpz_t t, u;
+        fmpz_init(t);
+        fmpz_init(u);
+        _fmpq_set_cfrac_basecase(fmpq_numref(x), t, fmpq_denref(x), u, c, n);
+        fmpz_clear(t);
+        fmpz_clear(u);
+    }
+    else
+    {
+        fmpz_mat_t P;
+        fmpz_mat_init(P, 2, 2);
+        _fmpq_set_cfrac_divconquer(P, c, n);
+        fmpz_set(fmpq_numref(x), fmpz_mat_entry(P, 0, 0));
+        fmpz_set(fmpq_denref(x), fmpz_mat_entry(P, 1, 0));
+        fmpz_mat_clear(P);
+    }
 }
-
