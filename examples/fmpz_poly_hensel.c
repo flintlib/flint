4291a5c1937d9505d6f88f201296c161e24c23b1
Andy Novocin <andy@novocin.com>
2011-12-21 23:07:27 +0000

    Fixed minor typos in fmpz_poly_hensel.c

--- examples/fmpz_poly_hensel.c
+++ examples/fmpz_poly_hensel.c
@@ -20,13 +20,13 @@ int main(void)
 
     */
 
-    fmpz_poly_t pol;
-    fmpz_poly_init(pol);
+    fmpz_poly_t f;
+    fmpz_poly_init(f);
 
     nmod_poly_factor_t facs;
     nmod_poly_factor_init(facs);
 
-    fmpz_poly_set_str(pol, "5  190713877264 0 354248 0 1");
+    fmpz_poly_set_str(f, "5  190713877264 0 354248 0 1");
     long p = 89;
     fmpz_t P, Q, PQ;
 
@@ -38,7 +38,7 @@ int main(void)
 
     nmod_poly_init(mod_pol, p);
 
-    fmpz_poly_get_nmod_poly(mod_pol, pol);
+    fmpz_poly_get_nmod_poly(mod_pol, f);
     nmod_poly_factor(facs, mod_pol);
 
     nmod_poly_factor_print(facs); 
@@ -59,9 +59,9 @@ int main(void)
     nmod_poly_init(bp, p);
     nmod_poly_init(dp, p);
 
-    fmpz_poly_t temp, temp2;
+    fmpz_poly_t temp, temp1;
     fmpz_poly_init(temp);
-    fmpz_poly_init(temp2);
+    fmpz_poly_init(temp1);
 
     nmod_poly_set(gp, facs->factors[0]);
     nmod_poly_set(hp, facs->factors[1]);
@@ -77,11 +77,11 @@ int main(void)
     fmpz_poly_set_nmod_poly(a, ap);
     fmpz_poly_set_nmod_poly(b, bp);
 
-    fmpz_poly_hensel_lift(g, h, a, b, pol, g, h, a, b, P, Q, PQ);
+    fmpz_poly_hensel_lift(g, h, a, b, f, g, h, a, b, P, Q, PQ);
     
     fmpz_poly_mul(temp, g, h);
     fmpz_poly_sub(temp, f, temp);
-    fmpz_poly_scalar_mod_fmpz(temp, PQ);
+    fmpz_poly_scalar_mod_fmpz(temp, temp, PQ);
     long result = fmpz_poly_is_zero(temp);
     if (result > 0)
         printf("hensel_lift part worked\n");
@@ -91,8 +91,8 @@ int main(void)
     fmpz_poly_mul(temp, g, a);
     fmpz_poly_mul(temp1, h, b);
     fmpz_poly_add(temp, temp, temp1);
-    fmpz_poly_scalar_mod_fmpz(temp, PQ);
-    long result = fmpz_poly_is_one(temp);
+    fmpz_poly_scalar_mod_fmpz(temp, temp, PQ);
+    result = fmpz_poly_is_one(temp);
     if (result > 0)
         printf("hensel_lift inverse part worked\n");
     else
@@ -106,8 +106,8 @@ int main(void)
     fmpz_poly_hensel_lift_without_inverse(g, h, f, g, h, a, b, P, Q, PQ);
     fmpz_poly_mul(temp, g, h);
     fmpz_poly_sub(temp, f, temp);
-    fmpz_poly_scalar_mod_fmpz(temp, PQ);
-    long result = fmpz_poly_is_zero(temp);
+    fmpz_poly_scalar_mod_fmpz(temp, temp, PQ);
+    result = fmpz_poly_is_zero(temp);
     if (result > 0)
         printf("hensel_lift_without_inverse worked\n");
     else
@@ -118,8 +118,8 @@ int main(void)
     fmpz_poly_mul(temp, g, a);
     fmpz_poly_mul(temp1, h, b);
     fmpz_poly_add(temp, temp, temp1);
-    fmpz_poly_scalar_mod_fmpz(temp, PQ);
-    long result = fmpz_poly_is_one(temp);
+    fmpz_poly_scalar_mod_fmpz(temp, temp, PQ);
+    result = fmpz_poly_is_one(temp);
     if (result > 0)
         printf("hensel_lift_only_inverse worked\n");
     else
@@ -130,7 +130,7 @@ int main(void)
     fmpz_poly_clear(temp1);
 
     nmod_poly_factor_clear(facs);
-    fmpz_poly_clear(pol);
+    fmpz_poly_clear(f);
     fmpz_poly_clear(g);
     fmpz_poly_clear(h);
     fmpz_poly_clear(a);
