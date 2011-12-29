337d8cd4aed27537d2bb480dd55b39cf7321f17f
SPancratz <spancratz@github.com>
2011-12-29 15:07:40 +0000

    Adds a wrapper "fmpz_mod_poly_gcd".

--- fmpz_mod_poly.h
+++ fmpz_mod_poly.h
@@ -347,6 +347,21 @@ void fmpz_mod_poly_gcd_euclidean(fmpz_mod_poly_t G,
                                  const fmpz_mod_poly_t A,
                                  const fmpz_mod_poly_t B);
 
+static __inline__ 
+long _fmpz_mod_poly_gcd(fmpz *G, const fmpz *A, long lenA, 
+                                 const fmpz *B, long lenB, 
+                                 const fmpz_t invB, const fmpz_t p)
+{
+    return _fmpz_mod_poly_gcd_euclidean(G, A, lenA, B, lenB, invB, p);
+}
+
+static __inline__ 
+void fmpz_mod_poly_gcd(fmpz_mod_poly_t G, 
+                       const fmpz_mod_poly_t A, const fmpz_mod_poly_t B)
+{
+    fmpz_mod_poly_gcd_euclidean(G, A, B);
+}
+
 /*  Derivative  **************************************************************/
 
 void _fmpz_mod_poly_derivative(fmpz *res, const fmpz *poly, long len, 
