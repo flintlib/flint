14ca9bb39a2f1ecefb5a9cce51d567d71cfe91f9
William Hart <goodwillhart@googlemail.com>
2012-01-06 00:15:02 +0000

    Further correction to copyright dates.

--- fft_tuning.h
+++ fft_tuning.h
@@ -1,6 +1,6 @@
 /* mul_fft -- radix 2 fft routines for MPIR.
 
-Copyright 2009, 2011 William Hart. All rights reserved.
+Copyright 2011 William Hart. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are
 permitted provided that the following conditions are met:
@@ -27,12 +27,6 @@ authors and should not be interpreted as representing official policies, either 
 or implied, of William Hart.
 
 */
-
-/******************************************************************************
-
-    Copyright (C) 2011 William Hart
- 
-******************************************************************************/
 
 #ifndef FFT_TUNING_H
 #define FFT_TUNING_H
