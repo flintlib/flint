8aba72249812c551899c964457592fd2166ca7b7
Frithjof Schulze <schulze@math.uni-hannover.de>
2012-01-17 22:13:31 +0100

    Add fmpz_tdiv_ui().

---
+++ fmpz/tdiv_ui.c
@@ -0,0 +1,56 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+
+ulong
+fmpz_tdiv_ui(const fmpz_t g, ulong h)
+{
+    fmpz c1 = *g;
+
+    if (h == 0UL)
+    {
+        printf("Exception: division by 0 in fmpz_tdiv_ui\n");
+        abort();
+    }
+
+    if (!COEFF_IS_MPZ(c1))      /* g is small */
+    {
+		/* We need the absolut value of the remainder and
+		   C 90 guarantees truncation towards zero. */
+		if (c1 < 0L)
+			return -c1 % h;
+		else
+			return c1 % h;
+    }
+    else                        /* g is large */
+    {
+        return mpz_tdiv_ui(COEFF_TO_PTR(c1), h);
+    }
+}
---
+++ fmpz/test/t-tdiv_ui.c
@@ -0,0 +1,78 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+
+int
+main(void)
+{
+    int i, result;
+    flint_rand_t state;
+
+    printf("tdiv_ui....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    for (i = 0; i < 100000; i++)
+    {
+        fmpz_t a;
+        mpz_t b;
+        ulong x, r1, r2;
+
+        fmpz_init(a);
+        mpz_init(b);
+
+        fmpz_randtest(a, state, 200);
+
+        fmpz_get_mpz(b, a);
+        x = n_randtest_not_zero(state);
+
+        r1 = fmpz_tdiv_ui(a, x);
+        r2 = mpz_tdiv_ui(b, x);
+
+        result = (r1 == r2);
+        if (!result)
+        {
+            printf("FAIL:\n");
+            gmp_printf
+                ("b = %Zd, x = %lu, r1 = %lu, r2 = %lu\n", b, x, r1, r2);
+            abort();
+        }
+
+        fmpz_clear(a);
+        mpz_clear(b);
+    }
+
+    flint_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return 0;
+}
