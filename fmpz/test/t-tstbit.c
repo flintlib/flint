c6bafb7711401fdfd7ad9cd9622d2e115efaaa04
SPancratz <spancratz@github.com>
2012-02-01 22:09:31 +0000

    Adds fmpz_tstbit.

---
+++ fmpz/test/t-tstbit.c
@@ -0,0 +1,79 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2012 Sebastian Pancratz
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "ulong_extras.h"
+#include "fmpz.h"
+
+int
+main(void)
+{
+    int i, result;
+    flint_rand_t state;
+
+    printf("tstbit....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    for (i = 0; i < 100000; i++)
+    {
+        int k, l;
+        ulong j;
+        fmpz_t a;
+        mpz_t b;
+
+        fmpz_init(a);
+        mpz_init(b);
+
+        fmpz_randtest(a, state, 2 * FLINT_BITS);
+        fmpz_get_mpz(b, a);
+        j = n_randint(state, 3 * FLINT_BITS);
+
+        k = fmpz_tstbit(a, j);
+        l = mpz_tstbit(b, j);
+
+        result = (k == l);
+
+        if (!result)
+        {
+            printf("FAIL:\n");
+            gmp_printf("b = %Zd, j = %lu k = %d, l = %d\n", b, j, k, l);
+            abort();
+        }
+
+        fmpz_clear(a);
+        mpz_clear(b);
+    }
+
+    flint_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return 0;
+}
---
+++ fmpz/tstbit.c
@@ -0,0 +1,46 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+   Copyright (C) 2012 Sebastian Pancratz
+
+******************************************************************************/
+
+#include "fmpz.h"
+
+int fmpz_tstbit(const fmpz_t f, ulong i)
+{
+    if (!COEFF_IS_MPZ(*f))
+    {
+        if (i < FLINT_BITS)
+        {
+            return ((1L << i) & *f) != 0;
+        }
+        else  /* i >= FLINT_BITS */
+        {
+            return *f < 0;
+        }
+    }
+    else
+    {
+        return mpz_tstbit(COEFF_TO_PTR(*f), i);
+    }
+}
+
