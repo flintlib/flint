2270813fbdcbf4d3312ededee27b64bf626c830f
SPancratz <spancratz@github.com>
2012-02-13 14:38:08 +0000

    Improve test code for qadic_sigma, also includes products now.

--- qadic/test/t-sigma.c
+++ qadic/test/t-sigma.c
@@ -192,6 +192,64 @@ main(void)
         qadic_ctx_clear(ctx);
     }
 
+    /* Check sigma(x * y) = sigma(x) * sigma(y) on Zq */
+    for (i = 0; i < 1000; i++)
+    {
+        fmpz_t p;
+        long d, N;
+        qadic_ctx_t ctx;
+
+        qadic_t a, b, s, s1, s2, lhs, rhs;
+
+        fmpz_init(p);
+        fmpz_set_ui(p, n_randprime(state, 2 + n_randint(state, 3), 1));
+        d = n_randint(state, 10) + 1;
+        N = n_randint(state, 10) + 1;
+        qadic_ctx_init_conway(ctx, p, d, N, "a", PADIC_SERIES);
+
+        qadic_init(a);
+        qadic_init(b);
+        qadic_init(s);
+        qadic_init(s1);
+        qadic_init(s2);
+        qadic_init(lhs);
+        qadic_init(rhs);
+
+        qadic_randtest_int(a, state, ctx);
+        qadic_randtest_int(b, state, ctx);
+
+        qadic_mul(s, a, b, ctx);
+        qadic_sigma(lhs, s, ctx);
+        qadic_sigma(s1, a, ctx);
+        qadic_sigma(s2, b, ctx);
+        qadic_mul(rhs, s1, s2, ctx);
+
+        result = (qadic_equal(lhs, rhs));
+        if (!result)
+        {
+            printf("FAIL (sigma(a*b) = sigma(a) * sigma(b)):\n\n");
+            printf("a = "), qadic_print_pretty(a, ctx), printf("\n");
+            printf("b = "), qadic_print_pretty(b, ctx), printf("\n");
+            printf("s = "), qadic_print_pretty(s, ctx), printf("\n");
+            printf("s1 = "), qadic_print_pretty(s1, ctx), printf("\n");
+            printf("s2 = "), qadic_print_pretty(s2, ctx), printf("\n");
+            printf("lhs = "), qadic_print_pretty(lhs, ctx), printf("\n");
+            printf("rhs = "), qadic_print_pretty(rhs, ctx), printf("\n");
+            abort();
+        }
+
+        qadic_clear(a);
+        qadic_clear(b);
+        qadic_clear(s);
+        qadic_clear(s1);
+        qadic_clear(s2);
+        qadic_clear(lhs);
+        qadic_clear(rhs);
+
+        fmpz_clear(p);
+        qadic_ctx_clear(ctx);
+    }
+
     flint_randclear(state);
     _fmpz_cleanup();
     printf("PASS\n");
