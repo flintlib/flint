64e1e3a3583369ca3977d3a66da47f6dc46b2c4a
SPancratz <spancratz@github.com>
2012-02-22 07:58:48 -0800

    Improvement to padic/log_rectangular

--- padic/log_rectangular.c
+++ padic/log_rectangular.c
@@ -19,13 +19,18 @@ extern long _padic_log_bound(long v, long N, long p);
  */
 static void 
 _padic_log_rectangular_series(fmpz_t z, const fmpz_t y, long n, 
-                              const fmpz_t p, long N, const fmpz_t P)
+                              const fmpz_t p, long N)
 {
+    fmpz_t P0;
+
+    fmpz_init(P0);
+    fmpz_pow_ui(P0, p, N);
+
     if (n <= 2)
     {
         if (n == 1)
         {
-            fmpz_mod(z, y, P);
+            fmpz_mod(z, y, P0);
         }
         else  /* n == 2;  z = y(1 + y/2) */
         {
@@ -35,39 +40,48 @@ _padic_log_rectangular_series(fmpz_t z, const fmpz_t y, long n,
             }
             else  /* => p and y are odd */
             {
-                fmpz_add(z, y, P);
+                fmpz_add(z, y, P0);
                 fmpz_fdiv_q_2exp(z, z, 1);
             }
             fmpz_add_ui(z, z, 1);
             fmpz_mul(z, z, y);
-            fmpz_mod(z, z, P);
+            fmpz_mod(z, z, P0);
         }
     }
     else
     {
         const long b = n_sqrt(n);
+        const long k = fmpz_fits_si(p) ? n_flog(n, fmpz_get_si(p)) : 0;
 
         long i, j;
-        fmpz_t c, f, m, t;
-        fmpz *ypow;
+        fmpz_t c, f, t, P1;
+        fmpz *ppow, *ypow;
 
+        ppow = _fmpz_vec_init(k + 1);
         ypow = _fmpz_vec_init(b + 1);
         fmpz_init(c);
         fmpz_init(f);
-        fmpz_init(m);
         fmpz_init(t);
+        fmpz_init(P1);
+
+        fmpz_pow_ui(P1, p, N + k);
+
+        fmpz_one(ppow + 0);
+        for (i = 1; i <= k; i++)
+            fmpz_mul(ppow + i, ppow + (i - 1), p);
 
         fmpz_one(ypow + 0);
         for (i = 1; i <= b; i++)
+        {
             fmpz_mul(ypow + i, ypow + (i - 1), y);
+            fmpz_mod(ypow + i, ypow + i, P1);
+        }
 
-        fmpz_zero(z);
-        fmpz_one(m);
-
-        for (j = 0; j < (n + (b-1)) / b; j++)
+        j = (n + (b-1)) / b - 1;
         {
             const long hi = FLINT_MIN(b, n - j*b);
 
+            /* Compute inner sum in c */
             fmpz_rfac_uiui(f, 1 + j*b, hi);
 
             fmpz_zero(c);
@@ -76,26 +90,54 @@ _padic_log_rectangular_series(fmpz_t z, const fmpz_t y, long n,
                 fmpz_divexact_ui(t, f, i+j*b);
                 fmpz_addmul(c, t, ypow + i);
             }
+            fmpz_mod(c, c, P1);
 
             i = fmpz_remove(f, f, p);
-            _padic_inv(f, f, p, N);
-            fmpz_pow_ui(t, p, i);
-            fmpz_divexact(c, c, t);
+            _padic_inv(f, f, p, N + k);
+            fmpz_mul(c, c, ppow + (k - i));
             fmpz_mul(c, c, f);
 
-            fmpz_mul(c, c, m);
-            fmpz_add(z, z, c);
-            fmpz_mod(z, z, P);
-
-            fmpz_mul(m, m, ypow + b);
+            /* Set z */
+            fmpz_mod(z, c, P1);
         }
+
+        for (j--; j >= 0; j--)
+        {
+            const long hi = FLINT_MIN(b, n - j*b);
+
+            /* Compute inner sum in c */
+            fmpz_rfac_uiui(f, 1 + j*b, hi);
+
+            fmpz_zero(c);
+            for (i = 1; i <= hi; i++)
+            {
+                fmpz_divexact_ui(t, f, i+j*b);
+                fmpz_addmul(c, t, ypow + i);
+            }
+            fmpz_mod(c, c, P1);
+
+            i = fmpz_remove(f, f, p);
+            _padic_inv(f, f, p, N + k);
+            fmpz_mul(c, c, ppow + (k - i));
+            fmpz_mul(c, c, f);
+
+            /* Set z = z y^b + c */
+            fmpz_mul(t, z, ypow + b);
+            fmpz_add(z, c, t);
+            fmpz_mod(z, z, P1);
+        }
+
+        fmpz_divexact(z, z, ppow + k);
 
         fmpz_clear(c);
         fmpz_clear(f);
-        fmpz_clear(m);
         fmpz_clear(t);
+        fmpz_clear(P1);
+        _fmpz_vec_clear(ppow, k + 1);
         _fmpz_vec_clear(ypow, b + 1);
     }
+
+    fmpz_clear(P0);
 }
 
 /*
@@ -125,16 +167,8 @@ static void _padic_log_rectangular(fmpz_t z, const fmpz_t y, long v, const padic
     {
         const long p = fmpz_get_si(ctx->p);
         const long i = _padic_log_bound(v, ctx->N, p) - 1;
-        const long k = n_flog(i, p);
 
-        fmpz_t P;
-
-        fmpz_init(P);
-        fmpz_pow_ui(P, ctx->p, ctx->N + k);
-        _padic_log_rectangular_series(z, y, i, ctx->p, ctx->N + k, P);
-        fmpz_pow_ui(P, ctx->p, ctx->N);
-        fmpz_mod(z, z, P);
-        fmpz_clear(P);
+        _padic_log_rectangular_series(z, y, i, ctx->p, ctx->N);
     }
     else
     {
