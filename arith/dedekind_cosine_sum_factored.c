98d1c22919cfaeabd680052bb8762ff987787efb
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-03-23 11:12:45 +0100

    fix potential overflow in square root code mod p^k

--- arith/dedekind_cosine_sum_factored.c
+++ arith/dedekind_cosine_sum_factored.c
@@ -82,7 +82,7 @@ n_sqrtmod_ppow(mp_limb_t a, mp_limb_t p, int k, mp_limb_t pk, mp_limb_t pkinv)
     {
         t = n_mulmod2_preinv(r, r, pk, pkinv);
         t = n_submod(t, a, pk);
-        t = n_mulmod2_preinv(t, n_invmod((2*r) % pk, pk), pk, pkinv);
+        t = n_mulmod2_preinv(t, n_invmod(n_addmod(r, r, pk), pk), pk, pkinv);
         r = n_submod(r, t, pk);
         i *= 2;
     }
