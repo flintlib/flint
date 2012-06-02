2c5cebdf8b30b38c77fc9476a80652bf8014dadb
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-06-02 17:29:30 +0200

    fix warnings for unused variables

--- fmpq_poly/test/t-interpolate_fmpz_vec.c
+++ fmpq_poly/test/t-interpolate_fmpz_vec.c
@@ -49,10 +49,9 @@ main(void)
         fmpq_poly_t P;
         fmpz *x, *y, *z;
         fmpq_t q;
-        long j, n, l, bits;
+        long j, n, bits;
 
         n = n_randint(state, 50);
-        l = n_randint(state, n + 1);
         bits = n_randint(state, 100);
 
         x = _fmpz_vec_init(n);
