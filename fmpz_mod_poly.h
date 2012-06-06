1b7d9407488f407389e8ab58d65067ed9cf7cbf0
Lina Kulakova <lina-mihailovna@Lina.(none)>
2012-06-06 17:45:25 +0400

    fmpz_mod_poly: add function pow_trunc_binexp

--- fmpz_mod_poly.h
+++ fmpz_mod_poly.h
@@ -270,6 +270,12 @@ void _fmpz_mod_poly_pow(fmpz *rop, const fmpz *op, long len, ulong e,
 
 void fmpz_mod_poly_pow(fmpz_mod_poly_t rop, const fmpz_mod_poly_t op, ulong e);
 
+void _fmpz_mod_poly_pow_trunc_binexp(fmpz * res, fmpz * poly,
+                                     ulong e, long trunc, const fmpz_t p);
+
+void fmpz_mod_poly_pow_trunc_binexp(fmpz_mod_poly_t res,
+                              const fmpz_mod_poly_t poly, ulong e, long trunc);
+
 /*  Division *****************************************************************/
 
 void _fmpz_mod_poly_divrem_basecase(fmpz * Q, fmpz * R, 
