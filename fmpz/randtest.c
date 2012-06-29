dc0a3aea3f7724286595c5b308b3278a29ebba24
William Hart <goodwillhart@googlemail.com>
2012-06-29 01:43:02 +0100

    Use n_randtest_bits in fmpz_randtest.

--- fmpz/randtest.c
+++ fmpz/randtest.c
@@ -56,7 +56,7 @@ fmpz_randtest_unsigned(fmpz_t f, flint_rand_t state, mp_bitcnt_t bits)
     {
         _fmpz_demote(f);
         if (m & 3UL)
-            *f = n_randbits(state, bits);
+            *f = n_randtest_bits(state, bits);
         else
         {
             m >>= 2;
