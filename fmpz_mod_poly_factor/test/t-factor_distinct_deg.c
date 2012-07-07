0af4e7448e0dfac5b6f4bd324241f6ec37c695af
Lina Kulakova <kulakova.lina@gmail.com>
2012-07-07 16:25:14 +0400

    fmpz_mod_poly_factor: fix some tests

--- fmpz_mod_poly_factor/test/t-factor_distinct_deg.c
+++ fmpz_mod_poly_factor/test/t-factor_distinct_deg.c
@@ -65,9 +65,10 @@ main(void)
         do
         {
             fmpz_mod_poly_randtest(poly, state, length);
-            fmpz_mod_poly_make_monic(poly, poly);
+            if (poly->length)
+                fmpz_mod_poly_make_monic(poly, poly);
         }
-        while ((!fmpz_mod_poly_is_irreducible(poly)) || (poly->length < 2));
+        while ((poly->length < 2) || (!fmpz_mod_poly_is_irreducible(poly)));
 
         fmpz_mod_poly_mul(poly1, poly1, poly);
 
@@ -79,11 +80,13 @@ main(void)
             {
                 length = n_randint(state, 7) + 2;
                 fmpz_mod_poly_randtest(poly, state, length);
-                fmpz_mod_poly_make_monic(poly, poly);
                 if (poly->length)
+                {
+                    fmpz_mod_poly_make_monic(poly, poly);
                     fmpz_mod_poly_divrem(q, r, poly1, poly);
+                }
             }
-            while ((!fmpz_mod_poly_is_irreducible(poly)) || (poly->length < 2) ||
+            while ((poly->length < 2) || (!fmpz_mod_poly_is_irreducible(poly)) ||
                 (r->length == 0));
 
             fmpz_mod_poly_mul(poly1, poly1, poly);
