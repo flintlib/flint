f29bb61dd18b01606d9a7158fe62206dacf93091
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-07-12 19:44:49 +0200

    fix nmod_mat_transpose

--- nmod_mat/transpose.c
+++ nmod_mat/transpose.c
@@ -43,25 +43,20 @@ nmod_mat_transpose(nmod_mat_t B, const nmod_mat_t A)
         abort();
     }
 
-    /* In-place, guaranteed to be square */
-    if (A == B)
+    if (A == B) /* In-place, guaranteed to be square */
     {
         for (i = 0; i < A->r - 1; i++)
-        {
-            for (j = i + 1; j < i; j++)
+            for (j = i + 1; j < A->c; j++)
             {
                 tmp = A->rows[i][j];
                 A->rows[i][j] = A->rows[j][i];
                 A->rows[j][i] = tmp;
             }
-        }
     }
-
-    /* Not aliased; general case */
-    for (i = 0; i < B->r; i++)
-        for (j = 0; j < B->c; j++)
-            B->rows[i][j] = A->rows[j][i];
-
-    if (B->mod.n != A->mod.n)
-        _nmod_vec_reduce(B->entries, B->entries, B->r * B->c, B->mod);
+    else /* Not aliased; general case */
+    {
+        for (i = 0; i < B->r; i++)
+            for (j = 0; j < B->c; j++)
+                B->rows[i][j] = A->rows[j][i];
+    }
 }
