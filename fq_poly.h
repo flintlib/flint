79232306eaab3b4db0ac263230903859d4239439
Sebastian Pancratz <sebastian.pancratz@gmail.com>
2012-09-16 18:09:59 +0100

    Adds fq_poly_mul().
    At this point, the algorithm selection is very basic.

--- fq_poly.h
+++ fq_poly.h
@@ -229,6 +229,13 @@ void fq_poly_mul_KS(fq_poly_t rop,
                     const fq_poly_t op1, const fq_poly_t op2, 
                     const fq_ctx_t ctx);
 
+void _fq_poly_mul(fq_struct *rop, const fq_struct *op1, long len1, 
+                                  const fq_struct *op2, long len2, 
+                                  const fq_ctx_t ctx);
+
+void fq_poly_mul(fq_poly_t rop, 
+                 const fq_poly_t op1, const fq_poly_t op2, const fq_ctx_t ctx);
+
 /* Squaring ******************************************************************/
 
 /* TODO:  Implement */
