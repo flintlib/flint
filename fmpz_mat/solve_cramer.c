74c81ed9b7f9475e7be73590816fafb4f0155269
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-09-28 17:43:19 +0200

    make return value from solve_cramer with empty rhs consistent with the other functions

--- fmpz_mat/solve_cramer.c
+++ fmpz_mat/solve_cramer.c
@@ -122,7 +122,7 @@ fmpz_mat_solve_cramer(fmpz_mat_t X, fmpz_t den,
 {
     long i, dim = fmpz_mat_nrows(A);
 
-    if (dim == 0)
+    if (dim == 0 || fmpz_mat_ncols(B) == 0)
     {
         fmpz_one(den);
         return 1;
