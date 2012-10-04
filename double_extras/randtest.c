36e38e61e70dbc77426bcfe535d627ac195ada6a
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-10-04 19:13:53 +0200

    a new double_extras module, with a fast Lambert W function

---
+++ double_extras/randtest.c
@@ -0,0 +1,53 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+   Copyright (C) 2012 Fredrik Johansson
+
+******************************************************************************/
+
+#include "double_extras.h"
+
+#define EXP_MINUS_32 2.3283064365386962891e-10
+#define EXP_MINUS_64 5.42101086242752217e-20
+
+double
+d_randtest(flint_rand_t state)
+{
+    mp_limb_t m1, m2;
+    double t;
+
+    if (FLINT_BITS == 64)
+    {
+        m1 = n_randlimb(state) | (1UL << (FLINT_BITS - 1));
+
+        t = ((double) m1) * EXP_MINUS_64;
+    }
+    else
+    {
+        m1 = n_randlimb(state) | (1UL << (FLINT_BITS - 1));
+        m2 = n_randlimb(state);
+
+        t = ((double) m1) * EXP_MINUS_32 +
+            ((double) m2) * EXP_MINUS_64;
+    }
+
+    return t;
+}
