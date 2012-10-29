4f22e91fbfd408c32474a1cce5e851bba8facba4
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-10-29 11:48:28 +0100

    use faster algorithm for fmpz_mat_rref, and improve test code

--- fmpz_mat/test/t-rref.c
+++ fmpz_mat/test/t-rref.c
@@ -19,7 +19,7 @@
 =============================================================================*/
 /******************************************************************************
 
-    Copyright (C) 2010 Fredrik Johansson
+    Copyright (C) 2010-2012 Fredrik Johansson
 
 ******************************************************************************/
 
@@ -30,118 +30,163 @@
 #include "fmpz.h"
 #include "fmpz_vec.h"
 #include "fmpz_mat.h"
+#include "perm.h"
 #include "ulong_extras.h"
 
-void check_rref(fmpz_mat_t A)
+/* checks that the rref has the right form */
+int check_rref(const fmpz_mat_t A, const fmpz_t den, long rank)
 {
-    long i, j, prev_pivot, prev_row_zero;
+    long i, j, k, prev_pivot;
 
-    prev_row_zero = 0;
+    /* bottom should be zero */
+    for (i = rank; i < A->r; i++)
+        for (j = 0; j < A->c; j++)
+            if (!fmpz_is_zero(fmpz_mat_entry(A, i, j)))
+                return 0;
+
     prev_pivot = -1;
 
-    for (i = 0; i < A->r; i++)
+    for (i = 0; i < rank; i++)
     {
         for (j = 0; j < A->c; j++)
         {
-            /* Found nonzero entry */
-            if (A->rows[i][j] != 0L)
+            if (!fmpz_is_zero(fmpz_mat_entry(A, i, j)))
             {
-                if (prev_row_zero)
-                {
-                    printf("nonzero row after zero row\n");
-                    abort();
-                }
-
+                /* pivot should have a higher column index than previous */
                 if (j <= prev_pivot)
+                    return 0;
+
+                /* column should be 0 ... 0 1 0 ... 0 */
+                for (k = 0; k < rank; k++)
                 {
-                    printf("pivot not strictly to the right of previous\n");
-                    abort();
+                    if (i == k && !fmpz_equal(fmpz_mat_entry(A, k, j), den))
+                        return 0;
+                    if (i != k && !fmpz_is_zero(fmpz_mat_entry(A, k, j)))
+                        return 0;
                 }
 
                 prev_pivot = j;
                 break;
             }
-
-            prev_row_zero = (j+1 == A->c);
         }
     }
-}
 
+    return 1;
+}
 
 int
 main(void)
 {
-    fmpz_mat_t A;
-    fmpz_t den;
+    long iter;
     flint_rand_t state;
-    long i, m, n, b, d, r, rank;
 
     printf("rref....");
     fflush(stdout);
 
     flint_randinit(state);
 
-    fmpz_init(den);
-
-    /* Maximally sparse matrices of given rank */
-    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
+    for (iter = 0; iter < 10000 * flint_test_multiplier(); iter++)
     {
+        fmpz_mat_t A, R, B, R2;
+        fmpz_t den, c, den2;
+        long j, k, m, n, b, d, r, rank1, rank2;
+        long *perm;
+        int equal;
+
         m = n_randint(state, 10);
         n = n_randint(state, 10);
+        r = n_randint(state, FLINT_MIN(m, n) + 1);
 
-        for (r = 0; r <= FLINT_MIN(m,n); r++)
-        {
-            b = 1 + n_randint(state, 10) * n_randint(state, 10);
-            d = n_randint(state, 2*m*n + 1);
-            fmpz_mat_init(A, m, n);
-            fmpz_mat_randrank(A, state, r, b);
-            rank = fmpz_mat_rref(A, den, A);
-            if (r != rank)
-            {
-                printf("FAIL:\n");
-                printf("wrong rank!\n");
-                abort();
-            }
-            check_rref(A);
-            fmpz_mat_clear(A);
-        }
-    }
+        fmpz_mat_init(A, m, n);
+        fmpz_mat_init(R, m, n);
+        fmpz_mat_init(B, 2 * m, n);
+        fmpz_mat_init(R2, 2 * m, n);
 
-    /* Dense */
-    for (i = 0; i < 1000 * flint_test_multiplier(); i++)
-    {
-        m = n_randint(state, 10);
-        n = n_randint(state, 10);
+        fmpz_init(c);
+        fmpz_init(den);
+        fmpz_init(den2);
 
-        for (r = 0; r <= FLINT_MIN(m,n); r++)
-        {
-            b = 1 + n_randint(state, 10) * n_randint(state, 10);
-            d = n_randint(state, 2*m*n + 1);
+        perm = _perm_init(2 * m);
 
-            fmpz_mat_init(A, m, n);
-            fmpz_mat_randrank(A, state, r, b);
+        /* sparse */
+        b = 1 + n_randint(state, 10) * n_randint(state, 10);
+        d = n_randint(state, 2*m*n + 1);
+        fmpz_mat_randrank(A, state, r, b);
 
+        /* dense */
+        if (n_randint(state, 2))
             fmpz_mat_randops(A, state, d);
 
-            rank = fmpz_mat_rref(A, den, A);
+        rank1 = fmpz_mat_rref(R, den, A);
 
-            if (r != rank)
-            {
-                printf("FAIL:\n");
-                printf("wrong rank!\n");
-                abort();
-            }
-
-            check_rref(A);
-
-            fmpz_mat_clear(A);
+        if (r != rank1)
+        {
+            printf("FAIL:\n");
+            printf("wrong rank!\n");
+            abort();
         }
-    }
 
-    fmpz_clear(den);
+        check_rref(R, den, rank1);
+
+        /* Concatenate the original matrix with the rref, scramble the rows,
+            and check that the rref is the same */
+        _perm_randtest(perm, 2 * m, state);
+
+        for (j = 0; j < m; j++)
+        {
+            fmpz_randtest_not_zero(c, state, 5);
+            for (k = 0; k < n; k++)
+                fmpz_mul(fmpz_mat_entry(B, perm[j], k), fmpz_mat_entry(A, j, k), c);
+        }
+
+        for (j = 0; j < m; j++)
+        {
+            fmpz_randtest_not_zero(c, state, 5);
+            for (k = 0; k < n; k++)
+                fmpz_mul(fmpz_mat_entry(B, perm[m + j], k), fmpz_mat_entry(R, j, k), c);
+        }
+
+        rank2 = fmpz_mat_rref(R2, den2, B);
+        equal = (rank1 == rank2);
+
+        if (equal)
+        {
+            fmpz_mat_scalar_mul_fmpz(R, R, den2);
+            fmpz_mat_scalar_mul_fmpz(R2, R2, den);
+
+            for (j = 0; j < rank2; j++)
+                for (k = 0; k < n; k++)
+                    equal = equal &&
+                        fmpz_equal(fmpz_mat_entry(R, j, k), fmpz_mat_entry(R2, j, k));
+            for (j = rank2; j < 2 * rank2; j++)
+                for (k = 0; k < n; k++)
+                    equal = equal && fmpz_is_zero(fmpz_mat_entry(R2, j, k));
+        }
+
+        if (!equal)
+        {
+            printf("FAIL (rank1 = %ld, rank2 = %ld)!\n", rank1, rank2);
+            fmpz_mat_print_pretty(A); printf("\n\n");
+            fmpz_mat_print_pretty(R); printf("\n\n");
+            fmpz_mat_print_pretty(R2); printf("\n\n");
+            abort();
+        }
+
+        fmpz_clear(c);
+        fmpz_clear(den);
+        fmpz_clear(den2);
+
+        _perm_clear(perm);
+
+        fmpz_mat_clear(A);
+        fmpz_mat_clear(R);
+        fmpz_mat_clear(B);
+        fmpz_mat_clear(R2);
+    }
 
     flint_randclear(state);
     _fmpz_cleanup();
     printf("PASS\n");
     return 0;
 }
+
