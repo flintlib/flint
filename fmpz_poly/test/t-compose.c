95f169c954bc2d09e69004319daca83a2eabb24c
William Hart <goodwillhart@googlemail.com>
2012-10-29 14:26:31 +0000

    Added failing test case for bug found in mpir.h header.

--- fmpz_poly/test/t-compose.c
+++ fmpz_poly/test/t-compose.c
@@ -43,6 +43,47 @@ main(void)
 
     flint_randinit(state);
 
+    /* Bill's bug */
+    {
+        fmpz_poly_t f, g, h, s, t;
+        long k;
+
+        fmpz_poly_init(f);
+        fmpz_poly_init(g);
+        fmpz_poly_init(h);
+        fmpz_poly_init(s);
+        fmpz_poly_init(t);
+        
+        fmpz_poly_set_str(g, "21  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 6");
+        fmpz_poly_set_str(h, "8  -2411740686 -274861162464 -4294966273 -35167058005888 4261511676 -1 8589869056 -70334401183747");
+
+        fmpz_poly_set_ui(t, 1);
+        for (k = 0; k < g->length; k++)
+        {
+            fmpz_poly_scalar_addmul_fmpz(s, t, g->coeffs + k);
+            fmpz_poly_mul(t, t, h);
+        }
+        
+        fmpz_poly_compose(f, g, h);
+
+        result = (fmpz_poly_equal(f, s));
+        if (!result)
+        {
+            printf("FAIL (Bill's bug):\n");
+            printf("g = "), fmpz_poly_print(g), printf("\n\n");
+            printf("h = "), fmpz_poly_print(h), printf("\n\n");
+            printf("f = "), fmpz_poly_print(f), printf("\n\n");
+            printf("s = "), fmpz_poly_print(s), printf("\n\n");
+            abort();
+        }
+
+        fmpz_poly_clear(f);
+        fmpz_poly_clear(g);
+        fmpz_poly_clear(h);
+        fmpz_poly_clear(s);
+        fmpz_poly_clear(t);
+    }
+
     /* Check aliasing of the first argument */
     for (i = 0; i < 10 * flint_test_multiplier(); i++)
     {
