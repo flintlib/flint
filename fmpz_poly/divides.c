b7ac5b735dd4be6d23a0122305523dbbb2d113b4
William Hart <goodwillhart@googlemail.com>
2012-11-06 01:58:38 +0000

    Added missing type.

--- fmpz_poly/divides.c
+++ fmpz_poly/divides.c
@@ -61,7 +61,7 @@ int fmpz_poly_divides(fmpz_poly_t q, const fmpz_poly_t a, const fmpz_poly_t b)
     }
 
     {
-        const lenQ = a->length - b->length + 1;
+        const long lenQ = a->length - b->length + 1;
         int res;
 
         if (q == a || q == b)
