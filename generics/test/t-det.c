a8a74b247ed38da804ea5b2ebd998e7cc5dcabb6
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-12-05 19:03:34 +0100

    avoid random ring code also in the det test code

--- generics/test/t-det.c
+++ generics/test/t-det.c
@@ -27,26 +27,6 @@
 #include <stdio.h>
 #include "generics.h"
 
-int bad_frac_type(ring_t r)
-{
-    if (r->type == TYPE_FRAC)
-    {
-        if (RING_NUMER(r) == RING_DENOM(r))
-            return 0;
-
-        if (RING_NUMER(r)->type == TYPE_POLY &&
-            RING_PARENT(RING_NUMER(r)) == RING_DENOM(r))
-            return 0;
-
-        return 1;
-    }
-
-    if (r->type == TYPE_POLY)
-        return bad_frac_type(RING_PARENT(r));
-
-    return 0;
-}
-
 int main()
 {
     flint_rand_t state;
@@ -56,75 +36,67 @@ int main()
 
     flint_randinit(state);
 
-    for (iter = 0; iter < 200; iter++)
+    for (iter = 0; iter < 1000; iter++)
     {
-        int i, depth;
-        ring_t rings[5];
+        ring_t ZZ, ZZx, MM;
         ring_t mat_ring;
         long size[5] = {4, 4, 4, 4, 4};
         long n;
         elem_mat_t A, B, C;
         elem_ptr a, b, ab, c;
 
-        /* XXX: we want to properly generate integral domains (?) */
-        do {
-            depth = ring_init_randtest(rings, state, 4);
-        } while (bad_frac_type(rings[depth-1]) ||
-            (rings[0]->type == TYPE_LIMB &&
-            (depth == 1 ||
-            (depth > 1 && rings[1]->type != TYPE_MOD))));
+        ring_init_fmpz(ZZ);
+        ring_init_poly(ZZx, ZZ);
+        ring_init_mat(MM, ZZx);
 
-        ring_init_mat(mat_ring, rings[depth-1]);
+        ring_init_mat(mat_ring, MM);
 
-        n = n_randint(state, 6);
-        elem_mat_init(A, n, n, mat_ring);
-        elem_mat_init(B, n, n, mat_ring);
-        elem_mat_init(C, n, n, mat_ring);
+        n = n_randint(state, 8);
+        elem_mat_init(A, n, n, MM);
+        elem_mat_init(B, n, n, MM);
+        elem_mat_init(C, n, n, MM);
 
-        ELEM_TMP_INIT(a, rings[depth-1]);
-        ELEM_TMP_INIT(b, rings[depth-1]);
-        ELEM_TMP_INIT(ab, rings[depth-1]);
-        ELEM_TMP_INIT(c, rings[depth-1]);
+        ELEM_TMP_INIT(a, ZZx);
+        ELEM_TMP_INIT(b, ZZx);
+        ELEM_TMP_INIT(ab, ZZx);
+        ELEM_TMP_INIT(c, ZZx);
 
-        elem_mat_randtest(A, state, size, mat_ring);
-        elem_mat_randtest(B, state, size, mat_ring);
+        elem_mat_randtest(A, state, size, MM);
+        elem_mat_randtest(B, state, size, MM);
 
-        elem_mat_mul(C, A, B, mat_ring);
+        elem_mat_mul(C, A, B, MM);
 
-        elem_mat_det(a, A, mat_ring);
-        elem_mat_det(b, B, mat_ring);
-        elem_mat_det(c, C, mat_ring);
+        elem_mat_det(a, A, MM);
+        elem_mat_det(b, B, MM);
+        elem_mat_det(c, C, MM);
 
-        elem_mul(ab, a, b, rings[depth-1]);
+        elem_mul(ab, a, b, ZZx);
 
-        if (!elem_equal(ab, c, rings[depth-1]))
+        if (!elem_equal(ab, c, ZZx))
         {
             printf("FAIL: det(A)*det(B) != det(A * B)\n");
-            ring_print(rings[depth-1]); printf("\n\n");
-            ring_print(mat_ring); printf("\n\n");
-            elem_mat_print(A, mat_ring); printf("\n\n");
-            elem_mat_print(B, mat_ring); printf("\n\n");
-            elem_mat_print(C, mat_ring); printf("\n\n");
-            elem_print(a, rings[depth-1]); printf("\n\n");
-            elem_print(b, rings[depth-1]); printf("\n\n");
-            elem_print(ab, rings[depth-1]); printf("\n\n");
-            elem_print(c, rings[depth-1]); printf("\n\n");
+            elem_mat_print(A, MM); printf("\n\n");
+            elem_mat_print(B, MM); printf("\n\n");
+            elem_mat_print(C, MM); printf("\n\n");
+            elem_print(a, ZZx); printf("\n\n");
+            elem_print(b, ZZx); printf("\n\n");
+            elem_print(ab, ZZx); printf("\n\n");
+            elem_print(c, ZZx); printf("\n\n");
             abort();
         }
 
-        ELEM_TMP_CLEAR(a, rings[depth-1]);
-        ELEM_TMP_CLEAR(b, rings[depth-1]);
-        ELEM_TMP_CLEAR(ab, rings[depth-1]);
-        ELEM_TMP_CLEAR(c, rings[depth-1]);
+        ELEM_TMP_CLEAR(a, ZZx);
+        ELEM_TMP_CLEAR(b, ZZx);
+        ELEM_TMP_CLEAR(ab, ZZx);
+        ELEM_TMP_CLEAR(c, ZZx);
 
-        elem_mat_clear(A, mat_ring);
-        elem_mat_clear(B, mat_ring);
-        elem_mat_clear(C, mat_ring);
+        elem_mat_clear(A, MM);
+        elem_mat_clear(B, MM);
+        elem_mat_clear(C, MM);
 
-        for (i = 0; i < depth; i++)
-            ring_clear(rings[i]);
-
-        ring_clear(mat_ring);
+        ring_clear(MM);
+        ring_clear(ZZx);
+        ring_clear(ZZ);
     }
 
     printf("PASS\n");
