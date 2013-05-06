9f12c3661b5f20a927efdfec186bfaef2d2bb9e5
Andres Wilhelm Goens Joksich <goens@selmer.warwick.ac.uk>
2013-05-06 11:27:45 +0100

    added tests to fq_mul_ui, si and fmpz

---
+++ fq/test/t-mul_ui.c
@@ -0,0 +1,131 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2013 Andres Goens
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpir.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fq.h" 
+#include "long_extras.h"
+
+int
+main(void)
+{
+    int i, result;
+    flint_rand_t state;
+
+    printf("mul_ui....");
+    fflush(stdout);
+
+    flint_randinit(state);
+    /* Check aliasing of a, b */
+    for (i = 0; i < 2000 * flint_test_multiplier(); i++)
+    {
+        fmpz_t p;
+        long d;
+        fq_ctx_t ctx;
+        ulong x;
+        fq_t a, b;
+
+        fmpz_init(p);
+        fmpz_set_ui(p, n_randprime(state, 2 + n_randint(state, 3), 1));
+        d = n_randint(state, 10) + 1;
+        fq_ctx_init_conway(ctx, p, d, "a");
+
+        fq_init(a);
+        fq_init(b);
+
+        fq_randtest(a, state, ctx);
+        x = z_randtest(state);
+        fq_mul_ui(b, a, x, ctx);
+        fq_mul_ui(a, a, x, ctx);
+
+        result = (fq_equal(a, b));
+        if (!result)
+        {
+            printf("FAIL:\n\n");
+            printf("a = "), fq_print_pretty(a, ctx), printf("\n");
+            printf("b = "), fq_print_pretty(b, ctx), printf("\n");
+	    printf("x = %lu\n",x);
+            abort();
+        }
+
+        fq_clear(a);
+        fq_clear(b);
+
+        fmpz_clear(p);
+        fq_ctx_clear(ctx);
+    }
+
+    /* compare with direct multiplication */
+    for (i = 0; i < 2000 * flint_test_multiplier(); i++)
+    {
+        fmpz_t p;
+        long d;
+        fq_ctx_t ctx;
+        ulong x;
+        fq_t a, c;
+	fmpz_poly_t b;
+
+        fmpz_init(p);
+        fmpz_set_ui(p, n_randprime(state, 2 + n_randint(state, 3), 1));
+        d = n_randint(state, 10) + 1;
+        fq_ctx_init_conway(ctx, p, d, "a");
+
+        fq_init(a);
+        fq_init(c);
+	fmpz_poly_init(b);
+
+        fq_randtest(a, state, ctx);
+        x = z_randtest(state);
+        fq_mul_ui(c, a, x, ctx);
+        fmpz_poly_scalar_mul_ui(b,a,x);
+	fq_reduce(b,ctx);
+
+
+        result = (fq_equal(c, b));
+        if (!result)
+        {
+            printf("FAIL:\n\n");
+            printf("a = "), fq_print_pretty(a, ctx), printf("\n");
+            printf("b = "), fq_print_pretty(b, ctx), printf("\n");
+	    printf("x = %lu\n",x);
+            abort();
+        }
+
+        fq_clear(a);
+        fq_clear(c);
+        fmpz_poly_clear(b);
+        fmpz_clear(p);
+        fq_ctx_clear(ctx);
+    }
+
+    flint_randclear(state);
+    _fmpz_cleanup();
+    printf("PASS\n");
+    return 0;
+}
