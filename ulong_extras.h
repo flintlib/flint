a0e1b03cfd51da4707bf09e1d8008b562cfddad8
Andres Wilhelm Goens Joksich <goens@selmer.warwick.ac.uk>
2013-05-09 17:25:02 +0100

    finished long->len_t, it builds

--- ulong_extras.h
+++ ulong_extras.h
@@ -136,7 +136,7 @@ mp_limb_t n_randtest(flint_rand_t state);
 
 mp_limb_t n_randtest_not_zero(flint_rand_t state);
 
-mp_limb_t n_randprime(flint_rand_t state, unsigned len_t bits, int proved);
+mp_limb_t n_randprime(flint_rand_t state, unsigned long bits, int proved);
 
 mp_limb_t n_randtest_prime(flint_rand_t state, int proved);
 
