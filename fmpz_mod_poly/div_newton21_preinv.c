31d956c02dc7db3c92bdaa0002739b18beec9c93
Martin Lee <martinlee84@web.de>
2013-09-04 11:56:49 +0200

    added 2-1 division using Newton inversion with a precomputed inverse

---
+++ fmpz_mod_poly/div_newton21_preinv.c
@@ -0,0 +1,99 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2011, 2010 Sebastian Pancratz
+    Copyright (C) 2008, 2009 William Hart
+
+******************************************************************************/
+
+#include <stdlib.h>
+#include "fmpz_vec.h"
+#include "fmpz_mod_poly.h"
+
+void _fmpz_mod_poly_div_newton21_preinv (fmpz* Q, const fmpz* A, slong lenA,
+                                   const fmpz* B, slong lenB, const fmpz* Binv,
+                                   slong lenBinv, const fmpz_t mod)
+{
+    const slong lenQ = lenA - lenB + 1;
+    fmpz * Arev;
+
+    Arev = _fmpz_vec_init(lenA);
+
+    _fmpz_poly_reverse(Arev, A, lenA, lenA);
+
+    _fmpz_mod_poly_mullow(Q, Arev, lenA, Binv, lenBinv, mod, lenQ);
+
+    _fmpz_poly_reverse(Q, Q, lenQ, lenQ);
+
+    _fmpz_vec_clear(Arev, lenA);
+}
+
+
+void fmpz_mod_poly_div_newton21_preinv(fmpz_mod_poly_t Q,
+                               const fmpz_mod_poly_t A, const fmpz_mod_poly_t B,
+                               const fmpz_mod_poly_t Binv)
+{
+    const slong lenA = A->length, lenB = B->length, lenQ = lenA - lenB + 1,
+                lenBinv= Binv->length;
+    fmpz *q;
+
+    if (lenB == 0)
+    {
+        printf("Exception (fmpz_mod_poly_div_newton). Division by zero.\n");
+        abort();
+    }
+
+    if (lenA < lenB)
+    {
+        fmpz_mod_poly_zero(Q);
+        return;
+    }
+
+    if (lenA > 2*lenB-2)
+    {
+        printf ("Exception (fmpz_mod_poly_div_newton).\n");
+    }
+
+    if (Q == A || Q == B || Q == Binv)
+    {
+        q = _fmpz_vec_init(lenQ);
+    }
+    else
+    {
+        fmpz_mod_poly_fit_length(Q, lenQ);
+        q = Q->coeffs;
+    }
+
+    _fmpz_mod_poly_div_newton21_preinv (q, A->coeffs, lenA, B->coeffs, lenB,
+                                    Binv->coeffs, lenBinv, &(B->p));
+
+    if (Q == A || Q == B || Q == Binv)
+    {
+        _fmpz_vec_clear(Q->coeffs, Q->alloc);
+        Q->coeffs = q;
+        Q->alloc  = lenQ;
+        Q->length = lenQ;
+    }
+    else
+    {
+        _fmpz_mod_poly_set_length(Q, lenQ);
+    }
+}
