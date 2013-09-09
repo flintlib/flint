1f6317ca73f4f10bad50768b53d0095280350019
Martin Lee <martinlee84@web.de>
2013-09-09 11:03:59 +0200

    added irreducibility test using ddf

--- nmod_poly.h
+++ nmod_poly.h
@@ -1099,6 +1099,8 @@ ulong nmod_poly_remove(nmod_poly_t f, const nmod_poly_t p);
 
 int nmod_poly_is_irreducible(const nmod_poly_t f);
 
+int nmod_poly_is_irreducible_ddf(const nmod_poly_t f);
+
 int _nmod_poly_is_squarefree(mp_srcptr f, slong len, nmod_t mod);
 
 int nmod_poly_is_squarefree(const nmod_poly_t f);
