fbc8aeb5a3c3d2a7fe51792139c3fd7ee7b792ba
Tom Bachmann <e_mc_h2@web.de>
2013-10-15 20:57:33 +0200

    fixes suggseted by bill

--- flintxx/test/t-fmpz_poly_matxx.cpp
+++ flintxx/test/t-fmpz_poly_matxx.cpp
@@ -177,12 +177,6 @@ test_functions()
     C.at(1, 1).set_coeff(0, 1);
     tassert(rank(C) == 2);
 
-#if 0
-    tassert(B.charpoly().get_coeff(0) == B.det());
-    tassert(charpoly(B).get_coeff(1) == -B.trace());
-    tassert(charpoly(B).lead() == 1);
-#endif
-
     Bp.set_randrank(rand, 1, 10);
     B = fmpz_poly_matxx::from_ground(Bp);
     tassert(!inv(B).get<0>());
