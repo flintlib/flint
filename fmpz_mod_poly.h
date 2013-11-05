896a7b622f1b042d9d1529689ca8f3483e0911ca
Martin Lee <martinlee84@web.de>
2013-11-05 13:57:35 +0100

    added powmod_x_fmpz_preinv to compute x^e mod f

--- fmpz_mod_poly.h
+++ fmpz_mod_poly.h
@@ -357,6 +357,15 @@ fmpz_mod_poly_powmod_fmpz_binexp_preinv(fmpz_mod_poly_t res,
                            const fmpz_mod_poly_t poly, const fmpz_t e,
                            const fmpz_mod_poly_t f, const fmpz_mod_poly_t finv);
 
+void
+_fmpz_mod_poly_powmod_x_fmpz_preinv(fmpz * res, const fmpz_t e, const fmpz * f,
+                                    slong lenf, const fmpz* finv, slong lenfinv,
+                                    const fmpz_t p);
+
+void
+fmpz_mod_poly_powmod_x_fmpz_preinv(fmpz_mod_poly_t res, const fmpz_t e,
+                           const fmpz_mod_poly_t f, const fmpz_mod_poly_t finv);
+
 /*  Division *****************************************************************/
 
 void _fmpz_mod_poly_divrem_basecase(fmpz * Q, fmpz * R, 
