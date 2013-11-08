baececb42390b831773e44efd8680af9c744e511
Mike Hansen <mhansen@gmail.com>
2013-11-08 17:12:18 +0000

    Add fq_poly_powmod_fmpz_sliding_preinv

--- fq_poly_templates.h
+++ fq_poly_templates.h
@@ -514,6 +514,23 @@ TEMPLATE(T, poly_powmod_ui_binexp_preinv)(TEMPLATE(T, poly_t) res, const TEMPLAT
                                 const TEMPLATE(T, poly_t) f, const TEMPLATE(T, poly_t) finv,
                                 const TEMPLATE(T, ctx_t) ctx);
 
+void
+_TEMPLATE(T, poly_powmod_fmpz_sliding_preinv)(TEMPLATE(T, struct) * res,
+                                              const TEMPLATE(T, struct) * poly,
+                                              const fmpz_t e, ulong k, const TEMPLATE(T, struct) * f,
+                                              slong lenf, const TEMPLATE(T, struct) * finv,
+                                              slong lenfinv, const TEMPLATE(T, ctx_t) ctx);
+
+void
+TEMPLATE(T, poly_powmod_fmpz_sliding_preinv)(TEMPLATE(T, poly_t) res,
+                                             const TEMPLATE(T, poly_t) poly,
+                                             const fmpz_t e, ulong k,
+                                             const TEMPLATE(T, poly_t) f,
+                                             const TEMPLATE(T, poly_t) finv,
+                                             const TEMPLATE(T, ctx_t) ctx);
+              
+
+                                         
 /*  Shifting  ****************************************************************/
 
 void _TEMPLATE(T, poly_shift_left)(TEMPLATE(T, struct) *rop, const TEMPLATE(T, struct) *op, long len, long n, const TEMPLATE(T, ctx_t) ctx);
