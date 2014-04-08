786d0a09eedbc1f1cdea44c542a9e82621363fb9
Fredrik Johansson <fredrik.johansson@gmail.com>
2014-04-08 16:08:21 +0200

    fast path for _fmpz_poly_sqr

--- fmpz_poly/sqr.c
+++ fmpz_poly/sqr.c
@@ -20,6 +20,7 @@
 /******************************************************************************
 
     Copyright (C) 2008, 2009 William Hart
+    Copyright (C) 2014 Fredrik Johansson
 
 ******************************************************************************/
 
@@ -29,9 +30,118 @@
 #include "fmpz_vec.h"
 #include "fmpz_poly.h"
 
+void _fmpz_poly_sqr_tiny1(fmpz * res, const fmpz * poly, slong len)
+{
+    slong i, j, c;
+
+    _fmpz_vec_zero(res, 2 * len - 1);
+
+    for (i = 0; i < len; i++)
+    {
+        c = poly[i];
+
+        if (c != 0)
+        {
+            res[2 * i] += c * c;
+
+            c *= 2;
+
+            for (j = i + 1; j < len; j++)
+                res[i + j] += poly[j] * c;
+        }
+    }
+}
+
+void _fmpz_poly_sqr_tiny2(fmpz * res, const fmpz * poly, slong len)
+{
+    slong i, j, k, c, d;
+    mp_limb_t hi, lo;
+    mp_ptr tmp;
+    TMP_INIT;
+
+    TMP_START;
+
+    tmp = TMP_ALLOC(2 * (2 * len - 1) * sizeof(mp_limb_t));
+
+    flint_mpn_zero(tmp, 2 * (2 * len - 1));
+
+    for (i = 0; i < len; i++)
+    {
+        c = poly[i];
+
+        if (c != 0)
+        {
+            smul_ppmm(hi, lo, c, c);
+            add_ssaaaa(tmp[4 * i + 1], tmp[4 * i],
+                       tmp[4 * i + 1], tmp[4 * i], hi, lo);
+
+            c *= 2;  /* does not overflow */
+
+            for (j = i + 1; j < len; j++)
+            {
+                k = i + j;
+
+                d = poly[j];
+
+                if (d != 0)
+                {
+                    smul_ppmm(hi, lo, c, d);
+                    add_ssaaaa(tmp[2 * k + 1], tmp[2 * k],
+                               tmp[2 * k + 1], tmp[2 * k], hi, lo);
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < 2 * len - 1; i++)
+    {
+        lo = tmp[2 * i];
+        hi = tmp[2 * i + 1];
+
+        if (((mp_limb_signed_t) hi) >= 0)
+        {
+            fmpz_set_uiui(res + i, hi, lo);
+        }
+        else
+        {
+            sub_ddmmss(hi, lo, 0, 0, hi, lo);
+            fmpz_neg_uiui(res + i, hi, lo);
+        }
+    }
+
+    TMP_END;
+    return;
+}
+
 void _fmpz_poly_sqr(fmpz * res, const fmpz * poly, slong len)
 {
     mp_size_t limbs;
+    slong bits, rbits;
+
+    if (len == 1)
+    {
+        fmpz_mul(res, poly, poly);
+        return;
+    }
+
+    bits = _fmpz_vec_max_bits(poly, len);
+    bits = FLINT_ABS(bits);
+
+    if (bits <= FLINT_BITS - 2 && len < 50 + 3 * bits)
+    {
+        rbits = 2 * bits + FLINT_BIT_COUNT(len);
+
+        if (rbits <= FLINT_BITS - 2)
+        {
+            _fmpz_poly_sqr_tiny1(res, poly, len);
+            return;
+        }
+        else if (rbits <= 2 * FLINT_BITS - 1)
+        {
+            _fmpz_poly_sqr_tiny2(res, poly, len);
+            return;
+        }
+    }
 
     if (len < 7)
     {
@@ -39,7 +149,7 @@ void _fmpz_poly_sqr(fmpz * res, const fmpz * poly, slong len)
         return;
     }
 
-    limbs = _fmpz_vec_max_limbs(poly, len);
+    limbs = (bits + FLINT_BITS - 1) / FLINT_BITS;
 
     if (len < 16 && limbs > 12)
         _fmpz_poly_sqr_karatsuba(res, poly, len);
