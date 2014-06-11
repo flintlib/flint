68c101b9848cbc4f59cdef2beaf23f71952e2cb7
Alex J. Best <alex.j.best@gmail.com>
2014-06-11 16:28:42 +0000

    Moved all variable declaration and initiation to start of function

--- fmpz_mat/hnf_xgcd.c
+++ fmpz_mat/hnf_xgcd.c
@@ -28,22 +28,23 @@
 void fmpz_mat_hnf_xgcd(fmpz_mat_t H, const fmpz_mat_t A)
 {
     slong j, j2, i, k, l;
+    fmpz_t r1d, r2d, b, u, v, d, q;
 
+    fmpz_init(r1d);
+    fmpz_init(r2d);
+    fmpz_init(b);
+    fmpz_init(u);
+    fmpz_init(v);
+    fmpz_init(d);
+    fmpz_init(q);
     fmpz_mat_set(H, A);
     for (j = 0, k = 0, l = (A->c - A->r)*(A->c > A->r); A->c - j != l; j++, k++)
     {
         for (i = k + 1; i != A->r; i++)
         {
             /* reduce row i - 1 with row i */
-            fmpz_t r1d, r2d, b, u, v, d;
             if (fmpz_is_zero(fmpz_mat_entry(H, i - 1, j)))
                 continue;
-            fmpz_init(r1d);
-            fmpz_init(r2d);
-            fmpz_init(b);
-            fmpz_init(u);
-            fmpz_init(v);
-            fmpz_init(d);
             fmpz_xgcd(d, u, v, fmpz_mat_entry(H, i, j), fmpz_mat_entry(H, i - 1, j));
             fmpz_divexact(r2d, fmpz_mat_entry(H, i - 1, j), d);
             fmpz_divexact(r1d, fmpz_mat_entry(H, i, j), d);
@@ -55,12 +56,6 @@ void fmpz_mat_hnf_xgcd(fmpz_mat_t H, const fmpz_mat_t A)
                 fmpz_submul(fmpz_mat_entry(H, i - 1, j2), r2d, fmpz_mat_entry(H, i, j2));
                 fmpz_set(fmpz_mat_entry(H, i, j2), b);
             }
-            fmpz_clear(r2d);
-            fmpz_clear(r1d);
-            fmpz_clear(b);
-            fmpz_clear(u);
-            fmpz_clear(v);
-            fmpz_clear(d);
         }
         for (j2 = j; j2 < A->c; j2++)
         {
@@ -86,8 +81,6 @@ void fmpz_mat_hnf_xgcd(fmpz_mat_t H, const fmpz_mat_t A)
             /* reduce higher entries of column j with row k */
             for (i = k - 1; i >= 0; i--)
             {
-                fmpz_t q;
-                fmpz_init(q);
                 fmpz_fdiv_q(q, fmpz_mat_entry(H, i, j),
                         fmpz_mat_entry(H, k, j));
                 for (j2 = j; j2 < A->c; j2++)
@@ -95,8 +88,14 @@ void fmpz_mat_hnf_xgcd(fmpz_mat_t H, const fmpz_mat_t A)
                     fmpz_submul(fmpz_mat_entry(H, i, j2), q,
                             fmpz_mat_entry(H, k, j2));
                 }
-                fmpz_clear(q);
             }
         }
     }
+    fmpz_clear(q);
+    fmpz_clear(r2d);
+    fmpz_clear(r1d);
+    fmpz_clear(b);
+    fmpz_clear(u);
+    fmpz_clear(v);
+    fmpz_clear(d);
 }
