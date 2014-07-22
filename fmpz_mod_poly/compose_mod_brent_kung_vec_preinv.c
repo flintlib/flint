d1b2bbd71ba5bbb91bdce4671897f294455b27fc
Martin Lee <martinlee84@web.de>
2014-07-22 12:36:44 +0200

    fixed a corner case

--- fmpz_mod_poly/compose_mod_brent_kung_vec_preinv.c
+++ fmpz_mod_poly/compose_mod_brent_kung_vec_preinv.c
@@ -151,6 +151,16 @@ fmpz_mod_poly_compose_mod_brent_kung_vec_preinv(fmpz_mod_poly_struct * res,
         return;
     }
 
+    if (len2 == 2)
+    {
+        for (i = 0; i < n; i++)
+        {
+            fmpz_mod_poly_init (res + i, &poly->p);
+            fmpz_mod_poly_set (res + i, polys + i);
+        }
+        return;
+    }
+
     for (i = 0; i < n; i++)
     {
         fmpz_mod_poly_init2(res + i, &poly->p, len2 - 1);
