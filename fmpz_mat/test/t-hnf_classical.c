9761fb0a2552bd5e4bf96627587c40fdd490eadd
Alex J Best <alex.j.bes@gmail.com>
2014-07-29 09:44:04 -0700

    changedprinted statement to be consistent with other tests

--- fmpz_mat/test/t-hnf_classical.c
+++ fmpz_mat/test/t-hnf_classical.c
@@ -83,7 +83,7 @@ main(void)
     slong iter;
     FLINT_TEST_INIT(state);
 
-    flint_printf("hnf classical....");
+    flint_printf("hnf_classical....");
     fflush(stdout);
 
     for (iter = 0; iter < 10000 * flint_test_multiplier(); iter++)
