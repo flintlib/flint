20dbf6deb2448e53bb60deb3b95dadcd6631a651
Alex J Best <alex.j.bes@gmail.com>
2014-08-01 12:40:07 -0700

    simplified modular hnf

--- fmpz_mat/hnf_modular.c
+++ fmpz_mat/hnf_modular.c
@@ -27,12 +27,13 @@
 
 void fmpz_mat_hnf_modular(fmpz_mat_t H, const fmpz_mat_t A, const fmpz_t D)
 {
-    slong j, j2, i, k;
+    slong j, i, k;
     fmpz_t R, R2, d, u, v, r1d, r2d, b, q;
+
+    fmpz_mat_set(H, A);
 
     fmpz_init_set(R, D);
     fmpz_init(R2);
-    fmpz_mat_set(H, A);
     fmpz_init(u);
     fmpz_init(u);
     fmpz_init(v);
@@ -42,59 +43,59 @@ void fmpz_mat_hnf_modular(fmpz_mat_t H, const fmpz_mat_t A, const fmpz_t D)
     fmpz_init(b);
     fmpz_init(q);
 
-    for (j = 0, k = 0; j != A->c; j++, k++)
+    for (k = 0; k != A->c; k++)
     {
         fmpz_fdiv_q_2exp(R2, R, 1);
 
-        if (fmpz_is_zero(fmpz_mat_entry(H, k, j)))
-            fmpz_set(fmpz_mat_entry(H, k, j), R);
+        if (fmpz_is_zero(fmpz_mat_entry(H, k, k)))
+            fmpz_set(fmpz_mat_entry(H, k, k), R);
         for (i = k + 1; i != A->r; i++)
         {
             /* reduce row i with row k mod R */
-            if (fmpz_is_zero(fmpz_mat_entry(H, i, j)))
+            if (fmpz_is_zero(fmpz_mat_entry(H, i, k)))
                 continue;
-            fmpz_xgcd(d, u, v, fmpz_mat_entry(H, k, j),
-                    fmpz_mat_entry(H, i, j));
-            fmpz_divexact(r1d, fmpz_mat_entry(H, k, j), d);
-            fmpz_divexact(r2d, fmpz_mat_entry(H, i, j), d);
-            for (j2 = j; j2 < A->c; j2++)
+            fmpz_xgcd(d, u, v, fmpz_mat_entry(H, k, k),
+                    fmpz_mat_entry(H, i, k));
+            fmpz_divexact(r1d, fmpz_mat_entry(H, k, k), d);
+            fmpz_divexact(r2d, fmpz_mat_entry(H, i, k), d);
+            for (j = k; j < A->c; j++)
             {
-                fmpz_mul(b, u, fmpz_mat_entry(H, k, j2));
-                fmpz_addmul(b, v, fmpz_mat_entry(H, i, j2));
-                fmpz_mul(fmpz_mat_entry(H, i, j2), r1d,
-                        fmpz_mat_entry(H, i, j2));
-                fmpz_submul(fmpz_mat_entry(H, i, j2), r2d,
-                        fmpz_mat_entry(H, k, j2));
-                fmpz_mod(fmpz_mat_entry(H, i, j2), fmpz_mat_entry(H, i, j2), R);
-                if (fmpz_cmp(fmpz_mat_entry(H, i, j2), R2) > 0)
-                    fmpz_sub(fmpz_mat_entry(H, i, j2),
-                            fmpz_mat_entry(H, i, j2), R);
-                fmpz_set(fmpz_mat_entry(H, k, j2), b);
-                fmpz_mod(fmpz_mat_entry(H, k, j2), fmpz_mat_entry(H, k, j2), R);
-                if (fmpz_cmp(fmpz_mat_entry(H, k, j2), R2) > 0)
-                    fmpz_sub(fmpz_mat_entry(H, k, j2),
-                            fmpz_mat_entry(H, k, j2), R);
+                fmpz_mul(b, u, fmpz_mat_entry(H, k, j));
+                fmpz_addmul(b, v, fmpz_mat_entry(H, i, j));
+                fmpz_mul(fmpz_mat_entry(H, i, j), r1d,
+                        fmpz_mat_entry(H, i, j));
+                fmpz_submul(fmpz_mat_entry(H, i, j), r2d,
+                        fmpz_mat_entry(H, k, j));
+                fmpz_mod(fmpz_mat_entry(H, i, j), fmpz_mat_entry(H, i, j), R);
+                if (fmpz_cmp(fmpz_mat_entry(H, i, j), R2) > 0)
+                    fmpz_sub(fmpz_mat_entry(H, i, j),
+                            fmpz_mat_entry(H, i, j), R);
+                fmpz_set(fmpz_mat_entry(H, k, j), b);
+                fmpz_mod(fmpz_mat_entry(H, k, j), fmpz_mat_entry(H, k, j), R);
+                if (fmpz_cmp(fmpz_mat_entry(H, k, j), R2) > 0)
+                    fmpz_sub(fmpz_mat_entry(H, k, j),
+                            fmpz_mat_entry(H, k, j), R);
             }
         }
 
-        fmpz_xgcd(d, u, v, fmpz_mat_entry(H, k, j), R);
-        for (j2 = j; j2 < A->c; j2++)
+        fmpz_xgcd(d, u, v, fmpz_mat_entry(H, k, k), R);
+        for (j = k; j < A->c; j++)
         {
-            fmpz_mul(fmpz_mat_entry(H, k, j2), u,
-                fmpz_mat_entry(H, k, j2));
-            fmpz_mod(fmpz_mat_entry(H, k, j2), fmpz_mat_entry(H, k, j2), R);
+            fmpz_mul(fmpz_mat_entry(H, k, j), u,
+                fmpz_mat_entry(H, k, j));
+            fmpz_mod(fmpz_mat_entry(H, k, j), fmpz_mat_entry(H, k, j), R);
         }
-        if (fmpz_is_zero(fmpz_mat_entry(H, k, j)))
-            fmpz_set(fmpz_mat_entry(H, k, j), R);
-        /* reduce higher entries of column j with row k */
+        if (fmpz_is_zero(fmpz_mat_entry(H, k, k)))
+            fmpz_set(fmpz_mat_entry(H, k, k), R);
+        /* reduce higher entries of column k with row k */
         for (i = k - 1; i >= 0; i--)
         {
-            fmpz_fdiv_q(q, fmpz_mat_entry(H, i, j),
-                    fmpz_mat_entry(H, k, j));
-            for (j2 = j; j2 < A->c; j2++)
+            fmpz_fdiv_q(q, fmpz_mat_entry(H, i, k),
+                    fmpz_mat_entry(H, k, k));
+            for (j = k; j < A->c; j++)
             {
-                fmpz_submul(fmpz_mat_entry(H, i, j2), q,
-                        fmpz_mat_entry(H, k, j2));
+                fmpz_submul(fmpz_mat_entry(H, i, j), q,
+                        fmpz_mat_entry(H, k, j));
             }
         }
         fmpz_divexact(R, R, d);
