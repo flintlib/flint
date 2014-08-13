c5e6c56192171d6c15ef9a8ef1b5dae645baba96
Alex J Best <alex.j.bes@gmail.com>
2014-08-13 16:33:15 -0700

    Allow transformation matrix to be requested along with HNF.

---
+++ fmpz_mat/hnf_transform.c
@@ -0,0 +1,61 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2014 Alex J. Best
+
+******************************************************************************/
+
+#include "fmpz_mat.h"
+
+void
+fmpz_mat_hnf_transform(fmpz_mat_t H, fmpz_mat_t U, const fmpz_mat_t A)
+{
+    slong i, j, m, n;
+    fmpz_mat_t A2, H2;
+
+    m = fmpz_mat_nrows(A);
+    n = fmpz_mat_ncols(A);
+
+    fmpz_mat_init(A2, m, n + m);
+    fmpz_mat_init(H2, m, n + m);
+
+    /* append an identity matrix to the right of A */
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+            fmpz_set(fmpz_mat_entry(A2, i, j), fmpz_mat_entry(A, i, j));
+        fmpz_set_ui(fmpz_mat_entry(A2, i, n + i), UWORD(1));
+    }
+
+    fmpz_mat_hnf(H2, A2);
+
+    /* recover H and U */
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+            fmpz_set(fmpz_mat_entry(H, i, j), fmpz_mat_entry(H2, i, j));
+        for (j = n; j < n + m; j++)
+            fmpz_set(fmpz_mat_entry(U, i, j - n), fmpz_mat_entry(H2, i, j));
+    }
+
+    fmpz_mat_clear(A2);
+    fmpz_mat_clear(H2);
+}
---
+++ fmpz_mat/test/t-hnf_transform.c
@@ -0,0 +1,105 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2014 Alex J. Best
+
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "fmpz_mat.h"
+
+int
+main(void)
+{
+    slong iter;
+    FLINT_TEST_INIT(state);
+
+    flint_printf("hnf_transform....");
+    fflush(stdout);
+
+    for (iter = 0; iter < 10000 * flint_test_multiplier(); iter++)
+    {
+        fmpz_mat_t A, H, H2, U;
+        slong m, n, b, d, r;
+        int equal;
+
+        m = n_randint(state, 10);
+        n = n_randint(state, 10);
+        r = n_randint(state, FLINT_MIN(m, n) + 1);
+
+        fmpz_mat_init(A, m, n);
+        fmpz_mat_init(H, m, n);
+        fmpz_mat_init(H2, m, n);
+        fmpz_mat_init(U, m, m);
+
+        /* sparse */
+        b = 1 + n_randint(state, 10) * n_randint(state, 10);
+        d = n_randint(state, 2*m*n + 1);
+        fmpz_mat_randrank(A, state, r, b);
+
+        /* dense */
+        if (n_randint(state, 2))
+            fmpz_mat_randops(A, state, d);
+
+        fmpz_mat_hnf_transform(H, U, A);
+        fmpz_mat_mul(H2, U, A);
+        equal = fmpz_mat_equal(H, H2);
+
+        if (!equal)
+        {
+            flint_printf("FAIL:\n");
+            flint_printf("multiplying by the transformation matrix should give the same HNF!\n");
+            fmpz_mat_print_pretty(A); flint_printf("\n\n");
+            fmpz_mat_print_pretty(U); flint_printf("\n\n");
+            fmpz_mat_print_pretty(H); flint_printf("\n\n");
+            fmpz_mat_print_pretty(H2); flint_printf("\n\n");
+            abort();
+        }
+
+        fmpz_mat_hnf(H2, A);
+        equal = fmpz_mat_equal(H, H2);
+
+        if (!equal)
+        {
+            flint_printf("FAIL:\n");
+            flint_printf("hnfs produced by different methods should be the same!\n");
+            fmpz_mat_print_pretty(A); flint_printf("\n\n");
+            fmpz_mat_print_pretty(H); flint_printf("\n\n");
+            fmpz_mat_print_pretty(H2); flint_printf("\n\n");
+            abort();
+        }
+
+        fmpz_mat_clear(U);
+        fmpz_mat_clear(H2);
+        fmpz_mat_clear(H);
+        fmpz_mat_clear(A);
+    }
+
+    FLINT_TEST_CLEANUP(state);
+
+    flint_printf("PASS\n");
+    return 0;
+}
+
