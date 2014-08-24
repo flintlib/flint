37001a8fb8bf2a8eab5a75440de604796d246381
Alex J Best <alex.j.bes@gmail.com>
2014-08-23 20:14:08 -0700

    Test previous commit

--- fmpz_mat/test/t-nullspace.c
+++ fmpz_mat/test/t-nullspace.c
@@ -44,10 +44,69 @@ main(void)
     flint_printf("nullspace....");
     fflush(stdout);    
 
+    /* small dimension */
     for (i = 0; i < 1000 * flint_test_multiplier(); i++)
     {
         m = n_randint(state, 10);
         n = n_randint(state, 10);
+
+        for (r = 0; r <= FLINT_MIN(m,n); r++)
+        {
+            b = 1 + n_randint(state, 10) * n_randint(state, 10);
+            d = n_randint(state, 2*m*n + 1);
+
+            fmpz_mat_init(A, m, n);
+            fmpz_mat_init(ker, n, n);
+            fmpz_mat_init(B, m, n);
+
+            fmpz_mat_randrank(A, state, r, b);
+            /* Densify */
+            if (n_randlimb(state) % 2)
+                fmpz_mat_randops(A, state, d);
+
+            nullity = fmpz_mat_nullspace(ker, A);
+            nulrank = fmpz_mat_rank(ker);
+
+            if (nullity != nulrank)
+            {
+                flint_printf("FAIL:\n");
+                flint_printf("rank(ker) != nullity!\n");
+                fmpz_mat_print_pretty(A);
+                flint_printf("\n");
+                abort();
+            }
+
+            if (nullity + r != n)
+            {
+                flint_printf("FAIL:\n");
+                flint_printf("nullity + rank != n\n");
+                fmpz_mat_print_pretty(A);
+                flint_printf("\n");
+                abort();
+            }
+
+            fmpz_mat_mul(B, A, ker);
+
+            if (fmpz_mat_rank(B) != 0)
+            {
+                flint_printf("FAIL:\n");
+                flint_printf("A * ker != 0\n");
+                fmpz_mat_print_pretty(A);
+                flint_printf("\n");
+                abort();
+            }
+
+            fmpz_mat_clear(A);
+            fmpz_mat_clear(ker);
+            fmpz_mat_clear(B);
+        }
+    }
+
+    /* larger dimension */
+    for (i = 0; i < 10 * flint_test_multiplier(); i++)
+    {
+        m = 25 + n_randint(state, 10);
+        n = 25 + n_randint(state, 10);
 
         for (r = 0; r <= FLINT_MIN(m,n); r++)
         {
