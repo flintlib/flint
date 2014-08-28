c5143b749d3aa833c239c1fb1e0d83658a50e9ac
Martin Lee <martinlee84@web.de>
2014-08-28 11:40:14 +0200

    added multithreaded versions of precompute_matrix and compose_mod_brent_kung_precomp_preinv for nmod_poly

--- nmod_poly/compose_mod_brent_kung_precomp_preinv.c
+++ nmod_poly/compose_mod_brent_kung_precomp_preinv.c
@@ -50,6 +50,28 @@ _nmod_poly_reduce_matrix_mod_poly (nmod_mat_t A, const nmod_mat_t B,
     _nmod_vec_clear (tmp1);
 }
 
+void *
+_nmod_poly_precompute_matrix_worker (void * arg_ptr)
+{
+    nmod_poly_matrix_precompute_arg_t arg =
+                           *((nmod_poly_matrix_precompute_arg_t *) arg_ptr);
+    /* Set rows of A to powers of poly1 */
+    slong i, n, m;
+
+    n = arg.poly2.length - 1;
+
+    m = n_sqrt(n) + 1;
+
+    arg.A.rows[0][0] = UWORD(1);
+    _nmod_vec_set(arg.A.rows[1], arg.poly1.coeffs, n);
+    for (i = 2; i < m; i++)
+        _nmod_poly_mulmod_preinv(arg.A.rows[i], arg.A.rows[i - 1], n,
+                                 arg.poly1.coeffs, n, arg.poly2.coeffs, n + 1,
+                                 arg.poly2inv.coeffs, n + 1, arg.poly2.mod);
+    flint_cleanup();
+    return NULL;
+}
+
 void
 _nmod_poly_precompute_matrix (nmod_mat_t A, mp_srcptr poly1, mp_srcptr poly2,
                               slong len2, mp_srcptr poly2inv, slong len2inv,
@@ -115,6 +137,78 @@ nmod_poly_precompute_matrix (nmod_mat_t A, const nmod_poly_t poly1,
                    poly2inv->length, A->mod);
 
     _nmod_vec_clear (ptr1);
+}
+
+void *
+_nmod_poly_compose_mod_brent_kung_precomp_preinv_worker(void * arg_ptr)
+{
+    nmod_poly_compose_mod_precomp_preinv_arg_t arg=
+                   *((nmod_poly_compose_mod_precomp_preinv_arg_t*) arg_ptr);
+    nmod_mat_t B, C;
+    mp_ptr t, h;
+    slong i, n, m;
+
+    n = arg.poly3.length - 1;
+
+    if (arg.poly3.length == 1)
+    {
+        flint_cleanup();
+        return NULL;
+    }
+    if (arg.poly1.length == 1)
+    {
+        arg.res.coeffs[0] = arg.poly1.coeffs[0];
+        flint_cleanup();
+        return NULL;
+    }
+
+    if (arg.poly3.length == 2)
+    {
+        arg.res.coeffs[0] = _nmod_poly_evaluate_nmod(arg.poly1.coeffs,
+                                             arg.poly1.length, arg.A.rows[1][0],
+                                             arg.poly3.mod);
+        flint_cleanup();
+        return NULL;
+    }
+
+    m = n_sqrt(n) + 1;
+
+    nmod_mat_init(B, m, m, arg.poly3.mod.n);
+    nmod_mat_init(C, m, n, arg.poly3.mod.n);
+
+    h = _nmod_vec_init(n);
+    t = _nmod_vec_init(n);
+
+    /* Set rows of B to the segments of poly1 */
+    for (i = 0; i < arg.poly1.length / m; i++)
+        _nmod_vec_set(B->rows[i], arg.poly1.coeffs + i * m, m);
+
+    _nmod_vec_set(B->rows[i], arg.poly1.coeffs + i * m, arg.poly1.length % m);
+
+    nmod_mat_mul(C, B, &arg.A);
+
+    /* Evaluate block composition using the Horner scheme */
+    _nmod_vec_set(arg.res.coeffs, C->rows[m - 1], n);
+    _nmod_poly_mulmod_preinv(h, arg.A.rows[m - 1], n, arg.A.rows[1], n,
+                             arg.poly3.coeffs, arg.poly3.length,
+                             arg.poly3inv.coeffs, arg.poly3inv.length,
+                             arg.poly3.mod);
+
+    for (i = m - 2; i >= 0; i--)
+    {
+        _nmod_poly_mulmod_preinv(t, arg.res.coeffs, n, h, n, arg.poly3.coeffs,
+                                 arg.poly3.length, arg.poly3inv.coeffs,
+                                 arg.poly3.length, arg.poly3.mod);
+        _nmod_poly_add(arg.res.coeffs, t, n, C->rows[i], n, arg.poly3.mod);
+    }
+
+    _nmod_vec_clear(h);
+    _nmod_vec_clear(t);
+
+    nmod_mat_clear(B);
+    nmod_mat_clear(C);
+    flint_cleanup();
+    return NULL;
 }
 
 void
