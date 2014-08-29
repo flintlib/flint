bf8e1ec9ee81aacb58e27ecbdc78526afd931d75
Alex J Best <alex.j.bes@gmail.com>
2014-08-29 10:30:21 -0700

    several improvements to rref_multi_mod, changed to use the new _nmod_mat_rref too

--- fmpz_mat/rref_multi_mod.c
+++ fmpz_mat/rref_multi_mod.c
@@ -46,14 +46,12 @@ int compare_pivots(const slong * p1, slong len1, const slong * p2, slong len2)
 slong
 fmpz_mat_rref_multi_mod(fmpz_mat_t R, fmpz_t den, const fmpz_mat_t A)
 {
-    fmpz_t bound, b, c, prod, one;
-    fmpq_t tmp;
-    fmpz_mat_t B;
+    fmpz_t bound, b, c, prod, one, num, d, u, t;
     fmpq_mat_t C;
     nmod_mat_t Amod;
     mp_limb_t p;
     slong i, j, m, n, rank, new_rank;
-    slong * pivots, * new_pivots;
+    slong * pivots, * new_pivots, * P;
 
     m = fmpz_mat_nrows(A);
     n = fmpz_mat_ncols(A);
@@ -63,31 +61,30 @@ fmpz_mat_rref_multi_mod(fmpz_mat_t R, fmpz_t den, const fmpz_mat_t A)
     fmpz_init(c);
     fmpz_init(prod);
     fmpz_init(one);
-    fmpq_init(tmp);
+
     nmod_mat_init(Amod, m, n, 2);
     fmpq_mat_init(C, m, n);
-    fmpz_mat_init(B, m, n);
     pivots = (slong *) flint_malloc(n * sizeof(slong));
     new_pivots = (slong *) flint_malloc(n * sizeof(slong));
 
     fmpz_one(one);
-    /*TODO use R for B */
 
     /* compute bound */
-    fmpz_one(c); /* TODO guess the height */
     for (i = 0; i < m; i++)
         for (j = 0; j < n; j++)
             if (fmpz_cmpabs(b, fmpz_mat_entry(A, i, j)) < 0)
-                fmpz_set(b, fmpz_mat_entry(A, i, j));
+                fmpz_abs(b, fmpz_mat_entry(A, i, j));
+    fmpz_set(c, b);
+    fmpz_mul_2exp(c, c, m);
     fmpz_mul(bound, b, c);
     fmpz_mul_si(bound, bound, n);
-    fmpz_abs(bound, bound);
     fmpz_add_ui(bound, bound, UWORD(1));
 
     p = UWORD(1) << NMOD_MAT_OPTIMAL_MODULUS_BITS;
     rank = -1;
     while (1)
     {
+        int success = 1;
         fmpz_one(prod);
 
         while (fmpz_cmp(prod, bound) <= 0)
@@ -96,48 +93,99 @@ fmpz_mat_rref_multi_mod(fmpz_mat_t R, fmpz_t den, const fmpz_mat_t A)
 
             _nmod_mat_set_mod(Amod, p);
             fmpz_mat_get_nmod_mat(Amod, A);
-            new_rank = _nmod_mat_rref(Amod, new_pivots);
+
+            P = _perm_init(m);
+            new_rank = _nmod_mat_rref(Amod, new_pivots, P);
+            _perm_clear(P);
+
             /* new rank and column profile is better */
             if (compare_pivots(pivots, rank, new_pivots, new_rank) < 0)
             {
                 rank = new_rank;
-                for (i = 0; i < rank; i++)
+                for (i = 0; i < n; i++)
                     pivots[i] = new_pivots[i];
 
-                fmpz_mat_set_nmod_mat(B, Amod);
+                fmpz_mat_set_nmod_mat(R, Amod);
                 fmpz_set_ui(prod, p);
             }
             else
             {
-                fmpz_mat_CRT_ui(B, B, prod, Amod, 1);
+                fmpz_mat_CRT_ui(R, R, prod, Amod, 1);
                 fmpz_mul_ui(prod, prod, p);
             }
         }
 
-        if (!fmpq_mat_set_fmpz_mat_mod_fmpz(C, B, prod))
+        /* fill in the nonpivot columns */
+        fmpz_init(num);
+        fmpz_init(t);
+        fmpz_init(d);
+        fmpz_init(u);
+
+        fmpz_one(d);
+
+        for (i = 0; i < m && success; i++)
+        {
+            for (j = 0; j < n - rank; j++)
+            {
+                fmpz_mul(t, d, fmpz_mat_entry(R, i, pivots[rank + j]));
+                fmpz_fdiv_qr(u, t, t, prod);
+
+                success = _fmpq_reconstruct_fmpz(num, den, t, prod);
+
+                fmpz_mul(den, den, d);
+                fmpz_set(d, den);
+
+                if (!success)
+                    break;
+
+                fmpz_set(fmpq_mat_entry_num(C, i, pivots[rank + j]), num);
+                fmpz_set(fmpq_mat_entry_den(C, i, pivots[rank + j]), den);
+                fmpq_canonicalise(fmpq_mat_entry(C, i, pivots[rank + j]));
+            }
+        }
+
+        fmpz_clear(num);
+        fmpz_clear(d);
+        fmpz_clear(u);
+        fmpz_clear(t);
+
+        if (!success)
         {
             fmpz_mul2_uiui(bound, bound, p, p);
             fmpz_mul2_uiui(bound, bound, p, p);
+            fmpz_mul(bound, bound, b);
             continue;
+        }
+
+        /* fill in the pivot columns */
+        for (i = 0; i < m; i++)
+        {
+            for (j = 0; j < rank; j++)
+            {
+                if (i == j)
+                    fmpq_one(fmpq_mat_entry(C, i, pivots[j]));
+                else
+                    fmpq_zero(fmpq_mat_entry(C, i, pivots[j]));
+            }
         }
 
         fmpz_one(den);
         for (i = 0; i < m; i++)
-        {
             for (j = 0; j < n; j++)
-                fmpz_lcm(den, den, fmpq_denref(fmpq_mat_entry(C, i, j)));
-        }
+                fmpz_lcm(den, den, fmpq_mat_entry_den(C, i, j));
+        /* multiply up */
         for (i = 0; i < m; i++)
         {
             for (j = 0; j < n; j++)
             {
-                _fmpq_mul(fmpq_numref(tmp), fmpq_denref(tmp),
-                        fmpq_mat_entry_num(C, i, j),
-                        fmpq_mat_entry_den(C, i, j), den, one);
-                if (fmpz_cmpabs(c, fmpq_numref(tmp)) < 0)
-                    fmpz_abs(c, fmpq_numref(tmp));
-                if (fmpz_cmpabs(c, fmpq_denref(tmp)) < 0)
-                    fmpz_abs(c, fmpq_denref(tmp));
+                fmpz_mul(fmpz_mat_entry(R, i, j), den,
+                        fmpq_mat_entry_num(C, i, j));
+                fmpz_divexact(fmpz_mat_entry(R, i, j), fmpz_mat_entry(R, i, j),
+                        fmpq_mat_entry_den(C, i, j));
+                if (fmpz_cmpabs(c, fmpq_mat_entry_num(C, i, j)) < 0)
+                    fmpz_abs(c, fmpq_mat_entry_num(C, i, j));
+                if (fmpz_cmpabs(c, fmpq_mat_entry_den(C, i, j)) < 0)
+                    fmpz_abs(c, fmpq_mat_entry_den(C, i, j));
             }
         }
 
@@ -146,33 +194,20 @@ fmpz_mat_rref_multi_mod(fmpz_mat_t R, fmpz_t den, const fmpz_mat_t A)
 
         if (fmpz_cmp(prod, bound) > 0) break;
         fmpz_mul2_uiui(bound, bound, p, p);
+        fmpz_mul2_uiui(bound, bound, p, p);
     }
 
     /* cleanup */
     flint_free(pivots);
     flint_free(new_pivots);
-    fmpz_mat_clear(B);
     nmod_mat_clear(Amod);
+    fmpq_mat_clear(C);
+
     fmpz_clear(b);
     fmpz_clear(c);
     fmpz_clear(prod);
     fmpz_clear(bound);
     fmpz_clear(one);
-    fmpq_clear(tmp);
-
-    /* multiply up */
-    for (i = 0; i < m; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            fmpz_mul(fmpz_mat_entry(R, i, j), den,
-                    fmpq_numref(fmpq_mat_entry(C, i, j)));
-            fmpz_divexact(fmpz_mat_entry(R, i, j), fmpz_mat_entry(R, i, j),
-                    fmpq_denref(fmpq_mat_entry(C, i, j)));
-        }
-    }
-
-    fmpq_mat_clear(C);
 
     return rank;
 }
