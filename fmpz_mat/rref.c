dae85854a8cda2e29d812a8a91db5e64c17fd6bc
Alex J Best <alex.j.bes@gmail.com>
2014-08-30 08:51:36 -0700

    Added general rref method and moved old rref to rref_fflu

--- fmpz_mat/rref.c
+++ fmpz_mat/rref.c
@@ -20,6 +20,7 @@
 /******************************************************************************
 
     Copyright (C) 2011-2012 Fredrik Johansson
+    Copyright (C) 2014 Alex J. Best
 
 ******************************************************************************/
 
@@ -28,77 +29,12 @@
 slong
 fmpz_mat_rref(fmpz_mat_t R, fmpz_t den, const fmpz_mat_t A)
 {
-    slong i, j, k, m, n, rank;
-    slong *pivots, *nonpivots;
+    slong rank;
 
-    rank = fmpz_mat_fflu(R, den, NULL, A, 0);
-    m = fmpz_mat_nrows(R);
-    n = fmpz_mat_ncols(R);
-
-    /* clear bottom */
-    for (i = rank; i < m; i++)
-        for (j = 0; j < n; j++)
-            fmpz_zero(fmpz_mat_entry(R, i, j));
-
-    /* Convert row echelon form to reduced row echelon form */
-    if (rank > 1)
-    {
-        fmpz_t tmp;
-        fmpz_init(tmp);
-
-        pivots = flint_malloc(sizeof(slong) * n);
-        nonpivots = pivots + rank;
-
-        for (i = j = k = 0; i < rank; i++)
-        {
-            while (fmpz_is_zero(fmpz_mat_entry(R, i, j)))
-            {
-                nonpivots[k] = j;
-                k++;
-                j++;
-            }
-            pivots[i] = j;
-            j++;
-        }
-        while (k < n - rank)
-        {
-            nonpivots[k] = j;
-            k++;
-            j++;
-        }
-
-        for (k = 0; k < n - rank; k++)
-        {
-            for (i = rank - 2; i >= 0; i--)
-            {
-                fmpz_mul(tmp, den, fmpz_mat_entry(R, i, nonpivots[k]));
-
-                for (j = i + 1; j < rank; j++)
-                {
-                    fmpz_submul(tmp, fmpz_mat_entry(R, i, pivots[j]),
-                        fmpz_mat_entry(R, j, nonpivots[k]));
-                }
-
-                fmpz_divexact(fmpz_mat_entry(R, i, nonpivots[k]),
-                    tmp, fmpz_mat_entry(R, i, pivots[i]));
-            }
-        }
-
-        /* clear pivot columns */
-        for (i = 0; i < rank; i++)
-        {
-            for (j = 0; j < rank; j++)
-            {
-                if (i == j)
-                    fmpz_set(fmpz_mat_entry(R, j, pivots[i]), den);
-                else
-                    fmpz_zero(fmpz_mat_entry(R, j, pivots[i]));
-            }
-        }
-
-        flint_free(pivots);
-        fmpz_clear(tmp);
-    }
+    if (A->c <= 25)
+        rank = fmpz_mat_rref_fflu(R, den, A);
+    else
+        rank = fmpz_mat_rref_multi_mod(R, den, A);
 
     return rank;
 }
