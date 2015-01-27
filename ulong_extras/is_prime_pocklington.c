027cc5cae959f5dbbfe8130bb3d778b5b2b129c9
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-01-27 10:25:20 +0530

    Fixed code to remove build error

--- ulong_extras/is_prime_pocklington.c
+++ ulong_extras/is_prime_pocklington.c
@@ -22,6 +22,7 @@ n_is_prime_pocklington(mp_limb_t n, ulong iterations)
         return (n == UWORD(2));
 
     rootn = n_sqrt(n);                      /* floor(sqrt(n)) */
+
     if (n == rootn*rootn)
         return 0;
 
@@ -29,10 +30,12 @@ n_is_prime_pocklington(mp_limb_t n, ulong iterations)
     n_factor_init(&factors);
     limit = (mp_limb_t) cbrt((double) n1);
 
-    if (n>9007199254740992)                /*checking if n>2^53 */     
-        limit+=1;
+    if (n>9007199254740992)                 /* if n exceeds 2^53, limit is increased by 1 as */    
+        limit+=1;                           /* the mantissa of n will be smaller than the actual value */
+                                            /* as double used in cbrt has only 53 bits of mantissa */
 
     cofactor = n_factor_partial(&factors, n1, limit, 1);
+
     if (cofactor != 1)                      /* check that cofactor is coprime to factors found */
     {
         for (i = 0; i < factors.num; i++)
@@ -50,7 +53,7 @@ n_is_prime_pocklington(mp_limb_t n, ulong iterations)
     F = n1/cofactor;                        /* n1 = F*cofactor */
     Fsq = F*F;
 
-    if (F <= rootn && F > limit)            /* cube root method applicable only if n^1/3 <= F < n^1/2 */
+    if (F <= rootn)                         /* cube root method applicable only if n^1/3 <= F < n^1/2 */
     {   
         c2 = n1/(Fsq);                      /* expressing n as c2*F^2 + c1*F + 1  */
         c1 = (n1 - c2*Fsq )/F;
@@ -65,7 +68,7 @@ n_is_prime_pocklington(mp_limb_t n, ulong iterations)
         mp_limb_t exp = n1 / factors.p[i];
         pass = 0;
 
-        for (j = 2; j < iterations && pass == 0; j++)
+        for (j = 2; j < iterations && pass == 0 && factors.p[i]!=1; j++)
         {
             b = n_powmod2_preinv(j, exp, n, ninv);
             if (n_powmod2_ui_preinv(b, factors.p[i], n, ninv) != UWORD(1))
