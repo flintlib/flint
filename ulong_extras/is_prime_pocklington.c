1abcf2821e99667d577f763f98e2d33e6df4bbdc
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-01-29 01:56:47 +0530

    removed hack, added copyright header
    - Remove the hack which was used because of bug in n_factor_partial. n_factor_partial has also been fixed accordingly so that it doesn't return 1 as a factor.
    
    - Added copyright header which was unintentionally removed earlier.

--- ulong_extras/is_prime_pocklington.c
+++ ulong_extras/is_prime_pocklington.c
@@ -1,3 +1,29 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2008 Peter Shrimpton
+    Copyright (C) 2009 William Hart
+    Copyright (C) 2015 Kushagra Singh
+
+******************************************************************************/
 #include <gmp.h>
 #define ulong ulongxx /* interferes with system includes */
 #include <math.h>
@@ -68,7 +94,7 @@ n_is_prime_pocklington(mp_limb_t n, ulong iterations)
         mp_limb_t exp = n1 / factors.p[i];
         pass = 0;
 
-        for (j = 2; j < iterations && pass == 0 && factors.p[i]!=1; j++)
+        for (j = 2; j < iterations && pass == 0; j++)
         {
             b = n_powmod2_preinv(j, exp, n, ninv);
             if (n_powmod2_ui_preinv(b, factors.p[i], n, ninv) != UWORD(1))
