f5e4c6efb717c3c4705474a9d5b0c763c6f83fc7
kush789 <singh.kushagra.1996@gmail.com>
2015-02-28 04:10:40 +0530

    Made n_rootrem using Newton's method more efficient.

--- ulong_extras/n_rootrem_newton_iteration.c
+++ ulong_extras/n_rootrem_newton_iteration.c
@@ -17,6 +17,12 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 =============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2015 William Hart
+    Copyright (C) 2015 Kushagra Singh
+
+******************************************************************************/
 
 
 #include <gmp.h>
@@ -28,91 +34,102 @@
 #include "ulong_extras.h"
 #include "fmpz.h"
 
-int
-n_rootrem_newton_iteration(mp_limb_t* base, mp_limb_t* remainder, mp_limb_t n, mp_limb_t root)
-{
-    mp_limb_t max_pow;
-    double x, dx, a, b, small_float;
+/* A table of precomputed inverses of values from 1 to 64 */
+/* inv_table[n] = i/n for all n in range[1, 64] */
+/* inv_table[0] is set to 0, albeit it will never be called */
 
-    if (root > 10)
-        small_float = 0.001;
-    else if (root >= 7)
-        small_float = 0.00001;
-    else 
-        small_float = 0.000001;
+double inv_table[] = { 0.000000000000000, 1.000000000000000, 0.500000000000000, 
+                       0.333333333333333, 0.250000000000000, 0.200000000000000, 
+                       0.166666666666667, 0.142857142857143, 0.125000000000000, 
+                       0.111111111111111, 0.100000000000000, 0.090909090909091, 
+                       0.083333333333333, 0.076923076923077, 0.071428571428571, 
+                       0.066666666666667, 0.062500000000000, 0.058823529411765, 
+                       0.055555555555556, 0.052631578947368, 0.050000000000000, 
+                       0.047619047619048, 0.045454545454545, 0.043478260869565, 
+                       0.041666666666667, 0.040000000000000, 0.038461538461538, 
+                       0.037037037037037, 0.035714285714286, 0.034482758620690, 
+                       0.033333333333333, 0.032258064516129, 0.031250000000000, 
+                       0.030303030303030, 0.029411764705882, 0.028571428571429, 
+                       0.027777777777778, 0.027027027027027, 0.026315789473684, 
+                       0.025641025641026, 0.025000000000000, 0.024390243902439, 
+                       0.023809523809524, 0.023255813953488, 0.022727272727273, 
+                       0.022222222222222, 0.021739130434783, 0.021276595744681, 
+                       0.020833333333333, 0.020408163265306, 0.020000000000000, 
+                       0.019607843137255, 0.019230769230769, 0.018867924528302, 
+                       0.018518518518519, 0.018181818181818, 0.017857142857143, 
+                       0.017543859649123, 0.017241379310345, 0.016949152542373, 
+                       0.016666666666667, 0.016393442622951, 0.016129032258065, 
+                       0.015873015873016, 0.015625000000000 };
+
+                       /* this table consists 65 values */
+
+double nth_root_estimate(double a, int n)
+{ 
+   slong i = *((slong *) &a);
+   const slong s = (1 << 10) - 1;
+   i = (i - (s << 52)) * inv_table[n] + (s << 52);
+   return *((double *) &i);
+}
+
+int
+n_rootrem(mp_limb_t* base, mp_limb_t* remainder, mp_limb_t n, mp_limb_t root)
+{
+    mp_limb_t maxpow, x, currval; 
+    double dx;
 
     if (n<1 || root<1)
         return 0;
+
+    if (root == 2)
+    {
+        *base = n_sqrtrem(remainder, n);
+        return 1;
+    }
 
     if (root == 1)
     {
         *base = n;
         *remainder = 0;
         return 1;
-    }   
-    if (n <= root)
+    }
+
+    maxpow = n_flog(n, 2);  /* max possible power, if greter then base is 1 */
+
+    if (root > maxpow)
     {
         *base = 1;
         *remainder = n - 1;
         return 1;
     }
 
-    if (root == 2)
-    {
-        *base = n_sqrt(n);      /* floor of sqrt(n) */
-        a = n_pow(*base, 2);
-
-        if (a>n)                /* doc says n_sqrt may return value rounded up */
-        {
-            *base = *base - 1;
-            a = n_pow(*base, 2);
-        }
-
-        *remainder = n - a;
-        return 0;
-    }
-
-    max_pow = n_clog(n, 2);      /* 2^(max_pow) > n */
-
-    if (n == n_pow(2, max_pow)  && (root == max_pow))
+    if (root == maxpow)
     {
         *base = 2;
-        *remainder = 0;
+        *remainder = n - n_pow(2, root);
         return 1;
     }
 
-    if (root >= max_pow)         /* if root > max_pow, base has to be 1 (we know its not 2) */
-    {
+    x =  floor(nth_root_estimate((double)n, root));
 
-        *base = 1;
-        *remainder = n - 1;
-        return 1;
-    }
+    /* one round of newton iteration */
 
-    /* Newton iteration begins */
+    currval = n_pow(x, root-1);
+    dx = n / currval;
+    dx-= x;
+    dx*= inv_table[root-1];
+    dx = floor(dx);
+    x += dx;
 
-    x = n/root;                 
+    *base = x;
 
-    dx = 0;
-    a = root;
-    b = root - 1;
+    while (n_pow(*base, root) <= n)
+        (*base) += 1;
 
-    while (1)
-    {
-        dx = (n / pow(x, b));   // dx = n / x^n-1
-        dx = dx - x;            // dx = ((n / x^n-1) - x)
-        dx = dx / n;            // dx = ((n / x^n-1) - x) / n
+    while (n_pow(*base, root) > n)
+        (*base) -= 1;
 
-        x = x + dx;
-
-        if (absolute(dx) < absolute(x)*(small_float))
-            break;
-    }
-
-    *remainder = x;
-    *base = *remainder;
-    *remainder = pow(*remainder, root);
+    *remainder = *base;
+    *remainder = n_pow(*remainder, root);
     *remainder = n - *remainder;
-
     return 1;
 }
