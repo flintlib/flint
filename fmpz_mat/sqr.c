f1196254f66f8d7c0a0685198dcbd169a157fa59
anubhav <anubhav.srivastava@students.iiit.ac.in>
2015-03-05 11:33:15 +0530

    Implemented squaring for matrix of dimension greater than 2 in fmpz_mat module

--- fmpz_mat/sqr.c
+++ fmpz_mat/sqr.c
@@ -20,6 +20,7 @@
 /******************************************************************************
 
     Copyright (C) 2012 Fredrik Johansson
+    Copyright (C) 2015 Anubhav Srivastava
 
 ******************************************************************************/
 
@@ -64,6 +65,115 @@ fmpz_mat_sqr(fmpz_mat_t B, const fmpz_mat_t A)
     }
     else
     {
-        fmpz_mat_mul(B, A, A);
+	 
+        if(n%2==0)
+        {
+            fmpz_mat_t window11, window12, window21, window22;
+            fmpz_mat_t s1, s2, s3, s4;
+            fmpz_mat_t p1, p2, p3, p4, p5, p6, p7;
+
+            fmpz_mat_init(s1, n/2, n/2);
+            fmpz_mat_init(s2, n/2, n/2);
+            fmpz_mat_init(s3, n/2, n/2);
+            fmpz_mat_init(s4, n/2, n/2);
+            fmpz_mat_init(p1, n/2, n/2);
+            fmpz_mat_init(p2, n/2, n/2);
+            fmpz_mat_init(p3, n/2, n/2);
+            fmpz_mat_init(p4, n/2, n/2);
+            fmpz_mat_init(p5, n/2, n/2);
+            fmpz_mat_init(p6, n/2, n/2);
+            fmpz_mat_init(p7, n/2, n/2);
+
+            fmpz_mat_window_init(window11, A, 0, 0, n/2, n/2);
+            fmpz_mat_window_init(window12, A, 0, n/2, n/2, n);
+            fmpz_mat_window_init(window21, A, n/2, 0, n, n/2);
+            fmpz_mat_window_init(window22, A, n/2, n/2, n, n);
+          
+            
+            fmpz_mat_add(s1, window22, window12);
+            fmpz_mat_sub(s2, window22, window21);
+            fmpz_mat_add(s3, s2, window12);
+            fmpz_mat_sub(s4, s3, window11);
+       
+
+            fmpz_mat_sqr(p1, s1);
+            fmpz_mat_sqr(p2, s2);
+            fmpz_mat_sqr(p3, s3);    
+            fmpz_mat_sqr(p4, window11);
+            fmpz_mat_mul(p5, window12, window21);
+            fmpz_mat_mul(p6, s4, window12);
+            fmpz_mat_mul(p7, window21, s4);
+            
+
+            fmpz_mat_zero(s1);
+            fmpz_mat_zero(s2);
+            fmpz_mat_zero(s3);
+           
+            fmpz_mat_add(s1, p3, p5);
+            fmpz_mat_sub(s2, p1, s1);
+            fmpz_mat_sub(s3, s1, p2);
+            
+            fmpz_mat_zero(s1);
+            fmpz_mat_zero(p1);
+            fmpz_mat_zero(p3);
+            fmpz_mat_zero(s4);
+            
+            fmpz_mat_add(p1, p4, p5);
+            fmpz_mat_sub(s1, s3, p6);
+            fmpz_mat_sub(p3, s2, p7);
+            fmpz_mat_add(s4, p2, s2);
+            
+            for(i=0;i<n/2;++i)
+            {
+                for(j=0;j<n/2;++j)
+                {
+                    fmpz_set(fmpz_mat_entry(B, i, j), fmpz_mat_entry(p1, i, j));
+                }
+            }
+
+            for(i=n/2;i<n;++i)
+            {
+                for(j=0;j<n/2;++j)
+                {
+                    fmpz_set(fmpz_mat_entry(B, i, j), fmpz_mat_entry(p3, i-n/2, j));
+                }
+            }
+
+            for(i=0;i<n/2;++i)
+            {
+                for(j=n/2;j<n;++j)
+                {
+                    fmpz_set(fmpz_mat_entry(B, i, j), fmpz_mat_entry(s1, i, j-n/2));
+                }
+            }
+
+            for(i=n/2;i<n;++i)
+            {
+                for(j=n/2;j<n;++j)
+                {
+                    fmpz_set(fmpz_mat_entry(B, i, j), fmpz_mat_entry(s4, i-n/2, j-n/2));
+                }
+            }
+   
+            fmpz_mat_window_clear(window11);
+            fmpz_mat_window_clear(window12);
+            fmpz_mat_window_clear(window21);
+            fmpz_mat_window_clear(window22);
+ 
+            fmpz_mat_clear(s1);
+            fmpz_mat_clear(s2);
+            fmpz_mat_clear(s3);
+            fmpz_mat_clear(s4);
+            fmpz_mat_clear(p1);
+            fmpz_mat_clear(p2);
+            fmpz_mat_clear(p3);
+            fmpz_mat_clear(p4);
+            fmpz_mat_clear(p5);
+            fmpz_mat_clear(p6);
+            fmpz_mat_clear(p7);
+
+        }
+        else
+            fmpz_mat_mul(B, A, A);
     }
 }
