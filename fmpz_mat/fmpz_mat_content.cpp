9b937af635c8687642a77226be572661d0bd0ed3
Prabhdeep <prabhdeep3114@gmail.com>
2015-03-06 11:58:07 +0530

    Calculates gcd of contents of fmpz_mat

---
+++ fmpz_mat/fmpz_mat_content.cpp
@@ -0,0 +1,53 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2015 Prabhdeep Singh Walia
+
+******************************************************************************/
+
+#include "fmpz_mat.h"
+#include "fmpz.h"
+
+void
+fmpz_mat_content(fmpz_t ret, const fmpz_mat_t A)
+{
+    slong i, j, k;
+    fmpz_t t;
+    
+    k = 0;
+    
+    if(A->r == 0 && A->c == 0)
+	fmpz_set_si(ret, k);
+    else
+    {
+	ret = fmpz_mat_entry(A, 0, 0);
+    
+	for(i = 0; i < A->r; i++)
+	{
+	    for(j = 0; j < A->c; j++)
+	    {
+		t = ret;
+		fmpz_gcd(ret, t, fmpz_mat_entry(A, i, j)); 
+	    }
+	}
+    }
+    
+}
