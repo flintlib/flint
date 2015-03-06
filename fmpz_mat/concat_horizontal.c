fefef0dccdfc54de5f7df6777fc9b715db6af19d
anubhav <anubhav.srivastava@students.iiit.ac.in>
2015-03-06 09:20:02 +0530

    Horizontal concatenation function for matrices

---
+++ fmpz_mat/concat_horizontal.c
@@ -0,0 +1,27 @@
+#include "fmpz_mat.h"
+
+void
+fmpz_mat_concat_horizontal(fmpz_mat_t res, const fmpz_mat_t mat1, const fmpz_mat_t mat2)
+{
+    slong i,j;
+    slong r1 = mat1->r;
+    slong c1 = mat1->c;
+    slong r2 = mat2->r;
+    slong c2 = mat2->c;
+    
+    for(i = 0; i < r1; i++)
+    {
+        for(j = 0;j < c1; j++)
+        {
+            fmpz_set(fmpz_mat_entry(res, i, j), fmpz_mat_entry(mat1, i, j));
+        }
+    }
+
+    for(i = 0; i < r2; i++)
+    {
+        for(j = 0;j < c2; j++)
+        {
+            fmpz_set(fmpz_mat_entry(res, i, (j + c1)), fmpz_mat_entry(mat2, i, j));
+        }
+    }
+}
