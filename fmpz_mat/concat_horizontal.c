d5b9e2b73c0eeafe18816b910673c6da5a4a853b
anubhav <anubhav.srivastava@students.iiit.ac.in>
2015-03-06 19:17:28 +0530

    Horizontal concatenation of two matrices

--- fmpz_mat/concat_horizontal.c
+++ fmpz_mat/concat_horizontal.c
@@ -1,3 +1,31 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2015 Anubhav Srivastava
+
+******************************************************************************/
+
+
+
+
 #include "fmpz_mat.h"
 
 void
@@ -9,19 +37,19 @@ fmpz_mat_concat_horizontal(fmpz_mat_t res, const fmpz_mat_t mat1, const fmpz_mat
     slong r2 = mat2->r;
     slong c2 = mat2->c;
     
-    for(i = 0; i < r1; i++)
+    for (i = 0; i < r1; i++)
     {
-        for(j = 0;j < c1; j++)
+        for (j = 0;j < c1; j++)
         {
             fmpz_set(fmpz_mat_entry(res, i, j), fmpz_mat_entry(mat1, i, j));
         }
     }
 
-    for(i = 0; i < r2; i++)
+    for (i = 0; i < r2; i++)
     {
-        for(j = 0;j < c2; j++)
+        for (j = 0;j < c2; j++)
         {
-            fmpz_set(fmpz_mat_entry(res, i, (j + c1)), fmpz_mat_entry(mat2, i, j));
+            fmpz_set(fmpz_mat_entry(res, i, j + c1), fmpz_mat_entry(mat2, i, j));
         }
     }
 }
