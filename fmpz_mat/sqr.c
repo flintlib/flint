e0e789ce9b79baecf79ef7cc5cfdaefbb550ff8c
anubhav <anubhav.srivastava@students.iiit.ac.in>
2015-03-10 13:04:54 +0530

    Updated Square function

--- fmpz_mat/sqr.c
+++ fmpz_mat/sqr.c
@@ -1,44 +1,46 @@
 /*=============================================================================
 
-  This file is part of FLINT.
+    This file is part of FLINT.
 
-  FLINT is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-  FLINT is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with FLINT; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-  =============================================================================*/
+=============================================================================*/
 /******************************************************************************
 
-  Copyright (C) 2012 Fredrik Johansson
-  Copyright (C) 2015 Anubhav Srivastava
+    Copyright (C) 2012 Fredrik Johansson
+    Copyright (C) 2015 Anubhav Srivastava
 
- ******************************************************************************/
+******************************************************************************/
 
 
 #include "fmpz_mat.h"
 
+#define E fmpz_mat_entry
+
 void
-fmpz_mat_sqr(fmpz_mat_t C, const fmpz_mat_t A)
+fmpz_mat_sqr(fmpz_mat_t B, const fmpz_mat_t A)
 {
     slong dim, n;
 
     n = A->r;
-
+    
     dim = n;
 
     if (dim <= 12)
     {
-        fmpz_mat_mul(C, A, A);    
+        fmpz_mat_mul(B, A, A);    
     }
     else
     {
@@ -49,11 +51,11 @@ fmpz_mat_sqr(fmpz_mat_t C, const fmpz_mat_t A)
 
         if (5*(ab + ab) > dim * dim )
         {
-            fmpz_mat_sqr_bodrato(C, A);
+            fmpz_mat_sqr_bodrato(B, A);
         }
         else
         {
-            fmpz_mat_mul(C, A, A);
+            fmpz_mat_mul(B, A, A);
         }
 
     }
