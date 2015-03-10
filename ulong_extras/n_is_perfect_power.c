d1edaa5f289718542bcf5ad1f88d0516bbd5b028
Nitin Kumar <ntnkmar@gmail.com>
2015-03-10 15:55:00 +0530

    n_is_perfect_power.c updated

--- ulong_extras/n_is_perfect_power.c
+++ ulong_extras/n_is_perfect_power.c
@@ -72,11 +72,21 @@ mp_limb_t n_rootrem(mp_limb_t * r, mp_limb_t x, unsigned int k)       /* taken f
 int n_is_perfect_power(mp_limb_t * r, mp_limb_t x)
 {
     int p[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61};                       /* prime less than 64 */
-    int q[] = {3, 7, 11, 29, 23, 53, 103, 191, 277, 349, 373, 593, 739, 947, 1129, 1697, 1889, 1831};     /* prime such that, q[i] % p[i] = 1 */
-    int z[] = {1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1};                            /* if x = y ^ p[i] for some y => (y ^ (q[i] - 1)) % q[i] = 1 or 0 */
-    int v[] = {0, 1, 2, 3, 2, 5, 6, 7, 8, 3, 10, 11, 12, 13, 14, 15, 2, 17, 18, 19, 20};                  /* v[i] is root of i and z[i] is it's power */
-    int i, power = 1,  j = 1;
-    double qinv;
+    int q[] = {3, 7, 11, 29, 23, 53, 103, 191, 47, 59, 311, 149, 83, 173, 283, 107, 709, 367};     /* prime such that, q[i] % p[i] = 1 */
+    int z[] = {1, 1, 1, 1, 2, 1, 1, 1, 3, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1};                            /* if x = y ^ p[i] for some y => (y ^ (q[i] - 1)) % q[i] = 1 or 0 */
+    int v[] = {0, 1, 2, 3, 2, 5, 6, 7, 2, 3, 10, 11, 12, 13, 14, 15, 2, 17, 18, 19, 20};                  /* v[i] is root of i and z[i] is it's power */
+    mp_limb_t qmodulo[18][10] = { { 3 }, { 67 }, { 1027 }, { 268570627 }, { 4194307 }, { 1082130434 , 1048577 },
+                            { 2 , 50380800 , 0 , 65 }, { 130 , 131200 , 262144 , 8192 , 16793600 , 1090519041 },
+                            { 2 , 16385 }, { 2 , 67108865 }, { 66 , 1048592 , 2147483648 , 0 , 0 , 0 , 16777216 , 0 , 524296 , 4325377 },
+                            { 2 , 4096 , 0 , 512 , 1048577 }, { 2 , 0 , 262145 }, { 2 , 0 , 536936448 , 0 , 0 , 4097 },
+                            { 2 , 12288 , 0 , 0 , 0 , 0 , 0 , 49152 , 67108865 }, { 2 , 0 , 0 , 1025 },
+                            { 2 , 0 , 134217729 , 0 , 0 , 134217728 , 0 , 24 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 6 , 1024 , 0 , 0 , 1056 , 0 , 0 , 17 },
+                            { 2 , 0 , 1572864 , 0 , 0 , 0 , 0 , 0 , 402653184 , 0 , 0 , 16385 }
+
+                          };             /* kth bit of qmodulo[i][j] is 1 if  y = 32 * (j - 1) + k, y ^ ((p[i] - 1) / q[i])  % q[i] is <= 1,  else 0 */
+                                        /* i.e q[i][0]th elements bits from (LSB to MSB )represent 0.1.2....31, q[i][1]th represent 32.33....64 residue class modulo i and so on */
+
+    int i, power = 1,  j = 1, pos, k;
     mp_limb_t root, prev;
 
     for (i = 0; i < 18; i++)
@@ -88,8 +98,12 @@ int n_is_perfect_power(mp_limb_t * r, mp_limb_t x)
             break;
         }
 
-        qinv = n_precompute_inverse(q[i]);
-        if (n_powmod_precomp(x, (q[i] - 1) / p[i], q[i], qinv) <= 1)
+        k = x % q[i];                                     /* class of x congruent modulo q[i] */
+        pos = k / 32 + (k % 32 == 0) ? 0 : 1;            /* position of correponding class of q[i] in qmodulo table */
+
+        if (pos == 0) pos = 1;                           /* in case q[i] divides x */
+
+        if ((qmodulo[i][pos - 1] & (1 << (k % 32))))     /* check if bit corresponding to k is set*/
         {
             prev = x;
             x = n_rootrem(r, x, p[i]);
@@ -108,8 +122,8 @@ int n_is_perfect_power(mp_limb_t * r, mp_limb_t x)
             else x = prev;
         }
     }
+
     *r = x;                                                             /* store root in  r */
     return power;                                                       /* return power */
 }
-
 
