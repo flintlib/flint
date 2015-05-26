e5b9555791258e58815dc6ddb2220e6cb2869d26
Kushagra Singh <kush@Kushagras-MacBook-Air.local>
2015-05-26 22:58:34 +0530

    Updated fmpz_ec_add() incorporating modular arithmetic.

--- fmpz/ec_add.c
+++ fmpz/ec_add.c
@@ -27,11 +27,19 @@
 #include "flint.h"
 #include "fmpz.h"
 
+/* P (x : z) = P1 (x1 : z1) + P2 (x2 : z2) where P0 (x0 : zo) is P - Q */
+
+/* 
+    Coordinates of P : 
+
+        x = 4 * z0 * (x1 * x2 - z1 * z2)^2 
+        z = 4 * x0 * (x2 * z1 - x1 * z2)^2 
+*/
+
 void 
-ec_add(fmpz_t x, fmpz_t y, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2, fmpz_t x0, fmpz_t z0)
+fmpz_ec_add(fmpz_t x, fmpz_t z, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2,
+       fmpz_t x0, fmpz_t z0, fmpz_t n)
 {
-    /* x = 4 * z0 * (x1 * x2 - z1 * z2)^2 mod n */
-    /* z = 4 * x0 * (x2 * z1 - x1 * z2)^2 mod n */
 
     fmpz_t u, v, w;
 
@@ -41,24 +49,42 @@ ec_add(fmpz_t x, fmpz_t y, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2, fmpz_t x0
 
     fmpz_sub(u, x2, z2);    /* u = (x2 - z2) */
     fmpz_add(v, x1, z1);    /* v = (x1 + z1) */
+
+    if (fmpz_cmp(v, n) > 0)
+        fmpz_sub(v, v, n);
+
     fmpz_mul(u, u, v);      /* u = (x2 - z2) * (x1 + z1) */
+    fmpz_mod(u, u, n);
 
     fmpz_sub(v, x1, z1);    /* v = (x1 - z1) */
     fmpz_add(w, x2, z2);    /* w = (x2 + z2) */
+    if (fmpz_cmp(w, n) > 0)
+        fmpz_sub(w, w, n);
+
     fmpz_mul(v, v, w);      /* v = (x1 - z1) * (x2 + z2) */
+    fmpz_mod(v, v, n);
 
     fmpz_add(w, u, v);      /* w = 2 * (x1 * x2 - z1 * z2) */
+    if (fmpz_cmp(w, n) > 0)
+        fmpz_sub(w, w, n);
+
     fmpz_sub(v, u, v);      /* v = 2 * (x2 * z1 - x1 * z2) */
 
     fmpz_mul(w, w, w);      /* w = 4 * (x1 * x2 - z1 * z2)^2 */
+    fmpz_mod(w, w, n);
+
     fmpz_mul(v, v, v);      /* v = 4 * (x2 * z1 - x1 * z2)^2 */
+    fmpz_mod(v, v, n);
     
     fmpz_mul(x, z0, w);     /* x = 4 * z0 * (x1 * x2 - z1 * z2)^2 */
+    fmpz_mod(x, x, n);
+
     fmpz_mul(z, x0, v);     /* z = 4 * x0 * (x2 * z1 - x1 * z2)^2 */
+    fmpz_mod(z, z, n);
 
     fmpz_clear(u);
     fmpz_clear(v);
     fmpz_clear(w);
-    
+
 }
 
