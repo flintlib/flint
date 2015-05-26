b6829024bb9b0ec66ab7ba564180b194c80e806b
Kushagra Singh <kush@Kushagras-MacBook-Air.local>
2015-05-26 10:51:50 +0530

    Corrected math in ec_add.c

--- fmpz/ec_add.c
+++ fmpz/ec_add.c
@@ -28,11 +28,10 @@
 #include "fmpz.h"
 
 void 
-fmpz_ec_add(fmpz_t x, fmpz_t y, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2,
-            fmpz_t x0, fmpz_t z0)
+ec_add(fmpz_t x, fmpz_t y, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2, fmpz_t x0, fmpz_t z0)
 {
-    /* x = x0 * ((x2 - z2) * (x1 + z1) + (x2 + z2) * (x1 - z1))^2 */
-    /* z = z0 * ((x2 - z2) * (x1 + z1) + (x2 + z2) * (x1 - z1))^2 */
+    /* x = 4 * z0 * (x1 * x2 - z1 * z2)^2 mod n */
+    /* z = 4 * x0 * (x2 * z1 - x1 * z2)^2 mod n */
 
     fmpz_t u, v, w;
 
@@ -40,26 +39,26 @@ fmpz_ec_add(fmpz_t x, fmpz_t y, fmpz_t x1, fmpz_t z1, fmpz_t x2, fmpz_t z2,
     fmpz_init(v);
     fmpz_init(w);
 
-    fmpz_sub(u, x2, z2);
-    fmpz_add(v, x1, z1);
-    fmpz_mul(u, u, v);
+    fmpz_sub(u, x2, z2);    /* u = (x2 - z2) */
+    fmpz_add(v, x1, z1);    /* v = (x1 + z1) */
+    fmpz_mul(u, u, v);      /* u = (x2 - z2) * (x1 + z1) */
 
-    fmpz_add(w, x2, z2);
-    fmpz_sub(v, x1, z1);
-    fmpz_mul(v, w, v);
+    fmpz_sub(v, x1, z1);    /* v = (x1 - z1) */
+    fmpz_add(w, x2, z2);    /* w = (x2 + z2) */
+    fmpz_mul(v, v, w);      /* v = (x1 - z1) * (x2 + z2) */
 
-    fmpz_add(w, u, v);
-    fmpz_sub(v, u, v);
+    fmpz_add(w, u, v);      /* w = 2 * (x1 * x2 - z1 * z2) */
+    fmpz_sub(v, u, v);      /* v = 2 * (x2 * z1 - x1 * z2) */
 
-    fmpz_mul(w, w, w);
-    fmpz_mul(v, v, v);
+    fmpz_mul(w, w, w);      /* w = 4 * (x1 * x2 - z1 * z2)^2 */
+    fmpz_mul(v, v, v);      /* v = 4 * (x2 * z1 - x1 * z2)^2 */
     
-    fmpz_mul(x, z0, w);
-    fmpz_mul(z, x0, v);
+    fmpz_mul(x, z0, w);     /* x = 4 * z0 * (x1 * x2 - z1 * z2)^2 */
+    fmpz_mul(z, x0, v);     /* z = 4 * x0 * (x2 * z1 - x1 * z2)^2 */
 
     fmpz_clear(u);
     fmpz_clear(v);
     fmpz_clear(w);
-
+    
 }
 
