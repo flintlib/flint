1d9c7fbe8b3206ede6126f8c251769425882af7f
Nitin <ntnkmar@gmail.com>
2015-06-03 14:57:22 +0530

    taking care of indentation

--- siqs/factor.c
+++ siqs/factor.c
@@ -97,11 +97,11 @@ mp_limb_t qsieve_factor(fmpz_t n, fmpz_factor_t factors)
 
     if (small_factor)
     {
-	    while (fmpz_fdiv_ui(qs_inf->n, small_factor) == 0)
-	    {
-	        fmpz_divexact_ui(x, qs_inf->n, small_factor);
-	        fmpz_init_set(qs_inf->n, x);
-	        exp++;
+        while (fmpz_fdiv_ui(qs_inf->n, small_factor) == 0)
+        {
+            fmpz_divexact_ui(x, qs_inf->n, small_factor);
+            fmpz_init_set(qs_inf->n, x);
+            exp++;
         }
 
         _fmpz_factor_append_ui(factors, small_factor, exp);
@@ -115,11 +115,11 @@ mp_limb_t qsieve_factor(fmpz_t n, fmpz_factor_t factors)
 
     if (small_factor)
     {
-	    while (fmpz_fdiv_ui(qs_inf->n, small_factor) == 0)
-	    {
-	        fmpz_divexact_ui(x, qs_inf->n, small_factor);
-	        fmpz_init_set(qs_inf->n, x);
-	        exp++;
+        while (fmpz_fdiv_ui(qs_inf->n, small_factor) == 0)
+        {
+            fmpz_divexact_ui(x, qs_inf->n, small_factor);
+            fmpz_init_set(qs_inf->n, x);
+            exp++;
         }
 
         _fmpz_factor_append_ui(factors, small_factor, exp);
