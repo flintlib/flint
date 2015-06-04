f6952b636820f5cf48525f18375a1f579985d41c
Vladimir Glazachev <glazachev.vladimir@gmail.com>
2015-06-04 20:37:41 +0300

    add some documentation

--- aprcl/unity_zpq_is_p_unity.c
+++ aprcl/unity_zpq_is_p_unity.c
@@ -29,11 +29,11 @@ ulong unity_zpq_p_unity(const unity_zpq f)
 {
     ulong i, is_punity;
     is_punity = f->p;
-    for (i = 1; i < f->p; i++)
+    for (i = 0; i < f->p; i++)
     {
         if (fmpz_equal_ui(f->polys[i]->coeffs + i, 1) == 1)
         {
-            if (is_punity != 0)
+            if (is_punity != f->p)
                 return 0;
             is_punity = i;
         }
@@ -51,7 +51,7 @@ int unity_zpq_is_p_unity(const unity_zpq f)
 int unity_zpq_is_p_unity_generator(const unity_zpq f)
 {
     ulong upow = unity_zpq_p_unity(f);
-    if (upow != f->p)
+    if (upow != f->p && upow != 0)
         if (n_gcd(upow, f->p) == 1)
             return 1;
     return 0;
