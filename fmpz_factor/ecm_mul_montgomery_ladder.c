1fc7488bd04bcab97b340dd8a78d6092f21577a6
Kushagra Singh <kush@Kushagras-MacBook-Air.local>
2015-06-06 20:19:14 +0530

    Fixed ecm_mul_montgomery_ladder, now handles corner case (multiplied by 0). Prototype updated.

--- fmpz_factor/ecm_mul_montgomery_ladder.c
+++ fmpz_factor/ecm_mul_montgomery_ladder.c
@@ -30,34 +30,46 @@
 /* P (x0 : z0) <- kP using Montgomery ladder algorithm */
 
 void
-fmpz_factor_ecm_mul_montgomery_ladder(fmpz_t x0, fmpz_t z0, fmpz_t k, fmpz_t a24, fmpz_t n)
+fmpz_factor_ecm_mul_montgomery_ladder(fmpz_t x, fmpz_t z, fmpz_t x0, fmpz_t z0, 
+                                      fmpz_t k, fmpz_t a24, fmpz_t n)
 {
+
+    if (fmpz_cmp_ui(k, 0) == 0) /* If multiplied by 0, return [0 :: 0] */
+    {
+        fmpz_set_ui(x, 0);
+        fmpz_set_ui(z, 0);
+        return;
+    }
+
     mp_limb_t i;
     fmpz_t x1, z1, x2, z2;      /* Q (x1 : z1), P (x2 : z2) */
 
     if (fmpz_is_one(k))
+    {
+        fmpz_set(x, x0);
+        fmpz_set(z, z0);
         return;
+    }
 
     fmpz_init_set(x1, x0);        /* Q <- P0 */
     fmpz_init_set(z1, z0);
     fmpz_init(x2);
     fmpz_init(z2);
 
-    fmpz_ec_double(x2, z2, x0, z0, n, a24); /* P <- 2P0 */
-
+    fmpz_factor_ecm_double(x2, z2, x0, z0, a24, n); /* P <- 2P0 */
     i = fmpz_sizeinbase(k, 2) - 2;
 
     while (1)
     {
         if (fmpz_tstbit(k, i) == 1)   /* ith bit is 1 */
         {
-            fmpz_ec_add(x1, z1, x1, z1, x2, z2, x0, z0, n); /* Q <- P + Q */
-            fmpz_ec_double(x2, z2, x2, z2, n, a24);         /* P <- 2 * P */
+            fmpz_factor_ecm_add(x1, z1, x1, z1, x2, z2, x0, z0, a24, n); /* Q <- P + Q */
+            fmpz_factor_ecm_double(x2, z2, x2, z2, a24, n);         /* P <- 2 * P */
         }
         else
         {   
-            fmpz_ec_add(x2, z2, x1, z1, x2, z2, x0, z0, n); /* P <- P + Q */
-            fmpz_ec_double(x1, z1, x1, z1, n, a24);         /* Q <- 2 * Q */
+            fmpz_factor_ecm_add(x2, z2, x1, z1, x2, z2, x0, z0, a24, n); /* P <- P + Q */
+            fmpz_factor_ecm_double(x1, z1, x1, z1, a24, n);         /* Q <- 2 * Q */
         }
 
         if (i == 0)
@@ -66,12 +78,11 @@ fmpz_factor_ecm_mul_montgomery_ladder(fmpz_t x0, fmpz_t z0, fmpz_t k, fmpz_t a24
             i -= 1;
     }
 
-    fmpz_set(x0, x1);
-    fmpz_set(z0, z1);
+    fmpz_set(x, x1);
+    fmpz_set(z, z1);
 
     fmpz_clear(x1);
     fmpz_clear(z1);
     fmpz_clear(x2);
     fmpz_clear(z2);
 }
-
