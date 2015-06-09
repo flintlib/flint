120403794c0f887845fbc85a3467b2edbaff7b0b
Vladimir Glazachev <glazachev.vladimir@gmail.com>
2015-06-09 20:24:26 +0300

    add test for composite

--- aprcl/test/t-is_prime_gauss.c
+++ aprcl/test/t-is_prime_gauss.c
@@ -45,9 +45,9 @@ int main(void)
         fmpz_t n;
         fmpz_init(n);
 
-        fmpz_randtest_unsigned(n, state, 30);
+        fmpz_randtest_unsigned(n, state, 50);
         while (fmpz_cmp_ui(n, 100) <= 0)
-            fmpz_randtest_unsigned(n, state, 30);
+            fmpz_randtest_unsigned(n, state, 50);
 
         pbprime = fmpz_is_probabprime(n);
         cycloprime = is_prime_gauss(n);
@@ -70,6 +70,7 @@ int main(void)
         fmpz_init(n);
         result = 1;
 
+        /* Test big primes. */
         fmpz_set_str(n, "40206835204840513073", 10);
         if (is_prime_gauss(n) == 0)
             result = 0;
@@ -80,6 +81,22 @@ int main(void)
 
         fmpz_set_str(n, "5991810554633396517767024967580894321153", 10);
         if (is_prime_gauss(n) == 0)
+            result = 0;
+
+        /* Test big composite. */
+        /* 1500450271 * 5915587277 */
+        fmpz_set_str(n, "8876044532898802067", 10);
+        if (is_prime_gauss(n) == 1)
+            result = 0;
+
+        /* 5915587277 * 54673257461630679457 */
+        fmpz_set_str(n, "323424426232167763068694468589", 10);
+        if (is_prime_gauss(n) == 1)
+            result = 0;
+
+        /* 48112959837082048697 * 66405897020462343733 */
+        fmpz_set_str(n, "3194984256290911228520362769161858765901", 10);
+        if (is_prime_gauss(n) == 1)
             result = 0;
 
         if (result == 0)
