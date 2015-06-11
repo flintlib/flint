f99065d46b1db45f27a1741b1cf66dff8e061bbc
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-06-12 02:32:38 +0530

    Both factor_ecm functions, and some code for timing them in profile.

---
+++ fmpz_factor/ecm_one.c
@@ -0,0 +1,170 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2015 Kushagra Singh
+
+******************************************************************************/
+
+/* Outer wrapper for ECM 
+   makes calls to stage I and stage II (one) */
+
+#include <gmp.h>
+#include "flint.h"
+#include "fmpz.h"
+    
+static
+ulong ecm_primorial_one[15] =
+{
+   UWORD(2), UWORD(6), UWORD(30), UWORD(210), UWORD(2310), UWORD(30030), UWORD(510510), UWORD(9699690),
+   UWORD(223092870), UWORD(6469693230), UWORD(200560490130), UWORD(7420738134810),
+   UWORD(304250263527210), UWORD(13082761331670030), UWORD(614889782588491410)
+};
+
+int
+fmpz_factor_ecm_one(fmpz_t f, mp_limb_t curves, mp_limb_t B1, mp_limb_t B2,
+                    flint_rand_t state, fmpz_t n)
+{
+    fmpz_t sig, nm8, a24;
+    mp_limb_t P, num, maxD, mmin, mmax, mdiff, prod, maxj;
+    int i, j, ret;
+    ecm_t ecm_inf;
+
+    fmpz_factor_ecm_init(ecm_inf);
+    fmpz_init(sig);
+    fmpz_init(nm8);
+    fmpz_init(a24);
+    fmpz_sub_ui(nm8, n, 8);
+    
+    ret = 0;
+
+    /************************ STAGE I PRECOMPUTATIONS ************************/
+
+    num = n_prime_pi(B1);   /* number of primes under B1 */
+
+    /* compute list of primes under B1 for stage I */
+    const mp_limb_t *prime_array = flint_malloc(num * sizeof(mp_limb_t));
+    prime_array = n_primes_arr_readonly(num);   
+
+    /************************ STAGE II PRECOMPUTATIONS ***********************/
+
+    maxD = n_sqrt(B2);
+
+    /* Selecting primorial */
+
+    j = 1;
+    while ((j < 15) && (ecm_primorial_one[j] < maxD))
+        j += 1;
+
+    P = ecm_primorial_one[j - 1]; 
+    
+    mmin = (B1 + (P/2)) / P;
+    mmax = ((B2 - P/2) + P - 1)/P;      /* ceil */
+    maxj = (P + 1)/2; 
+    mdiff = mmax - mmin + 1;
+
+    /* compute GCD_table */
+
+    ecm_inf->GCD_table = flint_malloc((maxj + 1) * sizeof(uint8_t));
+
+    for (j = 1; j <= maxj; j += 2)
+    {
+        if ((j%2) && n_gcd(j, P) == 1)
+            ecm_inf->GCD_table[j] = 1;  
+        else
+            ecm_inf->GCD_table[j] = 0;
+    }  
+
+    /* compute prime table */
+
+    ecm_inf->prime_table = flint_malloc(mdiff * sizeof(uint8_t*));
+
+    for (i = 0; i < mdiff; i++)
+        ecm_inf->prime_table[i] = flint_malloc((maxj + 1) * sizeof(uint8_t));
+
+    for (i = 0; i < mdiff; i++)
+    {
+        for (j = 1; j <= maxj; j += 2)
+        {
+            ecm_inf->prime_table[i][j] = 0;
+
+            /* if (i + mmin)*D + j
+               is prime, mark 1. Can be possibly prime
+               only if gcd(j, D) = 1 */
+
+            if (ecm_inf->GCD_table[j] == 1)
+            {
+                prod = (i + mmin)*P + j;
+                if (n_is_prime(prod))
+                    ecm_inf->prime_table[i][j] = 1;
+
+                prod = (i + mmin)*P - j;
+                if (n_is_prime(prod))
+                    ecm_inf->prime_table[i][j] = 1;
+            }
+        }
+    }
+
+    /****************************** TRY "CURVES" *****************************/
+
+    for (j = 0; j < curves; j++)
+    {
+        fmpz_randm(sig, state, nm8);
+        fmpz_add_ui(sig, sig, 7);
+
+
+        /************************ SELECT CURVE ************************/
+
+        if (fmpz_factor_ecm_select_curve(f, sig, n, ecm_inf)) 
+        {
+            /* Found factor while selecting curve,
+               very very lucky :) */
+            ret = 1;
+            goto cleanup;
+        }        
+
+        /************************** STAGE I ***************************/
+
+        if (fmpz_factor_ecm_stage_I(f, prime_array, num, B1, n, ecm_inf))
+        {
+            /* Found factor after stage I */
+            ret = 1;
+            goto cleanup;
+        }
+
+        /************************** STAGE II ***************************/
+
+        if(fmpz_factor_ecm_stage_II_one(f, B1, B2, P, n, ecm_inf))
+        {
+            /* Found factor after stage II */
+            ret = 1;
+            goto cleanup;
+        }        
+    }
+
+    cleanup:
+
+    fmpz_clear(sig);
+    fmpz_clear(nm8);
+    fmpz_clear(a24);
+    fmpz_factor_ecm_clear(ecm_inf);
+
+    return ret;
+}
---
+++ fmpz_factor/ecm_stage_II_two.c
@@ -0,0 +1,204 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2015 Kushagra Singh
+
+******************************************************************************/
+
+#include <gmp.h>
+#include "flint.h"
+#include "fmpz.h"
+
+/* Implementation of the stage I of ECM */
+
+int
+fmpz_factor_ecm_stage_II_two(fmpz_t f, mp_limb_t B1, mp_limb_t B2, mp_limb_t P,
+                             fmpz_t n, ecm_t ecm_inf)
+{
+    fmpz_t g, tim, Qx, Qz, Rx, Rz, Qdx, Qdz, a, b;
+    mp_limb_t mmin, mmax, maxj, mdiff, prod, Psq;
+    int i, j, ret;
+    fmpz *arrx, *arrz;
+
+    fmpz_init(Qx);
+    fmpz_init(Qz);
+    fmpz_init(Qdx);
+    fmpz_init(Qdz);
+    fmpz_init(Rx);
+    fmpz_init(Rz);
+    fmpz_init(a);
+    fmpz_init(b);
+    fmpz_init_set_ui(g, 1);
+
+    Psq = P*P;
+    mmin = (B1) / (Psq);
+    mmax = (B2) / (Psq);
+    mdiff = mmax - mmin + 1;
+
+    ret = 0;
+    j = 0;
+
+    fmpz_t jx[Psq], jz[Psq];
+
+    fmpz_init(jx[2]);
+    fmpz_init(jz[2]);
+
+    for (i = 1; i < P; i += 2)
+    {
+        fmpz_init(jx[i]);
+        fmpz_init(jz[i]);
+    }
+
+    /* For each odd j (j < P) , compute j * Q0 [x0 :: z0]  */
+
+    /* We are adding 2Q0 every time. Need to calculate all odd j's 
+       as (j - 2)Q0 is required for (j + 2)Q0 */
+
+    /* arr[1] = Q0 */
+    fmpz_set(jx[1], ecm_inf->x);  
+    fmpz_set(jz[1], ecm_inf->z);
+
+    /* arr[2] = 2Q0 */
+    fmpz_factor_ecm_double(jx[2], jz[2], jx[1], jz[1], n, ecm_inf);
+
+    /* arr[3] = 3Q0 */
+    fmpz_factor_ecm_add(jx[3], jz[3], jx[2], jz[2], jx[1], jz[1], 
+                        jx[1], jz[1], n, ecm_inf);
+
+    for (j = 5; j < P; j += 2)
+    {
+        /* jQ0 = (j - 2)Q0 + 2Q0 
+           Differnce is (j - 4)Q0 */
+
+        fmpz_factor_ecm_add(jx[j], jz[j], jx[j - 2], jz[j - 2], 
+                            jx[2], jz[2], jx[j - 4], jz[j - 4], 
+                            n, ecm_inf);     
+    }
+
+    /* arr[P] = double(arr[P/2]) */
+    /* P is always even,hence P/2 will always exist in array (is odd)*/
+    
+    fmpz_init(jx[P]);
+    fmpz_init(jz[P]);
+    fmpz_factor_ecm_double(jx[P], jz[P], jx[P/2], jz[P/2], n, ecm_inf);
+
+    /* computing P + i (i < P, gcd(i, P) = 1) from P, i, (P - i) */
+
+    for (i = P + 1; i < 2*P; i += 2)
+    {
+        int val = i - P;
+        if (ecm_inf->GCD_table[val] == 1)
+        {
+            fmpz_init(jx[i]);
+            fmpz_init(jz[i]);
+            fmpz_factor_ecm_add(jx[i], jz[i], jx[P], jz[P], 
+                                jx[val], jz[val], jx[P - val], jz[P - val],
+                                n, ecm_inf);
+        }
+    }
+
+    /* computing all P + i, 2P + i, ... , (P - 1)P + i 
+       s.t. (i < P) & gcd(i, P) = 1 */
+
+    /* computing nP + i (n > 1) from (n - 1)P, P and (n - 2)P */
+
+    for (i = 1; i < P; i += 2)
+    {
+        if (ecm_inf->GCD_table[i] == 1)
+        {
+            for (j = 2; j < P; j += 1)
+            {
+                fmpz_init(jx[j*P + i]);
+                fmpz_init(jz[j*P + i]);            
+                fmpz_factor_ecm_add(jx[j*P + i], jz[j*P + i], jx[(j-1)*P + i],
+                                    jz[(j-1)*P + i], jx[P], jz[P], 
+                                    jx[(j-2)*P + i], jz[(j-2)*P + i], n, 
+                                    ecm_inf);
+            }   
+        }
+    }
+
+    /* Q = P^2 * Q0 , point to be added (giant step difference) */
+
+    fmpz_set_ui(tim, Psq);
+    fmpz_factor_ecm_mul_montgomery_ladder(Qx, Qz, ecm_inf->x, ecm_inf->z, tim,
+                                          n, ecm_inf);
+    
+    /* R = mmin * Q , initial point (giant step) */
+
+    fmpz_set_ui(tim, mmin);
+    fmpz_factor_ecm_mul_montgomery_ladder(Rx, Rz, Qx, Qz, tim, n, ecm_inf);
+
+    /* Qd = (mmin - 1) * Q , difference between current gaint step 
+       and giant step difference. 
+       Difference between point and difference, *inception moment* */
+
+    fmpz_set_ui(tim, mmin - 1);
+    fmpz_factor_ecm_mul_montgomery_ladder(Qdx, Qdz, Qx, Qz, tim, n, ecm_inf);
+           
+    /* checking whether prime formed by (i*Q + j) is identity */
+    for (i = mmin; i <= mmax; i += 1)
+    {
+        for (j = 1; j <= Psq; j+=2)
+        {
+            if (ecm_inf->prime_table[i - mmin][j] == 1)
+            {
+                fmpz_mul(a, Rx, jz[j]);
+                fmpz_mod(a, a, n);
+
+                fmpz_mul(b, Rz, jx[j]);
+                fmpz_mod(b, b, n);
+
+                fmpz_sub(a, a, b);
+                fmpz_mul(g, g, a);
+                fmpz_mod(g, g, n);
+            }
+        }
+
+        fmpz_set(a, Rx);
+        fmpz_set(b, Rz);
+
+        /* R = R + Q    
+           difference is stored in Qd, initially (mmin - 1)Q */
+
+        fmpz_factor_ecm_add(Rx, Rz, Rx, Rz, Qx, Qz, Qdx, Qdz, n, ecm_inf);
+
+        fmpz_set(Qdx, a);
+        fmpz_set(Qdz, b);
+    }
+
+    fmpz_gcd(f, g, n);
+
+    if (!fmpz_is_one(f) && fmpz_cmp(f, n))
+        ret = 1;
+
+    fmpz_clear(Qx);
+    fmpz_clear(Qz);
+    fmpz_clear(Qdx);
+    fmpz_clear(Qdz);
+    fmpz_clear(Rx);
+    fmpz_clear(Rz);
+    fmpz_clear(a);
+    fmpz_clear(b);
+    fmpz_clear(g);
+
+    return ret;
+}
