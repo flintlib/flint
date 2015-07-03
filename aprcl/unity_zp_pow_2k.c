49151a76485fb5a5886eae6db6b59b0edc91dbc4
Vladimir Glazachev <glazachev.vladimir@gmail.com>
2015-07-03 03:08:51 +0300

    add in-place digit computation for 2^k-ary exponentiation

--- aprcl/unity_zp_pow_2k.c
+++ aprcl/unity_zp_pow_2k.c
@@ -53,42 +53,30 @@ void
 unity_zp_pow_2k_fmpz(unity_zp f, const unity_zp g, const fmpz_t pow)
 {
     ulong j, k, pow2k;
-    ulong *digits;
     slong i, e;
-    fmpz_t power;
-    unity_zp g_sqr, temp;
+    fmpz_t digit;
+    unity_zp temp;
     unity_zp *g_powers;
 
-    fmpz_init_set(power, pow);
-    unity_zp_init(g_sqr, f->p, f->exp, f->n);
+    fmpz_init(digit);
     unity_zp_init(temp, f->p, f->exp, f->n);
 
     /* g_sqr = g * g */
-    unity_zp_sqr(g_sqr, g);
+    unity_zp_sqr(temp, g);
 
     /* selects optimal k value for n */
     k = _unity_zp_pow_2k_find_k(pow);
     /* selects e such that 2^(ek) < n < 2^((e + 1) * k) */
     e = (fmpz_bits(pow) - 1) / k;
     /* computes 2^k */
-    pow2k = n_pow(2, k);
-
-    /* 
-        digits[i] = i-th digit of n in 2^k-ary base;
-        least signifant digit have number 0
-    */
-    digits = (ulong*) flint_malloc(sizeof(ulong) * (e + 1));
-    for (i = 0; i <= e; i++)
-    {
-        digits[i] = fmpz_tdiv_ui(power, pow2k);
-        fmpz_tdiv_q_ui(power, power, pow2k);
-    }
+    pow2k = 1 << k;
 
     /* 
         g_powers store odd powers of g up to 2^k - 1;
         g_powers[(i + 1) / 2] = g^i
     */
-    g_powers = (unity_zp*) flint_malloc(sizeof(unity_zp) * (n_pow(2, k - 1) + 1));
+    pow2k = 1 << (k - 1);
+    g_powers = (unity_zp*) flint_malloc(sizeof(unity_zp) * (pow2k + 1));
 
     /* sets g_powers[0] = 1 */
     unity_zp_init(g_powers[0], f->p, f->exp, f->n);
@@ -99,17 +87,24 @@ unity_zp_pow_2k_fmpz(unity_zp f, const unity_zp g, const fmpz_t pow)
     unity_zp_copy(g_powers[1], g);
 
     /* sets g_powers[i] = g^2 * g_powers[i - 1] */
-    for (i = 2; i <= n_pow(2, k - 1); i++)
+    for (i = 2; i <= pow2k; i++)
     {
         unity_zp_init(g_powers[i], f->p, f->exp, f->n);
-        unity_zp_mul(g_powers[i], g_powers[i - 1], g_sqr);
+        unity_zp_mul(g_powers[i], g_powers[i - 1], temp);
     }
 
     /* for all digits[i] */
     for (i = e; i >= 0; i--)
     {
-        /* if digits[i] == 0 set f = f^(2^k) */
-        if (digits[i] == 0)
+        /* 
+            digit contains i-th digit of pow in k-ary base;
+            k <= 11 so digit < 2^11 and fit into ulong
+        */
+        fmpz_fdiv_q_2exp(digit, pow, i * k);
+        fmpz_fdiv_r_2exp(digit, digit, k);
+
+        /* if digit == 0 set f = f^(2^k) */
+        if (*digit == 0)
         {
             for (j = 0; j < k; j++)
             {
@@ -122,9 +117,9 @@ unity_zp_pow_2k_fmpz(unity_zp f, const unity_zp g, const fmpz_t pow)
         {
             ulong t, b;
 
-            /* digits[i] = 2^t * b and b is odd */
-            t = p_power_in_q(digits[i], 2);
-            b = digits[i] / n_pow(2, t);
+            /* digit = 2^t * b and b is odd */
+            t = p_power_in_q(*digit, 2);
+            b = *digit / (1 << t);
 
             if (i == e) 
             {
@@ -154,13 +149,11 @@ unity_zp_pow_2k_fmpz(unity_zp f, const unity_zp g, const fmpz_t pow)
 
     }
 
-    for (i = 0; i <= n_pow(2, k - 1); i++)
+    for (i = 0; i <= pow2k; i++)
         unity_zp_clear(g_powers[i]);
     flint_free(g_powers);
-    flint_free(digits);
 
-    fmpz_clear(power);
-    unity_zp_clear(g_sqr);
+    fmpz_clear(digit);
     unity_zp_clear(temp);
 }
 
