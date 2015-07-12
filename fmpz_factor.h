6c8c46615e4be0ea94c613e931730361e22a7bae
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-07-12 13:46:19 +0530

    Updated fmpz_factor header file.

--- fmpz_factor.h
+++ fmpz_factor.h
@@ -110,58 +110,58 @@ FLINT_DLL void fmpz_factor_divisor_sigma(fmpz_t res, const fmpz_factor_t fac, ul
 
 typedef struct ecm_s {
 
-    fmpz_t t, u, v, w;      /* temp variables */
-    fmpz_t x, z;            /* the coordinates */
-    fmpz_t a24;             /* value (a + 2)/4 */
+  mp_ptr t, u, v, w;  /* temp variables */
+  mp_ptr x, z;    /* the coordinates */
+  mp_ptr a24;     /* value (a + 2)/4 */
+  mp_ptr ninv;    /* invere of n */
+  mp_ptr one;     /* one shifted */
 
-    unsigned char *GCD_table;     /* checks whether baby step int is
-                               coprime to Primorial or not */
+    unsigned char *GCD_table; /* checks whether baby step int is
+                           coprime to Primorial or not */
 
     unsigned char **prime_table;
+
+    mp_limb_t n_size;
+    mp_limb_t normbits;
 
 } ecm_s;
 
 typedef ecm_s ecm_t[1];
 
-FLINT_DLL void fmpz_factor_ecm_init(ecm_t ecm_inf);
+FLINT_DLL void fmpz_factor_ecm_init(ecm_t ecm_inf, mp_limb_t sz);
 
 FLINT_DLL void fmpz_factor_ecm_clear(ecm_t ecm_inf);
 
-FLINT_DLL void fmpz_factor_ecm_double(fmpz_t x, fmpz_t z, fmpz_t x0, fmpz_t z0,
-                                      fmpz_t n, ecm_t ecm_inf);
+FLINT_DLL void fmpz_factor_ecm_addmod(mp_ptr a, mp_ptr b, mp_ptr c, mp_ptr n,
+                                     mp_limb_t n_size);
 
-FLINT_DLL void fmpz_factor_ecm_add(fmpz_t x, fmpz_t z, fmpz_t x1, fmpz_t z1,
-                                   fmpz_t x2, fmpz_t z2, fmpz_t x0, fmpz_t z0,
-                                   fmpz_t n, ecm_t ecm_inf);
+FLINT_DLL void fmpz_factor_ecm_submod(mp_ptr x, mp_ptr a, mp_ptr b, mp_ptr n,
+                                     mp_limb_t n_size);
 
-FLINT_DLL void fmpz_factor_ecm_mul_montgomery_ladder(fmpz_t x, fmpz_t z, fmpz_t x0,
-                                                     fmpz_t z0, fmpz_t k, fmpz_t n,
+FLINT_DLL void fmpz_factor_ecm_double(mp_ptr x, mp_ptr z, mp_ptr x0, mp_ptr z0,
+                                      mp_ptr n, ecm_t ecm_inf);
+
+FLINT_DLL void fmpz_factor_ecm_add(mp_ptr x, mp_ptr z, mp_ptr x1, mp_ptr z1,
+                                   mp_ptr x2, mp_ptr z2, mp_ptr x0, mp_ptr z0,
+                                   mp_ptr n, ecm_t ecm_inf);
+
+FLINT_DLL void fmpz_factor_ecm_mul_montgomery_ladder(mp_ptr x, mp_ptr z,
+                                                     mp_ptr x0, mp_ptr z0,
+                                                     mp_limb_t k, mp_ptr n,
                                                      ecm_t ecm_inf);
 
-FLINT_DLL int fmpz_factor_ecm_select_curve(fmpz_t f, fmpz_t sig, fmpz_t n,
+FLINT_DLL int fmpz_factor_ecm_select_curve(mp_ptr f, mp_ptr sig, mp_ptr n,
                                            ecm_t ecm_inf);
 
-FLINT_DLL int fmpz_factor_ecm_stage_I(fmpz_t f, const mp_limb_t *prime_array,
-                                      mp_limb_t num, mp_limb_t B1, fmpz_t n, 
+FLINT_DLL int fmpz_factor_ecm_stage_I(mp_ptr f, const mp_limb_t *prime_array,
+                                      mp_limb_t num, mp_limb_t B1, mp_ptr n, 
                                       ecm_t ecm_inf);
 
-FLINT_DLL int fmpz_factor_ecm_stage_II_one(fmpz_t f, mp_limb_t B1, mp_limb_t B2,
-                                           mp_limb_t P, fmpz_t n, ecm_t ecm_inf);
+FLINT_DLL int fmpz_factor_ecm_stage_II(mp_ptr f, mp_limb_t B1, mp_limb_t B2,
+                                       mp_limb_t P, mp_ptr n, ecm_t ecm_inf);
 
-FLINT_DLL int fmpz_factor_ecm_stage_II_two(fmpz_t f, mp_limb_t B1, mp_limb_t B2,
-                                           mp_limb_t P, fmpz_t n, ecm_t ecm_inf);
-
-FLINT_DLL int fmpz_factor_ecm_stage_II_one_FFT(fmpz_t f, mp_limb_t B1, mp_limb_t B2,
-                                               mp_limb_t P, fmpz_t n, ecm_t ecm_inf);
-
-FLINT_DLL int fmpz_factor_ecm_one(fmpz_t f, mp_limb_t curves, mp_limb_t B1,
-                                  mp_limb_t B2, flint_rand_t state, fmpz_t n);
-
-FLINT_DLL int fmpz_factor_ecm_two(fmpz_t f, mp_limb_t curves, mp_limb_t B1,
-                                  mp_limb_t B2, flint_rand_t state, fmpz_t n);
-
-FLINT_DLL int fmpz_factor_ecm_stage_one_II(fmpz_t f, mp_limb_t B1, mp_limb_t B2, 
-										   mp_limb_t P, fmpz_t n, ecm_t ecm_inf);
+FLINT_DLL int fmpz_factor_ecm(fmpz_t f, mp_limb_t curves, mp_limb_t B1,
+                              mp_limb_t B2, flint_rand_t state, fmpz_t n_in);
 
 #ifdef __cplusplus
 }
