4155dac437e4b2632220cb50449828ad7b40acd1
William Hart <goodwillhart@googlemail.com>
2015-08-06 22:08:46 +0000

    Tried to fix some fits_si issues. Added some missing static __inline__'s.

--- fmpz/set_mpf.c
+++ fmpz/set_mpf.c
@@ -32,7 +32,7 @@
 void
 fmpz_set_mpf(fmpz_t f, const mpf_t x)
 {
-    if (flint_mpf_fits_slong_p(x))
+    if (flint_mpf_fits_si_p(x))
     {
         slong cx = flint_mpf_get_si(x);
         fmpz_set_si(f, cx);
