1c95c633cecf9e93f6c1e9f8d7caca9df54db615
Daniel S. Roche <roche@usna.edu>
2015-11-14 15:10:55 -0500

    Added fmpz_randprime and fmpz_nextprime

---
+++ fmpz/nextprime.c
@@ -0,0 +1,63 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Authored 2015 by Daniel S. Roche; US Government work in the public domain. 
+
+******************************************************************************/
+
+#include <gmp.h>
+#include "flint.h"
+#include "fmpz.h"
+#include "ulong_extras.h"
+
+int fmpz_nextprime(fmpz_t res, const fmpz_t n, int proved)
+{
+    if (fmpz_sgn(n) <= 0)
+    {
+        fmpz_set_ui(res, UWORD(2));
+        return 1;
+    }
+    else if (COEFF_IS_MPZ(*n))
+    {
+        /* n is big */
+        __mpz_struct *res_mpz = _fmpz_promote(res);
+        mpz_nextprime(res_mpz, COEFF_TO_PTR(*n));
+    }
+    else if (FLINT_BIT_COUNT(*n) >= FLINT_BITS - 2)
+    {
+        /* n is small, but res might not be */
+        mpz_t temp_n;
+        __mpz_struct *res_mpz = _fmpz_promote(res);
+        mpz_init_set_ui(temp_n, *n);
+        mpz_nextprime(res_mpz, temp_n);
+        _fmpz_demote_val(res);
+    }
+    else
+    {
+        /* n and res will both be small */
+        _fmpz_demote(res);
+        *res = n_nextprime(*n, proved);
+        return 1;
+    }
+    /* try proving primality, if necessary */
+    if (proved) return fmpz_is_prime(res);
+    else return 1;
+}
---
+++ fmpz/randprime.c
@@ -0,0 +1,54 @@
+/*=============================================================================
+
+    This file is part of FLINT.
+
+    FLINT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    FLINT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FLINT; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Authored 2015 by Daniel S. Roche; US Government work in the public domain. 
+
+******************************************************************************/
+
+#include <gmp.h>
+#include "flint.h"
+#include "fmpz.h"
+
+int fmpz_randprime(fmpz_t f, flint_rand_t state, mp_bitcnt_t bits, int proved)
+{
+    if (bits <= FLINT_BITS-3)
+    {
+        _fmpz_demote(f);
+        *f = n_randprime(state, bits, proved);
+        return 1;
+    }
+    else
+    {
+        /* Here I would like to just call
+         * fmpz_randbits(f, state, bits);
+         * but it has different semantics from n_randbits,
+         * and in particular may return integers with fewer bits.
+         */
+        __mpz_struct *mpz_ptr = _fmpz_promote(f);
+        _flint_rand_init_gmp(state);
+
+        mpz_urandomb(mpz_ptr, state->gmp_state, bits-1);
+        mpz_setbit(mpz_ptr, bits-1);
+        _fmpz_demote_val(f);
+
+        return fmpz_nextprime(f, f, proved);
+    }
+}
