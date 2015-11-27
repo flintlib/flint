907baa99b3a498c77e47b03ecf994478e5f65dda
William Hart <goodwillhart@googlemail.com>
2015-11-27 13:40:59 +0000

    mp_limb_t -> ulong.

--- ulong_extras.h
+++ ulong_extras.h
@@ -44,7 +44,7 @@
 
 typedef struct pair_s
 {
-    mp_limb_t x, y;
+    ulong x, y;
 } n_pair_t;
 
 #define FLINT_MAX_FACTORS_IN_LIMB 15
@@ -52,7 +52,7 @@ typedef struct pair_s
 typedef struct {
    int num;
    int exp[FLINT_MAX_FACTORS_IN_LIMB];
-   mp_limb_t p[FLINT_MAX_FACTORS_IN_LIMB];
+   ulong p[FLINT_MAX_FACTORS_IN_LIMB];
 } n_factor_t;
 
 #define FLINT_ODDPRIME_SMALL_CUTOFF 4096
@@ -87,8 +87,8 @@ typedef struct
     slong small_num;
     unsigned int * small_primes;
 
-    mp_limb_t sieve_a;
-    mp_limb_t sieve_b;
+    ulong sieve_a;
+    ulong sieve_b;
     slong sieve_i;
     slong sieve_num;
     char * sieve;
@@ -101,13 +101,13 @@ FLINT_DLL void n_primes_init(n_primes_t iter);
 
 FLINT_DLL void n_primes_clear(n_primes_t iter);
 
-FLINT_DLL void n_primes_extend_small(n_primes_t iter, mp_limb_t bound);
+FLINT_DLL void n_primes_extend_small(n_primes_t iter, ulong bound);
 
-FLINT_DLL void n_primes_sieve_range(n_primes_t iter, mp_limb_t a, mp_limb_t b);
+FLINT_DLL void n_primes_sieve_range(n_primes_t iter, ulong a, ulong b);
 
-FLINT_DLL void n_primes_jump_after(n_primes_t iter, mp_limb_t n);
+FLINT_DLL void n_primes_jump_after(n_primes_t iter, ulong n);
 
-ULONG_EXTRAS_INLINE mp_limb_t
+ULONG_EXTRAS_INLINE ulong
 n_primes_next(n_primes_t iter)
 {
     if (iter->small_i < iter->small_num)
@@ -128,7 +128,7 @@ n_primes_next(n_primes_t iter)
 
 extern const unsigned int flint_primes_small[];
 
-extern FLINT_TLS_PREFIX mp_limb_t * _flint_primes[FLINT_BITS];
+extern FLINT_TLS_PREFIX ulong * _flint_primes[FLINT_BITS];
 extern FLINT_TLS_PREFIX double * _flint_prime_inverses[FLINT_BITS];
 extern FLINT_TLS_PREFIX int _flint_primes_used;
 
@@ -136,33 +136,33 @@ FLINT_DLL void n_compute_primes(ulong num_primes);
 
 FLINT_DLL void n_cleanup_primes(void);
 
-FLINT_DLL const mp_limb_t * n_primes_arr_readonly(ulong n);
+FLINT_DLL const ulong * n_primes_arr_readonly(ulong n);
 FLINT_DLL const double * n_prime_inverses_arr_readonly(ulong n);
 
-FLINT_DLL mp_limb_t n_randlimb(flint_rand_t state);
+FLINT_DLL ulong n_randlimb(flint_rand_t state);
 
-FLINT_DLL mp_limb_t n_randint(flint_rand_t state, mp_limb_t limit);
+FLINT_DLL ulong n_randint(flint_rand_t state, ulong limit);
 
-FLINT_DLL mp_limb_t n_randbits(flint_rand_t state, unsigned int bits);
+FLINT_DLL ulong n_randbits(flint_rand_t state, unsigned int bits);
 
-FLINT_DLL mp_limb_t n_randtest_bits(flint_rand_t state, int bits);
+FLINT_DLL ulong n_randtest_bits(flint_rand_t state, int bits);
 
-FLINT_DLL mp_limb_t n_randtest(flint_rand_t state);
+FLINT_DLL ulong n_randtest(flint_rand_t state);
 
-FLINT_DLL mp_limb_t n_randtest_not_zero(flint_rand_t state);
+FLINT_DLL ulong n_randtest_not_zero(flint_rand_t state);
 
-FLINT_DLL mp_limb_t n_randprime(flint_rand_t state, ulong bits, int proved);
+FLINT_DLL ulong n_randprime(flint_rand_t state, ulong bits, int proved);
 
-FLINT_DLL mp_limb_t n_randtest_prime(flint_rand_t state, int proved);
+FLINT_DLL ulong n_randtest_prime(flint_rand_t state, int proved);
 
-FLINT_DLL mp_limb_t n_pow(mp_limb_t n, ulong exp);
+FLINT_DLL ulong n_pow(ulong n, ulong exp);
 
-FLINT_DLL mp_limb_t n_flog(mp_limb_t n, mp_limb_t b);
+FLINT_DLL ulong n_flog(ulong n, ulong b);
 
-FLINT_DLL mp_limb_t n_clog(mp_limb_t n, mp_limb_t b);
+FLINT_DLL ulong n_clog(ulong n, ulong b);
 
 ULONG_EXTRAS_INLINE 
-double n_precompute_inverse(mp_limb_t n)
+double n_precompute_inverse(ulong n)
 {
    return (double) 1 / (double) n;
 }
@@ -178,160 +178,160 @@ ulong n_preinvert_limb(ulong n)
    return ninv;
 }
 
-FLINT_DLL mp_limb_t n_mod_precomp(mp_limb_t a, mp_limb_t n, double ninv);
+FLINT_DLL ulong n_mod_precomp(ulong a, ulong n, double ninv);
 
-FLINT_DLL mp_limb_t n_mod2_precomp(mp_limb_t a, mp_limb_t n, double ninv);
+FLINT_DLL ulong n_mod2_precomp(ulong a, ulong n, double ninv);
 
-FLINT_DLL mp_limb_t n_divrem2_precomp(mp_limb_t * q, mp_limb_t a, 
-                                           mp_limb_t n, double npre);
+FLINT_DLL ulong n_divrem2_precomp(ulong * q, ulong a, 
+                                           ulong n, double npre);
 
-FLINT_DLL mp_limb_t n_mod2_preinv(mp_limb_t a, mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_mod2_preinv(ulong a, ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_ll_mod_preinv(mp_limb_t a_hi, mp_limb_t a_lo, 
-                                        mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_ll_mod_preinv(ulong a_hi, ulong a_lo, 
+                                        ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_lll_mod_preinv(mp_limb_t a_hi, mp_limb_t a_mi, 
-                        mp_limb_t a_lo, mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_lll_mod_preinv(ulong a_hi, ulong a_mi, 
+                        ulong a_lo, ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_mulmod_precomp(mp_limb_t a, mp_limb_t b, 
-                                           mp_limb_t n, double ninv);
+FLINT_DLL ulong n_mulmod_precomp(ulong a, ulong b, 
+                                           ulong n, double ninv);
 
-FLINT_DLL mp_limb_t n_mulmod2_preinv(mp_limb_t a, mp_limb_t b, 
-                                        mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_mulmod2_preinv(ulong a, ulong b, 
+                                        ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_mulmod_preinv(mp_limb_t a, mp_limb_t b, 
-                            mp_limb_t n, mp_limb_t ninv, ulong norm);
+FLINT_DLL ulong n_mulmod_preinv(ulong a, ulong b, 
+                            ulong n, ulong ninv, ulong norm);
 
-FLINT_DLL mp_limb_t n_powmod_ui_precomp(mp_limb_t a, mp_limb_t exp, mp_limb_t n, double npre);
+FLINT_DLL ulong n_powmod_ui_precomp(ulong a, ulong exp, ulong n, double npre);
 
-FLINT_DLL mp_limb_t n_powmod_precomp(mp_limb_t a, 
-                     mp_limb_signed_t exp, mp_limb_t n, double npre);
+FLINT_DLL ulong n_powmod_precomp(ulong a, 
+                     mp_limb_signed_t exp, ulong n, double npre);
 
 ULONG_EXTRAS_INLINE
-mp_limb_t n_powmod(mp_limb_t a, mp_limb_signed_t exp, mp_limb_t n)
+ulong n_powmod(ulong a, mp_limb_signed_t exp, ulong n)
 {
    double npre = n_precompute_inverse(n);
 
    return n_powmod_precomp(a, exp, n, npre);
 }
 
-FLINT_DLL mp_limb_t n_powmod2_preinv(mp_limb_t a, 
-                  mp_limb_signed_t exp, mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_powmod2_preinv(ulong a, 
+                  mp_limb_signed_t exp, ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_powmod2_ui_preinv(mp_limb_t a, mp_limb_t exp,
-                                            mp_limb_t n, mp_limb_t ninv);
+FLINT_DLL ulong n_powmod2_ui_preinv(ulong a, ulong exp,
+                                            ulong n, ulong ninv);
 
-FLINT_DLL mp_limb_t n_powmod_ui_preinv(mp_limb_t a, mp_limb_t exp, mp_limb_t n, 
-                                             mp_limb_t ninv, ulong norm);
+FLINT_DLL ulong n_powmod_ui_preinv(ulong a, ulong exp, ulong n, 
+                                             ulong ninv, ulong norm);
 
 ULONG_EXTRAS_INLINE
-mp_limb_t n_powmod2(mp_limb_t a, mp_limb_signed_t exp, mp_limb_t n)
+ulong n_powmod2(ulong a, mp_limb_signed_t exp, ulong n)
 {
-   mp_limb_t ninv = n_preinvert_limb(n);
+   ulong ninv = n_preinvert_limb(n);
 
    return n_powmod2_preinv(a, exp, n, ninv);
 }
 
 ULONG_EXTRAS_INLINE
-mp_limb_t n_addmod(mp_limb_t x, mp_limb_t y, mp_limb_t n)
+ulong n_addmod(ulong x, ulong y, ulong n)
 {
     return (n - y > x ? x + y : x + y - n);
 }
 
 ULONG_EXTRAS_INLINE
-mp_limb_t n_submod(mp_limb_t x, mp_limb_t y, mp_limb_t n)
+ulong n_submod(ulong x, ulong y, ulong n)
 {
     return (y > x ? x - y + n : x - y);
 }
 
 ULONG_EXTRAS_INLINE
-mp_limb_t n_negmod(mp_limb_t x, mp_limb_t n)
+ulong n_negmod(ulong x, ulong n)
 {
     return n_submod(0, x, n);
 }
 
-FLINT_DLL mp_limb_t n_sqrtmod(mp_limb_t a, mp_limb_t p);
+FLINT_DLL ulong n_sqrtmod(ulong a, ulong p);
 
-FLINT_DLL slong n_sqrtmod_2pow(mp_limb_t ** sqrt, mp_limb_t a, slong exp); 
+FLINT_DLL slong n_sqrtmod_2pow(ulong ** sqrt, ulong a, slong exp); 
 
-FLINT_DLL slong n_sqrtmod_primepow(mp_limb_t ** sqrt, mp_limb_t a, 
-                                              mp_limb_t p, slong exp);
+FLINT_DLL slong n_sqrtmod_primepow(ulong ** sqrt, ulong a, 
+                                              ulong p, slong exp);
 
-FLINT_DLL slong n_sqrtmodn(mp_limb_t ** sqrt, mp_limb_t a, n_factor_t * fac);
+FLINT_DLL slong n_sqrtmodn(ulong ** sqrt, ulong a, n_factor_t * fac);
 
-FLINT_DLL mp_limb_t n_gcd(mp_limb_t x, mp_limb_t y);
+FLINT_DLL ulong n_gcd(ulong x, ulong y);
 
 #define n_gcd_full n_gcd
 
-FLINT_DLL mp_limb_t n_xgcd(mp_limb_t * a, mp_limb_t * b, mp_limb_t x, mp_limb_t y);
+FLINT_DLL ulong n_xgcd(ulong * a, ulong * b, ulong x, ulong y);
 
-FLINT_DLL mp_limb_t n_invmod(mp_limb_t x, mp_limb_t y);
+FLINT_DLL ulong n_invmod(ulong x, ulong y);
 
-FLINT_DLL mp_limb_t n_gcdinv(mp_limb_t * a, mp_limb_t x, mp_limb_t y);
+FLINT_DLL ulong n_gcdinv(ulong * a, ulong x, ulong y);
 
 FLINT_DLL ulong n_revbin(ulong in, ulong bits);
 
-FLINT_DLL int n_jacobi(mp_limb_signed_t x, mp_limb_t y);
+FLINT_DLL int n_jacobi(mp_limb_signed_t x, ulong y);
 
-FLINT_DLL int n_jacobi_unsigned(mp_limb_t x, mp_limb_t y);
+FLINT_DLL int n_jacobi_unsigned(ulong x, ulong y);
 
-FLINT_DLL mp_limb_t n_sqrt(mp_limb_t a);
+FLINT_DLL ulong n_sqrt(ulong a);
 
-FLINT_DLL mp_limb_t n_sqrtrem(mp_limb_t * r, mp_limb_t a);
+FLINT_DLL ulong n_sqrtrem(ulong * r, ulong a);
 
-FLINT_DLL int n_is_square(mp_limb_t x);
+FLINT_DLL int n_is_square(ulong x);
 
 FLINT_DLL double n_cbrt_estimate(double a);
 
-FLINT_DLL mp_limb_t n_cbrt(mp_limb_t a);
+FLINT_DLL ulong n_cbrt(ulong a);
 
-FLINT_DLL mp_limb_t n_cbrt_binary_search(mp_limb_t x);
+FLINT_DLL ulong n_cbrt_binary_search(ulong x);
 
-FLINT_DLL mp_limb_t n_cbrt_newton_iteration(mp_limb_t n);
+FLINT_DLL ulong n_cbrt_newton_iteration(ulong n);
 
-FLINT_DLL mp_limb_t n_cbrt_chebyshev_approx(mp_limb_t n);
+FLINT_DLL ulong n_cbrt_chebyshev_approx(ulong n);
 
-FLINT_DLL mp_limb_t n_cbrtrem(mp_limb_t* remainder, mp_limb_t n);
+FLINT_DLL ulong n_cbrtrem(ulong* remainder, ulong n);
 
-FLINT_DLL int n_is_perfect_power235(mp_limb_t n);
+FLINT_DLL int n_is_perfect_power235(ulong n);
 
-FLINT_DLL int n_is_oddprime_small(mp_limb_t n);
+FLINT_DLL int n_is_oddprime_small(ulong n);
 
-FLINT_DLL int n_is_oddprime_binary(mp_limb_t n);
+FLINT_DLL int n_is_oddprime_binary(ulong n);
 
-FLINT_DLL int n_is_probabprime_fermat(mp_limb_t n, mp_limb_t i);
+FLINT_DLL int n_is_probabprime_fermat(ulong n, ulong i);
 
-FLINT_DLL int n_is_probabprime_fibonacci(mp_limb_t n);
+FLINT_DLL int n_is_probabprime_fibonacci(ulong n);
 
-FLINT_DLL int n_is_probabprime_lucas(mp_limb_t n);
+FLINT_DLL int n_is_probabprime_lucas(ulong n);
 
-FLINT_DLL int n_is_probabprime_BPSW(mp_limb_t n);
+FLINT_DLL int n_is_probabprime_BPSW(ulong n);
 
-FLINT_DLL int n_is_strong_probabprime_precomp(mp_limb_t n, 
-                              double npre, mp_limb_t a, mp_limb_t d);
+FLINT_DLL int n_is_strong_probabprime_precomp(ulong n, 
+                              double npre, ulong a, ulong d);
 
-FLINT_DLL int n_is_strong_probabprime2_preinv(mp_limb_t n, 
-                           mp_limb_t ninv, mp_limb_t a, mp_limb_t d);
+FLINT_DLL int n_is_strong_probabprime2_preinv(ulong n, 
+                           ulong ninv, ulong a, ulong d);
 
-FLINT_DLL int n_is_probabprime(mp_limb_t n);
+FLINT_DLL int n_is_probabprime(ulong n);
 
-FLINT_DLL int n_is_prime_pseudosquare(mp_limb_t n);
+FLINT_DLL int n_is_prime_pseudosquare(ulong n);
 
-FLINT_DLL int n_is_prime_pocklington(mp_limb_t n, ulong iterations);
+FLINT_DLL int n_is_prime_pocklington(ulong n, ulong iterations);
 
-FLINT_DLL int n_is_prime(mp_limb_t n);
+FLINT_DLL int n_is_prime(ulong n);
 
-FLINT_DLL mp_limb_t n_nth_prime(ulong n);
+FLINT_DLL ulong n_nth_prime(ulong n);
 
-FLINT_DLL void n_nth_prime_bounds(mp_limb_t *lo, mp_limb_t *hi, ulong n);
+FLINT_DLL void n_nth_prime_bounds(ulong *lo, ulong *hi, ulong n);
 
-FLINT_DLL ulong n_prime_pi(mp_limb_t n);
+FLINT_DLL ulong n_prime_pi(ulong n);
 
-FLINT_DLL void n_prime_pi_bounds(ulong *lo, ulong *hi, mp_limb_t n);
+FLINT_DLL void n_prime_pi_bounds(ulong *lo, ulong *hi, ulong n);
 
-FLINT_DLL int n_remove(mp_limb_t * n, mp_limb_t p);
+FLINT_DLL int n_remove(ulong * n, ulong p);
 
-FLINT_DLL int n_remove2_precomp(mp_limb_t * n, mp_limb_t p, double ppre);
+FLINT_DLL int n_remove2_precomp(ulong * n, ulong p, double ppre);
 
 ULONG_EXTRAS_INLINE
 void n_factor_init(n_factor_t * factors)
@@ -339,59 +339,59 @@ void n_factor_init(n_factor_t * factors)
     factors->num = UWORD(0);
 }
 
-FLINT_DLL void n_factor_insert(n_factor_t * factors, mp_limb_t p, ulong exp);
+FLINT_DLL void n_factor_insert(n_factor_t * factors, ulong p, ulong exp);
 
-FLINT_DLL mp_limb_t n_factor_trial_range(n_factor_t * factors, 
-                         mp_limb_t n, ulong start, ulong num_primes);
+FLINT_DLL ulong n_factor_trial_range(n_factor_t * factors, 
+                         ulong n, ulong start, ulong num_primes);
 
-FLINT_DLL mp_limb_t n_factor_trial_partial(n_factor_t * factors, mp_limb_t n, 
-                mp_limb_t * prod, ulong num_primes, mp_limb_t limit);
+FLINT_DLL ulong n_factor_trial_partial(n_factor_t * factors, ulong n, 
+                ulong * prod, ulong num_primes, ulong limit);
 
-FLINT_DLL mp_limb_t n_factor_trial(n_factor_t * factors, 
-                                  mp_limb_t n, ulong num_primes);
+FLINT_DLL ulong n_factor_trial(n_factor_t * factors, 
+                                  ulong n, ulong num_primes);
 
-FLINT_DLL mp_limb_t n_factor_partial(n_factor_t * factors, 
-                           mp_limb_t n, mp_limb_t limit, int proved);
+FLINT_DLL ulong n_factor_partial(n_factor_t * factors, 
+                           ulong n, ulong limit, int proved);
 
-FLINT_DLL mp_limb_t n_factor_power235(ulong *exp, mp_limb_t n);
+FLINT_DLL ulong n_factor_power235(ulong *exp, ulong n);
 
-FLINT_DLL mp_limb_t n_factor_one_line(mp_limb_t n, ulong iters);
+FLINT_DLL ulong n_factor_one_line(ulong n, ulong iters);
 
-FLINT_DLL mp_limb_t n_factor_lehman(mp_limb_t n);
+FLINT_DLL ulong n_factor_lehman(ulong n);
 
-FLINT_DLL mp_limb_t n_factor_SQUFOF(mp_limb_t n, ulong iters);
+FLINT_DLL ulong n_factor_SQUFOF(ulong n, ulong iters);
 
-FLINT_DLL void n_factor(n_factor_t * factors, mp_limb_t n, int proved);
+FLINT_DLL void n_factor(n_factor_t * factors, ulong n, int proved);
 
-FLINT_DLL mp_limb_t n_factor_pp1(mp_limb_t n, ulong B1, ulong c);
+FLINT_DLL ulong n_factor_pp1(ulong n, ulong B1, ulong c);
 
-FLINT_DLL int n_is_squarefree(mp_limb_t n);
+FLINT_DLL int n_is_squarefree(ulong n);
 
-FLINT_DLL int n_moebius_mu(mp_limb_t n);
+FLINT_DLL int n_moebius_mu(ulong n);
 
 FLINT_DLL void n_moebius_mu_vec(int * mu, ulong len);
 
-FLINT_DLL mp_limb_t n_euler_phi(mp_limb_t n);
+FLINT_DLL ulong n_euler_phi(ulong n);
 
-FLINT_DLL int n_sizeinbase(mp_limb_t n, int base);
+FLINT_DLL int n_sizeinbase(ulong n, int base);
 
-FLINT_DLL mp_limb_t n_nextprime(mp_limb_t n, int proved);
+FLINT_DLL ulong n_nextprime(ulong n, int proved);
 
-FLINT_DLL mp_limb_t n_factorial_mod2_preinv(ulong n, mp_limb_t p, mp_limb_t pinv);
+FLINT_DLL ulong n_factorial_mod2_preinv(ulong n, ulong p, ulong pinv);
 
-FLINT_DLL mp_limb_t n_factorial_fast_mod2_preinv(ulong n, mp_limb_t p, mp_limb_t pinv);
+FLINT_DLL ulong n_factorial_fast_mod2_preinv(ulong n, ulong p, ulong pinv);
 
-FLINT_DLL mp_limb_t n_primitive_root_prime_prefactor(mp_limb_t p, n_factor_t * factors);
+FLINT_DLL ulong n_primitive_root_prime_prefactor(ulong p, n_factor_t * factors);
 
-FLINT_DLL mp_limb_t n_primitive_root_prime(mp_limb_t p);
+FLINT_DLL ulong n_primitive_root_prime(ulong p);
 
-FLINT_DLL mp_limb_t n_discrete_log_bsgs(mp_limb_t b, mp_limb_t a, mp_limb_t n);
+FLINT_DLL ulong n_discrete_log_bsgs(ulong b, ulong a, ulong n);
 
-FLINT_DLL mp_limb_t n_root_estimate(double a, int n);
+FLINT_DLL ulong n_root_estimate(double a, int n);
 
-FLINT_DLL mp_limb_t n_rootrem(mp_limb_t* remainder, mp_limb_t n, mp_limb_t root);
+FLINT_DLL ulong n_rootrem(ulong* remainder, ulong n, ulong root);
 
-FLINT_DLL mp_limb_t n_root(mp_limb_t n, mp_limb_t root);
+FLINT_DLL ulong n_root(ulong n, ulong root);
 
 #ifdef __cplusplus
 }
