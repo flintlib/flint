9c46b47db1b4cac5069f8f6d5f197ad73a05b914
William Hart <goodwillhart@googlemail.com>
2015-11-28 00:19:29 +0000

    Clean up n_ll_mod_preinv, add new test case and remove Moller-Granlund implementation bug.

--- ulong_extras/ll_mod_preinv.c
+++ ulong_extras/ll_mod_preinv.c
@@ -19,7 +19,7 @@
 =============================================================================*/
 /******************************************************************************
 
-    Copyright (C) 2009, 2010 William Hart
+    Copyright (C) 2009, 2010, 2015 William Hart
 
 ******************************************************************************/
 
@@ -27,77 +27,57 @@
 #include "flint.h"
 #include "ulong_extras.h"
 
-/*
-
-mp_limb_t
-n_ll_mod_preinv(mp_limb_t a_hi, mp_limb_t a_lo, mp_limb_t n, mp_limb_t ninv)
-{
-    mp_limb_t q, r, norm;
-
-    if (a_hi > n) a_hi = n_mod2_preinv(a_hi, n, ninv);
-
-    count_leading_zeros(norm, n);
-
-    udiv_qrnnd_preinv(q, r, (a_hi<<norm) + r_shift(a_lo, FLINT_BITS-norm), a_lo<<norm, n<<norm, ninv);
-
-    return (r>>norm);
-}
-
-*/
-
 /* 
-   New method of Moller and Granlund
-   see paper Improved Division by Invariant Integers (Torbjorn Granlund
-   and Niels Moller) (preprint): (pp. 4)
-   http://www.lysator.liu.se/~nisse/archive/draft-division-paper.pdf 
+   Method of Niels Moller and Torbjorn Granlund, see "Improved Division by
+   Invariant Integers: (Algorithm 4)
+   https://gmplib.org/~tege/division-paper.pdf 
 */
-mp_limb_t
-n_ll_mod_preinv(mp_limb_t a_hi, mp_limb_t a_lo, mp_limb_t n, mp_limb_t ninv)
+
+ulong n_ll_mod_preinv(ulong a_hi, ulong a_lo, ulong n, ulong ninv)
 {
-    mp_limb_t q0, q1, r, norm;
+   ulong q0, q1, r, norm;
 
-    count_leading_zeros(norm, n);
+   FLINT_ASSERT(n != 0);
 
-    /* reduce u1 first i.e. u1 = n_mod2_preinv(u1, n, ninv) */
-    if (a_hi >= n)
-    {
-        const mp_limb_t u1 = r_shift(a_hi, FLINT_BITS - norm);
-        const mp_limb_t u0 = (a_hi << norm);
+   count_leading_zeros(norm, n);
 
-        n <<= norm;
+   /* reduce a_hi modulo n */
+   if (a_hi >= n)
+   {
+      const ulong u1 = r_shift(a_hi, FLINT_BITS - norm);
+      const ulong u0 = (a_hi << norm);
 
-        umul_ppmm(q1, q0, ninv, u1);
-        add_ssaaaa(q1, q0, q1, q0, u1, u0);
+      n <<= norm;
 
-        r = (u0 - (q1 + 1) * n);
+      umul_ppmm(q1, q0, ninv, u1);
+      add_ssaaaa(q1, q0, q1, q0, u1, u0);
 
-        if (r >= q0)
-            r += n;
+      a_hi = (u0 - (q1 + 1)*n);
 
-        if (r < n)
-            a_hi = (r >> norm);
-        else
-            a_hi = ((r - n) >> norm);
-    }
-    else
-        n <<= norm;
+      if (a_hi > q0)
+         a_hi += n;
 
-    /* now reduce the rest of the way */
-    {
-        const mp_limb_t u1 = (a_hi << norm) + r_shift(a_lo, FLINT_BITS - norm);
-        const mp_limb_t u0 = (a_lo << norm);
+      if (a_hi >= n) 
+         a_hi -= n;
+   } else
+   {
+      n <<= norm;
+      a_hi <<= norm;
+   }
 
-        umul_ppmm(q1, q0, ninv, u1);
-        add_ssaaaa(q1, q0, q1, q0, u1, u0);
+   /* now reduce the rest of the way */
+   {
+      const ulong u1 = a_hi + r_shift(a_lo, FLINT_BITS - norm);
+      const ulong u0 = (a_lo << norm);
 
-        r = (u0 - (q1 + 1) * n);
+      umul_ppmm(q1, q0, ninv, u1);
+      add_ssaaaa(q1, q0, q1, q0, u1, u0);
 
-        if (r >= q0)
-            r += n;
+      r = (u0 - (q1 + 1)*n);
 
-        if (r < n)
-            return (r >> norm);
-        else
-            return ((r - n) >> norm);
-    }
+      if (r > q0)
+         r += n;
+
+      return (r < n) ? (r >> norm) : ((r - n) >> norm);
+   }
 }
--- ulong_extras/test/t-ll_mod_preinv.c
+++ ulong_extras/test/t-ll_mod_preinv.c
@@ -19,33 +19,35 @@
 =============================================================================*/
 /******************************************************************************
 
-    Copyright (C) 2009 William Hart
+    Copyright (C) 2009, 2015 William Hart
 
 ******************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <gmp.h>
 #include "flint.h"
 #include "ulong_extras.h"
+#include "fmpz.h"
 
 int main(void)
 {
    int i, result;
+   fmpz_t n;
    FLINT_TEST_INIT(state);
    
    flint_printf("ll_mod_preinv....");
    fflush(stdout);
 
-   
-
+   /* check (m*d + r1) % d == r1 */
    for (i = 0; i < 100000 * flint_test_multiplier(); i++)
    {
-      mp_limb_t d, dinv, nh, nl, r1, r2, m;
+      ulong d, dinv, nh, nl, r1, r2, m;
 
       d = n_randtest_not_zero(state);
       m = n_randtest(state);
       r1 = n_randtest(state) % d;
+
+      /* <nh, nl> = m*d + r1 < (m + 1)*d <= 2^FLINT_BITS * d */
+
       umul_ppmm(nh, nl, m, d);
       add_ssaaaa(nh, nl, nh, nl, UWORD(0), r1);
 
@@ -57,11 +59,48 @@ int main(void)
       if (!result)
       {
          flint_printf("FAIL:\n");
+         flint_printf("<nh, nl> = (m*d + r1) but <nh, nl> % d != r1\n");
          flint_printf("nh = %wu, nl = %wu, d = %wu, dinv = %wu\n", nh, nl, d, dinv); 
          flint_printf("r1 = %wu, r2 = %wu\n", r1, r2);
          abort();
       }
    }
+
+   fmpz_init(n);
+      
+   /* compare with fmpz_fdiv_ui */
+   for (i = 0; i < 100000 * flint_test_multiplier(); i++)
+   {
+      ulong d, dinv, nh, nl, r1, r2;
+      
+      d = n_randtest_not_zero(state);
+      nh = n_randtest(state);
+      nl = n_randtest(state);
+
+      dinv = n_preinvert_limb(d);
+
+      /* n = <nh, nl> */
+      fmpz_set_ui(n, nh);
+      fmpz_mul_2exp(n, n, FLINT_BITS);
+      fmpz_add_ui(n, n, nl);
+
+      r1 = n_ll_mod_preinv(nh, nl, d, dinv);
+
+      r2 = fmpz_fdiv_ui(n, d);
+      
+      result = (r1 == r2);
+      if (!result)
+      {
+         flint_printf("FAIL:\n");
+         flint_printf("n = <nh, nl> but n % d does not agree with fmpz_fdiv_ui\n");
+         flint_printf("nh = %wu, nl = %wu, d = %wu, dinv = %wu\n", nh, nl, d, dinv); 
+         flint_printf("r1 = %wu, r2 = %wu\n", r1, r2);
+         fmpz_clear(n);
+         abort();
+      }
+   }
+
+   fmpz_clear(n);
 
    FLINT_TEST_CLEANUP(state);
    
