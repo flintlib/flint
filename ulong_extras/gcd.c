2c01decb79112cd6f2059abd72ddc57cbec3eb25
William Hart <goodwillhart@googlemail.com>
2015-12-22 07:59:50 +0000

    Cleanup of ulong_extras_gcd.

--- ulong_extras/gcd.c
+++ ulong_extras/gcd.c
@@ -30,96 +30,91 @@
 
 #if (defined (__amd64__) || defined (__i386__) || defined (__i486__))	 
 
-mp_limb_t
-n_gcd(mp_limb_t x, mp_limb_t y)
+mp_limb_t n_gcd(mp_limb_t x, mp_limb_t y)
 {
-    register mp_limb_t s0, s1, f;
+   register mp_limb_t s0, s1, f;
 
-    if (x == 0) return y;
-    if (y == 0) return x;
+   if (x == 0) return y;
+   if (y == 0) return x;
 
-    count_trailing_zeros(s0, x);
-    count_trailing_zeros(s1, y);
+   count_trailing_zeros(s0, x);
+   count_trailing_zeros(s1, y);
 
-    f = FLINT_MIN(s0, s1);
+   f = FLINT_MIN(s0, s1);
 
-    x >>= s0;
-    y >>= s1;
+   x >>= s0;
+   y >>= s1;
 
-    while (x != y)
-    {
-        if (x < y)
-        {
-            y -= x;
-            count_trailing_zeros(s1, y);
-            y >>= s1;
-        }
-        else
-        {
-            x -= y;
-            count_trailing_zeros(s0, x);
-            x >>= s0;
-        }
-    }
+   while (x != y)
+   {
+      if (x < y)
+      {
+         y -= x;
+         count_trailing_zeros(s1, y);
+         y >>= s1;
+      } else
+      {
+         x -= y;
+         count_trailing_zeros(s0, x);
+         x >>= s0;
+      }
+   }
 
-    return x << f;
+   return x << f;
 }
 
 #else
 
-mp_limb_t
-n_gcd(mp_limb_t x, mp_limb_t y)
+mp_limb_t n_gcd(mp_limb_t x, mp_limb_t y)
 {
-    mp_limb_t u3, v3;
-    mp_limb_t quot, rem;
+   mp_limb_t u3, v3;
+   mp_limb_t quot, rem;
 
-    if (x >= y)
-    {
-        u3 = x;
-        v3 = y;
-    }
-    else
-    {
-        u3 = y;
-        v3 = x;
-    }
+   if (x >= y)
+   {
+      u3 = x;
+      v3 = y;
+   } else
+   {
+      u3 = y;
+      v3 = x;
+   }
 
-    if ((mp_limb_signed_t) (x & y) < WORD(0))  /* x and y both have top bit set */
-    {
-        quot = u3 - v3;
-        u3 = v3;
-        v3 = quot;
-    }
+   if ((mp_limb_signed_t) (x & y) < WORD(0))  /* x and y both have top bit set */
+   {
+      quot = u3 - v3;
+      u3 = v3;
+      v3 = quot;
+   }
 
-    while ((mp_limb_signed_t) (v3 << 1) < WORD(0))  /* second value has second msb set */
-    {
-        quot = u3 - v3;
-        u3   = v3;
-        if (quot < v3)             v3 = quot;
-        else if (quot < (v3 << 1)) v3 = quot - u3;
-        else                       v3 = quot - (u3 << 1);
-    }
+   while ((mp_limb_signed_t) (v3 << 1) < WORD(0))  /* second value has second msb set */
+   {
+      quot = u3 - v3;
+      u3   = v3;
+      if (quot < v3)             v3 = quot;
+      else if (quot < (v3 << 1)) v3 = quot - u3;
+      else                       v3 = quot - (u3 << 1);
+   }
 
-    while (v3)
-    {
-        if (u3 < (v3 << 2))  /* overflow not possible due to top 2 bits of v3 not being set */
-        {
-            quot = u3 - v3;
-            u3   = v3;
-            if (quot < v3)             v3 = quot;
-            else if (quot < (v3 << 1)) v3 = quot - u3;
-            else                       v3 = quot - (u3 << 1);
-        }
-        else
-        {
-            quot = u3 / v3;
-            rem  = u3 - v3 * quot;
-            u3   = v3;
-            v3   = rem;
-        }
-    }
+   while (v3)
+   {
+      if (u3 < (v3 << 2))  /* overflow not possible due to top 2 bits of v3 not being set */
+      {
+         quot = u3 - v3;
+         u3   = v3;
+         if (quot < v3)             v3 = quot;
+         else if (quot < (v3 << 1)) v3 = quot - u3;
+         else                       v3 = quot - (u3 << 1);
+      } else
+      {
+         quot = u3 / v3;
+         rem  = u3 - v3 * quot;
+         u3   = v3;
+         v3   = rem;
+      }
+   }
 
-    return u3;
+   return u3;
 }
 
 #endif
