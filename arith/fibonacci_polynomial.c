9b0bf086cbdaf043fb0e8b0357ed9ebc0cd03d54
shivin9 <shivin.srivastava@gmail.com>
2016-01-09 19:51:30 +0530

    removed extra libraries and corrected stylistic errors

--- arith/fibonacci_polynomial.c
+++ arith/fibonacci_polynomial.c
@@ -27,64 +27,64 @@
 
 void _arith_fibonacci_polynomial(fmpz * coeffs, ulong n)
 {
-	fmpz * r;
-	int even;
-	slong k;
-	ulong L;
+    fmpz * r;
+    int even;
+    slong k;
+    ulong L;
 
-	L = n / 2;
-	even = 1 - (n % 2);
-	/* set the first two coefficients of poly depending parity of n */
-	if (even)
-	{
-		fmpz_zero(coeffs);
-		fmpz_one(coeffs + 1);
-		fmpz_mul_ui(coeffs + 1, coeffs + 1, L);
-	}
+    L = n / 2;
+    even = 1 - (n % 2);
+    /* set the first two coefficients of poly depending parity of n */
+    if (even)
+    {
+        fmpz_zero(coeffs);
+        fmpz_one(coeffs + 1);
+        fmpz_mul_ui(coeffs + 1, coeffs + 1, L);
+    }
 
-	else 
-	{
-		fmpz_one(coeffs);
-		fmpz_zero(coeffs + 1);
-	}
+    else 
+    {
+        fmpz_one(coeffs);
+        fmpz_zero(coeffs + 1);
+    }
 
-	r = coeffs + even;
-	r += 2;
+    r = coeffs + even;
+    r += 2;
 
-	/* calculate the coefficients of the polynomial*/
-	for (k = 2 + even; k < n; k += 2)
-	{
-		fmpz_mul2_uiui(r, r - 2, L + k / 2, L + k / 2 - k + 1);
-		fmpz_divexact2_uiui(r, r, k, k - 1);
-		r += 2;
-	}
+    /* calculate the coefficients of the polynomial*/
+    for (k = 2 + even; k < n; k += 2)
+    {
+        fmpz_mul2_uiui(r, r - 2, L + k / 2, L + k / 2 - k + 1);
+        fmpz_divexact2_uiui(r, r, k, k - 1);
+        r += 2;
+    }
 
-	/* set the alternate coefficients to 0 again depending on the parity*/
-	for (k = 1 + even; k < n; k += 2)
-	{
-		fmpz_zero(coeffs + k);
-	}
+    /* set the alternate coefficients to 0 again depending on the parity*/
+    for (k = 1 + even; k < n; k += 2)
+    {
+        fmpz_zero(coeffs + k);
+    }
 }
 
 
 void arith_fibonacci_polynomial(fmpz_poly_t poly, ulong n)
 {
-	if (n == 0)
-	{
-		fmpz_poly_set_ui(poly, UWORD(0));
-		return;
-	}
+    if (n == 0)
+    {
+        fmpz_poly_set_ui(poly, UWORD(0));
+        return;
+    }
 
-	if (n == 1)
-	{
-		fmpz_poly_set_ui(poly, UWORD(1));
-	}
+    if (n == 1)
+    {
+        fmpz_poly_set_ui(poly, UWORD(1));
+    }
 
-	else
-	{
-		fmpz_poly_fit_length(poly, n);
-		_arith_fibonacci_polynomial(poly->coeffs, n);
-	}
-	_fmpz_poly_set_length(poly, n);
+    else
+    {
+        fmpz_poly_fit_length(poly, n);
+        _arith_fibonacci_polynomial(poly->coeffs, n);
+    }
+    _fmpz_poly_set_length(poly, n);
 }
 
--- arith/test/t-fibonacci_polynomial.c
+++ arith/test/t-fibonacci_polynomial.c
@@ -23,60 +23,56 @@
 
  ******************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <gmp.h>
 #include "arith.h"
 #include "flint.h"
-#include "profiler.h"
 #include "fmpz.h"
-#include "fmpz_mat.h"
 #include "fmpz_poly.h"
 
 int main(void)
 {
-	fmpz_poly_t Pn, Pn1, Pn2, R;
+    fmpz_poly_t Pn, Pn1, Pn2, R;
 
-	slong n;
+    slong n;
 
-	FLINT_TEST_INIT(state);
+    FLINT_TEST_INIT(state);
 
-	flint_printf("fibonacci_polynomial....");
-	fflush(stdout);
+    flint_printf("fibonacci_polynomial....");
+    fflush(stdout);
 
-	fmpz_poly_init(Pn);
-	fmpz_poly_init(Pn1);
-	fmpz_poly_init(Pn2);
-	fmpz_poly_init(R);
+    fmpz_poly_init(Pn);
+    fmpz_poly_init(Pn1);
+    fmpz_poly_init(Pn2);
+    fmpz_poly_init(R);
 
-	fmpz_poly_set_ui(Pn, UWORD(0));
-	fmpz_poly_set_ui(Pn1, UWORD(1));
+    fmpz_poly_set_ui(Pn, UWORD(0));
+    fmpz_poly_set_ui(Pn1, UWORD(1));
 
-	for (n = 0; n <= 500; n++)
-	{
-		arith_fibonacci_polynomial(R, n);
+    for (n = 0; n <= 500; n++)
+    {
+        arith_fibonacci_polynomial(R, n);
 
-		if (!fmpz_poly_equal(Pn, R))
-		{
-			flint_printf("FAIL: n = %wd\n", n);
-			flint_printf("Direct: "); fmpz_poly_print_pretty(R, "x"); flint_printf("\n");
-			flint_printf("Recur.: "); fmpz_poly_print_pretty(Pn, "x"); flint_printf("\n");
-			abort();
-		}
+        if (!fmpz_poly_equal(Pn, R))
+        {
+            flint_printf("FAIL: n = %wd\n", n);
+            flint_printf("Direct: "); fmpz_poly_print_pretty(R, "x"); flint_printf("\n");
+            flint_printf("Recur.: "); fmpz_poly_print_pretty(Pn, "x"); flint_printf("\n");
+            abort();
+        }
 
-		fmpz_poly_shift_left(Pn2, Pn1, 1);
-		fmpz_poly_add(Pn2, Pn2, Pn);
+        fmpz_poly_shift_left(Pn2, Pn1, 1);
+        fmpz_poly_add(Pn2, Pn2, Pn);
 
-		fmpz_poly_swap(Pn, Pn1);
-		fmpz_poly_swap(Pn1, Pn2);
-	}
+        fmpz_poly_swap(Pn, Pn1);
+        fmpz_poly_swap(Pn1, Pn2);
+    }
 
-	fmpz_poly_clear(Pn);
-	fmpz_poly_clear(Pn1);
-	fmpz_poly_clear(Pn2);
-	fmpz_poly_clear(R);
+    fmpz_poly_clear(Pn);
+    fmpz_poly_clear(Pn1);
+    fmpz_poly_clear(Pn2);
+    fmpz_poly_clear(R);
 
-	FLINT_TEST_CLEANUP(state);
-	flint_printf("PASS\n");
-	return 0;
+    FLINT_TEST_CLEANUP(state);
+    flint_printf("PASS\n");
+    return 0;
 }
