fddd1250b11bfabae5b3510da1a3d29c86dff003
Isuru Fernando <isuruf@gmail.com>
2016-04-23 20:44:10 +0530

    appveyor: cache for msys build

--- .appveyor_msys_build.sh
+++ .appveyor_msys_build.sh
@@ -2,7 +2,10 @@ export PATH=/c/msys64/mingw$ABI/bin:$PATH
 
 cd /c/projects/flint2
 
-./.build_dependencies
+if [ ! -d "/c/projects/flint2/mpir-2.7.0" ]; then
+    ./.build_dependencies
+fi
+
 ./configure ABI=$ABI --with-mpir=mpir-2.7.0 --with-mpfr=mpfr-3.1.4 --disable-shared
 make -j4
 make -j4 check
