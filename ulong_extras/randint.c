91ee553243d14d98e33f2d35865fa9e1fb66faaa
Apoorv Mishra <apoorvmishra101092@gmail.com>
2017-08-11 18:47:26 +0530

    Improved n_randint to get uniformly distributed random values

--- ulong_extras/randint.c
+++ ulong_extras/randint.c
@@ -13,8 +13,43 @@
 #include "flint.h"
 #include "ulong_extras.h"
 
+/* Current implementation follows */
+
+/*
 mp_limb_t n_randint(flint_rand_t state, mp_limb_t limit) 
 {
-   if (limit == UWORD(0)) return n_randlimb(state);
-   else return n_randlimb(state) % limit;
+    if (limit == UWORD(0)) return n_randlimb(state);
+    else return n_randlimb(state) % limit;
 }
+*/
+
+/* Current implementation ends */
+
+/* New implementation follows */
+
+mp_limb_t n_randint(flint_rand_t state, mp_limb_t limit) 
+{
+    mp_limb_t rand_max = UWORD_MAX;
+
+    if (limit == UWORD(0)) 
+    {
+    	return n_randlimb(state);
+    } 
+    else
+    {
+    	mp_limb_t x, y, r;
+
+    	x = rand_max / limit;
+		y = x * limit;
+
+		do 
+		{
+			r = n_randlimb(state);
+		}
+		while(r >= y);
+
+		return r / x;
+    }
+}
+
+/* New implementation ends */
