8008ffad83ba8afb14cddb082cb7bfd4f8d8dafd
William Hart <goodwillhart@googlemail.com>
2018-01-08 09:45:09 +0100

    Implement heuristic sqrt_KS for fmpz_poly, plus test and docs.

--- fmpz_poly.h
+++ fmpz_poly.h
@@ -1045,6 +1045,11 @@ FLINT_DLL int _fmpz_poly_sqrt_divconquer(fmpz * res, const fmpz * poly,
 
 FLINT_DLL int fmpz_poly_sqrt_divconquer(fmpz_poly_t b, const fmpz_poly_t a);
 
+FLINT_DLL int _fmpz_poly_sqrt_KS(fmpz *rop, const fmpz *op,
+                                                         slong len, int exact);
+
+FLINT_DLL int fmpz_poly_sqrt_KS(fmpz_poly_t b, const fmpz_poly_t a);
+
 FLINT_DLL int _fmpz_poly_sqrt(fmpz * res, const fmpz * poly, slong len);
 
 FLINT_DLL int fmpz_poly_sqrt(fmpz_poly_t b, const fmpz_poly_t a);
