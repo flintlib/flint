13c23cc260525a7a246e2fdf2451c14559329620
William Hart <goodwillhart@googlemail.com>
2018-01-22 18:21:48 +0100

    Attempt to force a rebuild of the cache (again).

--- .build_dependencies.cmd
+++ .build_dependencies.cmd
@@ -8,7 +8,7 @@ rename mpir-2.7.2 mpir
 cd mpir\build.vc14
 msbuild.exe lib_mpir_gc/lib_mpir_gc.vcxproj /p:Configuration=Release /p:Platform=%PLATFORM% /p:PlatformToolset=v140 /verbosity:minimal
 cd ..\..
-
+ 
 curl -fsSL -o mpfr.zip https://github.com/BrianGladman/mpfr/archive/master.zip
 7z x mpfr.zip > NUL
 rename mpfr-master mpfr
