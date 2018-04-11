3c88549dac5c42f6fc93de41dba61591d7f05b9c
Claus Fieker <fieker@mathematik.uni-kl.de>
2018-04-11 16:06:25 +0200

    make sure gcd_full does not change the input

--- mpn_extras/gcd_full.c
+++ mpn_extras/gcd_full.c
@@ -36,24 +36,15 @@ mp_size_t flint_mpn_gcd_full(mp_ptr arrayg,
    flint_mpn_zero(arrayg, m);
 
    /* set in1 to shifted array1 */
-   if (b1 == 0)
-      in1 = array1 + s1;
-   else
-   {
-      in1 = flint_malloc(len1*sizeof(mp_limb_t));
-      mpn_rshift(in1, array1 + s1, len1, b1);
-      len1 -= (in1[len1 - 1] == 0); 
-   }
+   in1 = flint_malloc(len1*sizeof(mp_limb_t));
+   mpn_rshift(in1, array1 + s1, len1, b1);
+   len1 -= (in1[len1 - 1] == 0); 
 
    /* set in2 to shifted array2 */
-   if (b2 == 0)
-      in2 = array2 + s2;
-   else
-   {
-      in2 = flint_malloc(len2*sizeof(mp_limb_t));
-      mpn_rshift(in2, array2 + s2, len2, b2);
-      len2 -= (in2[len2 - 1] == 0); 
-   }
+   in2 = flint_malloc(len2*sizeof(mp_limb_t));
+   mpn_rshift(in2, array2 + s2, len2, b2);
+   len2 -= (in2[len2 - 1] == 0); 
+   
    
    /* compute gcd of shifted values */
    if (len1 >= len2)
@@ -69,8 +60,8 @@ mp_size_t flint_mpn_gcd_full(mp_ptr arrayg,
    }
 
    /* clean up */
-   if (b1) flint_free(in1);
-   if (b2) flint_free(in2);
+   flint_free(in1);
+   flint_free(in2);
 
    /* return total number of limbs in output */
    return m + leng;
