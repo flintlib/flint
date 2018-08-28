25a537ee3752a5363cde7b476d0d12933a919b7c
tthsqe12 <tthsqe12@gmail.com>
2018-08-28 15:24:42 +0200

    move splitter to mpoly

--- mpoly.h
+++ mpoly.h
@@ -639,6 +639,14 @@ FLINT_DLL void mpoly_search_monomials(
                 const ulong * a, slong a_len, const ulong * b, slong b_len,
                                                slong N, const ulong * cmpmask);
 
+FLINT_DLL void mpoly_main_variable_split_LEX(slong * ind, ulong * pexp,
+                                 const ulong * Aexp, slong l1, slong Alen,
+                                  const ulong * mults, slong num, slong Abits);
+
+FLINT_DLL void mpoly_main_variable_split_DEG(slong * ind, ulong * pexp,
+                                  const ulong * Aexp,  slong l1, slong Alen,
+                                            ulong deg, slong num, slong Abits);
+
 FLINT_DLL int mpoly_termexp_fits_si(ulong * exps, slong bits,
                                               slong n, const mpoly_ctx_t mctx);
 
