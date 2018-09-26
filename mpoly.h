75c97586d4e4bea201fcb89bd029cf3978b310ca
tthsqe12 <tthsqe12@gmail.com>
2018-09-26 17:43:22 +0200

    add eval functions

--- mpoly.h
+++ mpoly.h
@@ -134,6 +134,8 @@ FLINT_DLL void mpoly_rbtree_clear(mpoly_rbtree_t tree, void ** dataout, slong * 
 FLINT_DLL mpoly_rbnode_struct * mpoly_rbtree_get(int * new,
                                          struct mpoly_rbtree *tree, slong rcx);
 
+FLINT_DLL mpoly_rbnode_struct * mpoly_rbtree_get_fmpz(int * new,
+                                        struct mpoly_rbtree *tree, fmpz_t rcx);
 
 /* Orderings *****************************************************************/
 
