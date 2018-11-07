86f7843624db692c37c840fa65c7b91d9c05d73c
fredrik <fredrik.johansson@gmail.com>
2018-11-07 21:39:52 +0100

    fix a declaration

--- flint.h
+++ flint.h
@@ -229,7 +229,7 @@ void flint_randclear(flint_rand_t state)
 }
 
 FLINT_INLINE
-flint_rand_s * flint_rand_alloc()
+flint_rand_s * flint_rand_alloc(void)
 {
     return (flint_rand_s *) flint_malloc(sizeof(flint_rand_s));
 }
