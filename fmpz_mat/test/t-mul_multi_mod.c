15854930047f0297022ba3afd74620c9848d45f6
fredrik <fredrik.johansson@gmail.com>
2018-11-10 13:35:51 +0100

    speed up fmpz_mat_mul_multi_mod using fast basecase CRT code

--- fmpz_mat/test/t-mul_multi_mod.c
+++ fmpz_mat/test/t-mul_multi_mod.c
@@ -27,8 +27,6 @@ int main(void)
     flint_printf("mul_multi_mod....");
     fflush(stdout);
 
-    
-
     for (i = 0; i < 100 * flint_test_multiplier(); i++)
     {
         slong m, n, k;
@@ -44,6 +42,40 @@ int main(void)
 
         fmpz_mat_randtest(A, state, n_randint(state, 200) + 1);
         fmpz_mat_randtest(B, state, n_randint(state, 200) + 1);
+
+        /* Make sure noise in the output is ok */
+        fmpz_mat_randtest(C, state, n_randint(state, 200) + 1);
+
+        fmpz_mat_mul_classical_inline(C, A, B);
+        fmpz_mat_mul_multi_mod(D, A, B);
+
+        if (!fmpz_mat_equal(C, D))
+        {
+            flint_printf("FAIL: results not equal\n");
+            abort();
+        }
+
+        fmpz_mat_clear(A);
+        fmpz_mat_clear(B);
+        fmpz_mat_clear(C);
+        fmpz_mat_clear(D);
+    }
+
+    for (i = 0; i < 100 * flint_test_multiplier(); i++)
+    {
+        slong m, n, k;
+
+        m = n_randint(state, 3);
+        n = n_randint(state, 3);
+        k = n_randint(state, 3);
+
+        fmpz_mat_init(A, m, n);
+        fmpz_mat_init(B, n, k);
+        fmpz_mat_init(C, m, k);
+        fmpz_mat_init(D, m, k);
+
+        fmpz_mat_randtest(A, state, n_randint(state, 25000) + 1);
+        fmpz_mat_randtest(B, state, n_randint(state, 25000) + 1);
 
         /* Make sure noise in the output is ok */
         fmpz_mat_randtest(C, state, n_randint(state, 200) + 1);
