953a482e2ec3b6125d92db93e5ef63aa48a5c0ad
tthsqe12 <tthsqe12@gmail.com>
2018-11-21 17:06:17 +0100

    make set_termcoeff_fmpq less stupid

--- fmpq_mpoly/set_termcoeff_fmpq.c
+++ fmpq_mpoly/set_termcoeff_fmpq.c
@@ -11,26 +11,50 @@
 
 #include "fmpq_mpoly.h"
 
-void
-fmpq_mpoly_set_termcoeff_fmpq(fmpq_mpoly_t poly,
+
+void fmpq_mpoly_set_termcoeff_fmpq(fmpq_mpoly_t A,
                            slong n, const fmpq_t x, const fmpq_mpoly_ctx_t ctx)
 {
     if (fmpq_is_zero(x))
     {
-        fmpz_mpoly_set_termcoeff_fmpz(poly->zpoly, n, fmpq_numref(x), ctx->zctx);
-        /* poly is not guaranteed to have a nonzero coeff, so no canonicalise */
+        /* we can easily get a zero coeff by zeroing the coeff of the zpoly */
+        fmpz_mpoly_set_termcoeff_fmpz(A->zpoly, n, fmpq_numref(x), ctx->zctx);
 
-    } else
+    }
+    else if (fmpq_is_zero(A->content))
     {
+        /*
+            if the A->content is zero,
+                set the nth coeff to 1
+                set all other coeffs to 0
+                set the content to x
+        */
         fmpz_t t;
-        fmpz_init(t);
-        fmpz_mul(t, fmpq_numref(poly->content), fmpq_denref(x));
-        fmpz_mpoly_scalar_mul_fmpz(poly->zpoly, poly->zpoly, t, ctx->zctx);
-        fmpz_mul(t, fmpq_denref(poly->content), fmpq_numref(x));
-        fmpz_set_ui(fmpq_numref(poly->content), UWORD(1));
-        fmpz_mul(fmpq_denref(poly->content), fmpq_denref(poly->content), fmpq_denref(x));
-        fmpz_mpoly_set_termcoeff_fmpz(poly->zpoly, n, t, ctx->zctx);
+        fmpz_init_set_ui(t, UWORD(1));
+        fmpq_set(A->content, x);
+        _fmpz_vec_zero(A->zpoly->coeffs, A->zpoly->length);
+        fmpz_mpoly_set_termcoeff_fmpz(A->zpoly, n, t, ctx->zctx);
         fmpz_clear(t);
     }
-    return;
+    else
+    {
+        /*
+            if A->content != 0 and x != 0,
+                compute nun/den = x / A->content
+                scale zpoly by den
+                divide A->content by den
+                set the nth coeff of zpoly to num
+        */
+        fmpq_t t;
+        fmpq_init(t);
+        fmpq_div(t, x, A->content);
+        if (!fmpz_is_one(fmpq_denref(t)))
+        {
+            fmpq_div_fmpz(A->content, A->content, fmpq_denref(t));
+            _fmpz_vec_scalar_mul_fmpz(A->zpoly->coeffs, A->zpoly->coeffs,
+                                             A->zpoly->length, fmpq_denref(t));            
+        }
+        fmpz_mpoly_set_termcoeff_fmpz(A->zpoly, n, fmpq_numref(t), ctx->zctx);
+        fmpq_clear(t);
+    }
 }
