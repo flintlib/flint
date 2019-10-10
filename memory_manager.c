28da0e80b2b7db8068292de857804fd98aea594d
John H. Palmieri <jhpalmieri64@gmail.com>
2019-10-10 15:08:58 -0700

    flush standard output in flint_memory_error

--- memory_manager.c
+++ memory_manager.c
@@ -49,6 +49,7 @@ void __flint_set_memory_functions_init()
 static void flint_memory_error(size_t size)
 {
     flint_printf("Exception (FLINT memory_manager). Unable to allocate memory (%ld).\n", size);
+    fflush(stdout);
     flint_abort();
 }
 
