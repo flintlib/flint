ddbbc945fa80ea87e8e9111cdb53aa80573c80a7
Édouard Rousseau <erousseau@enst.fr>
2019-10-16 13:23:14 +0200

    added the files

---
+++ fmpz_mod_mat/mul.c
@@ -0,0 +1,18 @@
+/*
+    Copyright (C) 2017 Luca De Feo
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.  See <http://www.gnu.org/licenses/>.
+*/
+
+#include "fmpz_mod_mat.h"
+
+void fmpz_mod_mat_mul(fmpz_mod_mat_t C, const fmpz_mod_mat_t A, const fmpz_mod_mat_t B)
+{
+    fmpz_mat_mul(C->mat, A->mat, B->mat);
+    _fmpz_mod_mat_reduce(C);
+}
