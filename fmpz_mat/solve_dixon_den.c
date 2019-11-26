ad8f9d4232e41b966efb0bc1a058c0cea994c71e
William Hart <goodwillhart@googlemail.com>
2019-11-26 23:36:18 +0100

    Optimise solve_dixon_den for early termination.

--- fmpz_mat/solve_dixon_den.c
+++ fmpz_mat/solve_dixon_den.c
@@ -19,14 +19,16 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
                     const fmpz_t N, const fmpz_t D)
 {
     fmpz_t bound, ppow;
-    fmpz_mat_t x, d, y, Ay;
+    fmpz_mat_t x, d, y, Ay, AX, Bden;
     fmpq_mat_t x_q;
-    fmpz_t prod, mod;
+    fmpz_t prod, mod, xknum, xkden;
+    fmpz * xvec_num, * xvec_den;
     mp_limb_t * crt_primes;
     nmod_mat_t * A_mod;
     nmod_mat_t Ay_mod, d_mod, y_mod;
-    slong i, n, cols, num_primes;
-    int stabilised = 0; /* whether p-adic lifting has stabilised */
+    slong i, j, k, l, n, s, cols, num_primes, nz_count = 0;
+    slong nz_r[5], nz_c[5]; /* rows and cols of nonzero entries of x */
+    int stabilised; /* has lifting stabilised (at 5 nonzero entries) */
 
     n = A->r;
     cols = B->c;
@@ -35,13 +37,20 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
     fmpz_init(ppow);
     fmpz_init(prod);
     fmpz_init(mod);
+    fmpz_init(xknum);
+    fmpz_init(xkden);
 
+    fmpz_mat_init(Bden, B->r, B->c);
+    fmpz_mat_init(AX, B->r, B->c);
     fmpz_mat_init(x, n, cols);
     fmpz_mat_init(y, n, cols);
     fmpz_mat_init(Ay, n, cols);
     fmpz_mat_init_set(d, B);
 
     fmpq_mat_init(x_q, n, cols);
+
+    xvec_num = _fmpz_vec_init(5);
+    xvec_den = _fmpz_vec_init(5);
 
     /* Compute bound for the needed modulus. TODO: if one of N and D
        is much smaller than the other, we could use a tighter bound (i.e. 2ND).
@@ -56,10 +65,10 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
 
     crt_primes = fmpz_mat_dixon_get_crt_primes(&num_primes, A, p);
     A_mod = (nmod_mat_t *) flint_malloc(sizeof(nmod_mat_t) * num_primes);
-    for (i = 0; i < num_primes; i++)
+    for (j = 0; j < num_primes; j++)
     {
-        nmod_mat_init(A_mod[i], n, n, crt_primes[i]);
-        fmpz_mat_get_nmod_mat(A_mod[i], A);
+        nmod_mat_init(A_mod[j], n, n, crt_primes[j]);
+        fmpz_mat_get_nmod_mat(A_mod[j], A);
     }
 
     nmod_mat_init(Ay_mod, n, cols, UWORD(1));
@@ -68,6 +77,8 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
 
     fmpz_one(ppow);
 
+    i = 1; /* working with p^i */
+    
     while (fmpz_cmp(ppow, bound) <= 0)
     {
         /* y = A^(-1) * d  (mod p) */
@@ -82,17 +93,92 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
         if (fmpz_cmp(ppow, bound) > 0)
             break;
 
+	if ((i & (i - 1)) == 0) /* check if v is a power of 2 */
+        {
+           /* on first iteration, identify five nonzero entries */
+	   if (nz_count == 0)
+           {
+              slong stride;
+              
+              /* count nonzero entries */
+	      for (k = 0; k < x->r; k++)
+	         for (l = 0; l < x->c; l++)
+	            if (!fmpz_is_zero(fmpz_mat_entry(x, k, l)))
+		       nz_count++;
+
+              stride = (nz_count - 1)/5;
+
+	      nz_count = 0;
+
+              /* find five nonzero entries equally spaced */
+              for (k = 0, s = 0; k < x->r && s < 5; k++)
+              {
+                 for (l = 0; l < x->c && s < 5; l++)
+                 {
+                    if (!fmpz_is_zero(fmpz_mat_entry(x, k, l)))
+		    {
+		       nz_count++;
+
+                       if (stride == 0)
+		       {
+		          for ( ; s < 5; s++)
+			     nz_r[s] = k, nz_c[s] = l;
+		       } else if (stride == 1 || (nz_count % stride) == 1)
+                       {
+                          nz_r[s] = k, nz_c[s] = l;
+		          s++;
+                       }
+		    }
+                 }
+              }
+	   }
+
+	   /* check if the five nonzero entries are stabilised */
+	   stabilised = nz_count != 0;
+
+           for (s = 0; s < 5 && stabilised; s++)
+           {
+	      /* set stabilised to success of reconstruction */
+              if ((stabilised = _fmpq_reconstruct_fmpz(xknum, xkden,
+			fmpz_mat_entry(x, nz_r[s], nz_c[s]), ppow)))
+              {
+                 _fmpq_canonicalise(xknum, xkden);
+
+		 stabilised &= fmpz_equal(xknum, xvec_num + s);
+                 stabilised &= fmpz_equal(xkden, xvec_den + s);
+
+		 fmpz_set(xvec_num + s, xknum);
+		 fmpz_set(xvec_den + s, xkden);
+	      }     
+           }
+
+	   /* full matrix stabilisation check */
+	   if (stabilised || i == 1)
+           {
+              fmpq_mat_set_fmpz_mat_mod_fmpz(x_q, x, ppow);
+              fmpq_mat_get_fmpz_mat_matwise(X, den, x_q);
+
+              fmpz_mat_mul(AX, A, X);
+              fmpz_mat_scalar_mul_fmpz(Bden, B, den);
+
+	      if (fmpz_mat_equal(AX, Bden))
+	         goto dixon_done;
+           }
+	}
+
+	i++;
+
         /* d = (d - Ay) / p */
 #if USE_SLOW_MULTIPLICATION
         fmpz_mat_set_nmod_mat_unsigned(y, y_mod);
         fmpz_mat_mul(Ay, A, y);
 #else
-        for (i = 0; i < num_primes; i++)
+        for (j = 0; j < num_primes; j++)
         {
-            _nmod_mat_set_mod(y_mod, crt_primes[i]);
-            _nmod_mat_set_mod(Ay_mod, crt_primes[i]);
-            nmod_mat_mul(Ay_mod, A_mod[i], y_mod);
-            if (i == 0)
+            _nmod_mat_set_mod(y_mod, crt_primes[j]);
+            _nmod_mat_set_mod(Ay_mod, crt_primes[j]);
+            nmod_mat_mul(Ay_mod, A_mod[j], y_mod);
+            if (j == 0)
             {
                 fmpz_mat_set_nmod_mat(Ay, Ay_mod);
                 fmpz_set_ui(prod, crt_primes[0]);
@@ -100,7 +186,7 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
             else
             {
                 fmpz_mat_CRT_ui(Ay, Ay, prod, Ay_mod, 1);
-                fmpz_mul_ui(prod, prod, crt_primes[i]);
+                fmpz_mul_ui(prod, prod, crt_primes[j]);
             }
         }
 #endif
@@ -110,22 +196,27 @@ _fmpz_mat_solve_dixon_den(fmpz_mat_t X, fmpz_t den,
         fmpz_mat_scalar_divexact_ui(d, d, p);
     }
 
-dixon_done:
-
     /* TODO can be changed to one step */
     fmpq_mat_set_fmpz_mat_mod_fmpz(x_q, x, ppow);
     fmpq_mat_get_fmpz_mat_matwise(X, den, x_q);
+
+dixon_done:
+
+    _fmpz_vec_clear(xvec_num, 5);
+    _fmpz_vec_clear(xvec_den, 5);
 
     nmod_mat_clear(y_mod);
     nmod_mat_clear(d_mod);
     nmod_mat_clear(Ay_mod);
 
-    for (i = 0; i < num_primes; i++)
-        nmod_mat_clear(A_mod[i]);
+    for (j = 0; j < num_primes; j++)
+        nmod_mat_clear(A_mod[j]);
 
     flint_free(A_mod);
     flint_free(crt_primes);
 
+    fmpz_clear(xknum);
+    fmpz_clear(xkden);
     fmpz_clear(bound);
     fmpz_clear(ppow);
     fmpz_clear(prod);
@@ -133,6 +224,8 @@ dixon_done:
 
     fmpq_mat_clear(x_q);
 
+    fmpz_mat_clear(AX);
+    fmpz_mat_clear(Bden);
     fmpz_mat_clear(x);
     fmpz_mat_clear(y);
     fmpz_mat_clear(d);
