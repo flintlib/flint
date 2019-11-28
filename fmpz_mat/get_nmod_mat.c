eaa83c694bbd56fb9df9c851d66e6a87aa1128ef
William Hart <goodwillhart@googlemail.com>
2019-11-28 17:22:18 +0100

    Special case symmetric matrices.

--- fmpz_mat/get_nmod_mat.c
+++ fmpz_mat/get_nmod_mat.c
@@ -17,7 +17,27 @@ fmpz_mat_get_nmod_mat(nmod_mat_t Amod, const fmpz_mat_t A)
     slong i, j;
     mp_limb_t m = Amod->mod.n;
 
-    for (i = 0; i < A->r; i++)
-        for (j = 0; j < A->c; j++)
-            Amod->rows[i][j] = fmpz_fdiv_ui(A->rows[i]+j, m);
+    if (fmpz_mat_is_square(A))
+    {
+       int symmetric = fmpz_mat_is_square(A);
+
+       for (i = 0; i < A->r; i++)
+       {
+           Amod->rows[i][i] = fmpz_fdiv_ui(A->rows[i]+i, m);
+
+	   for (j = i + 1; j < A->c; j++)
+	   {
+               Amod->rows[i][j] = fmpz_fdiv_ui(A->rows[i] + j, m);
+               if ((symmetric &= fmpz_equal(A->rows[j] + i, A->rows[i] + j)))
+	           Amod->rows[j][i] = Amod->rows[i][j];
+               else
+                   Amod->rows[j][i] = fmpz_fdiv_ui(A->rows[j] + i, m);
+	   }
+        }
+    } else
+    {
+        for (i = 0; i < A->r; i++)
+            for (j = 0; j < A->c; j++)
+                Amod->rows[i][j] = fmpz_fdiv_ui(A->rows[i] + j, m);
+    }
 }
