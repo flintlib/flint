7d404a572d297ff182bba7cc254f007e06725f4b
William Hart <goodwillhart@googlemail.com>
2019-11-29 13:19:48 +0100

    Fix condition in solve for fflu to be used.

--- fmpz_mat/solve.c
+++ fmpz_mat/solve.c
@@ -18,7 +18,7 @@ fmpz_mat_solve(fmpz_mat_t X, fmpz_t den,
 {
     if (fmpz_mat_nrows(A) <= 3)
         return fmpz_mat_solve_cramer(X, den, A, B);
-    else if (fmpz_mat_nrows(A) <= 20 && fmpz_mat_ncols(A) <= 20)
+    else if (fmpz_mat_nrows(A) <= 20)
         return fmpz_mat_solve_fflu(X, den, A, B);
     else if (fmpz_mat_ncols(B) == 1)
 	return fmpz_mat_solve_dixon_den(X, den, A, B);
