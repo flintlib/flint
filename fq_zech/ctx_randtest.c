082ca0321936cb374519f44a2efd0fd4693e5303
fredrik <fredrik.johansson@gmail.com>
2020-03-05 22:22:50 +0100

    speed up zech randtests further

--- fq_zech/ctx_randtest.c
+++ fq_zech/ctx_randtest.c
@@ -23,7 +23,10 @@ fq_zech_ctx_randtest(fq_zech_ctx_t ctx, flint_rand_t state)
 
     fmpz_init(p);
     fmpz_set_ui(p, n_randprime(state, 2 + n_randint(state, 4), 1));
-    max_d = floor(log(n_pow(2, 15)) / log(fmpz_get_ui(p)));
+    if (n_randlimb(state) % 16 == 0)   /* slow */
+        max_d = floor(log(n_pow(2, 15)) / log(fmpz_get_ui(p)));
+    else
+        max_d = floor(log(n_pow(2, 11)) / log(fmpz_get_ui(p)));
     d = n_randint(state, max_d - 1) + 2;
     fq_zech_ctx_init_random(ctx, p, d, "a");
     fmpz_clear(p);
