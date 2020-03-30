9fd6b4e345ae4fea03a5349b8cdaf7b27bfebc9e
tthsqe12 <tthsqe12@gmail.com>
2020-03-30 20:01:23 +0200

    fix sdiv_qrnnd fallback

--- test/t-sdiv_qrnnd.c
+++ test/t-sdiv_qrnnd.c
@@ -26,6 +26,7 @@ int main(void)
 
     for (i = 0; i < 1000000*flint_test_multiplier(); i++)
     {
+        int nsgn;
         mp_limb_signed_t d, nh, nl, q, r, ph, pl;
 
         do
@@ -36,22 +37,41 @@ int main(void)
 
         nl = n_randtest(state);
 
+        if (nh < 0)
+            nsgn = -1;
+        else if (nh > 0 || nl != 0)
+            nsgn = +1;
+        else
+            nsgn = 0;
+
         sdiv_qrnnd(q, r, nh, nl, d);
         smul_ppmm(ph, pl, d, q);
         add_ssaaaa(ph, pl, ph, pl, FLINT_SIGN_EXT(r), r);
 
-        /* check n = q*d + r and |r| < |d| when possible */
-        if (ph != nh ||
-            pl != nl ||
-            (d > 0 && WORD_MIN + d <= 0 && (r >= d || r <= -d)) ||
-            (d < 0 && WORD_MAX + d >= 0 && (r <= d || r >= -d)))
+        /* check n = q*d + r */
+        if (ph != nh || pl != nl)
         {
-            flint_printf("FAIL:\n");
+            flint_printf("FAIL: check identity\n");
             flint_printf("nh = %wd, nl = %wd\n", nh, nl);
             flint_printf("d = %wd\n", d);
             flint_printf("q = %wd\n", q);
             flint_printf("r = %wd\n", r);
             flint_printf("ph = %wu, pl = %wu\n", ph, pl);
+            flint_abort();
+        }
+
+        /* check rounding of q was towards zero */
+        if ((nsgn >= 0 && d > 0 && !(0 <= r && r <= d)) ||
+            (nsgn >= 0 && d < 0 && WORD_MAX + d >= 0 && !(0 <= r && r <= -d)) ||
+            (nsgn < 0 && d > 0 && WORD_MIN + d <= 0 && !(-d <= r && r <= 0)) ||
+            (nsgn < 0 && d < 0 && !(d <= r && r <= 0)))
+        {
+            flint_printf("FAIL: check remainder\n");
+            flint_printf("nsgn: %d\n", nsgn);
+            flint_printf("nh = %wd, nl = %wd\n", nh, nl);
+            flint_printf("d = %wd\n", d);
+            flint_printf("q = %wd\n", q);
+            flint_printf("r = %wd\n", r);
             flint_abort();
         }
     }
