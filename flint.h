74477d09aa0938834deb7a6eb5c3e5ea98beccea
fredrik <fredrik.johansson@gmail.com>
2020-04-29 13:35:29 +0200

    update FLINT_VERSION (not the official release yet, just to allow other software that builds against flint to deal with incompatible changes)

--- flint.h
+++ flint.h
@@ -55,9 +55,9 @@
 /* flint version number */
 
 #define __FLINT_VERSION 2
-#define __FLINT_VERSION_MINOR 5
-#define __FLINT_VERSION_PATCHLEVEL 3
-#define FLINT_VERSION "2.5.3"
+#define __FLINT_VERSION_MINOR 6
+#define __FLINT_VERSION_PATCHLEVEL 0
+#define FLINT_VERSION "2.6.0"
 #define __FLINT_RELEASE (__FLINT_VERSION * 10000 + \
                          __FLINT_VERSION_MINOR * 100 + \
                          __FLINT_VERSION_PATCHLEVEL)
