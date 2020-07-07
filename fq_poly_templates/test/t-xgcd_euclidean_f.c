499d027dc0e377cd55bedf1b1f7e2bc5e15eaa6c
William Hart <goodwillhart@googlemail.com>
2020-07-07 15:18:10 +0200

    Fix a bug in fmpz_mod_poly_gcdiv_euclidean, triggered by a new test in fq_poly (xgcd_euclidean).

--- fq_poly_templates/test/t-xgcd_euclidean_f.c
+++ fq_poly_templates/test/t-xgcd_euclidean_f.c
@@ -97,7 +97,7 @@ main(void)
     /* Special case, arguments share a factor ******************************* */
 
     /* Compare with result from GCD and check correctness */
-    for (i = 0; i < 5 * flint_test_multiplier(); i++)
+    for (i = 0; i < 50 * flint_test_multiplier(); i++)
     {
         TEMPLATE(T, ctx_t) ctx;
         TEMPLATE(T, t) f1, f2;
@@ -117,9 +117,9 @@ main(void)
         TEMPLATE(T, poly_init) (t, ctx);
         TEMPLATE(T, poly_init) (v, ctx);
         TEMPLATE(T, poly_init) (w, ctx);
-        TEMPLATE(T, poly_randtest) (a, state, n_randint(state, 100), ctx);
-        TEMPLATE(T, poly_randtest) (b, state, n_randint(state, 100), ctx);
-        TEMPLATE(T, poly_randtest) (f, state, n_randint(state, 20), ctx);
+        TEMPLATE(T, poly_randtest) (a, state, n_randint(state, 10), ctx);
+        TEMPLATE(T, poly_randtest) (b, state, n_randint(state, 10), ctx);
+        TEMPLATE(T, poly_randtest) (f, state, n_randint(state, 5), ctx);
         TEMPLATE(T, poly_mul) (a, a, f, ctx);
         TEMPLATE(T, poly_mul) (b, b, f, ctx);
 
