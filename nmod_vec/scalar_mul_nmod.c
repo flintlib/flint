9a0867ec811d87a43ca88ae9ea27cbbd0ccc8353
Debian User <user@debian-armhf>
2020-07-22 12:36:45 +0000

    mpn_mul_1 expects length > 0

--- nmod_vec/scalar_mul_nmod.c
+++ nmod_vec/scalar_mul_nmod.c
@@ -18,6 +18,9 @@
 void _nmod_vec_scalar_mul_nmod(mp_ptr res, mp_srcptr vec, 
                                slong len, mp_limb_t c, nmod_t mod)
 {
+    if (len < 1)
+        return;
+
     if (len > 10 && mod.n < UWORD_HALF)
     {
         _nmod_vec_scalar_mul_nmod_shoup(res, vec, len, c, mod);
