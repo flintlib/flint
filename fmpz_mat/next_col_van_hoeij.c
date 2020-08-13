124204858036319e523dfdad61454e7ed6590270
Max Horn <max@quendi.de>
2020-08-13 11:24:08 +0200

    Change www.gnu.org URLs to use HTTPS

--- fmpz_mat/next_col_van_hoeij.c
+++ fmpz_mat/next_col_van_hoeij.c
@@ -7,7 +7,7 @@
     FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
     by the Free Software Foundation; either version 2.1 of the License, or
-    (at your option) any later version.  See <http://www.gnu.org/licenses/>.
+    (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
 #include "fmpz.h"
