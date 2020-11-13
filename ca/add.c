d17490a949cb3d9755ac48caf8b28491a6cd96c0
fredrik <fredrik.johansson@gmail.com>
2020-11-13 15:10:32 +0100

    optimize ca_sub

--- ca/add.c
+++ ca/add.c
@@ -292,23 +292,129 @@ ca_add(ca_t res, const ca_t x, const ca_t y, ca_ctx_t ctx)
 }
 
 void
-ca_sub(ca_t res, const ca_t x, const ca_t y, ca_ctx_t ctx)
+ca_sub_fmpq(ca_t res, const ca_t x, const fmpq_t y, ca_ctx_t ctx)
 {
-    ca_t t;
-    ca_init(t, ctx);
-    ca_neg(t, y, ctx);
-    ca_add(res, x, t, ctx);
-    ca_clear(t, ctx);
+    ca_field_srcptr field;
+
+    if (fmpq_is_zero(y) || CA_IS_SPECIAL(x))
+    {
+        ca_set(res, x, ctx);
+        return;
+    }
+
+    if (CA_IS_QQ(x, ctx))
+    {
+        _ca_make_fmpq(res, ctx);
+        fmpq_sub(CA_FMPQ(res), CA_FMPQ(x), y);
+    }
+    else
+    {
+        field = CA_FIELD(x, ctx);
+        _ca_make_field_element(res, field, ctx);
+
+        if (CA_FIELD_IS_NF(field))
+            nf_elem_sub_fmpq(CA_NF_ELEM(res), CA_NF_ELEM(x), y, CA_FIELD_NF(field));
+        else
+            fmpz_mpoly_q_sub_fmpq(CA_MPOLY_Q(res), CA_MPOLY_Q(x), y, CA_FIELD_MCTX(field, ctx));
+    }
 }
 
 void
-ca_sub_fmpq(ca_t res, const ca_t x, const fmpq_t y, ca_ctx_t ctx)
+ca_sub(ca_t res, const ca_t x, const ca_t y, ca_ctx_t ctx)
 {
-    fmpq_t t;
-    fmpq_init(t);
-    fmpq_neg(t, y);
-    ca_add_fmpq(res, x, t, ctx);
-    fmpq_clear(t);
+    ca_field_srcptr xfield, yfield, zfield;
+
+    xfield = CA_FIELD(x, ctx);
+    yfield = CA_FIELD(y, ctx);
+
+    if (CA_IS_QQ(x, ctx) && (xfield == yfield))
+    {
+        _ca_make_fmpq(res, ctx);
+        fmpq_sub(CA_FMPQ(res), CA_FMPQ(x), CA_FMPQ(y));
+        return;
+    }
+
+    if (CA_IS_QQ(y, ctx))
+    {
+        if (res == y)
+        {
+            fmpq_t t;
+            fmpq_init(t);
+            fmpq_set(t, CA_FMPQ(y));
+            ca_sub_fmpq(res, x, t, ctx);
+            fmpq_clear(t);
+        }
+        else
+        {
+            ca_sub_fmpq(res, x, CA_FMPQ(y), ctx);
+        }
+        return;
+    }
+
+    if (CA_IS_QQ(x, ctx))
+    {
+        if (res == x)
+        {
+            fmpq_t t;
+            fmpq_init(t);
+            fmpq_set(t, CA_FMPQ(x));
+            ca_sub_fmpq(res, y, t, ctx);
+            ca_neg(res, res, ctx);
+            fmpq_clear(t);
+        }
+        else
+        {
+            ca_sub_fmpq(res, y, CA_FMPQ(x), ctx);
+            ca_neg(res, res, ctx);
+        }
+        return;
+    }
+
+    if (CA_IS_SPECIAL(x) || CA_IS_SPECIAL(y))
+    {
+        ca_t t;
+        ca_init(t, ctx);
+        ca_neg(t, y, ctx);
+        ca_add(res, x, t, ctx);
+        ca_clear(t, ctx);
+        return;
+    }
+
+    /* In-field operation. */
+    if (xfield == yfield)
+    {
+        zfield = xfield;
+        _ca_make_field_element(res, zfield, ctx);
+
+        if (CA_FIELD_IS_NF(zfield))
+        {
+            nf_elem_sub(CA_NF_ELEM(res), CA_NF_ELEM(x), CA_NF_ELEM(y), CA_FIELD_NF(zfield));
+        }
+        else
+        {
+            fmpz_mpoly_q_sub(CA_MPOLY_Q(res), CA_MPOLY_Q(x), CA_MPOLY_Q(y), CA_FIELD_MCTX(zfield, ctx));
+            _ca_mpoly_q_reduce_ideal(CA_MPOLY_Q(res), zfield, ctx);
+        }
+
+        ca_condense_field(res, ctx);
+
+        return;
+    }
+
+    /* Coerce to a common field. */
+    {
+        ca_t t, u;
+
+        ca_init(t, ctx);
+        ca_init(u, ctx);
+
+        ca_merge_fields(t, u, x, y, ctx);
+        ca_sub(res, t, u, ctx);
+        ca_condense_field(res, ctx);
+
+        ca_clear(t, ctx);
+        ca_clear(u, ctx);
+    }
 }
 
 void
