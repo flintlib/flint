8f15e5de1997e159e264246612ae696931050b00
William Hart <goodwillhart@googlemail.com>
2020-11-19 17:11:38 +0100

    Remove unneeded prototype.

--- nmod_mat.h
+++ nmod_mat.h
@@ -193,9 +193,6 @@ FLINT_DLL void nmod_mat_addmul(nmod_mat_t D, const nmod_mat_t C,
 FLINT_DLL void nmod_mat_submul(nmod_mat_t D, const nmod_mat_t C,
                                 const nmod_mat_t A, const nmod_mat_t B);
 
-FLINT_DLL void nmod_mat_mul_tril_classical(nmod_mat_t C, const nmod_mat_t A,
-                                                            const nmod_mat_t B);
-
 /* Exponent */
 
 FLINT_DLL void _nmod_mat_pow(nmod_mat_t dest, const nmod_mat_t mat, ulong pow);
