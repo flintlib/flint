ecb073e69a63abca4418e2936da5dc9e7a772e55
William Hart <goodwillhart@googlemail.com>
2020-12-09 13:27:26 +0100

    Fixes #808

--- fq_zech/get_str_pretty.c
+++ fq_zech/get_str_pretty.c
@@ -15,8 +15,18 @@
 char *
 fq_zech_get_str_pretty(const fq_zech_t op, const fq_zech_ctx_t ctx)
 {
-    slong num_chars = op->value == 0 ? 1 : n_clog(op->value + 1, 10);
-    char * s = flint_malloc((num_chars + strlen(ctx->fq_nmod_ctx->var) + 2) * sizeof(char));
-    flint_sprintf(s, "%s^%wd", ctx->fq_nmod_ctx->var, op->value);
+    char * s;
+    
+    if (fq_zech_is_zero(op, ctx))
+    {
+        s = flint_malloc(2*sizeof(char));
+        flint_sprintf(s, "0");
+    } else
+    {
+       slong num_chars = op->value == 0 ? 1 : n_clog(op->value + 1, 10);
+       s = flint_malloc((num_chars + strlen(ctx->fq_nmod_ctx->var) + 2) * sizeof(char));
+       flint_sprintf(s, "%s^%wd", ctx->fq_nmod_ctx->var, op->value);
+    }
+    
     return s;
 }
