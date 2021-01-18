bee0180821ef615a6e6ef859c878fbc3807eeeaf
William Hart <goodwillhart@googlemail.com>
2021-01-18 07:16:04 +0100

    Update soname, version and make dist for version 2.7.1.

--- flint.h
+++ flint.h
@@ -56,8 +56,8 @@
 
 #define __FLINT_VERSION 2
 #define __FLINT_VERSION_MINOR 7
-#define __FLINT_VERSION_PATCHLEVEL 0
-#define FLINT_VERSION "2.7.0"
+#define __FLINT_VERSION_PATCHLEVEL 1
+#define FLINT_VERSION "2.7.1"
 #define __FLINT_RELEASE (__FLINT_VERSION * 10000 + \
                          __FLINT_VERSION_MINOR * 100 + \
                          __FLINT_VERSION_PATCHLEVEL)
