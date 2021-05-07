10c388dafbc4aabdd4925a4c18520202b068dc6b
William Hart <goodwillhart@googlemail.com>
2021-05-07 15:35:53 +0200

    Simplify fq_default_ctx_modulus in the fq case.

--- fq_default/ctx_modulus.c
+++ fq_default/ctx_modulus.c
@@ -15,34 +15,16 @@ void fq_default_ctx_modulus(fmpz_mod_poly_t p, const fq_default_ctx_t ctx)
 {
    if (ctx->type == 1)
    {
-      nmod_poly_t mod;
-      nmod_poly_init(mod, fmpz_get_ui(fq_zech_ctx_prime(ctx->ctx.fq_zech)));
-      nmod_poly_set(mod, fq_zech_ctx_modulus(ctx->ctx.fq_zech));
+      nmod_poly_struct const * mod = fq_zech_ctx_modulus(ctx->ctx.fq_zech);
       fmpz_mod_poly_set_nmod_poly(p, mod);
-      nmod_poly_clear(mod);
    } else if (ctx->type == 2)
    {
-      nmod_poly_t mod;
-      nmod_poly_init(mod, fmpz_get_ui(fq_nmod_ctx_prime(ctx->ctx.fq_nmod)));
-      nmod_poly_set(mod, fq_nmod_ctx_modulus(ctx->ctx.fq_nmod));
+      nmod_poly_struct const * mod = fq_nmod_ctx_modulus(ctx->ctx.fq_nmod);
       fmpz_mod_poly_set_nmod_poly(p, mod);
-      nmod_poly_clear(mod);
    } else
    {
-      fmpz_mod_poly_struct const * modulus = fq_ctx_modulus(ctx->ctx.fq);
-      slong len = modulus->length;
-
-      /* fit_length */
-      if (p->alloc < len)
-      {
-         slong alloc = FLINT_MAX(2*p->alloc, len);
-         p->coeffs = (fmpz *) flint_realloc(p->coeffs, alloc*sizeof(fmpz));
-         flint_mpn_zero((mp_ptr) (p->coeffs + p->alloc), alloc - p->alloc);
-      }
-
-      _fmpz_vec_set(p->coeffs, modulus->coeffs, len);
-
-      _fmpz_mod_poly_set_length(p, len);
+      fmpz_mod_ctx_struct const * mod = ctx->ctx.fq->ctxp;
+      fmpz_mod_poly_set(p, ctx->ctx.fq->modulus, mod);
    }
 }
 
