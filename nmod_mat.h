708f963d52e1151b1c9174272e27af5898a81450
William Hart <goodwillhart@googlemail.com>
2021-05-27 15:15:04 +0200

    Further deprecation of nmod_mat_scalar_mul_add to macro.

--- nmod_mat.h
+++ nmod_mat.h
@@ -171,14 +171,9 @@ FLINT_DLL void nmod_mat_scalar_mul(nmod_mat_t B, const nmod_mat_t A, mp_limb_t c
 FLINT_DLL void nmod_mat_scalar_addmul_ui(nmod_mat_t dest,
                        const nmod_mat_t X, const nmod_mat_t Y, const mp_limb_t b);
 
-NMOD_MAT_INLINE
-void nmod_mat_scalar_mul_add(nmod_mat_t dest, const nmod_mat_t X,
-                                const mp_limb_t b, const nmod_mat_t Y)
-{
-    flint_printf("WARNING: nmod_mat_scalar_mul_add is deprecated. "
-                 "Please use nmod_mat_scalar_addmul_ui\n");
-    nmod_mat_scalar_addmul_ui(dest, X, Y, b);
-}
+
+# deprecated
+#define nmod_mat_scalar_mul_add(dest, X, b, Y) nmod_mat_scalar_addmul_ui(dest, X, Y, b)
 
 NMOD_MAT_INLINE
 void nmod_mat_scalar_mul_fmpz(nmod_mat_t res, const nmod_mat_t M, const fmpz_t c)
