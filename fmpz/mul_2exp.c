e635efc343a1192b704aed5c6d6183300edfae11
Albin Ahlbäck <albin.ahlback@gmail.com>
2022-01-31 01:42:49 +0100

    Speed up fmpz_mul_2exp along with profiling code

--- fmpz/mul_2exp.c
+++ fmpz/mul_2exp.c
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2009 William Hart
+    Copyright (C) 2022 Albin Ahlbäck
 
     This file is part of FLINT.
 
@@ -9,38 +10,103 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include <gmp.h>
-#include "flint.h"
-#include "ulong_extras.h"
 #include "fmpz.h"
+#include <string.h>
 
 void
 fmpz_mul_2exp(fmpz_t f, const fmpz_t g, ulong exp)
 {
-    fmpz d = *g;
+    slong c1 = *g;
+    ulong c1abs, c1bits;
+    __mpz_struct * mf;
 
-    if (!COEFF_IS_MPZ(d))       /* g is small */
+    if (c1 == 0)
     {
-        ulong dabs = FLINT_ABS(d);
-        ulong bits = FLINT_BIT_COUNT(dabs);
-        if (bits == 0)
-        {
-            fmpz_set_si(f, 0);
-        }
-        else if (bits + exp <= FLINT_BITS - 2)  /* result will fit in small */
-        {
-            fmpz_set_si(f, d << exp);
-        }
-        else                    /* result is large */
-        {
-            __mpz_struct * mf = _fmpz_promote(f);   /* g is saved */
-            flint_mpz_set_si(mf, d);
-            mpz_mul_2exp(mf, mf, exp);
-        }
+        fmpz_zero(f);
+        return;
     }
-    else                        /* g is large */
+
+    c1abs = FLINT_ABS(c1);
+    c1bits = FLINT_BIT_COUNT(c1abs);
+
+    if (c1bits + exp <= FLINT_BITS - 2)  /* Result fits inside a small fmpz */
     {
-        __mpz_struct * mf = _fmpz_promote(f);   /* g is already large */
-        mpz_mul_2exp(mf, COEFF_TO_PTR(d), exp);
+        if (COEFF_IS_MPZ(*f))
+            _fmpz_clear_mpz(*f);
+
+        *f = c1 << exp;
+    }
+    else if (c1bits <= FLINT_BITS - 2)   /* g is small */
+    {
+        ulong expred = exp % GMP_NUMB_BITS;
+        int alloc = 1 + (FLINT_BITS != GMP_NUMB_BITS) + exp / GMP_NUMB_BITS
+            + ((c1bits + expred) > GMP_NUMB_BITS);
+        mp_limb_t * limbs;
+        slong tmp;
+
+        /* Ensure enough limbs are allocated for f */
+        if (!COEFF_IS_MPZ(*f))
+        {
+            /* TODO: Initialize the new mpz with alloc limbs instead of
+             * reallocating them. */
+            mf = _fmpz_new_mpz();
+            *f = PTR_TO_COEFF(mf);
+            _mpz_realloc(mf, alloc);
+        }
+        else
+        {
+            mf = COEFF_TO_PTR(*f);
+            if (mf->_mp_alloc < alloc)
+                _mpz_realloc(mf, alloc);
+        }
+        limbs = mf->_mp_d;
+        mf->_mp_size = (c1 > 0) ? alloc : -alloc;
+        memset(limbs, 0, sizeof(mp_limb_t) * (alloc - 1));
+
+        tmp = (slong) (c1bits + expred) - GMP_NUMB_BITS;
+        if (tmp <= 0)
+        {
+            limbs[alloc - 1] = c1abs << expred;
+#if FLINT_BITS != GMP_NUMB_BITS
+            return;
+#endif
+        }
+#if FLINT_BITS == GMP_NUMB_BITS
+        else
+        {
+            limbs[alloc - 1] = c1abs >> (FLINT_BITS - expred);
+            limbs[alloc - 2] = c1abs << expred;
+        }
+#else   /* FLINT_BITS == 2 * GMP_NUMB_BITS */
+        tmp -= GMP_NUMB_BITS;
+        if (tmp <= 0)
+        {
+            ulong * haha = (ulong *) (limbs + alloc - 2);
+            *haha = c1abs << expred;
+        }
+        else
+        {
+            ulong * haha = (ulong *) (limbs + alloc - 3);
+            *haha = c1abs << expred;
+            limbs[alloc - 1] = c1abs >> tmp;
+        }
+#endif
+    }
+    else                                /* g is large */
+    {
+        __mpz_struct * mg = COEFF_TO_PTR(c1);
+
+        if (!COEFF_IS_MPZ(*f))
+        {
+            /* TODO: Initialize the new mpz with alloc limbs instead of
+             * reallocating them. */
+            mf = _fmpz_new_mpz();
+            *f = PTR_TO_COEFF(mf);
+            _mpz_realloc(mf, FLINT_ABS(mg->_mp_size) + exp / GMP_NUMB_BITS + 1);
+        }
+        else
+            mf = COEFF_TO_PTR(*f);
+
+        mpz_mul_2exp(mf, mg, exp);
     }
 }
