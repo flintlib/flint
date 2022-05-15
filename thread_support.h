34eaae7130b02f4dd92daae01613e3291e3f3b30
Fredrik Johansson <fredrik.johansson@gmail.com>
2022-05-15 18:49:32 +0200

    generic parallel_do, parallel_binary_splitting; make thread_support a module

--- thread_support.h
+++ thread_support.h
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2020 Daniel Schultz
+    Copyright (C) 2022 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -26,6 +27,24 @@ FLINT_DLL slong flint_request_threads(thread_pool_handle ** handles,
 
 FLINT_DLL void flint_give_back_threads(thread_pool_handle * handles,
                                                             slong num_handles);
+
+#define FLINT_PARALLEL_UNIFORM 1
+#define FLINT_PARALLEL_STRIDED 2
+#define FLINT_PARALLEL_DYNAMIC 4
+#define FLINT_PARALLEL_BSPLIT_LEFT_INPLACE 8
+#define FLINT_PARALLEL_VERBOSE 512
+
+typedef void (* do_func_t)(slong i, void * args);
+
+FLINT_DLL void flint_parallel_do(do_func_t f, void * args, slong n, int thread_limit, int flags);
+
+typedef void (* bsplit_merge_func_t)(void *, void *, void *, void *);
+typedef void (* bsplit_basecase_func_t)(void *, slong, slong, void *);
+typedef void (* bsplit_init_func_t)(void *, void *);
+typedef void (* bsplit_clear_func_t)(void *, void *);
+
+FLINT_DLL void flint_parallel_binary_splitting(void * res, bsplit_basecase_func_t basecase, bsplit_merge_func_t merge,
+    size_t sizeof_res, bsplit_init_func_t init, bsplit_clear_func_t clear, void * args, slong a, slong b, slong basecase_cutoff, int thread_limit, int flags);
 
 #ifdef __cplusplus
 }
