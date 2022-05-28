107c43b0f440ea54de61bb69cf69646cec5f2325
Albin Ahlbäck <albin.ahlback@gmail.com>
2022-05-28 15:34:32 +0200

    Fix nmod_mpoly_factor's tests

--- nmod_mpoly_factor/test/t-factor_zassenhaus.c
+++ nmod_mpoly_factor/test/t-factor_zassenhaus.c
@@ -9,6 +9,7 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
+#include <stdio.h>
 #include "nmod_mpoly_factor.h"
 
 /* check total number of factors with multiplicity is between lower and upper */
