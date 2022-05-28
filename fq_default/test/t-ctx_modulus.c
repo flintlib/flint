8698a99d940a253895e701b772ec8c0073d32426
Albin Ahlbäck <albin.ahlback@gmail.com>
2022-05-28 15:37:55 +0200

    Fix fq_default's tests

--- fq_default/test/t-ctx_modulus.c
+++ fq_default/test/t-ctx_modulus.c
@@ -9,12 +9,9 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "fq_default.h"
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <gmp.h>
-#include "flint.h"
+#include "fq_default.h"
 #include "nmod_poly.h"
 #include "ulong_extras.h"
 
