a7686b201c646ccb1e3c49618402e73ab7ed4c21
Albin Ahlbäck <albin.ahlback@gmail.com>
2022-05-28 13:48:06 +0200

    Fix fq_poly_factor's tests

--- fq_poly_factor/test/t-factor.c
+++ fq_poly_factor/test/t-factor.c
@@ -13,9 +13,8 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "fq_poly.h"
-
-
+#include <stdio.h>
+#include "fq_poly_factor.h"
 
 #ifdef T
 #undef T
