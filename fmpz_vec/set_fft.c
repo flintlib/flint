691b9fe0a5fa71d72b54b10fc91feb4ddfd9714d
Albin Ahlbäck <albin.ahlback@gmail.com>
2022-06-04 00:50:19 +0200

    Fix fmpz_vec_set_fft

--- fmpz_vec/set_fft.c
+++ fmpz_vec/set_fft.c
@@ -9,12 +9,8 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include <stdlib.h>
-#include <gmp.h>
-#include "flint.h"
-#include "fmpz.h"
+#include "thread_support.h"
 #include "fmpz_vec.h"
-#include "fmpz_poly.h"
 #include "fft.h"
 
 static void _fmpz_vec_set_fft_coeff(fmpz * coeffs_m, slong i,
