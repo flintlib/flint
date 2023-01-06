a595d944e895d85a6b7ab1c585e547e4cdde8e7f
Fredrik Johansson <fredrik.johansson@gmail.com>
2023-01-06 17:25:27 +0100

    add a bunch of copyright headers

--- gr/fmpz_poly.c
+++ gr/fmpz_poly.c
@@ -1,3 +1,14 @@
+/*
+    Copyright (C) 2023 Fredrik Johansson
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.  See <http://www.gnu.org/licenses/>.
+*/
+
 #include "gr.h"
 #include "flint/fmpz_poly.h"
 #include "flint/fmpz_mat.h"
--- gr_mat.h
+++ gr_mat.h
@@ -1,3 +1,14 @@
+/*
+    Copyright (C) 2023 Fredrik Johansson
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.  See <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef GR_MAT_H
 #define GR_MAT_H
 
@@ -204,7 +215,7 @@ WARN_UNUSED_RESULT int gr_mat_pascal(gr_mat_t mat, int triangular, gr_ctx_t ctx)
 WARN_UNUSED_RESULT int gr_mat_stirling(gr_mat_t mat, int kind, gr_ctx_t ctx);
 WARN_UNUSED_RESULT int gr_mat_hilbert(gr_mat_t mat, gr_ctx_t ctx);
 WARN_UNUSED_RESULT int gr_mat_hadamard(gr_mat_t mat, gr_ctx_t ctx);
-/* todo: hadamard, dft, dct */
+/* todo: dft, dct */
 
 WARN_UNUSED_RESULT int gr_mat_transpose(gr_mat_t B, const gr_mat_t A, gr_ctx_t ctx);
 
