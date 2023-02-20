17fbf4444e60b503f1e4655aad48e4537e0859b4
Fredrik Johansson <fredrik.johansson@gmail.com>
2023-02-20 12:55:30 +0100

    missing copyright headers

--- gr_poly/test/t-hgcd.c
+++ gr_poly/test/t-hgcd.c
@@ -2,7 +2,9 @@
     Copyright (C) 2011 William Hart
     Copyright (C) 2011 Sebastian Pancratz
     Copyright (C) 2013 Mike Hansen
+
     This file is part of FLINT.
+
     FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
     by the Free Software Foundation; either version 2.1 of the License, or
