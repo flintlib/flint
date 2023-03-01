30cf6286bef4b4d012f4f6d6f1b91b3a2a3363c1
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-03-02 00:24:04 +0100

    Fuse fmpz_mpoly/equal*

--- src/fmpz_mpoly/equal.c
+++ src/fmpz_mpoly/equal.c
@@ -10,10 +10,6 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include <gmp.h>
-#include <stdlib.h>
-#include "flint.h"
-#include "fmpz.h"
 #include "fmpz_mpoly.h"
 
 int _fmpz_mpoly_equal(fmpz * poly1, ulong * exps1,
@@ -84,4 +80,61 @@ int fmpz_mpoly_equal(const fmpz_mpoly_t poly1, const fmpz_mpoly_t poly2,
       flint_free(ptr2);
 
    return r;
+}
+
+int fmpz_mpoly_equal_fmpz(const fmpz_mpoly_t A,
+                                    const fmpz_t c, const fmpz_mpoly_ctx_t ctx)
+{
+    slong N;
+
+    if (fmpz_is_zero(c))
+        return A->length == 0;
+
+    if (A->length != 1)
+        return 0;
+
+    N = mpoly_words_per_exp(A->bits, ctx->minfo);
+
+    if (!mpoly_monomial_is_zero(A->exps + N*0, N))
+        return 0;
+
+    return fmpz_equal(A->coeffs + 0, c);
+}
+
+int fmpz_mpoly_equal_si(const fmpz_mpoly_t A,
+                                           slong c, const fmpz_mpoly_ctx_t ctx)
+{
+    slong N;
+
+    if (c == 0)
+        return A->length == 0;
+
+    if (A->length != 1)
+        return 0;
+
+    N = mpoly_words_per_exp(A->bits, ctx->minfo);
+
+    if (!mpoly_monomial_is_zero(A->exps + N*0, N))
+        return 0;
+
+    return fmpz_equal_si(A->coeffs + 0, c);
+}
+
+int fmpz_mpoly_equal_ui(const fmpz_mpoly_t A,
+                                           ulong c, const fmpz_mpoly_ctx_t ctx)
+{
+    slong N;
+
+    if (c == 0)
+        return A->length == 0;
+
+    if (A->length != 1)
+        return 0;
+
+    N = mpoly_words_per_exp(A->bits, ctx->minfo);
+
+    if (!mpoly_monomial_is_zero(A->exps + N*0, N))
+        return 0;
+
+    return fmpz_equal_ui(A->coeffs + 0, c);
 }
