11c200bde4926fa0323c4b449a1ace27cd016125
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-03-02 00:55:18 +0100

    Fuse fmpz_mpoly/pow*

--- src/fmpz_mpoly/pow.c
+++ src/fmpz_mpoly/pow.c
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2016 William Hart
-    Copyright (C) 2017,2021 Daniel Schultz
+    Copyright (C) 2017, 2018, 2021 Daniel Schultz
 
     This file is part of FLINT.
 
@@ -10,9 +10,107 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "fmpz_mpoly.h"
 #include "long_extras.h"
-#include "fmpz_mod_mpoly.h"
+#include "fmpz_mpoly.h"
+
+int fmpz_mpoly_pow_ui(fmpz_mpoly_t A, const fmpz_mpoly_t B,
+                                           ulong k, const fmpz_mpoly_ctx_t ctx)
+{
+    if (B->length == 0)
+    {
+        fmpz_mpoly_set_ui(A, k == 0, ctx);
+        return 1;
+    }
+    else if (k <= 2)
+    {
+        if (k == 2)
+        {
+            fmpz_mpoly_mul(A, B, B, ctx);
+        }
+        else if (k == 1)
+        {
+            fmpz_mpoly_set(A, B, ctx);
+        }
+        else
+        {
+            fmpz_mpoly_one(A, ctx);
+        }
+        return 1;
+    }
+    else
+    {
+        ulong limit = (ulong)(WORD_MAX)/(ulong)(2*sizeof(fmpz));
+
+        if (B->length > 1 && k > limit/(ulong)(B->length - 1))
+            return 0;
+
+        fmpz_mpoly_pow_fps(A, B, k, ctx);
+        return 1;
+    }
+}
+
+int fmpz_mpoly_pow_fmpz(fmpz_mpoly_t A, const fmpz_mpoly_t B,
+                                    const fmpz_t k, const fmpz_mpoly_ctx_t ctx)
+{
+    slong i;
+    fmpz * maxBfields;
+    flint_bitcnt_t exp_bits;
+    TMP_INIT;
+
+    if (fmpz_sgn(k) < 0)
+        flint_throw(FLINT_ERROR, "Negative power in fmpz_mpoly_pow_fmpz");
+
+    if (fmpz_fits_si(k))
+        return fmpz_mpoly_pow_ui(A, B, fmpz_get_ui(k), ctx);
+
+    /*
+        we are raising a polynomial to an unreasonable exponent
+        It must either be zero or a monomial with unit coefficient
+    */
+
+    if (B->length == 0)
+    {
+        fmpz_mpoly_zero(A, ctx);
+        return 1;
+    }
+
+    if (B->length != 1)
+        return 0;
+
+    if (!fmpz_is_pm1(B->coeffs + 0))
+        return 0;
+
+    TMP_START;
+
+    maxBfields = (fmpz *) TMP_ALLOC(ctx->minfo->nfields*sizeof(fmpz));
+    for (i = 0; i < ctx->minfo->nfields; i++)
+        fmpz_init(maxBfields + i);
+    mpoly_max_fields_fmpz(maxBfields, B->exps, B->length, B->bits, ctx->minfo);
+
+    _fmpz_vec_scalar_mul_fmpz(maxBfields, maxBfields, ctx->minfo->nfields, k);
+
+    exp_bits = _fmpz_vec_max_bits(maxBfields, ctx->minfo->nfields);
+    exp_bits = FLINT_MAX(MPOLY_MIN_BITS, exp_bits + 1);
+    exp_bits = mpoly_fix_bits(exp_bits, ctx->minfo);
+
+    fmpz_mpoly_fit_length(A, 1, ctx);
+    fmpz_mpoly_fit_bits(A, exp_bits, ctx);
+    A->bits = exp_bits;
+
+    fmpz_set_si(A->coeffs + 0,
+                     (fmpz_is_one(B->coeffs + 0) || fmpz_is_even(k)) ? 1 : -1);
+
+    mpoly_pack_vec_fmpz(A->exps + 0, maxBfields, exp_bits, ctx->minfo->nfields, 1);
+
+    _fmpz_mpoly_set_length(A, 1, ctx);
+
+    for (i = 0; i < ctx->minfo->nfields; i++)
+        fmpz_clear(maxBfields + i);
+
+    TMP_END;
+
+    return 1;
+}
 
 /*
 Dense algorithm (reference) for the k^th power:
