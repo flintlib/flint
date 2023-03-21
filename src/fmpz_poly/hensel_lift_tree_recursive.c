bd6f121d85a9d913f662e6548b2fdc8b17bc52f5
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-03-21 05:38:36 +0100

    blabla

--- src/fmpz_poly/hensel_lift_tree_recursive.c
+++ src/fmpz_poly/hensel_lift_tree_recursive.c
@@ -18,24 +18,37 @@ void fmpz_poly_hensel_lift_tree_recursive(slong *link,
     fmpz_poly_t *v, fmpz_poly_t *w, fmpz_poly_t f, slong j, slong inv,
     const fmpz_t p0, const fmpz_t p1)
 {
+    flint_printf("enter fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
     if (j >= 0)
     {
         if (inv == 1)
+        {
+            flint_printf("mid1 fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
             fmpz_poly_hensel_lift(v[j], v[j + 1], w[j], w[j + 1], f,
                                   v[j], v[j + 1], w[j], w[j + 1],
                                   p0, p1);
+        }
         else if (inv == -1)
+        {
+            flint_printf("mid2 fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
             fmpz_poly_hensel_lift_only_inverse(w[j], w[j+1],
                                  v[j], v[j+1], w[j], w[j+1], p0, p1);
+        }
         else
+        {
+            flint_printf("mid3 fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
             fmpz_poly_hensel_lift_without_inverse(v[j], v[j+1], f,
                                                   v[j], v[j+1], w[j], w[j+1],
                                                   p0, p1);
+        }
 
+        flint_printf("mid4 fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
         fmpz_poly_hensel_lift_tree_recursive(link, v, w, v[j], link[j],
             inv, p0, p1);
+        flint_printf("mid5 fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
         fmpz_poly_hensel_lift_tree_recursive(link, v, w, v[j+1], link[j+1],
             inv, p0, p1);
     }
+    flint_printf("exit fmpz_poly_hensel_lift_tree_recursive\n"); fflush(stdout);
 }
 
