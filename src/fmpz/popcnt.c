9b251d6865d3554a3934ca2cb00597b287093b72
Fredrik Johansson <fredrik.johansson@gmail.com>
2023-03-22 14:11:21 +0100

    simplify fmpz_popcnt

--- src/fmpz/popcnt.c
+++ src/fmpz/popcnt.c
@@ -11,43 +11,28 @@
 
 #include "flint.h"
 #include "gmpcompat.h"
-#include "ulong_extras.h"
 #include "fmpz.h"
-
-#ifdef FLINT_USES_POPCNT
-static __inline__ flint_bitcnt_t shortCount(slong val)
-{
-#if defined(_WIN64) || defined(__mips64)
-   return __builtin_popcountll(val);
-#else
-   return __builtin_popcountl(val);
-#endif
-}
-#else
-/* A naive implementation if neither your processor nor your compiler want to
- * do the work. */
-static __inline__ flint_bitcnt_t shortCount(slong val)
-{
-        flint_bitcnt_t cnt;
-        for(cnt=0; val; val >>= 1) {
-                cnt += val & 1;
-        }
-        return cnt;
-}
-#endif
 
 flint_bitcnt_t fmpz_popcnt(const fmpz_t c)
 {
-        fmpz c1;
-        c1 = *c;
-        if(!COEFF_IS_MPZ(c1))
-        {
-                if(*c < 0) return 0;
-                else return shortCount(*c);
-        } else
-        {
-                __mpz_struct *t = COEFF_TO_PTR(c1);
-		if(flint_mpz_cmp_si(t,0) < 0) return 0;
-                else return mpz_popcount(t);
-        }
+    mp_limb_t d;
+    fmpz c1 = *c;
+
+    if (!COEFF_IS_MPZ(c1))
+    {
+        if (c1 < 0)
+            return 0;
+
+        d = c1;
+        return mpn_popcount(&d, 1);
+    }
+    else
+    {
+        __mpz_struct *t = COEFF_TO_PTR(c1);
+
+        if (mpz_sgn(t) < 0)
+            return 0;
+        else
+            return mpz_popcount(t);
+    }
 }
