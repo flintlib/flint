e6926b83a5df4d214dab02cbb162b43ed007ec16
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-03-23 13:26:46 +0100

    Group things together in fmpq.h

--- src/fmpq.h
+++ src/fmpq.h
@@ -101,35 +101,27 @@ FMPQ_INLINE void fmpq_abs(fmpq_t dest, const fmpq_t src)
 }
 
 int _fmpq_cmp(const fmpz_t p, const fmpz_t q, const fmpz_t r, const fmpz_t s);
-
 int fmpq_cmp(const fmpq_t x, const fmpq_t y);
 
 int _fmpq_cmp_fmpz(const fmpz_t p, const fmpz_t q, const fmpz_t r);
-
 int fmpq_cmp_fmpz(const fmpq_t x, const fmpz_t y);
 
 int _fmpq_cmp_ui(const fmpz_t p, const fmpz_t q, ulong c);
-
 int fmpq_cmp_ui(const fmpq_t x, ulong c);
 
 int _fmpq_cmp_si(const fmpz_t p, const fmpz_t q, slong c);
-
 int fmpq_cmp_si(const fmpq_t x, slong c);
 
 void _fmpq_canonicalise(fmpz_t num, fmpz_t den);
-
 void fmpq_canonicalise(fmpq_t res);
 
 int _fmpq_is_canonical(const fmpz_t num, const fmpz_t den);
-
 int fmpq_is_canonical(const fmpq_t x);
 
 void _fmpq_set_ui(fmpz_t rnum, fmpz_t rden, ulong p, ulong q);
-
 void fmpq_set_ui(fmpq_t res, ulong p, ulong q);
 
 void _fmpq_set_si(fmpz_t rnum, fmpz_t rden, slong p, ulong q);
-
 void fmpq_set_si(fmpq_t res, slong p, ulong q);
 
 FMPQ_INLINE int fmpq_equal_ui(fmpq_t q, slong n)
@@ -194,113 +186,75 @@ int _fmpq_print(const fmpz_t num, const fmpz_t den);
 int fmpq_print(const fmpq_t x);
 
 void _fmpq_randtest(fmpz_t num, fmpz_t den, flint_rand_t state, flint_bitcnt_t bits);
-
 void fmpq_randtest(fmpq_t res, flint_rand_t state, flint_bitcnt_t bits);
 
 void fmpq_randtest_not_zero(fmpq_t res, flint_rand_t state, flint_bitcnt_t bits);
 
 void _fmpq_randbits(fmpz_t num, fmpz_t den, flint_rand_t state, flint_bitcnt_t bits);
-
 void fmpq_randbits(fmpq_t res, flint_rand_t state, flint_bitcnt_t bits);
 
 void _fmpq_add_small(fmpz_t rnum, fmpz_t rden, slong p1, ulong q1, slong p2, ulong q2);
 
 void _fmpq_mul_small(fmpz_t rnum, fmpz_t rden, slong p1, ulong q1, slong p2, ulong q2);
 
-void _fmpq_add(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-    const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_add(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_add(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
 
-void _fmpq_add_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                                      const fmpz_t q, slong r);
-
+void _fmpq_add_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, slong r);
 void fmpq_add_si(fmpq_t res, const fmpq_t op1, slong c);
 
-void _fmpq_add_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-		                                      const fmpz_t q, ulong r);
-
+void _fmpq_add_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, ulong r);
 void fmpq_add_ui(fmpq_t res, const fmpq_t op1, ulong c);
 
-void _fmpq_add_fmpz(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                               const fmpz_t q, const fmpz_t r);
-
+void _fmpq_add_fmpz(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, const fmpz_t r);
 void fmpq_add_fmpz(fmpq_t res, const fmpq_t op1, const fmpz_t c);
 
-void _fmpq_sub(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-    const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_sub(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_sub(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
 
-void _fmpq_sub_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                                      const fmpz_t q, slong r);
-
+void _fmpq_sub_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, slong r);
 void fmpq_sub_si(fmpq_t res, const fmpq_t op1, slong c);
 
-void _fmpq_sub_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-		                                      const fmpz_t q, ulong r);
-
+void _fmpq_sub_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, ulong r);
 void fmpq_sub_ui(fmpq_t res, const fmpq_t op1, ulong c);
 
-void _fmpq_sub_fmpz(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                               const fmpz_t q, const fmpz_t r);
-
+void _fmpq_sub_fmpz(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, const fmpz_t r);
 void fmpq_sub_fmpz(fmpq_t res, const fmpq_t op1, const fmpz_t c);
 
-void _fmpq_mul_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                                      const fmpz_t q, slong r);
-
+void _fmpq_mul_si(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, slong r);
 void fmpq_mul_si(fmpq_t res, const fmpq_t op1, slong c);
 
-void _fmpq_mul_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p,
-                                                      const fmpz_t q, ulong r);
-
+void _fmpq_mul_ui(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, ulong r);
 void fmpq_mul_ui(fmpq_t res, const fmpq_t op1, ulong c);
 
-void _fmpq_mul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-    const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_mul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_mul(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
-
 
 void fmpq_mul_fmpz(fmpq_t res, const fmpq_t op, const fmpz_t x);
 
-void _fmpq_pow_si(fmpz_t rnum, fmpz_t rden,
-                  const fmpz_t opnum, const fmpz_t opden, slong e);
-
+void _fmpq_pow_si(fmpz_t rnum, fmpz_t rden, const fmpz_t opnum, const fmpz_t opden, slong e);
 void fmpq_pow_si(fmpq_t rop, const fmpq_t op, slong e);
 
 int fmpq_pow_fmpz(fmpq_t a, const fmpq_t b, const fmpz_t e);
 
-void _fmpq_addmul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-    const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_addmul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_addmul(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
 
-
-void _fmpq_submul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-    const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_submul(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_submul(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
-
 
 void fmpq_inv(fmpq_t dest, const fmpq_t src);
 
-void _fmpq_div(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num,
-                const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
-
+void _fmpq_div(fmpz_t rnum, fmpz_t rden, const fmpz_t op1num, const fmpz_t op1den, const fmpz_t op2num, const fmpz_t op2den);
 void fmpq_div(fmpq_t res, const fmpq_t op1, const fmpq_t op2);
 
 void fmpq_div_fmpz(fmpq_t res, const fmpq_t op, const fmpz_t x);
-
 
 void fmpq_mul_2exp(fmpq_t res, const fmpq_t x, flint_bitcnt_t exp);
 
 void fmpq_div_2exp(fmpq_t res, const fmpq_t x, flint_bitcnt_t exp);
 
-
 int _fmpq_mod_fmpz(fmpz_t res, const fmpz_t num, const fmpz_t den, const fmpz_t mod);
-
 int fmpq_mod_fmpz(fmpz_t res, const fmpq_t x, const fmpz_t mod);
 
 void _fmpq_gcd(fmpz_t rnum, fmpz_t rden, const fmpz_t p, const fmpz_t q, const fmpz_t r, const fmpz_t s);
@@ -310,53 +264,34 @@ void _fmpq_gcd_cofactors(fmpz_t ng, fmpz_t dg, fmpz_t A, fmpz_t B, const fmpz_t 
 void fmpq_gcd_cofactors(fmpq_t g, fmpz_t A, fmpz_t B, const fmpq_t a, const fmpq_t b);
 
 int _fmpq_reconstruct_fmpz(fmpz_t num, fmpz_t den, const fmpz_t a, const fmpz_t m);
-
 int fmpq_reconstruct_fmpz(fmpq_t res, const fmpz_t a, const fmpz_t m);
 
-int _fmpq_reconstruct_fmpz_2_naive(fmpz_t n, fmpz_t d,
-               const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
-
-int _fmpq_reconstruct_fmpz_2(fmpz_t n, fmpz_t d,
-               const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
-
-int fmpq_reconstruct_fmpz_2(fmpq_t res,
-               const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
+int _fmpq_reconstruct_fmpz_2_naive(fmpz_t n, fmpz_t d, const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
+int _fmpq_reconstruct_fmpz_2(fmpz_t n, fmpz_t d, const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
+int fmpq_reconstruct_fmpz_2(fmpq_t res, const fmpz_t a, const fmpz_t m, const fmpz_t N, const fmpz_t D);
 
 flint_bitcnt_t fmpq_height_bits(const fmpq_t x);
 
 void fmpq_height(fmpz_t height, const fmpq_t x);
 
-void _fmpq_next_calkin_wilf(fmpz_t rnum, fmpz_t rden,
-    const fmpz_t num, const fmpz_t den);
-
+void _fmpq_next_calkin_wilf(fmpz_t rnum, fmpz_t rden, const fmpz_t num, const fmpz_t den);
 void fmpq_next_calkin_wilf(fmpq_t res, const fmpq_t x);
 
-void _fmpq_next_signed_calkin_wilf(fmpz_t rnum, fmpz_t rden,
-    const fmpz_t num, const fmpz_t den);
-
+void _fmpq_next_signed_calkin_wilf(fmpz_t rnum, fmpz_t rden, const fmpz_t num, const fmpz_t den);
 void fmpq_next_signed_calkin_wilf(fmpq_t res, const fmpq_t x);
 
-void _fmpq_next_minimal(fmpz_t rnum, fmpz_t rden,
-    const fmpz_t num, const fmpz_t den);
-
+void _fmpq_next_minimal(fmpz_t rnum, fmpz_t rden, const fmpz_t num, const fmpz_t den);
 void fmpq_next_minimal(fmpq_t res, const fmpq_t x);
 
-void _fmpq_next_signed_minimal(fmpz_t rnum, fmpz_t rden,
-    const fmpz_t num, const fmpz_t den);
-
+void _fmpq_next_signed_minimal(fmpz_t rnum, fmpz_t rden, const fmpz_t num, const fmpz_t den);
 void fmpq_next_signed_minimal(fmpq_t res, const fmpq_t x);
 
-void fmpq_farey_neighbors(fmpq_t left, fmpq_t right,
-                                             const fmpq_t mid, const fmpz_t Q);
+void fmpq_farey_neighbors(fmpq_t left, fmpq_t right, const fmpq_t mid, const fmpz_t Q);
 
+void _fmpq_simplest_between(fmpz_t mid_num, fmpz_t mid_den, const fmpz_t l_num, const fmpz_t l_den, const fmpz_t r_num, const fmpz_t r_den);
 void fmpq_simplest_between(fmpq_t mid, const fmpq_t l, const fmpq_t r);
 
-void _fmpq_simplest_between(fmpz_t mid_num, fmpz_t mid_den,
-                                       const fmpz_t l_num, const fmpz_t l_den,
-                                       const fmpz_t r_num, const fmpz_t r_den);
-
 slong fmpq_get_cfrac_naive(fmpz * c, fmpq_t rem, const fmpq_t x, slong n);
-
 slong fmpq_get_cfrac(fmpz * c, fmpq_t rem, const fmpq_t x, slong n);
 
 void fmpq_set_cfrac(fmpq_t x, const fmpz * c, slong n);
@@ -364,11 +299,9 @@ void fmpq_set_cfrac(fmpq_t x, const fmpz * c, slong n);
 slong fmpq_cfrac_bound(const fmpq_t x);
 
 void fmpq_dedekind_sum_naive(fmpq_t s, const fmpz_t h, const fmpz_t k);
-
 void fmpq_dedekind_sum(fmpq_t s, const fmpz_t h, const fmpz_t k);
 
 void _fmpq_harmonic_ui(fmpz_t num, fmpz_t den, ulong n);
-
 void fmpq_harmonic_ui(fmpq_t x, ulong n);
 
 fmpq * _fmpq_vec_init(slong len);
@@ -478,11 +411,9 @@ FMPQ_INLINE void _fmpq_ball_swap(_fmpq_ball_t x, _fmpq_ball_t y)
 
 int _fmpq_ball_gt_one(const _fmpq_ball_t x);
 
-void _fmpq_hgcd(_fmpq_cfrac_list_t s, _fmpz_mat22_t M,
-                                                   fmpz_t x_num, fmpz_t x_den);
+void _fmpq_hgcd(_fmpq_cfrac_list_t s, _fmpz_mat22_t M, fmpz_t x_num, fmpz_t x_den);
 
-void _fmpq_ball_get_cfrac(_fmpq_cfrac_list_t s, _fmpz_mat22_t M,
-                                                    int needM, _fmpq_ball_t x);
+void _fmpq_ball_get_cfrac(_fmpq_cfrac_list_t s, _fmpz_mat22_t M, int needM, _fmpq_ball_t x);
 
 /* Inlines *******************************************************************/
 
