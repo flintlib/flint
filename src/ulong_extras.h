d8e373774ef69ce10a99d00c896aa86d3005a2c1
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-03-26 13:03:40 +0200

    FLINT_MAX -> FLINT_MIN in n_gcd

--- src/ulong_extras.h
+++ src/ulong_extras.h
@@ -316,7 +316,7 @@ ulong n_gcd(ulong x, ulong y)
         x >>= mx;
         y >>= my;
         res = (x != 1 && y != 1) ? mpn_gcd_11(x, y) : 1;
-        res <<= FLINT_MAX(mx, my);
+        res <<= FLINT_MIN(mx, my);
         return res;
 #else
         return mpn_gcd_1(&x, 1, y);
