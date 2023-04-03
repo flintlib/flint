452f2d1c9319d9b139bee6c8b7bc40486b6ff62b
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-04-03 18:29:44 +0200

    Fix blas functions

--- src/fmpz_mat/mul_blas.c
+++ src/fmpz_mat/mul_blas.c
@@ -9,20 +9,15 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include <stdint.h>
-#include "flint.h"
-#include "fmpz.h"
-#include "fmpz_vec.h"
 #include "fmpz_mat.h"
-#include "ulong_extras.h"
-#include "n_poly.h"
-#include "mpn_extras.h"
 
 #if FLINT_USES_BLAS && FLINT_BITS == 64
 
-#include "thread_pool.h"
+#include <stdint.h>
+#include "nmod.h"
+#include "fmpz.h"
+#include "thread_support.h"
 #include "cblas.h"
-
 
 typedef struct {
     slong m;
