6f296a62d5f8764094687495ef5f9c267f3b0a67
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-04-03 22:51:25 +0200

    Merge ulong_extras/factor_trial*

--- src/ulong_extras/factor_trial.c
+++ src/ulong_extras/factor_trial.c
@@ -9,10 +9,63 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
 #include "ulong_extras.h"
 
 mp_limb_t n_factor_trial(n_factor_t * factors, mp_limb_t n, ulong num_primes)
 {
     return n_factor_trial_range(factors, n, UWORD(0), num_primes);
+}
+
+mp_limb_t n_factor_trial_partial(n_factor_t * factors, mp_limb_t n, mp_limb_t * prod, ulong num_primes, mp_limb_t limit)
+{
+   unsigned int exp;
+   mp_limb_t p;
+   double ppre;
+   ulong i;
+   const mp_limb_t * primes;
+   const double * inverses;
+
+   (*prod) = 1;
+   primes = n_primes_arr_readonly(num_primes);
+   inverses = n_prime_inverses_arr_readonly(num_primes);
+
+   for (i = 0; i < num_primes; i++)
+   {
+      p = primes[i];
+      if (p*p > n) break;
+      ppre = inverses[i];
+      exp = n_remove2_precomp(&n, p, ppre);
+      if (exp)
+      {
+         n_factor_insert(factors, p, exp);
+         (*prod) *= n_pow(p, exp);
+         if (*prod > limit) break;
+      }
+   }
+
+   return n;
+}
+
+mp_limb_t n_factor_trial_range(n_factor_t * factors, mp_limb_t n, ulong start, ulong num_primes)
+{
+   unsigned int exp;
+   mp_limb_t p;
+   double ppre;
+   ulong i;
+   const mp_limb_t * primes;
+   const double * inverses;
+
+   primes = n_primes_arr_readonly(num_primes);
+   inverses = n_prime_inverses_arr_readonly(num_primes);
+
+   for (i = start; i < num_primes; i++)
+   {
+      p = primes[i];
+      if (p*p > n) break;
+      ppre = inverses[i];
+      exp = n_remove2_precomp(&n, p, ppre);
+      if (exp) n_factor_insert(factors, p, exp);
+   }
+
+   return n;
 }
