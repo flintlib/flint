c9498061d69b45c75ebaf43d156a8a82bede4166
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-04-03 23:43:44 +0200

    Merge ulong_extras/nth_prime*

--- src/ulong_extras/nth_prime.c
+++ src/ulong_extras/nth_prime.c
@@ -9,7 +9,6 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
 #include "ulong_extras.h"
 
 mp_limb_t n_nth_prime(ulong n)
@@ -23,3 +22,22 @@ mp_limb_t n_nth_prime(ulong n)
     return n_primes_arr_readonly(n)[n-1];
 }
 
+void n_nth_prime_bounds(mp_limb_t *lo, mp_limb_t *hi, ulong n)
+{
+    int bits, ll;
+    double llo, lhi;
+
+    /* Lower and upper bounds for ln(n) */
+    bits = FLINT_BIT_COUNT(n);
+    llo = (bits-1) * 0.6931471;
+    lhi = bits * 0.6931472;
+
+    /* Lower bound for ln(ln(n)) */
+    if      (n < 16)        ll = 0;
+    else if (n < 1619)      ll = 1;
+    else if (n < 528491312) ll = 2;
+    else                    ll = 3;
+
+    *lo = (mp_limb_t) (n * (llo + ll - 1));
+    *hi = (mp_limb_t) (n * (lhi + (ll+1) - (n >= 15985 ? 0.9427 : 0.0)));
+}
