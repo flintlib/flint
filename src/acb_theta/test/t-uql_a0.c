40d091639025694f9e302beed11f0ad12b3a31ee
Jean <jean.kieffer@math.cnrs.fr>
2023-07-18 10:55:01 +0200

    Remove nb_z from uql_a0

--- src/acb_theta/test/t-uql_a0.c
+++ src/acb_theta/test/t-uql_a0.c
@@ -28,16 +28,15 @@ int main(void)
         slong n = 1 << g;
         slong prec = 200 + n_randint(state, 200);
         slong bits = n_randint(state, 5);
-        slong nb_z = 1 + n_randint(state, 2);
         acb_mat_t tau;
         acb_ptr z, th, test;
         slong k;
         ulong a;
         
         acb_mat_init(tau, g, g);
-        z = _acb_vec_init(g * nb_z);
-        th = _acb_vec_init(n * nb_z);
-        test = _acb_vec_init(n * nb_z);
+        z = _acb_vec_init(g);
+        th = _acb_vec_init(n);
+        test = _acb_vec_init(n);
 
         /* Possibly generate large imaginary parts for tau and z */
         acb_siegel_randtest_reduced(tau, state, prec, bits);
@@ -46,39 +45,36 @@ int main(void)
             acb_mul_2exp_si(acb_mat_entry(tau, g - 1, g - 1),
                 acb_mat_entry(tau, g - 1, g - 1), 10);
         }
-        for (k = 0; k < nb_z * g; k++)
+        for (k = 0; k < g; k++)
         {
             acb_urandom(&z[k], state, prec);
         }
-        acb_theta_uql_a0(th, z, nb_z, tau, prec);
+        acb_theta_uql_a0(th, z, tau, prec);
         for (a = 0; a < n; a++)
         {
-            for (k = 0; k < nb_z; k++)
-            {
-                acb_theta_naive_ind(test + k * n + a, a << g, z + k * g, 1, tau, prec);
-            }
+            acb_theta_naive_ind(&test[a], a << g, z, 1, tau, prec);
         }
 
-        if (!_acb_vec_overlaps(th, test, n * nb_z)
+        if (!_acb_vec_overlaps(th, test, n)
             || !acb_is_finite(&th[0]))
         {
             flint_printf("FAIL\n");
-            flint_printf("g = %wd, prec = %wd, nb_z = %wd, tau:\n", g, prec, nb_z);
+            flint_printf("g = %wd, prec = %wd, tau:\n", g, prec);
             acb_mat_printd(tau, 10);
             flint_printf("z:\n");
-            _acb_vec_printd(z, g * nb_z, 10);
+            _acb_vec_printd(z, g, 10);
             flint_printf("\nvalues:\n");
-            _acb_vec_printd(th, n * nb_z, 10);
+            _acb_vec_printd(th, n, 10);
             flint_printf("\n");
-            _acb_vec_printd(test, n * nb_z, 10);
+            _acb_vec_printd(test, n, 10);
             flint_printf("\n");
             flint_abort();
         }
         
         acb_mat_clear(tau);
-        _acb_vec_clear(z, g * nb_z);
-        _acb_vec_clear(th, n * nb_z);
-        _acb_vec_clear(test, n * nb_z);
+        _acb_vec_clear(z, g);
+        _acb_vec_clear(th, n);
+        _acb_vec_clear(test, n);
     }
     
     flint_randclear(state);
--- src/acb_theta/uql_a0.c
+++ src/acb_theta/uql_a0.c
@@ -42,7 +42,7 @@ acb_theta_uql_cut(const arb_mat_t cho, const arf_t R2, slong prec)
     return res;
 }
 
-static void acb_theta_uql_a0_basecase(acb_ptr th, acb_srcptr z, slong nb_z,
+static void acb_theta_uql_a0_basecase(acb_ptr th, acb_srcptr z,
     const acb_mat_t tau, slong g, slong prec)
 {
     acb_mat_t tau_rec;
@@ -50,10 +50,7 @@ static void acb_theta_uql_a0_basecase(acb_ptr th, acb_srcptr z, slong nb_z,
 
     if (g == 0)
     {
-        for (k = 0; k < nb_z; k++)
-        {
-            acb_one(&th[k]);
-        }
+        acb_one(&th[0]);
     }
     else
     {
@@ -65,11 +62,12 @@ static void acb_theta_uql_a0_basecase(acb_ptr th, acb_srcptr z, slong nb_z,
                 acb_set(acb_mat_entry(tau_rec, j, k), acb_mat_entry(tau, j, k));
             }
         }
-        acb_theta_ql_a0(th, z, nb_z, tau_rec, prec);        
+        acb_theta_ql_a0(th, z, 1, tau_rec, prec);
         acb_mat_clear(tau_rec);
     }
 }
 
+/* If return 0, leave pt unchanged */
 static int
 acb_theta_uql_a0_has_pt(slong *pt, acb_srcptr z, const arb_mat_t Yinv,
     const arb_mat_t cho, const arf_t R2, ulong a, slong prec)
@@ -124,34 +122,31 @@ acb_theta_uql_a0_has_pt(slong *pt, acb_srcptr z, const arb_mat_t Yinv,
 }
 
 static void
-acb_theta_uql_a0_rec(acb_ptr th, acb_srcptr z, slong nb_z, const acb_mat_t tau,
+acb_theta_uql_a0_rec(acb_ptr th, acb_srcptr z, const acb_mat_t tau,
     const arb_mat_t cho, const arf_t R2, slong g, slong d, slong prec)
 {
-    acb_ptr th_rec, z_rec, fac;
+    acb_ptr th_rec, z_rec;
+    acb_t fac;
     arb_mat_t Yinv;
-    slong* ind_z_rec;
-    slong nb_z_rec;
     slong n, pt;
-    ulong a;
     slong k, j;
-    int has_pt;
+    int has_pt_0, has_pt_1;
+    ulong a;
 
-    flint_printf("(uql_a0_rec) calling with d = %wd, g = %wd, nb_z = %wd\n", d, g, nb_z);
+    flint_printf("(uql_a0_rec) calling with d = %wd, g = %wd\n", d, g);
 
     if (d == g)
     {
-        acb_theta_uql_a0_basecase(th, z, nb_z, tau, g, prec);
+        acb_theta_uql_a0_basecase(th, z, tau, g, prec);
         return;
     }
     
-    /* Recursive call, perhaps doubling nb_z to account for a = 0 and 1 */
+    /* Recursive call */
     n = 1 << (g - 1);
-    th_rec = _acb_vec_init(2 * nb_z * n);
-    z_rec = _acb_vec_init(2 * (g - 1) * nb_z);
-    nb_z_rec = 0;
-    ind_z_rec = flint_malloc(2 * nb_z * sizeof(slong));
-    fac = _acb_vec_init(2 * nb_z);
+    th_rec = _acb_vec_init(n);
+    z_rec = _acb_vec_init(g - 1);
     arb_mat_init(Yinv, g, g);
+    acb_init(fac);
 
     /* Set Yinv */
     for (k = 0; k < g; k++)
@@ -163,80 +158,54 @@ acb_theta_uql_a0_rec(acb_ptr th, acb_srcptr z, slong nb_z, const acb_mat_t tau,
     }
     arb_mat_inv(Yinv, Yinv, prec);
 
-    /* Collect z_rec and cofactors */
-    for (k = 0; k < nb_z; k++)
-    {        
-        for (a = 0; a <= 1; a++)
-        {
-            /* Get lattice points */
-            has_pt = acb_theta_uql_a0_has_pt(&pt, z + k * g, Yinv, cho, R2, a,
-                ACB_THETA_ELD_DEFAULT_PREC);
-            
-            if (has_pt)
-            {
-                ind_z_rec[2 * k + a] = nb_z_rec;
-                flint_printf("(uql_a0) pt = %wd\n", pt);
-                
-                /* Get new vector z */
-                _acb_vec_set(z_rec + (g - 1) * nb_z_rec, z + k * g, g - 1);
-                for (j = 0; j < g - 1; j++)
-                {
-                    acb_addmul_si(z_rec + (g - 1) * nb_z_rec + j,
-                        acb_mat_entry(tau, j, g), pt, prec);
-                }
-                /* Get cofactor */
-                acb_mul_si(&fac[2 * k + a], acb_mat_entry(tau, g - 1, g - 1), pt, prec);
-                acb_addmul_si(&fac[2 * k + a], &z[k * g + (g - 1)], 2, prec);
-                acb_mul_si(&fac[2 * k + a], &fac[2 * k + a], pt, prec);
-                acb_exp_pi_i(&fac[2 * k + a], &fac[2 * k + a], prec);
-                
-                nb_z_rec += 1;
-            }
-            else
-            {
-                ind_z_rec[2 * k + a] = -1;
-                
-                for (j = 0; j < n; j++)
-                {
-                    acb_zero(&th[k * 2 * n + 2 * j + a]);
-                }
-            }
-        }
-    }
+    has_pt_0 = acb_theta_uql_a0_has_pt(&pt, z, Yinv, cho, R2, 0,
+        ACB_THETA_ELD_DEFAULT_PREC);
+    has_pt_1 = acb_theta_uql_a0_has_pt(&pt, z, Yinv, cho, R2, 1,
+        ACB_THETA_ELD_DEFAULT_PREC);
     
-    /* Recursive call and reconstruct theta values */
-    acb_theta_uql_a0_rec(th_rec, z_rec, nb_z_rec, tau, cho, R2, g - 1, d, prec);
-    
-    flint_printf("th_rec:\n");
-    _acb_vec_printd(th_rec, nb_z_rec * n, 10);
-    flint_printf("\n");
-
-    for (k = 0; k < nb_z; k++)
+    if (has_pt_0 && has_pt_1)
     {
-        for (a = 0; a <= 1; a++)
+        flint_printf("(uql_a0) Error: two points_n");
+        flint_abort();
+    }
+    else if (!has_pt_0 && !has_pt_1)
+    {
+        _acb_vec_zero(th, 2 * n);
+    }
+    else /* One point */
+    {
+        a = has_pt_0 ? 0 : 1;
+        _acb_vec_set(z_rec, z, g - 1);
+        for (j = 0; j < g - 1; j++)
         {
-            if (ind_z_rec[2 * k + a] == -1)
-            {
-                continue;
-            }
-            for (j = 0; j < n; j++)
-            {
-                acb_mul(&th[k * 2 * n + 2 * j + a], &fac[2 * k + a],
-                    &th_rec[ind_z_rec[2 * k + a] * n + j], prec);
-            }
+            acb_addmul_si(&z_rec[j], acb_mat_entry(tau, j, g), pt, prec);
+        }
+        acb_mul_si(fac, acb_mat_entry(tau, g - 1, g - 1), pt, prec);
+        acb_addmul_si(fac, &z[g - 1], 2, prec);
+        acb_mul_si(fac, fac, pt, prec);
+        acb_exp_pi_i(fac, fac, prec);
+
+        acb_theta_uql_a0_rec(th_rec, z_rec, tau, cho, R2, g - 1, d, prec);
+        
+        flint_printf("th_rec:\n");
+        _acb_vec_printd(th_rec, n, 10);
+        flint_printf("\n");
+
+        for (j = 0; j < n; j++)
+        {
+            acb_mul(&th[2 * j + a], &th_rec[j], fac, prec);
+            acb_zero(&th[2 * j + ((a + 1) % 2)]);
         }
     }
     
-    _acb_vec_clear(th_rec, 2 * nb_z * n);
-    _acb_vec_clear(z_rec, 2 * (g - 1) * nb_z);
-    flint_free(ind_z_rec);
-    _acb_vec_clear(fac, 2 * nb_z);
+    _acb_vec_clear(th_rec, n);
+    _acb_vec_clear(z_rec, g - 1);
     arb_mat_clear(Yinv);
+    acb_clear(fac);
 }
 
 void
-acb_theta_uql_a0(acb_ptr th, acb_srcptr z, slong nb_z,
-    const acb_mat_t tau, slong prec)
+acb_theta_uql_a0(acb_ptr th, acb_srcptr z, const acb_mat_t tau, slong prec)
 {
     slong g = acb_mat_nrows(tau);
     arb_mat_t Y;
@@ -251,7 +220,7 @@ acb_theta_uql_a0(acb_ptr th, acb_srcptr z, slong nb_z,
     arb_mat_init(cho, g, g);
     arb_init(pi);
     arf_init(R2);
-    arf_init(eps);    
+    arf_init(eps);
 
     acb_mat_get_imag(Y, tau);
     arb_const_pi(pi, prec);
@@ -264,7 +233,7 @@ acb_theta_uql_a0(acb_ptr th, acb_srcptr z, slong nb_z,
     acb_theta_naive_radius(R2, Y, ord, eps, prec);
 
     d = acb_theta_uql_cut(cho, R2, ACB_THETA_ELD_DEFAULT_PREC);
-    acb_theta_uql_a0_rec(th, z, nb_z, tau, cho, R2, g, d, prec);
+    acb_theta_uql_a0_rec(th, z, tau, cho, R2, g, d, prec);
 
     arb_mat_clear(Y);
     arb_mat_clear(cho);
