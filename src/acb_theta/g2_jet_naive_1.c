bf962ce2e4f007963a6129ef3eeb8c75b3c7cd7a
Jean <jean.kieffer@math.cnrs.fr>
2023-09-20 11:04:09 -0400

    Add acb_dot strategy in g2_jet_naive_1

--- src/acb_theta/g2_jet_naive_1.c
+++ src/acb_theta/g2_jet_naive_1.c
@@ -10,6 +10,9 @@
 */
 
 #include "acb_theta.h"
+#include "profiler.h"
+
+#define ACB_THETA_G2_JET_NAIVE_1_THRESHOLD 100
 
 static void
 worker_dim0(acb_ptr dth, slong nb, const acb_t term, slong* coords, slong g,
@@ -47,14 +50,17 @@ worker_dim1(acb_ptr dth, acb_srcptr v1, acb_srcptr v2, const slong* precs, slong
     const acb_t cofactor, const slong* coords, slong ord, slong g, slong prec, slong fullprec)
 {
     slong n = 1 << g;
-    acb_ptr v3, aux;
+    acb_ptr v3, aux, sums_1, sums_2, diffs;
     acb_t x;
     slong a0, a1, b;
     slong* dots;
-    slong i, ind;
+    slong i, ind0, ind1;
 
     v3 = _acb_vec_init(len);
     aux = _acb_vec_init(2 * 3 * n);
+    sums_1 = _acb_vec_init(4);
+    sums_2 = _acb_vec_init(4);
+    diffs = _acb_vec_init(8);
     dots = flint_malloc(n * sizeof(slong));
     acb_init(x);
 
@@ -65,27 +71,111 @@ worker_dim1(acb_ptr dth, acb_srcptr v1, acb_srcptr v2, const slong* precs, slong
     {
         dots[b] = acb_theta_char_dot_slong(b, coords, g);
     }
-    for (i = 0; i < len; i++)
-    {
-        acb_mul(&v3[i], &v1[i], &v2[i], precs[i]);
-    }
 
-    /* Main loop */
-    for (i = 0; i < len; i++)
+    if (len > ACB_THETA_G2_JET_NAIVE_1_THRESHOLD)
     {
+        /* Store multiplications in v3 */
+        for (i = 0; i < len; i++)
+        {
+            acb_mul(&v3[i], &v1[i], &v2[i], precs[i]);
+        }
+        /* Main loop */
+        for (i = 0; i < len; i++)
+        {
+            for (b = 0; b < n; b++)
+            {
+                acb_mul_powi(x, &v3[i], (dots[b] + i * (b >> (g - 1))) % 4);
+                ind0 = 3 * n * (i % 2) + 3 * b;
+                if ((dots[b] + i * (b >> (g - 1))) % 2 == 0)
+                {
+                    acb_add(&aux[ind0], &aux[ind0], x, prec);
+                }
+                else
+                {
+                    acb_addmul_si(&aux[ind0 + 1], x, coords[0] + i, prec);
+                    acb_addmul_si(&aux[ind0 + 2], x, coords[1], prec);
+                }
+            }
+        }
+    }
+    else
+    {
+        /* Compute dot products and sum them appropriately */
+        for (i = 0; i < len; i++)
+        {
+            acb_mul_si(&v3[i], &v2[i], coords[0] + i, precs[i]);
+        }
+        for (i = 0; i < 4; i++)
+        {
+            acb_dot(&sums_1[i], NULL, 0, v1 + i, 4, v2 + i, 4, (len + 3 - i) / 4, prec);
+            acb_dot(&sums_2[i], NULL, 0, v1 + i, 4, v3 + i, 4, (len + 3 - i) / 4, prec);
+        }
+        acb_add(&diffs[0], &sums_1[0], &sums_1[2], prec);
+        acb_add(&diffs[1], &sums_1[1], &sums_1[3], prec);
+        acb_sub(&diffs[2], &sums_1[0], &sums_1[2], prec);
+        acb_sub(&diffs[3], &sums_1[1], &sums_1[3], prec);
+        acb_add(&diffs[4], &sums_2[0], &sums_2[2], prec);
+        acb_add(&diffs[5], &sums_2[1], &sums_2[3], prec);
+        acb_sub(&diffs[6], &sums_2[0], &sums_2[2], prec);
+        acb_sub(&diffs[7], &sums_2[1], &sums_2[3], prec);
+
+        /* Loop over b */
         for (b = 0; b < n; b++)
         {
-            acb_mul_powi(x, &v3[i], (dots[b] + i * (b >> (g - 1))) % 4);
-            ind = 3 * n * (i % 2) + 3 * b;
-            if ((dots[b] + i * (b >> (g - 1))) % 2 == 0)
+            ind0 = 3 * b;
+            ind1 = 3 * (n + b);
+            if ((b >> (g - 1)) == 0)
             {
-                acb_add(&aux[ind], &aux[ind], x, prec);
+                if (dots[b] % 2 == 0)
+                {
+                    /* All even */
+                    acb_mul_powi(x, &diffs[0], dots[b]);
+                    acb_add(&aux[ind0], &aux[ind0], x, prec);
+                    acb_mul_powi(x, &diffs[1], dots[b]);
+                    acb_add(&aux[ind1], &aux[ind1], x, prec);
+                }
+                else
+                {
+                    /* All odd; use v3 for derivative wrt z1 */
+                    acb_mul_powi(x, &diffs[4], dots[b]);
+                    acb_add(&aux[ind0 + 1], &aux[ind0 + 1], x, prec);
+                    acb_mul_powi(x, &diffs[0], dots[b]);
+                    acb_add(&aux[ind0 + 2], &aux[ind0 + 2], x, prec);
+                    acb_mul_powi(x, &diffs[5], dots[b]);
+                    acb_add(&aux[ind1 + 1], &aux[ind1 + 1], x, prec);
+                    acb_mul_powi(x, &diffs[1], dots[b]);
+                    acb_add(&aux[ind1 + 2], &aux[ind1 + 2], x, prec);
+                }
             }
             else
             {
-                acb_addmul_si(&aux[ind + 1], x, coords[0] + i, prec);
-                acb_addmul_si(&aux[ind + 2], x, coords[1], prec);
+                /* Alternating, with different signs for a0 and a1 */
+                if (dots[b] % 2 == 0)
+                {
+                    /* a0 even, a1 odd */
+                    acb_mul_powi(x, &diffs[2], dots[b]);
+                    acb_add(&aux[ind0], &aux[ind0], x, prec);
+                    acb_mul_powi(x, &diffs[7], dots[b] + 1);
+                    acb_add(&aux[ind1 + 1], &aux[ind1 + 1], x, prec);
+                    acb_mul_powi(x, &diffs[3], dots[b] + 1);
+                    acb_add(&aux[ind1 + 2], &aux[ind1 + 2], x, prec);
+                }
+                else
+                {
+                    /* a0 odd, a1 even */
+                    acb_mul_powi(x, &diffs[3], dots[b] + 1);
+                    acb_add(&aux[ind1], &aux[ind1], x, prec);
+                    acb_mul_powi(x, &diffs[6], dots[b]);
+                    acb_add(&aux[ind0 + 1], &aux[ind0 + 1], x, prec);
+                    acb_mul_powi(x, &diffs[2], dots[b]);
+                    acb_add(&aux[ind0 + 2], &aux[ind0 + 2], x, prec);
+                }
             }
+        }
+        /* Multiply d/dz2 entries by coords[1] */
+        for (i = 0; i < 2 * n; i++)
+        {
+            acb_mul_si(&aux[3 * i + 2], &aux[3 * i + 2], coords[1], prec);
         }
     }
 
@@ -107,9 +197,13 @@ worker_dim1(acb_ptr dth, acb_srcptr v1, acb_srcptr v2, const slong* precs, slong
 
     _acb_vec_clear(v3, len);
     _acb_vec_clear(aux, 2 * 3 * n);
+    _acb_vec_clear(sums_1, 4);
+    _acb_vec_clear(sums_2, 4);
+    _acb_vec_clear(diffs, 8);
     flint_free(dots);
     acb_clear(x);
 }
+
 
 void
 acb_theta_g2_jet_naive_1(acb_ptr dth, const acb_mat_t tau, slong prec)
