ecf20029cff1923efb2ae4eb7fb4d2d856f9a3d0
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-11 16:48:04 +0200

    Merge nmod_poly taylor_shift

--- src/nmod_poly/taylor_shift.c
+++ src/nmod_poly/taylor_shift.c
@@ -9,6 +9,8 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
+#include "nmod.h"
+#include "nmod_vec.h"
 #include "nmod_poly.h"
 
 void
@@ -29,4 +31,105 @@ nmod_poly_taylor_shift(nmod_poly_t g, const nmod_poly_t f, mp_limb_t c)
         nmod_poly_set(g, f);
 
     _nmod_poly_taylor_shift(g->coeffs, c, g->length, g->mod);
+}
+
+void
+_nmod_poly_taylor_shift_convolution(mp_ptr p, mp_limb_t c, slong len, nmod_t mod)
+{
+    slong i, n = len - 1;
+    mp_limb_t f, d;
+    mp_ptr t, u;
+
+    if (c == 0 || len <= 1)
+        return;
+
+    t = _nmod_vec_init(len);
+    u = _nmod_vec_init(len);
+
+    f = 1;
+    for (i = 2; i <= n; i++)
+    {
+        f = n_mulmod2_preinv(f, i, mod.n, mod.ninv);
+        p[i] = n_mulmod2_preinv(p[i], f, mod.n, mod.ninv);
+    }
+
+    _nmod_poly_reverse(p, p, len, len);
+
+    t[n] = 1;
+    for (i = n; i > 0; i--)
+        t[i - 1] = n_mulmod2_preinv(t[i], i, mod.n, mod.ninv);
+
+    if (c == mod.n - 1)
+    {
+        for (i = 1; i <= n; i += 2)
+            t[i] = nmod_neg(t[i], mod);
+    }
+    else if (c != 1)
+    {
+        d = c;
+
+        for (i = 1; i <= n; i++)
+        {
+            t[i] = n_mulmod2_preinv(t[i], d, mod.n, mod.ninv);
+            d = n_mulmod2_preinv(d, c, mod.n, mod.ninv);
+        }
+    }
+
+    _nmod_poly_mullow(u, p, len, t, len, len, mod);
+
+    f = n_mulmod2_preinv(f, f, mod.n, mod.ninv);
+    f = n_invmod(f, mod.n);
+
+    for (i = n; i >= 0; i--)
+    {
+        p[i] = n_mulmod2_preinv(u[n - i], f, mod.n, mod.ninv);
+        f = n_mulmod2_preinv(f, (i == 0) ? 1 : i, mod.n, mod.ninv);
+    }
+
+    _nmod_vec_clear(t);
+    _nmod_vec_clear(u);
+}
+
+void
+nmod_poly_taylor_shift_convolution(nmod_poly_t g, const nmod_poly_t f,
+    mp_limb_t c)
+{
+    if (f != g)
+        nmod_poly_set(g, f);
+
+    _nmod_poly_taylor_shift_convolution(g->coeffs, c, g->length, g->mod);
+}
+
+void
+_nmod_poly_taylor_shift_horner(mp_ptr poly, mp_limb_t c, slong n, nmod_t mod)
+{
+    slong i, j;
+
+    if (c == 1)
+    {
+        for (i = n - 2; i >= 0; i--)
+            for (j = i; j < n - 1; j++)
+                poly[j] = nmod_add(poly[j], poly[j + 1], mod);
+    }
+    else if (c == mod.n - 1)
+    {
+        for (i = n - 2; i >= 0; i--)
+            for (j = i; j < n - 1; j++)
+                poly[j] = nmod_sub(poly[j], poly[j + 1], mod);
+    }
+    else if (c != 0)
+    {
+        for (i = n - 2; i >= 0; i--)
+            for (j = i; j < n - 1; j++)
+                NMOD_ADDMUL(poly[j], poly[j + 1], c, mod);
+    }
+}
+
+void
+nmod_poly_taylor_shift_horner(nmod_poly_t g, const nmod_poly_t f, mp_limb_t c)
+{
+    if (f != g)
+        nmod_poly_set(g, f);
+
+    _nmod_poly_taylor_shift_horner(g->coeffs, c, g->length, g->mod);
 }
