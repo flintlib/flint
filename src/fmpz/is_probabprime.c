6375d8631379ec08d1f4fdd51a6e5d1c8ae297f0
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-11 17:24:34 +0200

    Merge fmpz is_probabprime

--- src/fmpz/is_probabprime.c
+++ src/fmpz/is_probabprime.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012 William Hart
+    Copyright (C) 2012, 2014 William Hart
     Copyright (C) 2020 Fredrik Johansson
 
     This file is part of FLINT.
@@ -10,10 +10,9 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
+#include "mpn_extras.h"
 #include "ulong_extras.h"
 #include "fmpz.h"
-#include "mpn_extras.h"
 
 int
 fmpz_is_probabprime(const fmpz_t n)
@@ -55,4 +54,90 @@ fmpz_is_probabprime(const fmpz_t n)
 
         return fmpz_is_probabprime_BPSW(n);
     }
+}
+
+int fmpz_is_probabprime_BPSW(const fmpz_t n)
+{
+   fmpz_t b;
+   int res = 1;
+
+   fmpz_init_set_ui(b, 2);
+
+   if (!fmpz_is_strong_probabprime(n, b) || !fmpz_is_probabprime_lucas(n))
+      res = 0;
+
+   fmpz_clear(b);
+
+   return res;
+}
+
+int fmpz_is_probabprime_lucas(const fmpz_t n)
+{
+   fmpz_t A, Q, D, t, m, Vm, Vm1;
+   int res = 0;
+
+   if (fmpz_cmp_ui(n, 1) <= 0)
+      return 0;
+
+   if (fmpz_is_even(n))
+      return fmpz_cmp_ui(n, 2) == 0; /* ensure 2, n coprime */
+
+   if (fmpz_is_square(n))
+      return 0;
+
+   fmpz_init(A);
+   fmpz_init(Q);
+   fmpz_init(D);
+   fmpz_init(t);
+   fmpz_init(m);
+   fmpz_init(Vm);
+   fmpz_init(Vm1);
+
+   fmpz_set_si(D, WORD(-3)); /* -3 becomes 5 after first iteration */
+
+   do {
+      /* check D = 5, -7, 9, -11 such that (D/n) = -1 */
+      do {
+         if (fmpz_sgn(D) > 0)
+            fmpz_add_ui(D, D, 2);
+         else
+            fmpz_sub_ui(D, D, 2);
+
+         fmpz_neg(D, D);
+      } while (fmpz_jacobi(D, n) != -1); /* this ensures D, n coprime */
+
+      fmpz_sub_ui(t, D, 1);
+      fmpz_neg(t, t);
+      fmpz_tdiv_q_2exp(Q, t, 2);
+
+      fmpz_gcd(t, Q, n); /* require Q, n coprime */
+   } while (fmpz_equal(t, n));
+
+   if (fmpz_is_one(t)) /* check no factor found */
+   {
+      fmpz_invmod(A, Q, n); /* A = P^2Q^-1 - 2 mod n, where P = 1 */
+      fmpz_sub_ui(A, A, 2);
+      if (fmpz_sgn(A) < 0)
+         fmpz_add(A, A, n);
+
+      fmpz_add_ui(m, n, 1); /* m = (n - jacobi(D/n))/2 = (n + 1)/2 */
+      fmpz_tdiv_q_2exp(m, m, 1);
+
+      fmpz_lucas_chain(Vm, Vm1, A, m, n);
+
+      fmpz_mul(Vm, Vm, A);
+      fmpz_submul_ui(Vm, Vm1, 2);
+
+      res = fmpz_divisible(Vm, n);
+   }
+
+   fmpz_clear(A);
+   fmpz_clear(Q);
+   fmpz_clear(D);
+   fmpz_clear(t);
+   fmpz_clear(m);
+   fmpz_clear(Vm);
+   fmpz_clear(Vm1);
+
+   return res;
 }
