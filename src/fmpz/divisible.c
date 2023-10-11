7bdc5cf7807119341177080ad6041833b9af4aa2
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-11 17:09:26 +0200

    Merge fmpz divisible

--- src/fmpz/divisible.c
+++ src/fmpz/divisible.c
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2011 Sebastian Pancratz
+    Copyright (C) 2022 Albin Ahlbäck
 
     This file is part of FLINT.
 
@@ -9,7 +10,6 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
 #include "gmpcompat.h"
 #include "ulong_extras.h"
 #include "fmpz.h"
@@ -53,3 +53,16 @@ int fmpz_divisible(const fmpz_t x, const fmpz_t p)
     }
 }
 
+int
+fmpz_divisible_si(const fmpz_t f, slong g)
+{
+    slong c = *f;
+
+    if (c == WORD(0))
+        return 1;
+
+    if (!COEFF_IS_MPZ(c))
+        return !(c % g);
+    else
+        return flint_mpz_divisible_ui_p(COEFF_TO_PTR(c), FLINT_ABS(g));
+}
