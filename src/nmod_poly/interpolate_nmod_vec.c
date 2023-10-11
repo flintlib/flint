05cd06adff58f6e2bdd7363d462a8b7f5d282c71
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-11 16:11:43 +0200

    Merge nmod_poly interpolate_nmod_vec

--- src/nmod_poly/interpolate_nmod_vec.c
+++ src/nmod_poly/interpolate_nmod_vec.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2011 Fredrik Johansson
+    Copyright (C) 2011, 2012 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -9,6 +9,8 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
+#include "nmod.h"
+#include "nmod_vec.h"
 #include "nmod_poly.h"
 
 void
@@ -36,6 +38,263 @@ nmod_poly_interpolate_nmod_vec(nmod_poly_t poly,
         nmod_poly_fit_length(poly, n);
         poly->length = n;
         _nmod_poly_interpolate_nmod_vec(poly->coeffs,
+            xs, ys, n, poly->mod);
+        _nmod_poly_normalise(poly);
+    }
+}
+
+void
+_nmod_poly_interpolate_nmod_vec_barycentric(mp_ptr poly,
+                            mp_srcptr xs, mp_srcptr ys, slong n, nmod_t mod)
+{
+    mp_ptr P, Q, w;
+    slong i, j;
+
+    if (n == 1)
+    {
+        poly[0] = ys[0];
+        return;
+    }
+
+    P = _nmod_vec_init(n + 1);
+    Q = _nmod_vec_init(n);
+    w = _nmod_vec_init(n);
+
+    _nmod_poly_product_roots_nmod_vec(P, xs, n, mod);
+
+    for (i = 0; i < n; i++)
+    {
+        w[i] = UWORD(1);
+        for (j = 0; j < n; j++)
+        {
+            if (i != j)
+                w[i] = nmod_mul(w[i], nmod_sub(xs[i], xs[j], mod), mod);
+        }
+        w[i] = n_invmod(w[i], mod.n);
+    }
+
+    _nmod_vec_zero(poly, n);
+
+    for (i = 0; i < n; i++)
+    {
+        _nmod_poly_div_root(Q, P, n + 1, xs[i], mod);
+        _nmod_vec_scalar_addmul_nmod(poly, Q, n,
+            nmod_mul(w[i], ys[i], mod), mod);
+    }
+
+    _nmod_vec_clear(P);
+    _nmod_vec_clear(Q);
+    _nmod_vec_clear(w);
+}
+
+void
+nmod_poly_interpolate_nmod_vec_barycentric(nmod_poly_t poly,
+                                    mp_srcptr xs, mp_srcptr ys, slong n)
+{
+    if (n == 0)
+    {
+        nmod_poly_zero(poly);
+    }
+    else
+    {
+        nmod_poly_fit_length(poly, n);
+        poly->length = n;
+        _nmod_poly_interpolate_nmod_vec_barycentric(poly->coeffs,
+            xs, ys, n, poly->mod);
+        _nmod_poly_normalise(poly);
+    }
+}
+
+void
+_nmod_poly_interpolation_weights(mp_ptr w, const mp_ptr * tree, slong len, nmod_t mod)
+{
+    mp_ptr tmp;
+    slong i, n, height;
+
+    if (len == 0)
+        return;
+
+    if (len == 1)
+    {
+        w[0] = 1;
+        return;
+    }
+
+    tmp = _nmod_vec_init(len + 1);
+    height = FLINT_CLOG2(len);
+    n = WORD(1) << (height - 1);
+
+    _nmod_poly_mul(tmp, tree[height-1], n + 1,
+                        tree[height-1] + (n + 1), (len - n + 1), mod);
+
+    _nmod_poly_derivative(tmp, tmp, len + 1, mod);
+    _nmod_poly_evaluate_nmod_vec_fast_precomp(w, tmp, len, tree, len, mod);
+
+    for (i = 0; i < len; i++)
+        w[i] = n_invmod(w[i], mod.n);
+
+    _nmod_vec_clear(tmp);
+}
+
+void
+_nmod_poly_interpolate_nmod_vec_fast_precomp(mp_ptr poly, mp_srcptr ys,
+    const mp_ptr * tree, mp_srcptr weights, slong len, nmod_t mod)
+{
+    mp_ptr t, u, pa, pb;
+    slong i, pow, left;
+
+    if (len == 0)
+        return;
+
+    t = _nmod_vec_init(len);
+    u = _nmod_vec_init(len);
+
+    for (i = 0; i < len; i++)
+        poly[i] = nmod_mul(weights[i], ys[i], mod);
+
+    for (i = 0; i < FLINT_CLOG2(len); i++)
+    {
+        pow = (WORD(1) << i);
+        pa = tree[i];
+        pb = poly;
+        left = len;
+
+        while (left >= 2 * pow)
+        {
+            _nmod_poly_mul(t, pa, pow + 1, pb + pow, pow, mod);
+            _nmod_poly_mul(u, pa + pow + 1, pow + 1, pb, pow, mod);
+            _nmod_vec_add(pb, t, u, 2 * pow, mod);
+
+            left -= 2 * pow;
+            pa += 2 * pow + 2;
+            pb += 2 * pow;
+        }
+
+        if (left > pow)
+        {
+            _nmod_poly_mul(t, pa, pow + 1, pb + pow, left - pow, mod);
+            _nmod_poly_mul(u, pb, pow, pa + pow + 1, left - pow + 1, mod);
+            _nmod_vec_add(pb, t, u, left, mod);
+        }
+    }
+
+    _nmod_vec_clear(t);
+    _nmod_vec_clear(u);
+}
+
+
+void
+_nmod_poly_interpolate_nmod_vec_fast(mp_ptr poly,
+                            mp_srcptr xs, mp_srcptr ys, slong len, nmod_t mod)
+{
+    mp_ptr * tree;
+    mp_ptr w;
+
+    tree = _nmod_poly_tree_alloc(len);
+    _nmod_poly_tree_build(tree, xs, len, mod);
+
+    w = _nmod_vec_init(len);
+    _nmod_poly_interpolation_weights(w, tree, len, mod);
+
+    _nmod_poly_interpolate_nmod_vec_fast_precomp(poly, ys, tree, w, len, mod);
+
+    _nmod_vec_clear(w);
+    _nmod_poly_tree_free(tree, len);
+}
+
+void
+nmod_poly_interpolate_nmod_vec_fast(nmod_poly_t poly,
+                                    mp_srcptr xs, mp_srcptr ys, slong n)
+{
+    if (n == 0)
+    {
+        nmod_poly_zero(poly);
+    }
+    else
+    {
+        nmod_poly_fit_length(poly, n);
+        poly->length = n;
+        _nmod_poly_interpolate_nmod_vec_fast(poly->coeffs,
+            xs, ys, n, poly->mod);
+        _nmod_poly_normalise(poly);
+    }
+}
+
+static void
+_interpolate_newton(mp_ptr ys, mp_srcptr xs, slong n, nmod_t mod)
+{
+    mp_limb_t p, q, t;
+    slong i, j;
+
+    for (i = 1; i < n; i++)
+    {
+        t = ys[i - 1];
+
+        for (j = i; j < n; j++)
+        {
+            p = nmod_sub(ys[j], t, mod);
+            q = nmod_sub(xs[j], xs[j - i], mod);
+            t = ys[j];
+            q = n_invmod(q, mod.n);
+            ys[j] = n_mulmod2_preinv(p, q, mod.n, mod.ninv);
+        }
+    }
+}
+
+static void
+_newton_to_monomial(mp_ptr ys, mp_srcptr xs, slong n, nmod_t mod)
+{
+    mp_limb_t t;
+    slong i, j;
+
+    for (i = n - 2; i >= 0; i--)
+    {
+        t = ys[i];
+        ys[i] = ys[i + 1];
+
+        for (j = i + 1; j < n - 1; j++)
+        {
+            ys[j] = nmod_sub(ys[j + 1],
+                n_mulmod2_preinv(ys[j], xs[i], mod.n, mod.ninv), mod);
+        }
+
+        ys[n - 1] = nmod_sub(t,
+            n_mulmod2_preinv(ys[n - 1], xs[i], mod.n, mod.ninv), mod);
+    }
+
+    _nmod_poly_reverse(ys, ys, n, n);
+}
+
+void
+_nmod_poly_interpolate_nmod_vec_newton(mp_ptr poly, mp_srcptr xs,
+    mp_srcptr ys, slong n, nmod_t mod)
+{
+    if (n == 1)
+    {
+        poly[0] = ys[0];
+    }
+    else
+    {
+        _nmod_vec_set(poly, ys, n);
+        _interpolate_newton(poly, xs, n, mod);
+        while (n > 0 && !poly[n-1]) n--;
+        _newton_to_monomial(poly, xs, n, mod);
+    }
+}
+
+void
+nmod_poly_interpolate_nmod_vec_newton(nmod_poly_t poly,
+                                    mp_srcptr xs, mp_srcptr ys, slong n)
+{
+    if (n == 0)
+    {
+        nmod_poly_zero(poly);
+    }
+    else
+    {
+        nmod_poly_fit_length(poly, n);
+        poly->length = n;
+        _nmod_poly_interpolate_nmod_vec_newton(poly->coeffs,
             xs, ys, n, poly->mod);
         _nmod_poly_normalise(poly);
     }
