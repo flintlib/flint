11b1fe18cfc3e1f7f1fe1754ae4ce8ad023d741f
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-18 11:51:42 +0200

    Unify acf tests

--- src/acf/test/t-approx_dot.c
+++ src/acf/test/t-approx_dot.c
@@ -9,18 +9,13 @@
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
+#include "test_helpers.h"
 #include "acf.h"
 #include "acb.h"
 
-int main(void)
+TEST_FUNCTION_START(acf_approx_dot, state)
 {
     slong iter;
-    flint_rand_t state;
-
-    flint_printf("approx_dot....");
-    fflush(stdout);
-
-    flint_randinit(state);
 
     for (iter = 0; iter < 10000 * 0.1 * flint_test_multiplier(); iter++)
     {
@@ -237,8 +232,5 @@ int main(void)
         _acf_vec_clear(y, len);
     }
 
-    flint_randclear(state);
-    flint_cleanup();
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
