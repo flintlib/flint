0951d89658627516c0b348480543564cc1e6545e
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-18 16:17:49 +0200

    Unify ca_ext tests

--- src/ca_ext/test/t-cache_insert.c
+++ src/ca_ext/test/t-cache_insert.c
@@ -9,18 +9,13 @@
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
+#include "test_helpers.h"
 #include "ca.h"
 #include "ca_ext.h"
 
-int main(void)
+TEST_FUNCTION_START(ca_ext_cache_insert, state)
 {
     slong iter;
-    flint_rand_t state;
-
-    flint_printf("cache_insert....");
-    fflush(stdout);
-
-    flint_randinit(state);
 
     for (iter = 0; iter < 1000 * 0.1 * flint_test_multiplier(); iter++)
     {
@@ -62,8 +57,5 @@ int main(void)
         qqbar_clear(x);
     }
 
-    flint_randclear(state);
-    flint_cleanup();
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
