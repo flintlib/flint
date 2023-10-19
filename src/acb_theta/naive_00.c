9cdd2a777a2cb91709b2945115b5f4e45dc74d24
Jean <jean.kieffer@math.cnrs.fr>
2023-10-19 12:50:12 -0400

    Remove naive_00_direct

--- src/acb_theta/naive_00.c
+++ src/acb_theta/naive_00.c
@@ -12,36 +12,6 @@
 #include "acb_theta.h"
 
 static void
-acb_theta_naive_00_direct(acb_ptr th, acb_srcptr cs, arb_srcptr us, const acb_theta_eld_t E,
-    acb_srcptr new_zs, slong nb, const acb_mat_t tau, slong prec)
-{
-    slong g = acb_mat_nrows(tau);
-    slong nb_pts = acb_theta_eld_nb_pts(E);
-    slong* pts;
-    acb_t t;
-    slong j, k;
-
-    acb_init(t);
-    pts = flint_malloc(nb_pts * g * sizeof(slong));
-
-    acb_theta_eld_points(pts, E);
-    for (k = 0; k < nb; k++)
-    {
-        acb_zero(&th[k]);
-        for (j = 0; j < nb_pts; j++)
-        {
-            acb_theta_naive_term(t, new_zs + k * g, tau, NULL, pts + j * g, prec);
-            acb_add(&th[k], &th[k], t, prec);
-        }
-        acb_mul(&th[k], &th[k], &cs[k], prec);
-        acb_add_error_arb(&th[k], &us[k]);
-    }
-
-    acb_clear(t);
-    flint_free(pts);
-}
-
-static void
 worker(acb_ptr th, acb_srcptr v1, acb_srcptr v2, const slong* precs, slong len,
     const acb_t cofactor, const slong* coords, slong ord, slong g, slong prec, slong fullprec)
 {
@@ -64,7 +34,6 @@ acb_theta_naive_00_gen(acb_ptr th, acb_srcptr zs, slong nb, const acb_mat_t tau,
     acb_ptr cs;
     arb_ptr us;
     acb_ptr new_zs;
-    mag_t test;
     slong k;
 
     acb_theta_eld_init(E, g, g);
@@ -72,7 +41,6 @@ acb_theta_naive_00_gen(acb_ptr th, acb_srcptr zs, slong nb, const acb_mat_t tau,
     cs = _acb_vec_init(nb);
     us = _arb_vec_init(nb);
     new_zs = _acb_vec_init(g * nb);
-    mag_init(test);
 
     acb_theta_naive_ellipsoid(E, new_zs, cs, us, zs, nb, tau, prec);
     prec = acb_theta_naive_fullprec(E, prec);
@@ -83,19 +51,11 @@ acb_theta_naive_00_gen(acb_ptr th, acb_srcptr zs, slong nb, const acb_mat_t tau,
         acb_theta_naive_worker(&th[k], 1, &cs[k], &us[k], E, D, k, 0, prec, worker);
     }
 
-    arb_get_mag(test, &us[0]);
-    mag_mul_2exp_si(test, test, 50);
-    if (mag_cmp(test, arb_radref(acb_realref(&th[0]))) < 0)
-    {
-        acb_theta_naive_00_direct(th, cs, us, E, new_zs, nb, tau, prec);
-    }
-
     acb_theta_eld_clear(E);
     acb_theta_precomp_clear(D);
     _acb_vec_clear(cs, nb);
     _arb_vec_clear(us, nb);
     _acb_vec_clear(new_zs, g * nb);
-    mag_clear(test);
 }
 
 void
