c130d469417d5b258e6539d5b44b8a6982bc6c82
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-31 10:21:40 +0100

    Unify mpf_vec tests

--- src/mpf_vec/test/t-set_fmpz_vec.c
+++ src/mpf_vec/test/t-set_fmpz_vec.c
@@ -11,20 +11,13 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
-#include "fmpz.h"
+#include "test_helpers.h"
 #include "fmpz_vec.h"
 #include "mpf_vec.h"
-#include "ulong_extras.h"
 
-int
-main(void)
+TEST_FUNCTION_START(mpf_vec_set_fmpz_vec, state)
 {
     int i;
-    FLINT_TEST_INIT(state);
-
-    flint_printf("set_fmpz_vec....");
-    fflush(stdout);
 
     for (i = 0; i < 1000 * flint_test_multiplier(); i++)
     {
@@ -61,8 +54,5 @@ main(void)
         _mpf_vec_clear(d2, len);
     }
 
-    FLINT_TEST_CLEANUP(state);
-
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
