c778c249f95837cf0161f66c4448c1af109d6e66
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-31 10:17:20 +0100

    Unify mpfr_mat tests

--- src/mpfr_mat/test/t-mul_classical.c
+++ src/mpfr_mat/test/t-mul_classical.c
@@ -10,18 +10,12 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
+#include "test_helpers.h"
 #include "mpfr_mat.h"
-#include "ulong_extras.h"
 
-int
-main(void)
+TEST_FUNCTION_START(mpfr_mat_mul_classical, state)
 {
     slong i;
-    FLINT_TEST_INIT(state);
-
-    flint_printf("mul_classical....");
-    fflush(stdout);
 
     /* Check aliasing C and A */
     for (i = 0; i < 100 * flint_test_multiplier(); i++)
@@ -87,8 +81,5 @@ main(void)
         mpfr_mat_clear(C);
     }
 
-    FLINT_TEST_CLEANUP(state);
-
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
