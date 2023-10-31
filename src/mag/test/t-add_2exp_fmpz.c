b69f210da14ceaab87b8f42b9369877f729c725d
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-10-31 10:03:40 +0100

    Unify mag tests

--- src/mag/test/t-add_2exp_fmpz.c
+++ src/mag/test/t-add_2exp_fmpz.c
@@ -9,18 +9,13 @@
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
+#include "test_helpers.h"
 #include "arf.h"
 #include "mag.h"
 
-int main(void)
+TEST_FUNCTION_START(mag_add_2exp_fmpz, state)
 {
     slong iter;
-    flint_rand_t state;
-
-    flint_printf("add_2exp_fmpz....");
-    fflush(stdout);
-
-    flint_randinit(state);
 
     for (iter = 0; iter < 100000 * 0.1 * flint_test_multiplier(); iter++)
     {
@@ -80,8 +75,5 @@ int main(void)
         fmpz_clear(e);
     }
 
-    flint_randclear(state);
-    flint_cleanup();
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
--- src/mag/test/t-fast_mul_2exp_si.c
+++ src/mag/test/t-fast_mul_2exp_si.c
@@ -9,18 +9,13 @@
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
 */
 
+#include "test_helpers.h"
 #include "arf.h"
 #include "mag.h"
 
-int main(void)
+TEST_FUNCTION_START(mag_fast_mul_2exp_si, state)
 {
     slong iter;
-    flint_rand_t state;
-
-    flint_printf("fast_mul_2exp_si....");
-    fflush(stdout);
-
-    flint_randinit(state);
 
     for (iter = 0; iter < 100000 * 0.1 * flint_test_multiplier(); iter++)
     {
@@ -64,8 +59,5 @@ int main(void)
         mag_clear(yb);
     }
 
-    flint_randclear(state);
-    flint_cleanup();
-    flint_printf("PASS\n");
-    return 0;
+    TEST_FUNCTION_END(state);
 }
