5b3307572c411e78c099a584045f319a65e24c19
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-10 12:33:02 +0100

    Cleanup d_mat.h

--- src/d_mat/swap.c
+++ src/d_mat/swap.c
@@ -17,3 +17,16 @@ d_mat_swap(d_mat_t mat1, d_mat_t mat2)
 {
     FLINT_SWAP(d_mat_struct, *mat1, *mat2);
 }
+
+void
+d_mat_swap_entrywise(d_mat_t mat1, d_mat_t mat2)
+{
+    slong i, j;
+    for (i = 0; i < d_mat_nrows(mat1); i++)
+    {
+        double * row1 = mat1->rows[i];
+        double * row2 = mat2->rows[i];
+        for (j = 0; j < d_mat_ncols(mat1); j++)
+            FLINT_SWAP(double, row1[j], row2[j]);
+    }
+}
