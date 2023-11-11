b8ce01b3eb8e78e9125023934052066c3170b667
Fabian Gundlach <git@fabiangundlach.org>
2023-11-11 16:15:17 +0100

    Reset nmod_poly_factor_t before writing to it

--- src/nmod_poly_factor/factor_squarefree.c
+++ src/nmod_poly_factor/factor_squarefree.c
@@ -22,9 +22,10 @@ nmod_poly_factor_squarefree(nmod_poly_factor_t res, const nmod_poly_t f)
     mp_limb_t p;
     slong deg, i;
 
+    res->num = 0;
+
     if (f->length <= 1)
     {
-        res->num = 0;
         return;
     }
 
