676471ddc043c3c0eb94bde9ad71cc295ed3d907
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-11 16:36:01 +0100

    Cleanup fq_vec_templates.h

--- src/fq_vec_templates.h
+++ src/fq_vec_templates.h
@@ -14,21 +14,17 @@
 #include "templates.h"
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /*  Memory management  *******************************************************/
 
 TEMPLATE(T, struct) * _TEMPLATE(T, vec_init)(slong len, const TEMPLATE(T, ctx_t) ctx);
-
-void _TEMPLATE(T, vec_clear)(TEMPLATE(T, struct) * vec, slong len,
-                             const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_clear)(TEMPLATE(T, struct) * vec, slong len, const TEMPLATE(T, ctx_t) ctx);
 
 /*  Randomisation  ***********************************************************/
-void _TEMPLATE(T, vec_randtest)(TEMPLATE(T, struct) * f,
-                           flint_rand_t state,
-                           slong len,
-                           const TEMPLATE(T, ctx_t) ctx);
+
+void _TEMPLATE(T, vec_randtest)(TEMPLATE(T, struct) * f, flint_rand_t state, slong len, const TEMPLATE(T, ctx_t) ctx);
 
 /*  Norms  *******************************************************************/
 
@@ -43,73 +39,38 @@ int _TEMPLATE(T, vec_print)(const TEMPLATE(T, struct) * vec, slong len, const TE
 /*  Conversions  *************************************************************/
 
 /*  Assignment and basic manipulation  ***************************************/
-void _TEMPLATE(T, vec_set)(TEMPLATE(T, struct) * v,
-                      const TEMPLATE(T, struct) * f,
-                      slong len, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, vec_swap)(TEMPLATE(T, struct) * vec1,
-                       TEMPLATE(T, struct) * vec2,
-                       slong len2, const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_set)(TEMPLATE(T, struct) * v, const TEMPLATE(T, struct) * f, slong len, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, vec_zero)(TEMPLATE(T, struct) * v,
-                       slong len,
-                       const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_swap)(TEMPLATE(T, struct) * vec1, TEMPLATE(T, struct) * vec2, slong len2, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, vec_neg)(TEMPLATE(T, struct) * vec1,
-                      const TEMPLATE(T, struct) * vec2,
-                      slong len2,
-                      const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_zero)(TEMPLATE(T, struct) * v, slong len, const TEMPLATE(T, ctx_t) ctx);
 
 /*  Comparison  **************************************************************/
-int _TEMPLATE(T, vec_is_zero)(const TEMPLATE(T, struct) * vec, slong len,
-                          const TEMPLATE(T, ctx_t) ctx);
 
-int _TEMPLATE(T, vec_equal)(const TEMPLATE(T, struct) * vec1,
-                        const TEMPLATE(T, struct) * vec2, slong len,
-                        const TEMPLATE(T, ctx_t) ctx);
+int _TEMPLATE(T, vec_is_zero)(const TEMPLATE(T, struct) * vec, slong len, const TEMPLATE(T, ctx_t) ctx);
+
+int _TEMPLATE(T, vec_equal)(const TEMPLATE(T, struct) * vec1, const TEMPLATE(T, struct) * vec2, slong len, const TEMPLATE(T, ctx_t) ctx);
 
 /*  Sorting  *****************************************************************/
 
 /*  Addition  ****************************************************************/
 
-void _TEMPLATE(T, vec_add)(TEMPLATE(T, struct) * res,
-                           const TEMPLATE(T, struct) * vec1,
-                           const TEMPLATE(T, struct) * vec2,
-                           slong len2,
-                           const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_add)(TEMPLATE(T, struct) * res, const TEMPLATE(T, struct) * vec1, const TEMPLATE(T, struct) * vec2, slong len2, const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_sub)(TEMPLATE(T, struct) * res, const TEMPLATE(T, struct) * vec1, const TEMPLATE(T, struct) * vec2, slong len2, const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_neg)(TEMPLATE(T, struct) * vec1, const TEMPLATE(T, struct) * vec2, slong len2, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, vec_sub)(TEMPLATE(T, struct) * res,
-                           const TEMPLATE(T, struct) * vec1,
-                           const TEMPLATE(T, struct) * vec2,
-                           slong len2,
-                           const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, TEMPLATE(vec_scalar_addmul, T))(TEMPLATE(T, struct) * poly1, const TEMPLATE(T, struct) * poly2, slong len2, const TEMPLATE(T, t) x, const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, TEMPLATE(vec_scalar_submul, T))(TEMPLATE(T, struct) * poly1, const TEMPLATE(T, struct) * poly2, slong len2, const TEMPLATE(T, t) x, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, TEMPLATE(vec_scalar_addmul, T))(TEMPLATE(T, struct) * poly1,
-                                             const TEMPLATE(T, struct) * poly2,
-                                             slong len2,
-                                             const TEMPLATE(T, t) x,
-                                             const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE3(T, vec_scalar_mul, T) (TEMPLATE(T, struct) * poly1, const TEMPLATE(T, struct) * poly2, slong len2, const TEMPLATE(T, t) x, const TEMPLATE(T, ctx_t) ctx);
 
-void _TEMPLATE(T, TEMPLATE(vec_scalar_submul, T))(TEMPLATE(T, struct) * poly1,
-                                             const TEMPLATE(T, struct) * poly2,
-                                             slong len2,
-                                             const TEMPLATE(T, t) x,
-                                             const TEMPLATE(T, ctx_t) ctx);
+/* Dot product ***************************************************************/
 
-void _TEMPLATE3(T, vec_scalar_mul, T) (TEMPLATE(T, struct) * poly1,
-                                  const TEMPLATE(T, struct) * poly2,
-                                  slong len2, const TEMPLATE(T, t) x,
-                                  const TEMPLATE(T, ctx_t) ctx);
-
-/* ****************************************************************************/
-void _TEMPLATE(T, vec_dot)(TEMPLATE(T, t) res,
-                      const TEMPLATE(T, struct) * vec1,
-                      const TEMPLATE(T, struct) * vec2,
-                      slong len2,
-                      const TEMPLATE(T, ctx_t) ctx);
+void _TEMPLATE(T, vec_dot)(TEMPLATE(T, t) res, const TEMPLATE(T, struct) * vec1, const TEMPLATE(T, struct) * vec2, slong len2, const TEMPLATE(T, ctx_t) ctx);
 
 #ifdef __cplusplus
- }
+}
 #endif
 
 #endif
