4a20cae66d5cf102a503466ddb292967acfc44c5
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-11 16:33:52 +0100

    Cleanup fq_vec.h

--- src/fq_vec.h
+++ src/fq_vec.h
@@ -28,7 +28,6 @@ do {                                                \
         (i)--;                                      \
 } while (0)
 
-
 #define FQ_VEC_SWAP(vec1, len1, vec2, len2)   \
 do {                                          \
     fq_struct *__t;                           \
@@ -40,7 +39,6 @@ do {                                          \
     (len1) = (len2);                          \
     (len2) = __tn;                            \
 } while (0);
-
 
 #define T fq
 #define CAP_T FQ
