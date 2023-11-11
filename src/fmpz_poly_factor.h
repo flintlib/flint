3e2c99409a6914e9ba8fdea6e75ba88e043321cc
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-11 20:05:09 +0100

    Cleanup fmpz_poly_factor.h

--- src/fmpz_poly_factor.h
+++ src/fmpz_poly_factor.h
@@ -28,56 +28,37 @@ extern "C" {
 #endif
 
 void fmpz_poly_factor_init(fmpz_poly_factor_t fac);
-
 void fmpz_poly_factor_init2(fmpz_poly_factor_t fac, slong alloc);
-
 void fmpz_poly_factor_realloc(fmpz_poly_factor_t fac, slong alloc);
+void fmpz_poly_factor_clear(fmpz_poly_factor_t fac);
 
 void fmpz_poly_factor_fit_length(fmpz_poly_factor_t fac, slong len);
 
-void fmpz_poly_factor_clear(fmpz_poly_factor_t fac);
+void fmpz_poly_factor_set(fmpz_poly_factor_t res, const fmpz_poly_factor_t fac);
 
-void fmpz_poly_factor_set(fmpz_poly_factor_t res,
-                                                 const fmpz_poly_factor_t fac);
+void fmpz_poly_factor_insert(fmpz_poly_factor_t fac, const fmpz_poly_t p, slong exp);
 
-void fmpz_poly_factor_insert(fmpz_poly_factor_t fac,
-                             const fmpz_poly_t p, slong exp);
-
-void fmpz_poly_factor_concat(fmpz_poly_factor_t res,
-                             const fmpz_poly_factor_t fac);
+void fmpz_poly_factor_concat(fmpz_poly_factor_t res, const fmpz_poly_factor_t fac);
 
 void fmpz_poly_factor_print(const fmpz_poly_factor_t fac);
 
-void fmpz_poly_factor_zassenhaus_recombination(
-            fmpz_poly_factor_t final_fac, const fmpz_poly_factor_t lifted_fac,
-                               const fmpz_poly_t F, const fmpz_t P, slong exp);
-
-void fmpz_poly_factor_squarefree(fmpz_poly_factor_t fac,
-                                                          const fmpz_poly_t F);
+void fmpz_poly_factor_squarefree(fmpz_poly_factor_t fac, const fmpz_poly_t F);
 
 void fmpz_poly_factor_mignotte(fmpz_t B, const fmpz_poly_t f);
 
-void _fmpz_poly_factor_zassenhaus(fmpz_poly_factor_t final_fac,
-              slong exp, const fmpz_poly_t f, slong cutoff, int use_van_hoeij);
+void fmpz_poly_factor_zassenhaus_recombination(fmpz_poly_factor_t final_fac, const fmpz_poly_factor_t lifted_fac, const fmpz_poly_t F, const fmpz_t P, slong exp);
 
-void fmpz_poly_factor_zassenhaus(fmpz_poly_factor_t fac,
-                                                          const fmpz_poly_t G);
+void _fmpz_poly_factor_zassenhaus(fmpz_poly_factor_t final_fac, slong exp, const fmpz_poly_t f, slong cutoff, int use_van_hoeij);
+void fmpz_poly_factor_zassenhaus(fmpz_poly_factor_t fac, const fmpz_poly_t G);
 
-void _fmpz_poly_factor_quadratic(fmpz_poly_factor_t fac,
-                                               const fmpz_poly_t f, slong exp);
+void _fmpz_poly_factor_quadratic(fmpz_poly_factor_t fac, const fmpz_poly_t f, slong exp);
+void _fmpz_poly_factor_cubic(fmpz_poly_factor_t fac, const fmpz_poly_t f, slong exp);
 
-void _fmpz_poly_factor_cubic(fmpz_poly_factor_t fac,
-                                               const fmpz_poly_t f, slong exp);
+slong _fmpz_poly_factor_CLD_mat(fmpz_mat_t res, const fmpz_poly_t f, fmpz_poly_factor_t lifted_fac, fmpz_t P, ulong k);
 
-slong _fmpz_poly_factor_CLD_mat(fmpz_mat_t res, const fmpz_poly_t f,
-                             fmpz_poly_factor_t lifted_fac, fmpz_t P, ulong k);
+int fmpz_poly_factor_van_hoeij_check_if_solved(fmpz_mat_t M, fmpz_poly_factor_t final_fac, fmpz_poly_factor_t lifted_fac, const fmpz_poly_t f, fmpz_t P, slong exp, fmpz_t lc);
 
-int fmpz_poly_factor_van_hoeij_check_if_solved(fmpz_mat_t M,
-          fmpz_poly_factor_t final_fac, fmpz_poly_factor_t lifted_fac,
-                          const fmpz_poly_t f, fmpz_t P, slong exp, fmpz_t lc);
-
-void fmpz_poly_factor_van_hoeij(fmpz_poly_factor_t final_fac,
-        const nmod_poly_factor_t fac, const fmpz_poly_t f, slong exp, ulong p);
+void fmpz_poly_factor_van_hoeij(fmpz_poly_factor_t final_fac, const nmod_poly_factor_t fac, const fmpz_poly_t f, slong exp, ulong p);
 
 void fmpz_poly_factor(fmpz_poly_factor_t fac, const fmpz_poly_t G);
 
@@ -127,8 +108,7 @@ void zassenhaus_prune_start_add_factors(zassenhaus_prune_t Z)
     Z->new_total = 0;
 }
 
-void zassenhaus_prune_add_factor(zassenhaus_prune_t Z,
-                                                         slong deg, slong exp);
+void zassenhaus_prune_add_factor(zassenhaus_prune_t Z, slong deg, slong exp);
 
 void zassenhaus_prune_end_add_factors(zassenhaus_prune_t Z);
 
@@ -146,10 +126,7 @@ int zassenhaus_prune_degree_is_possible(const zassenhaus_prune_t Z, slong d)
     return Z->pos_degs[d];
 }
 
-void fmpz_poly_factor_zassenhaus_recombination_with_prune(
-            fmpz_poly_factor_t final_fac, const fmpz_poly_factor_t lifted_fac,
-                                const fmpz_poly_t F, const fmpz_t P, slong exp,
-                                                   const zassenhaus_prune_t Z);
+void fmpz_poly_factor_zassenhaus_recombination_with_prune(fmpz_poly_factor_t final_fac, const fmpz_poly_factor_t lifted_fac, const fmpz_poly_t F, const fmpz_t P, slong exp, const zassenhaus_prune_t Z);
 
 #ifdef __cplusplus
 }
