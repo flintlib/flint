78429c6dd5519399f083439d3638f8e8e5bac98b
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-13 01:47:33 +0100

    Cleanup mpf_mat.h

--- src/mpf_mat.h
+++ src/mpf_mat.h
@@ -37,27 +37,15 @@ typedef struct
 
 typedef mpf_mat_struct mpf_mat_t[1];
 
-MPF_MAT_INLINE
-mpf * mpf_mat_entry(const mpf_mat_t mat, slong i, slong j)
-{
-   return mat->rows[i] + j;
-}
+MPF_MAT_INLINE mpf * mpf_mat_entry(const mpf_mat_t mat, slong i, slong j) { return mat->rows[i] + j; }
 
-MPF_MAT_INLINE
-slong mpf_mat_nrows(const mpf_mat_t mat)
-{
-    return mat->r;
-}
-
-MPF_MAT_INLINE
-slong mpf_mat_ncols(const mpf_mat_t mat)
-{
-    return mat->c;
-}
+MPF_MAT_INLINE slong mpf_mat_nrows(const mpf_mat_t mat) { return mat->r; }
+MPF_MAT_INLINE slong mpf_mat_ncols(const mpf_mat_t mat) { return mat->c; }
 
 /* Memory management  ********************************************************/
 
 void mpf_mat_init(mpf_mat_t mat, slong rows, slong cols, flint_bitcnt_t prec);
+void mpf_mat_clear(mpf_mat_t mat);
 
 void mpf_mat_swap(mpf_mat_t mat1, mpf_mat_t mat2);
 
@@ -73,38 +61,20 @@ mpf_mat_swap_entrywise(mpf_mat_t mat1, mpf_mat_t mat2)
 
 void mpf_mat_set(mpf_mat_t mat1, const mpf_mat_t mat2);
 
-void mpf_mat_clear(mpf_mat_t mat);
-
 int mpf_mat_equal(const mpf_mat_t mat1, const mpf_mat_t mat2);
-
 int mpf_mat_approx_equal(const mpf_mat_t mat1, const mpf_mat_t mat2, flint_bitcnt_t bits);
 
 int mpf_mat_is_zero(const mpf_mat_t mat);
 
-MPF_MAT_INLINE int
-mpf_mat_is_empty(const mpf_mat_t mat)
-{
-    return (mat->r == 0) || (mat->c == 0);
-}
-
-MPF_MAT_INLINE int
-mpf_mat_is_square(const mpf_mat_t mat)
-{
-    return (mat->r == mat->c);
-}
+MPF_MAT_INLINE int mpf_mat_is_empty(const mpf_mat_t mat) { return mat->r == 0 || mat->c == 0; }
+MPF_MAT_INLINE int mpf_mat_is_square(const mpf_mat_t mat) { return mat->r == mat->c; }
 
 void mpf_mat_zero(mpf_mat_t mat);
-
 void mpf_mat_one(mpf_mat_t mat);
-
 
 /* Conversions ***************************************************************/
 
 void mpf_mat_set_fmpz_mat(mpf_mat_t B, const fmpz_mat_t A);
-
-/* Backwards compatibility (this will generate errors for non-GCC compatible
- * compilers) */
-#define fmpz_mat_get_mpf_mat(B, A) _Pragma("GCC warning \"'fmpz_mat_get_mpf_mat' is deprecated in favor for 'mpf_mat_set_fmpz_mat'\"") mpf_mat_set_fmpz_mat(B, A)
 
 /* Input and output  *********************************************************/
 
@@ -114,14 +84,14 @@ void mpf_mat_print(const mpf_mat_t mat);
 
 void mpf_mat_randtest(mpf_mat_t mat, flint_rand_t state, flint_bitcnt_t bits);
 
-/* Multiplication */
+/* Multiplication  ***********************************************************/
 
 void mpf_mat_mul(mpf_mat_t C, const mpf_mat_t A, const mpf_mat_t B);
 
-/* Permutations */
+/* Permutations  *************************************************************/
 
-MPF_MAT_INLINE void
-mpf_mat_swap_rows(mpf_mat_t mat, slong r, slong s)
+MPF_MAT_INLINE
+void mpf_mat_swap_rows(mpf_mat_t mat, slong r, slong s)
 {
     if (r != s)
     {
@@ -133,7 +103,7 @@ mpf_mat_swap_rows(mpf_mat_t mat, slong r, slong s)
     }
 }
 
-/* Gram-Schmidt Orthogonalisation and QR Decomposition  ********************************************************/
+/* Gram-Schmidt Orthogonalisation and QR Decomposition  **********************/
 
 void mpf_mat_gso(mpf_mat_t B, const mpf_mat_t A);
 
