d912c29960876e513f4ea8c3b25fda75f6c1a5b4
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-14 14:54:34 +0100

    Change copyright notices from Calcium to FLINT

--- src/ca_poly/compose.c
+++ src/ca_poly/compose.c
@@ -3,9 +3,9 @@
     Copyright (C) 2012 Sebastian Pancratz
     Copyright (C) 2012, 2016, 2020 Fredrik Johansson
 
-    This file is part of Calcium.
+    This file is part of FLINT.
 
-    Calcium is free software: you can redistribute it and/or modify it under
+    FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
     by the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
