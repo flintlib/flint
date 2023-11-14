ae3999e3ac9a338ee21dd48aac033dc55632d964
Albin Ahlbäck <albin.ahlback@gmail.com>
2023-11-14 14:53:53 +0100

    Change copyright from Arb to FLINT

--- src/acb_poly/divrem.c
+++ src/acb_poly/divrem.c
@@ -2,9 +2,9 @@
     Copyright (C) 2011 William Hart
     Copyright (C) 2012 Fredrik Johansson
 
-    This file is part of Arb.
+    This file is part of FLINT.
 
-    Arb is free software: you can redistribute it and/or modify it under
+    FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
     by the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
--- src/arb_mat/io.c
+++ src/arb_mat/io.c
@@ -2,9 +2,9 @@
     Copyright (C) 2012 Fredrik Johansson
     Copyright (C) 2015 Arb authors
 
-    This file is part of Arb.
+    This file is part of FLINT.
 
-    Arb is free software: you can redistribute it and/or modify it under
+    FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
     by the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.  See <http://www.gnu.org/licenses/>.
