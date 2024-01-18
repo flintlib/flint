766d421365ce467c45d3e9e5f10cfffd6c05199b
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-18 11:07:04 +0100

    Add n_poly types into n_poly_types.h

--- src/n_poly.h
+++ src/n_poly.h
@@ -16,114 +16,11 @@
 #include "nmod_vec.h"
 #include "nmod_poly.h"
 #include "fq_nmod.h"
+#include "n_poly_types.h"
 
 #ifdef __cplusplus
  extern "C" {
 #endif
-
-/* arrays of ulong */
-typedef struct
-{
-    mp_limb_t * coeffs;
-    slong alloc;
-    slong length;
-} n_poly_struct;
-
-typedef n_poly_struct n_poly_t[1];
-
-typedef n_poly_struct n_fq_poly_struct;
-
-typedef n_poly_t n_fq_poly_t;
-
-/* arrays of arrays of ulong */
-typedef struct
-{
-    n_poly_struct * coeffs;
-    slong alloc;
-    slong length;
-} n_bpoly_struct;
-
-typedef n_bpoly_struct n_bpoly_t[1];
-
-typedef n_bpoly_struct n_fq_bpoly_struct;
-
-typedef n_bpoly_t n_fq_bpoly_t;
-
-/* arrays of arrays of arrays of ulong */
-typedef struct
-{
-    n_bpoly_struct * coeffs;
-    slong alloc;
-    slong length;
-} n_tpoly_struct;
-
-typedef n_tpoly_struct n_tpoly_t[1];
-
-typedef n_tpoly_struct n_fq_tpoly_struct;
-
-typedef n_tpoly_t n_fq_tpoly_t;
-
-/* sparse arrays of ulong */
-typedef struct
-{
-    ulong * exps;
-    mp_limb_t * coeffs;
-    slong length;
-    slong alloc;
-} n_polyu_struct;
-
-typedef n_polyu_struct n_polyu_t[1];
-
-typedef n_polyu_struct n_fq_polyu_struct;
-
-typedef n_polyu_t n_fq_polyu_t;
-
-/*
-    sparse arrays of arrays of ulong
-    n_polyu1n => one exponent is in the exps[i]
-    n_polyu2n => two exponents are packed into the exps[i]
-    ...
-*/
-typedef struct
-{
-    n_poly_struct * coeffs;
-    ulong * exps;
-    slong length;
-    slong alloc;
-} n_polyun_struct;
-
-typedef n_polyun_struct n_polyun_t[1];
-
-typedef n_polyun_struct n_fq_polyun_struct;
-
-typedef n_polyun_t n_fq_polyun_t;
-
-/* n_poly stack */
-typedef struct
-{
-    n_poly_struct ** array;
-    slong alloc;
-    slong top;
-} n_poly_stack_struct;
-
-typedef n_poly_stack_struct n_poly_stack_t[1];
-
-/* n_bpoly stack */
-typedef struct
-{
-    n_bpoly_struct ** array;
-    slong alloc;
-    slong top;
-} n_bpoly_stack_struct;
-
-typedef n_bpoly_stack_struct n_bpoly_stack_t[1];
-
-typedef struct {
-    n_poly_stack_t poly_stack;
-    n_bpoly_stack_t bpoly_stack;
-} n_poly_bpoly_stack_struct;
-
-typedef n_poly_bpoly_stack_struct n_poly_bpoly_stack_t[1];
 
 /* n_polyun stack */
 typedef struct
@@ -1699,20 +1596,6 @@ void n_fq_pow_cache_mulpow_fmpz(
     const fq_nmod_ctx_t ctx);
 
 /*****************************************************************************/
-
-typedef struct {
-    mp_limb_t * M;
-    mp_limb_t * T;
-    mp_limb_t * Q;
-    mp_limb_t * array;
-    slong alloc;
-    slong d;
-    slong radix;
-    mp_limb_t w;
-} nmod_eval_interp_struct;
-
-typedef nmod_eval_interp_struct nmod_eval_interp_t[1];
-
 
 void nmod_eval_interp_init(nmod_eval_interp_t E);
 
