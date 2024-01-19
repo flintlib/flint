3a95a558641062ccff88fbb326746ee20ba7450c
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-19 12:32:42 +0100

    fmpz equal

--- src/fmpz/equal.c
+++ src/fmpz/equal.c
@@ -1,5 +1,7 @@
 /*
     Copyright (C) 2009 William Hart
+    Copyright (C) 2010, 2012 Sebastian Pancratz
+    Copyright (C) 2011 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -9,8 +11,7 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
-#include "flint.h"
-#include "ulong_extras.h"
+#include "gmpcompat.h"
 #include "fmpz.h"
 
 int fmpz_equal(const fmpz_t f, const fmpz_t g)
@@ -20,4 +21,19 @@ int fmpz_equal(const fmpz_t f, const fmpz_t g)
     if (!COEFF_IS_MPZ(*f)) return (*f == *g);  /* if f is large it can't be equal to g */
     else if (!COEFF_IS_MPZ(*g)) return 0;  /* f is large, so if g isn't... */
     else return (mpz_cmp(COEFF_TO_PTR(*f), COEFF_TO_PTR(*g)) == 0);
+}
+
+int fmpz_equal_si(const fmpz_t f, slong g)
+{
+    fmpz c = *f;
+
+    return !COEFF_IS_MPZ(c) ? (c == g) : !flint_mpz_cmp_si(COEFF_TO_PTR(c), g);
+}
+
+int fmpz_equal_ui(const fmpz_t f, ulong g)
+{
+    fmpz c = *f;
+
+    return !COEFF_IS_MPZ(c) ? ((c >= 0) & (c == g)) :
+                              !flint_mpz_cmp_ui(COEFF_TO_PTR(c), g);
 }
