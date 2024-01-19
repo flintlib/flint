ac7e1de382953a7b4b06e744ffd01babbed195e4
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-19 16:06:01 +0100

    nmod_mpoly equal

--- src/nmod_mpoly/equal.c
+++ src/nmod_mpoly/equal.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2017 Daniel Schultz
+    Copyright (C) 2017, 2018 Daniel Schultz
 
     This file is part of FLINT.
 
@@ -9,6 +9,7 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
+#include "nmod.h"
 #include "mpoly.h"
 #include "nmod_mpoly.h"
 
@@ -77,4 +78,26 @@ int nmod_mpoly_equal(const nmod_mpoly_t poly1, const nmod_mpoly_t poly2,
         flint_free(ptr2);
 
     return r;
+}
+
+int nmod_mpoly_equal_ui(const nmod_mpoly_t A,
+                                           ulong c, const nmod_mpoly_ctx_t ctx)
+{
+    slong N;
+
+    if (c >= ctx->mod.n)
+        NMOD_RED(c, c, ctx->mod);
+
+    if (c == 0)
+        return A->length == 0;
+
+    if (A->length != 1)
+        return 0;
+
+    N = mpoly_words_per_exp(A->bits, ctx->minfo);
+
+    if (!mpoly_monomial_is_zero(A->exps + N*0, N))
+        return 0;
+
+    return A->coeffs[0] == c;
 }
