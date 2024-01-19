589c8a03a0b333d0573e1f5a63b2a83799c07528
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-19 15:29:22 +0100

    fmpz_poly set

--- src/fmpz_poly/set.c
+++ src/fmpz_poly/set.c
@@ -1,5 +1,7 @@
 /*
     Copyright (C) 2008, 2009 William Hart
+    Copyright (C) 2010 Sebastian Pancratz
+    Copyright (C) 2011 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -25,5 +27,82 @@ fmpz_poly_set(fmpz_poly_t poly1, const fmpz_poly_t poly2)
             fmpz_set(poly1->coeffs + i, poly2->coeffs + i);
 
         _fmpz_poly_set_length(poly1, len);
+    }
+}
+
+void
+fmpz_poly_set_nmod_poly(fmpz_poly_t res, const nmod_poly_t poly)
+{
+    slong len = poly->length;
+
+    if (len == 0)
+    {
+        fmpz_poly_zero(res);
+    }
+    else
+    {
+        slong i;
+        fmpz_poly_fit_length(res, len);
+        for (i = 0; i < len; i++)
+            fmpz_set_ui_smod(res->coeffs + i, poly->coeffs[i], poly->mod.n);
+        _fmpz_poly_set_length(res, len);
+    }
+}
+
+void
+fmpz_poly_set_nmod_poly_unsigned(fmpz_poly_t res, const nmod_poly_t poly)
+{
+    slong len = poly->length;
+
+    if (len == 0)
+    {
+        fmpz_poly_zero(res);
+    }
+    else
+    {
+        slong i;
+        fmpz_poly_fit_length(res, len);
+        for (i = 0; i < len; i++)
+            fmpz_set_ui(res->coeffs + i, poly->coeffs[i]);
+        _fmpz_poly_set_length(res, len);
+    }
+}
+
+void
+fmpz_poly_set_fmpz(fmpz_poly_t poly, const fmpz_t c)
+{
+    if (fmpz_is_zero(c))
+        fmpz_poly_zero(poly);
+    else
+    {
+        fmpz_poly_fit_length(poly, 1);
+        fmpz_set(poly->coeffs, c);
+        _fmpz_poly_set_length(poly, 1);
+    }
+}
+
+void
+fmpz_poly_set_si(fmpz_poly_t poly, slong c)
+{
+    if (c == 0)
+        fmpz_poly_zero(poly);
+    else
+    {
+        fmpz_poly_fit_length(poly, 1);
+        fmpz_set_si(poly->coeffs, c);
+        _fmpz_poly_set_length(poly, 1);
+    }
+}
+
+void
+fmpz_poly_set_ui(fmpz_poly_t poly, ulong c)
+{
+    if (c == UWORD(0))
+        fmpz_poly_zero(poly);
+    else
+    {
+        fmpz_poly_fit_length(poly, 1);
+        fmpz_set_ui(poly->coeffs, c);
+        _fmpz_poly_set_length(poly, 1);
     }
 }
