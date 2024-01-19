26f62cbfdc7058805edc80b63326599b91c5f301
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-19 16:00:22 +0100

    nmod_mpoly set_term_exp

--- src/nmod_mpoly/set_term_exp.c
+++ src/nmod_mpoly/set_term_exp.c
@@ -12,6 +12,25 @@
 #include "mpoly.h"
 #include "nmod_mpoly.h"
 
+void nmod_mpoly_set_term_exp_fmpz(nmod_mpoly_t A, slong i,
+                                fmpz * const * exp, const nmod_mpoly_ctx_t ctx)
+{
+    slong N;
+    flint_bitcnt_t exp_bits;
+
+    if (i >= (ulong) A->length)
+    {
+        flint_throw(FLINT_ERROR, "nmod_mpoly_set_term_exp_fmpz: index out of range");
+    }
+
+    exp_bits = mpoly_exp_bits_required_pfmpz(exp, ctx->minfo);
+    exp_bits = mpoly_fix_bits(exp_bits, ctx->minfo);
+    nmod_mpoly_fit_length_fit_bits(A, A->length, exp_bits, ctx);
+
+    N = mpoly_words_per_exp(A->bits, ctx->minfo);
+    mpoly_set_monomial_pfmpz(A->exps + N*i, exp, A->bits, ctx->minfo);
+}
+
 void nmod_mpoly_set_term_exp_ui(nmod_mpoly_t A, slong i,
                                  const ulong * exp, const nmod_mpoly_ctx_t ctx)
 {
