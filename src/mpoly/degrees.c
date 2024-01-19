821ab29f27de7836dda90a474e4eb216a3450e79
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-19 16:23:19 +0100

    mpoly degree

--- src/mpoly/degrees.c
+++ src/mpoly/degrees.c
@@ -13,8 +13,71 @@
 #include "fmpz.h"
 #include "mpoly.h"
 
-/* this file does not need to change with new orderings */
+slong mpoly_degree_si(const ulong * exps,
+                slong len, flint_bitcnt_t bits, slong var, const mpoly_ctx_t mctx)
+{
+    if (len == 0)
+        return -WORD(1);
 
+    /* sometimes we don't have to look very far */
+    if (mctx->ord == ORD_LEX && var == 0)
+        len = 1;
+
+    if (bits <= FLINT_BITS)
+    {
+        slong r;
+        ulong mask = (-UWORD(1)) >> (FLINT_BITS - bits);
+        slong offset, shift, N, i;
+
+        mpoly_gen_offset_shift_sp(&offset, &shift, var, bits, mctx);
+        N = mpoly_words_per_exp_sp(bits, mctx);
+
+        i = 0;
+        r = (exps[N*i + offset] >> shift) & mask;
+        for (i = 1; i < len; i++)
+        {
+            ulong k = (exps[N*i + offset] >> shift) & mask;
+            if (r < k)
+                r = k;
+        }
+        return r;
+    }
+    else
+    {
+        slong * degs, r;
+        TMP_INIT;
+        TMP_START;
+        degs = (slong *) TMP_ALLOC(mctx->nvars*sizeof(slong));
+        mpoly_degrees_si(degs, exps, len, bits, mctx);
+        r = degs[var];
+        TMP_END;
+        return r;
+    }
+}
+
+
+void mpoly_degree_fmpz(fmpz_t deg, const ulong * exps,
+                slong len, flint_bitcnt_t bits, slong var, const mpoly_ctx_t mctx)
+{
+    slong i;
+    fmpz * degs;
+    TMP_INIT;
+
+    TMP_START;
+    degs = (fmpz *) TMP_ALLOC(mctx->nvars*sizeof(fmpz));
+    for (i = 0; i < mctx->nvars; i++)
+        fmpz_init(degs + i);
+
+    mpoly_degrees_ffmpz(degs, exps, len, bits, mctx);
+
+    fmpz_swap(deg, degs + var);
+    for (i = 0; i < mctx->nvars; i++)
+        fmpz_clear(degs + i);
+
+    TMP_END;
+}
+
+/* this file does not need to change with new orderings */
 void mpoly_degrees_si(
     slong * user_degs,
     const ulong * poly_exps,
@@ -130,7 +193,6 @@ void mpoly_degrees_si_threaded(
     flint_free(args);
 }
 
-
 void mpoly_degrees_ffmpz(
     fmpz * user_degs,
     const ulong * poly_exps,
@@ -197,4 +259,46 @@ void mpoly_degrees_pfmpz(
         fmpz_clear(max_fields + i);
 
     TMP_END;
+}
+
+int mpoly_degrees_fit_si(const ulong * poly_exps, slong len,
+                                   flint_bitcnt_t bits, const mpoly_ctx_t mctx)
+{
+    slong i, j, N;
+    int ret;
+    fmpz * tmp_exps;
+    TMP_INIT;
+
+    if (len == 0)
+    {
+        return 1;
+    }
+
+    TMP_START;
+    tmp_exps = (fmpz *) TMP_ALLOC(mctx->nvars*sizeof(fmpz));
+    for (j = 0; j < mctx->nvars; j++) {
+        fmpz_init(tmp_exps + j);
+    }
+
+    N = mpoly_words_per_exp(bits, mctx);
+
+    ret = 1;
+    for (i = 0; i < len; i++)
+    {
+        mpoly_get_monomial_ffmpz(tmp_exps, poly_exps + N*i, bits, mctx);
+        for (j = 0; j < mctx->nvars; j++)
+        {
+            if (!fmpz_fits_si(tmp_exps + j))
+            {
+                ret = 0;
+                break;
+            }
+        }
+    }
+
+    for (j = 0; j < mctx->nvars; j++)
+        fmpz_clear(tmp_exps + j);
+
+    TMP_END;
+    return ret;
 }
