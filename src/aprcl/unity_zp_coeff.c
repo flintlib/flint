0761f7f3b8246cce5ccb5d1ea6e6a94def8cec9e
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-20 07:37:53 +0100

    aprcl unity_zp_coeff

--- src/aprcl/unity_zp_coeff.c
+++ src/aprcl/unity_zp_coeff.c
@@ -51,3 +51,44 @@ unity_zp_coeff_add_ui(unity_zp f, ulong ind, ulong x)
         fmpz_sub(f->poly->coeffs + ind, f->poly->coeffs + ind,
                                                  fmpz_mod_ctx_modulus(f->ctx));
 }
+
+void
+unity_zp_coeff_inc(unity_zp f, ulong ind)
+{
+    if (ind >= f->poly->length)
+    {
+        fmpz_mod_poly_set_coeff_ui(f->poly, ind, 1, f->ctx);
+        return;
+    }
+
+    fmpz_add_ui(f->poly->coeffs + ind, f->poly->coeffs + ind, 1);
+    if (fmpz_equal(f->poly->coeffs + ind, fmpz_mod_ctx_modulus(f->ctx)))
+        fmpz_set_ui(f->poly->coeffs + ind, 0);
+}
+
+void
+unity_zp_coeff_dec(unity_zp f, ulong ind)
+{
+    if (ind >= f->poly->length)
+    {
+        fmpz_mod_poly_set_coeff_si(f->poly, ind, -1, f->ctx);
+        return;
+    }
+
+    fmpz_sub_ui(f->poly->coeffs + ind, f->poly->coeffs + ind, 1);
+    if (fmpz_equal_si(f->poly->coeffs + ind, -1))
+        fmpz_add(f->poly->coeffs + ind, f->poly->coeffs + ind,
+                                                 fmpz_mod_ctx_modulus(f->ctx));
+}
+
+void
+unity_zp_coeff_set_fmpz(unity_zp f, ulong ind, const fmpz_t x)
+{
+    fmpz_mod_poly_set_coeff_fmpz(f->poly, ind, x, f->ctx);
+}
+
+void
+unity_zp_coeff_set_ui(unity_zp f, ulong ind, ulong x)
+{
+    fmpz_mod_poly_set_coeff_ui(f->poly, ind, x, f->ctx);
+}
