24abf231abbf51688fb418df4cb0eba41fbba6f4
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-20 10:15:09 +0100

    fmpq_poly evaluate

--- src/fmpq_poly/evaluate.c
+++ src/fmpq_poly/evaluate.c
@@ -54,3 +54,32 @@ fmpq_poly_evaluate_fmpq(fmpq_t res, const fmpq_poly_t poly, const fmpq_t a)
         fmpq_clear(t);
     }
 }
+
+void
+_fmpq_poly_evaluate_fmpz(fmpz_t rnum, fmpz_t rden, const fmpz * poly,
+                         const fmpz_t den, slong len, const fmpz_t a)
+{
+    fmpz_t d;
+
+    _fmpz_poly_evaluate_horner_fmpz(rnum, poly, len, a);
+
+    fmpz_init(d);
+    fmpz_gcd(d, rnum, den);
+    if (*d != WORD(1))
+    {
+        fmpz_divexact(rnum, rnum, d);
+        fmpz_divexact(rden, den, d);
+    }
+    else
+    {
+        fmpz_set(rden, den);
+    }
+    fmpz_clear(d);
+}
+
+void
+fmpq_poly_evaluate_fmpz(fmpq_t res, const fmpq_poly_t poly, const fmpz_t a)
+{
+    _fmpq_poly_evaluate_fmpz(fmpq_numref(res), fmpq_denref(res),
+                             poly->coeffs, poly->den, poly->length, a);
+}
