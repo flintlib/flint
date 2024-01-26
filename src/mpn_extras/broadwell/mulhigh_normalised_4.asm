5df1b26631e9cd1ce9f2278e4d7631f381ee84f4
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-01-26 13:50:41 +0100

    Add flint_mpn_mulhigh_normalised_n

---
+++ src/mpn_extras/broadwell/mulhigh_normalised_4.asm
@@ -0,0 +1,95 @@
+#
+#   Copyright (C) 2024 Albin Ahlbäck
+#
+#   This file is part of FLINT.
+#
+#   FLINT is free software: you can redistribute it and/or modify it under
+#   the terms of the GNU Lesser General Public License (LGPL) as published
+#   by the Free Software Foundation; either version 2.1 of the License, or
+#   (at your option) any later version.  See <https://www.gnu.org/licenses/>.
+#
+
+include(`config.m4')dnl
+dnl
+.text
+
+.global	FUNC(flint_mpn_mulhigh_normalised_4)
+.p2align	4, 0x90
+TYPE(flint_mpn_mulhigh_normalised_4)
+
+FUNC(flint_mpn_mulhigh_normalised_4):
+	.cfi_startproc
+	push	%rbx
+	push	%rbp
+
+	mov	%rdx, %rcx
+	mov	0*8(%rdx), %rdx
+	xor	%r9d, %r9d
+
+	mulx	2*8(%rsi), %r8, %rax
+	mulx	3*8(%rsi), %r8, %r10
+	adcx	%r8, %rax
+	adcx	%r9, %r10
+
+	mov	1*8(%rcx), %rdx
+	mulx	1*8(%rsi), %r8, %rbx
+	mulx	2*8(%rsi), %r8, %r11
+	adcx	%rbx, %rax
+	adox	%r8, %rax
+	adcx	%r11, %r10
+	mulx	3*8(%rsi), %r8, %r11
+	adox	%r8, %r10
+	adcx	%r9, %r11
+	adox	%r9, %r11
+
+	mov	2*8(%rcx), %rdx
+	mulx	0*8(%rsi), %r8, %rbp
+	mulx	1*8(%rsi), %r8, %rbx
+	adcx	%rbp, %rax
+	adox	%r8, %rax
+	adcx	%rbx, %r10
+	mulx	2*8(%rsi), %r8, %rbx
+	adox	%r8, %r10
+	adcx	%rbx, %r11
+	mulx	3*8(%rsi), %r8, %rbx
+	adox	%r8, %r11
+	adcx	%r9, %rbx
+	adox	%r9, %rbx
+
+	mov	3*8(%rcx), %rdx
+	mulx	0*8(%rsi), %r8, %rbp
+	adcx	%r8, %rax
+	adcx	%rbp, %r10
+	mulx	1*8(%rsi), %r8, %rbp
+	adox	%r8, %r10
+	adox	%rbp, %r11
+	mulx	2*8(%rsi), %r8, %rbp
+	adcx	%r8, %r11
+	adcx	%rbp, %rbx
+	mulx	3*8(%rsi), %r8, %rbp
+	adox	%r8, %rbx
+	adcx	%r9, %rbp
+	adox	%r9, %rbp
+
+	mov	$0, %rdx
+	test	%rbp, %rbp
+	setns	%dl
+	js	.Lcontinue
+	add	%rax, %rax
+	adc	%r10, %r10
+	adc	%r11, %r11
+	adc	%rbx, %rbx
+	adc	%rbp, %rbp
+.Lcontinue:
+	mov	%r10, 0*8(%rdi)
+	mov	%r11, 1*8(%rdi)
+	mov	%rbx, 2*8(%rdi)
+	mov	%rbp, 3*8(%rdi)
+
+	pop	%rbp
+	pop	%rbx
+
+	ret
+.flint_mpn_mulhigh_normalised_4_end:
+SIZE(flint_mpn_mulhigh_normalised_4, .flint_mpn_mulhigh_normalised_4_end)
+.cfi_endproc
---
+++ src/mpn_extras/test/t-mulhigh_normalised_n.c
@@ -0,0 +1,99 @@
+/*
+    Copyright (C) 2024 Albin Ahlbäck
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.  See <https://www.gnu.org/licenses/>.
+*/
+
+#include "test_helpers.h"
+#include "mpn_extras.h"
+
+#if FLINT_MPN_MULHIGH_N_FUNC_TAB_WIDTH != 0
+
+# define N_MAX FLINT_MPN_MULHIGH_N_FUNC_TAB_WIDTH
+
+void mpfr_mulhigh_n(mp_ptr rp, mp_srcptr xp, mp_srcptr yp, mp_size_t n);
+
+TEST_FUNCTION_START(flint_mpn_mulhigh_normalised_n, state)
+{
+    slong ix;
+    int result;
+
+    for (ix = 0; ix < 100000 * flint_test_multiplier(); ix++)
+    {
+        mp_limb_t rp_n[N_MAX + 1] = {UWORD(0)};
+        mp_limb_t rp_u[N_MAX + 1] = {UWORD(0)};
+        mp_limb_t xp[N_MAX];
+        mp_limb_t yp[N_MAX];
+        mp_size_t n;
+        struct mp_limb_pair_t res_norm;
+        mp_limb_t retlimb, normalised;
+
+        n = 1 + n_randint(state, N_MAX);
+
+        mpn_random2(xp, n);
+        mpn_random2(yp, n);
+        xp[n - 1] |= (UWORD(1) << (FLINT_BITS - 1));
+        yp[n - 1] |= (UWORD(1) << (FLINT_BITS - 1));
+
+        rp_u[0] = flint_mpn_mulhigh_n(rp_u + 1, xp, yp, n);
+        res_norm = flint_mpn_mulhigh_normalised_n(rp_n + 1, xp, yp, n);
+        retlimb = res_norm.m1;
+        normalised = res_norm.m2;
+        rp_n[0] = retlimb;
+
+        result = ((rp_n[n] & (UWORD(1) << (FLINT_BITS - 1))) != UWORD(0));
+        if (!result)
+            TEST_FUNCTION_FAIL(
+                    "Top bit not set in normalised result\n"
+                    "ix = %wd\n"
+                    "n = %wd\n"
+                    "xp = %{ulong*}\n"
+                    "yp = %{ulong*}\n"
+                    "rp_n = %{ulong*}\n"
+                    "rp_u = %{ulong*}\n",
+                    ix, n, xp, n, yp, n, rp_n, n + 1, rp_u, n + 1);
+
+        if (normalised)
+        {
+            result = (mpn_lshift(rp_u, rp_u, n + 1, 1) == 0);
+            result = result && (mpn_cmp(rp_n, rp_u, n + 1) == 0);
+            if (!result)
+                TEST_FUNCTION_FAIL(
+                        "rp_n != rp_u << 1 when normalised\n"
+                        "ix = %wd\n"
+                        "n = %wd\n"
+                        "xp = %{ulong*}\n"
+                        "yp = %{ulong*}\n"
+                        "rp_n = %{ulong*}\n"
+                        "rp_u = %{ulong*}\n",
+                        ix, n, xp, n, yp, n, rp_n, n + 1, rp_u, n + 1);
+        }
+        else
+        {
+            result = (mpn_cmp(rp_n, rp_u, n + 1) == 0);
+            if (!result)
+                TEST_FUNCTION_FAIL(
+                        "rp_n != rp_u when unnormalised\n"
+                        "ix = %wd\n"
+                        "n = %wd\n"
+                        "xp = %{ulong*}\n"
+                        "yp = %{ulong*}\n"
+                        "rp_n = %{ulong*}\n"
+                        "rp_u = %{ulong*}\n",
+                        ix, n, xp, n, yp, n, rp_n, n + 1, rp_u, n + 1);
+        }
+    }
+
+    TEST_FUNCTION_END(state);
+}
+#else
+TEST_FUNCTION_START(flint_mpn_mulhigh_normalised_n, state)
+{
+    TEST_FUNCTION_END_SKIPPED(state);
+}
+#endif
