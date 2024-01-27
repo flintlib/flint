bb577d330c2add092ebce214b2707631d8c1a100
Fredrik Johansson <fredrik.johansson@gmail.com>
2024-01-27 11:57:23 +0100

    stronger multithreaded testing for fmpz_mat_mul_multimod

--- src/fmpz_mat/test/t-mul_multi_mod.c
+++ src/fmpz_mat/test/t-mul_multi_mod.c
@@ -21,9 +21,9 @@ TEST_FUNCTION_START(fmpz_mat_mul_multi_mod, state)
     {
         slong m, n, k;
 
-        m = n_randint(state, 50);
-        n = n_randint(state, 50);
-        k = n_randint(state, 50);
+        m = n_randint(state, 100);
+        n = n_randint(state, 100);
+        k = n_randint(state, 100);
 
         fmpz_mat_init(A, m, n);
         fmpz_mat_init(B, n, k);
@@ -35,6 +35,8 @@ TEST_FUNCTION_START(fmpz_mat_mul_multi_mod, state)
 
         /* Make sure noise in the output is ok */
         fmpz_mat_randtest(C, state, n_randint(state, 200) + 1);
+
+        flint_set_num_threads(1 + n_randint(state, 4));
 
         fmpz_mat_mul_classical(C, A, B);
         fmpz_mat_mul_multi_mod(D, A, B);
@@ -71,6 +73,8 @@ TEST_FUNCTION_START(fmpz_mat_mul_multi_mod, state)
         /* Make sure noise in the output is ok */
         fmpz_mat_randtest(C, state, n_randint(state, 200) + 1);
 
+        flint_set_num_threads(1 + n_randint(state, 4));
+
         fmpz_mat_mul_classical(C, A, B);
         fmpz_mat_mul_multi_mod(D, A, B);
 
@@ -83,6 +87,38 @@ TEST_FUNCTION_START(fmpz_mat_mul_multi_mod, state)
 
         fmpz_mat_clear(A);
         fmpz_mat_clear(B);
+        fmpz_mat_clear(C);
+        fmpz_mat_clear(D);
+    }
+
+    for (i = 0; i < 100 * flint_test_multiplier(); i++)
+    {
+        slong n;
+
+        n = n_randint(state, 100);
+
+        fmpz_mat_init(A, n, n);
+        fmpz_mat_init(C, n, n);
+        fmpz_mat_init(D, n, n);
+
+        fmpz_mat_randtest(A, state, n_randint(state, 200) + 1);
+
+        /* Make sure noise in the output is ok */
+        fmpz_mat_randtest(C, state, n_randint(state, 200) + 1);
+
+        flint_set_num_threads(1 + n_randint(state, 4));
+
+        fmpz_mat_mul_classical(C, A, A);
+        fmpz_mat_mul_multi_mod(D, A, A);
+
+        if (!fmpz_mat_equal(C, D))
+        {
+            flint_printf("FAIL: results not equal (squaring)\n");
+            fflush(stdout);
+            flint_abort();
+        }
+
+        fmpz_mat_clear(A);
         fmpz_mat_clear(C);
         fmpz_mat_clear(D);
     }
