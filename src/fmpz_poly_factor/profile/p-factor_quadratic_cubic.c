213a4cff74d1bff9f0e07b63c57b6f3f8876e3a0
Albin Ahlbäck <albin.ahlback@gmail.com>
2024-02-01 18:01:21 +0100

    Change to LGPL v3 license in all files
    Done via
    
        sed -i 's/by the Free Software Foundation; either version 2.1 of the License, or/by the Free Software Foundation; either version 3 of the License, or/g' $(find . -type f)

--- src/fmpz_poly_factor/profile/p-factor_quadratic_cubic.c
+++ src/fmpz_poly_factor/profile/p-factor_quadratic_cubic.c
@@ -3,7 +3,7 @@
     This file is part of FLINT.
     FLINT is free software: you can redistribute it and/or modify it under
     the terms of the GNU Lesser General Public License (LGPL) as published
-    by the Free Software Foundation; either version 2.1 of the License, or
+    by the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
