4a2c26e9d19d281b2a784b20885373b35421ee76
Fredrik Johansson <fredrik.johansson@gmail.com>
2024-03-30 16:20:32 +0100

    tuning fix

--- src/gr/mpn_mod.c
+++ src/gr/mpn_mod.c
@@ -1916,17 +1916,17 @@ int _gr_mpn_mod_mat_mul_waksman(gr_mat_t C, const gr_mat_t A, const gr_mat_t B, 
 }
 
 
+/* todo: retune this when arithmetic is more optimized */
 /* note: mul_strassen can be very slightly faster than classical and multi_mod,
    but the range is so narrow that we don't bother */
 
-/* todo: retune this when arithmetic is more optimized */
+/* note: same cutoff as multi_mod for n = 2 means we don't use it */
 static const slong mpn_mod_mat_mul_waksman_cutoff[MPN_MOD_MAX_LIMBS + 1] =
 {
-    0, 0, WORD_MAX, 20, 10, 8, 8, 8, 6, 5, 5, 4, 4, 4, 4, 4, 4,
+    0, 0, 300, 20, 10, 8, 8, 8, 6, 5, 5, 4, 4, 4, 4, 4, 4,
 };
 
 
-/* todo: retune this when arithmetic is more optimized */
 static const int mpn_mod_mat_mul_multi_mod_cutoff[MPN_MOD_MAX_LIMBS + 1] =
 {
     0, 0, 300, 80, 80, 90, 100, 90, 90, 90, 80, 80, 70, 70, 70, 70, 70,
