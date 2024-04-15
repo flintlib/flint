78381e96ff6721f2c5050c1a6b4fe79917e8104f
Fredrik Johansson <fredrik.johansson@gmail.com>
2024-04-15 15:40:13 +0200

    use the gcd method

--- src/mpn_mod/ctx.c
+++ src/mpn_mod/ctx.c
@@ -114,6 +114,7 @@ gr_method_tab_input _mpn_mod_methods_input[] =
     {GR_METHOD_POLY_INV_SERIES, (gr_funcptr) _mpn_mod_poly_inv_series},
     {GR_METHOD_POLY_DIV_SERIES, (gr_funcptr) _mpn_mod_poly_div_series},
     {GR_METHOD_POLY_DIVREM,     (gr_funcptr) _mpn_mod_poly_divrem},
+    {GR_METHOD_POLY_GCD,        (gr_funcptr) _mpn_mod_poly_gcd},
 /*
     {GR_METHOD_POLY_ROOTS,      (gr_funcptr) mpn_mod_roots_gr_poly},
 */
