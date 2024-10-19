4a887b45cbdd3d891e9eac06e988a7221ee5744c
Vincent Neiger <vneiger@users.noreply.github.com>
2024-10-20 00:21:32 +0200

    add .h file

---
+++ src/n_fft.h
@@ -0,0 +1,24 @@
+/*
+    Copyright (C) 2024 Vincent Neiger
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.  See <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef N_POLY_TYPES_H
+#define N_POLY_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* N_FFT_H */
