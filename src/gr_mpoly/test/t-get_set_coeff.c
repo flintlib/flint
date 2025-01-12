a541dadecb8dfe8c8832fcb532198f682f80d930
Fredrik Johansson <fredrik.johansson@gmail.com>
2025-01-12 23:52:34 +0100

    improve test coverage

--- src/gr_mpoly/test/t-get_set_coeff.c
+++ src/gr_mpoly/test/t-get_set_coeff.c
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2020 Daniel Schultz
-    Copyright (C) 2022 Fredrik Johansson
+    Copyright (C) 2022, 2025 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -11,6 +11,8 @@
 */
 
 #include "test_helpers.h"
+#include "fmpz.h"
+#include "fmpq.h"
 #include "fmpz_vec.h"
 #include "gr_mpoly.h"
 
@@ -28,6 +30,7 @@ TEST_FUNCTION_START(gr_mpoly_get_set_coeff, state)
         flint_bitcnt_t exp_bits;
         int status;
         slong nvars;
+        int which;
 
         gr_ctx_init_random(cctx, state);
         gr_mpoly_ctx_init_rand(ctx, state, cctx, 20);
@@ -48,11 +51,47 @@ TEST_FUNCTION_START(gr_mpoly_get_set_coeff, state)
 
             status = GR_SUCCESS;
 
-            GR_MUST_SUCCEED(gr_randtest(c, state, cctx));
             for (k = 0; k < nvars; k++)
                 exp[k] = n_randtest(state);
 
-            status |= gr_mpoly_set_coeff_scalar_ui(f, c, exp, ctx);
+            which = n_randint(state, 5);
+
+            if (which == 0)
+            {
+                GR_MUST_SUCCEED(gr_randtest(c, state, cctx));
+                status |= gr_mpoly_set_coeff_scalar_ui(f, c, exp, ctx);
+            }
+            else if (which == 1)
+            {
+                ulong uc = n_randtest(state);
+                status |= gr_set_ui(c, uc, cctx);
+                status |= gr_mpoly_set_coeff_ui_ui(f, uc, exp, ctx);
+            }
+            else if (which == 2)
+            {
+                slong sc = n_randtest(state);
+                status |= gr_set_si(c, sc, cctx);
+                status |= gr_mpoly_set_coeff_si_ui(f, sc, exp, ctx);
+            }
+            else if (which == 3)
+            {
+                fmpz_t zc;
+                fmpz_init(zc);
+                fmpz_randtest(zc, state, 100);
+                status |= gr_set_fmpz(c, zc, cctx);
+                status |= gr_mpoly_set_coeff_fmpz_ui(f, zc, exp, ctx);
+                fmpz_clear(zc);
+            }
+            else if (which == 4)
+            {
+                fmpq_t qc;
+                fmpq_init(qc);
+                fmpq_randtest(qc, state, 100);
+                status |= gr_set_fmpq(c, qc, cctx);
+                status |= gr_mpoly_set_coeff_fmpq_ui(f, qc, exp, ctx);
+                fmpq_clear(qc);
+            }
+
             gr_mpoly_assert_canonical(f, ctx);
             status |= gr_mpoly_get_coeff_scalar_ui(d, f, exp, ctx);
 
@@ -79,10 +118,46 @@ TEST_FUNCTION_START(gr_mpoly_get_set_coeff, state)
 
             status = GR_SUCCESS;
 
-            GR_MUST_SUCCEED(gr_randtest(c, state, cctx));
             _fmpz_vec_randtest(exp, state, nvars, exp_bits);
 
-            status |= gr_mpoly_set_coeff_scalar_fmpz(f, c, exp, ctx);
+            which = n_randint(state, 5);
+
+            if (which == 0)
+            {
+                GR_MUST_SUCCEED(gr_randtest(c, state, cctx));
+                status |= gr_mpoly_set_coeff_scalar_fmpz(f, c, exp, ctx);
+            }
+            else if (which == 1)
+            {
+                ulong uc = n_randtest(state);
+                status |= gr_set_ui(c, uc, cctx);
+                status |= gr_mpoly_set_coeff_ui_fmpz(f, uc, exp, ctx);
+            }
+            else if (which == 2)
+            {
+                slong sc = n_randtest(state);
+                status |= gr_set_si(c, sc, cctx);
+                status |= gr_mpoly_set_coeff_si_fmpz(f, sc, exp, ctx);
+            }
+            else if (which == 3)
+            {
+                fmpz_t zc;
+                fmpz_init(zc);
+                fmpz_randtest(zc, state, 100);
+                status |= gr_set_fmpz(c, zc, cctx);
+                status |= gr_mpoly_set_coeff_fmpz_fmpz(f, zc, exp, ctx);
+                fmpz_clear(zc);
+            }
+            else if (which == 4)
+            {
+                fmpq_t qc;
+                fmpq_init(qc);
+                fmpq_randtest(qc, state, 100);
+                status |= gr_set_fmpq(c, qc, cctx);
+                status |= gr_mpoly_set_coeff_fmpq_fmpz(f, qc, exp, ctx);
+                fmpq_clear(qc);
+            }
+
             gr_mpoly_assert_canonical(f, ctx);
             status |= gr_mpoly_get_coeff_scalar_fmpz(d, f, exp, ctx);
 
