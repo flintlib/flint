b1d7b8632989b62ae26ff22c445a96c66f8a8076
Fredrik Johansson <fredrik.johansson@gmail.com>
2025-01-23 17:28:03 +0100

    Use delayed canonicalization in fmpz_mod_mat_reduce_row; also handle error for gr

--- src/fmpz_mod_mat/reduce_row.c
+++ src/fmpz_mod_mat/reduce_row.c
@@ -1,6 +1,7 @@
 /*
     Copyright (C) 2015 William Hart
     Copyright (C) 2021 Daniel Schultz
+    Copyright (C) 2025 Fredrik Johansson
 
     This file is part of FLINT.
 
@@ -10,52 +11,73 @@
     (at your option) any later version.  See <https://www.gnu.org/licenses/>.
 */
 
+#include "gr.h"
 #include "fmpz.h"
 #include "fmpz_mod.h"
 #include "fmpz_mod_mat.h"
 
-slong fmpz_mod_mat_reduce_row(fmpz_mod_mat_t A, slong * P, slong * L,
-                                             slong m, const fmpz_mod_ctx_t ctx)
+int fmpz_mod_mat_reduce_row(slong * column, fmpz_mod_mat_t A, slong * P, slong * L,
+                                         slong m, const fmpz_mod_ctx_t ctx)
 {
-    slong n = fmpz_mod_mat_ncols(A, ctx), i, j, r;
-    fmpz_t h;
+    slong n = fmpz_mod_mat_ncols(A, ctx), i, j, r, res = -WORD(1);
+    fmpz_t d, h;
+    int status = GR_SUCCESS;
 
+    fmpz_init(d);
     fmpz_init(h);
 
     for (i = 0; i < n; i++)
     {
+        if (i != 0)
+            fmpz_mod_set_fmpz(fmpz_mod_mat_entry(A, m, i),
+                              fmpz_mod_mat_entry(A, m, i), ctx);
+
         if (!fmpz_is_zero(fmpz_mod_mat_entry(A, m, i)))
         {
             r = P[i];
+
             if (r != -WORD(1))
             {
                 for (j = i + 1; j < L[r]; j++)
                 {
-                    fmpz_mod_mul(h, fmpz_mod_mat_entry(A, r, j),
-                                    fmpz_mod_mat_entry(A, m, i), ctx);
-                    fmpz_mod_sub(fmpz_mod_mat_entry(A, m, j),
-                                 fmpz_mod_mat_entry(A, m, j), h, ctx);
+                    fmpz_submul(fmpz_mod_mat_entry(A, m, j),
+                        fmpz_mod_mat_entry(A, r, j),
+                        fmpz_mod_mat_entry(A, m, i));
                 }
 
                 fmpz_zero(fmpz_mod_mat_entry(A, m, i));
             }
             else
             {
-                fmpz_mod_inv(h, fmpz_mod_mat_entry(A, m, i), ctx);
-                fmpz_one(fmpz_mod_mat_entry(A, m, i));
+                /* fmpz_mod_inv(h, fmpz_mod_mat_entry(A, m, i), ctx); */
+                fmpz_gcdinv(d, h, fmpz_mod_mat_entry(A, m, i), ctx->n);
+                if (!fmpz_is_one(d))
+                {
+                    status = GR_DOMAIN;
+                    goto cleanup;
+                }
+
+                fmpz_mod_set_ui(fmpz_mod_mat_entry(A, m, i), 1, ctx);
 
                 for (j = i + 1; j < L[m]; j++)
+                {
+                    fmpz_mod_set_fmpz(fmpz_mod_mat_entry(A, m, j),
+                        fmpz_mod_mat_entry(A, m, j), ctx);
                     fmpz_mod_mul(fmpz_mod_mat_entry(A, m, j),
-                                 fmpz_mod_mat_entry(A, m, j), h, ctx);
+                        fmpz_mod_mat_entry(A, m, j), h, ctx);
+                }
 
                 P[i] = m;
+                res = i;
+                break;
 
-                fmpz_clear(h);
-                return i;
             }
         }
     }
 
+cleanup:
     fmpz_clear(h);
-    return -WORD(1);
+    fmpz_clear(d);
+    *column = res;
+    return status;
 }
