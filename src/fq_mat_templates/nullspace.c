e324f55eaff939aa2b1f57f6c3f7363276e82f36
Fredrik Johansson <fredrik.johansson@gmail.com>
2025-01-30 21:41:34 +0100

    Add gr_mat_nullspace_no_resize, gr_mat_nullspace_from_rref; make fq_*_mat nullspace and rref gr wrappers

--- src/fq_mat_templates/nullspace.c
+++ src/fq_mat_templates/nullspace.c
@@ -12,75 +12,18 @@
 
 #ifdef T
 
+#include "gr.h"
+#include "gr_mat.h"
 #include "templates.h"
 
 slong
 TEMPLATE(T, mat_nullspace) (TEMPLATE(T, mat_t) X, const TEMPLATE(T, mat_t) A,
                             const TEMPLATE(T, ctx_t) ctx)
 {
-    slong i, j, k, m, n, rank, nullity;
-    slong *p;
-    slong *pivots;
-    slong *nonpivots;
-    TEMPLATE(T, mat_t) tmp;
-
-    m = A->r;
-    n = A->c;
-
-    p = flint_malloc(sizeof(slong) * FLINT_MAX(m, n));
-
-    TEMPLATE(T, mat_init_set) (tmp, A, ctx);
-    rank = TEMPLATE(T, mat_rref) (tmp, tmp, ctx);
-    nullity = n - rank;
-
-    TEMPLATE(T, mat_zero) (X, ctx);
-
-    if (rank == 0)
-    {
-        for (i = 0; i < nullity; i++)
-            TEMPLATE(T, one) (TEMPLATE(T, mat_entry) (X, i, i), ctx);
-    }
-    else if (nullity)
-    {
-        pivots = p;             /* length = rank */
-        nonpivots = p + rank;   /* length = nullity */
-
-        for (i = j = k = 0; i < rank; i++)
-        {
-            while (TEMPLATE(T, is_zero)
-                   (TEMPLATE(T, mat_entry) (tmp, i, j), ctx))
-            {
-                nonpivots[k] = j;
-                k++;
-                j++;
-            }
-            pivots[i] = j;
-            j++;
-        }
-        while (k < nullity)
-        {
-            nonpivots[k] = j;
-            k++;
-            j++;
-        }
-
-        for (i = 0; i < nullity; i++)
-        {
-            for (j = 0; j < rank; j++)
-            {
-                TEMPLATE(T, neg) (TEMPLATE(T, mat_entry) (X, pivots[j], i),
-                                  TEMPLATE(T, mat_entry) (tmp, j,
-                                                          nonpivots[i]), ctx);
-            }
-
-            TEMPLATE(T, one) (TEMPLATE(T, mat_entry) (X, nonpivots[i], i),
-                              ctx);
-        }
-    }
-
-    flint_free(p);
-    TEMPLATE(T, mat_clear) (tmp, ctx);
-
+    gr_ctx_t gr_ctx;
+    slong nullity;
+    TEMPLATE3(_gr_ctx_init, T, from_ref)(gr_ctx, ctx);
+    GR_MUST_SUCCEED(gr_mat_nullspace_no_resize(&nullity, (gr_mat_struct *) X, (const gr_mat_struct *) A, gr_ctx));
     return nullity;
 }
 
--- src/fq_mat_templates/rref.c
+++ src/fq_mat_templates/rref.c
@@ -12,151 +12,18 @@
 
 #ifdef T
 
+#include "gr.h"
+#include "gr_mat.h"
 #include "templates.h"
-
-#include "perm.h"
 
 slong
 TEMPLATE(T, mat_rref) (TEMPLATE(T, mat_t) B, const TEMPLATE(T, mat_t) A, const TEMPLATE(T, ctx_t) ctx)
 {
-    slong i, j, k, n, rank;
-    slong *pivots;
-    slong *nonpivots;
-    slong *P;
-    TEMPLATE(T, struct) * e;
-    TEMPLATE(T, mat_t) U, V;
-
-    TEMPLATE(T, mat_set)(B, A, ctx);
-
-    if (TEMPLATE(T, mat_is_zero)(B, ctx))
-        return 0;
-
-    if (B->r == 1)
-    {
-        TEMPLATE(T, struct) * c;
-        slong i, j;
-        slong r = 0;
-
-        for (i = 0; i < B->c; i++)
-        {
-            c = TEMPLATE(T, mat_entry)(B, 0, i);
-            if (!TEMPLATE(T, is_zero)(c, ctx))
-            {
-                r = 1;
-                if (TEMPLATE(T, is_one)(c, ctx))
-                    break;
-
-                TEMPLATE(T, inv)(c, c, ctx);
-                for (j = i + 1;j < B->c; j++)
-                {
-                    TEMPLATE(T, mul)(TEMPLATE(T, mat_entry)(B, 0, j), TEMPLATE(T, mat_entry)(B, 0, j), c, ctx);
-                }
-                TEMPLATE(T, one)(c, ctx);
-                break;
-            }
-        }
-        return r;
-    }
-
-    n = A->c;
-
-    P = _perm_init(TEMPLATE(T, mat_nrows) (B, ctx));
-    rank = TEMPLATE(T, mat_lu) (P, B, 0, ctx);
-    _perm_clear(P);
-
-    if (rank == 0)
-        return rank;
-
-    /* Clear L */
-    for (i = 0; i < B->r; i++)
-        for (j = 0; j < FLINT_MIN(i, rank); j++)
-            TEMPLATE(T, zero) (TEMPLATE(T, mat_entry) (B, i, j), ctx);
-
-    /* We now reorder U to proper upper triangular form U | V
-       with U full-rank triangular, set V = U^(-1) V, and then
-       put the column back in the original order.
-
-       An improvement for some matrices would be to compress V by
-       discarding columns containing nothing but zeros. */
-
-    TEMPLATE(T, mat_init) (U, rank, rank, ctx);
-    TEMPLATE(T, mat_init) (V, rank, n - rank, ctx);
-
-    pivots = flint_malloc(sizeof(slong) * rank);
-    nonpivots = flint_malloc(sizeof(slong) * (n - rank));
-
-    for (i = j = k = 0; i < rank; i++)
-    {
-        while (TEMPLATE(T, is_zero) (TEMPLATE(T, mat_entry) (B, i, j), ctx))
-        {
-            nonpivots[k] = j;
-            k++;
-            j++;
-        }
-        pivots[i] = j;
-        j++;
-    }
-    while (k < n - rank)
-    {
-        nonpivots[k] = j;
-        k++;
-        j++;
-    }
-
-    for (i = 0; i < rank; i++)
-    {
-        for (j = 0; j <= i; j++)
-        {
-            e = TEMPLATE(T, mat_entry) (B, j, pivots[i]);
-            TEMPLATE(T, mat_entry_set) (U, j, i, e, ctx);
-        }
-    }
-
-    for (i = 0; i < n - rank; i++)
-    {
-        for (j = 0; j < rank; j++)
-        {
-            e = TEMPLATE(T, mat_entry) (B, j, nonpivots[i]);
-            TEMPLATE(T, mat_entry_set) (V, j, i, e, ctx);
-        }
-    }
-
-    TEMPLATE(T, mat_solve_triu) (V, U, V, 0, ctx);
-
-    /* Clear pivot columns */
-    for (i = 0; i < rank; i++)
-    {
-        for (j = 0; j <= i; j++)
-        {
-            if (i == j)
-            {
-                TEMPLATE(T, one) (TEMPLATE(T, mat_entry) (B, j, pivots[i]),
-                                  ctx);
-            }
-            else
-            {
-                TEMPLATE(T, zero) (TEMPLATE(T, mat_entry) (B, j, pivots[i]),
-                                   ctx);
-            }
-        }
-    }
-
-    /* Write back the actual content */
-    for (i = 0; i < n - rank; i++)
-    {
-        for (j = 0; j < rank; j++)
-            TEMPLATE(T, mat_entry_set) (B, j, nonpivots[i],
-                                        TEMPLATE(T, mat_entry) (V, j, i), ctx);
-    }
-
-    TEMPLATE(T, mat_clear) (U, ctx);
-    TEMPLATE(T, mat_clear) (V, ctx);
-
-    flint_free(pivots);
-    flint_free(nonpivots);
-
+    gr_ctx_t gr_ctx;
+    slong rank;
+    TEMPLATE3(_gr_ctx_init, T, from_ref)(gr_ctx, ctx);
+    GR_MUST_SUCCEED(gr_mat_rref_lu(&rank, (gr_mat_struct *) B, (const gr_mat_struct *) A, gr_ctx));
     return rank;
 }
-
 
 #endif
