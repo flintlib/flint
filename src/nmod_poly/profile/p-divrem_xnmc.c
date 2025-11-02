d535b580be43d9cf4074316145658de7f052bdb3
Vincent Neiger <vneiger@users.noreply.github.com>
2025-11-02 12:39:36 +0100

    update profile file

--- src/nmod_poly/profile/p-divrem_xnmc.c
+++ src/nmod_poly/profile/p-divrem_xnmc.c
@@ -127,149 +127,135 @@ void sample_interface(void * arg, ulong count)
 
 void sample_generic(void * arg, ulong count)
 {
-    ulong n;
+    ulong modn;
     nmod_t mod;
     info_t * info = (info_t *) arg;
     flint_bitcnt_t bits = info->bits;
     slong length = info->length;
+    ulong n = info->n;
+    ulong c;
+
     slong i, j;
 
-    ulong * poly;
-    ulong pt;
+    nn_ptr qr;
 
     FLINT_TEST_INIT(state);
 
-    poly = _nmod_vec_init(length);
-
-    for (i = 0; i < count; i++)
+    for (i = 0; i < (slong)count; i++)
     {
-        n = n_randbits(state, bits);
-        if (n == UWORD(0)) n++;
+        modn = n_randprime(state, bits, 1);
+        nmod_init(&mod, modn);
 
-        nmod_init(&mod, n);
-
+        qr = _nmod_vec_init(length);
         for (j = 0; j < length; j++)
-            poly[j] = n_randint(state, n);
+            qr[j] = n_randint(state, modn);
 
-        pt = n_randint(state, n);
+        c = _get_c_from_ctype(info->ctype, state, modn);
 
         prof_start();
-        for (j = 0; j < 100; j++)
-            _nmod_poly_evaluate_nmod(poly, length, pt, mod);
+        _nmod_poly_divrem_xnmc(qr, qr, length, n, c, mod);
         prof_stop();
-    }
 
-    _nmod_vec_clear(poly);
+        _nmod_vec_clear(qr);
+    }
 
     FLINT_TEST_CLEAR(state);
 }
 
 void sample_precomp(void * arg, ulong count)
 {
-    ulong n;
-    nmod_t mod;
+    ulong modn;
     info_t * info = (info_t *) arg;
     flint_bitcnt_t bits = info->bits;
     slong length = info->length;
+    ulong n = info->n;
+    ulong c;
+
     slong i, j;
 
-    ulong * poly;
-    ulong pt;
+    nn_ptr qr;
 
     FLINT_TEST_INIT(state);
 
-    poly = _nmod_vec_init(length);
-
-    for (i = 0; i < count; i++)
+    for (i = 0; i < (slong)count; i++)
     {
-        n = n_randbits(state, bits);
-        if (n == UWORD(0)) n++;
+        modn = n_randprime(state, bits, 1);
 
-        nmod_init(&mod, n);
-
+        qr = _nmod_vec_init(length);
         for (j = 0; j < length; j++)
-            poly[j] = n_randint(state, n);
+            qr[j] = n_randint(state, modn);
 
-        pt = n_randint(state, n);
+        c = _get_c_from_ctype(info->ctype, state, modn);
 
         prof_start();
-        for (j = 0; j < 100; j++)
-        {
-            const ulong pt_precomp = n_mulmod_precomp_shoup(pt, n);
-            _nmod_poly_evaluate_nmod_precomp(poly, length, pt, pt_precomp, n);
-        }
+        const ulong c_precomp = n_mulmod_precomp_shoup(c, modn);
+        _nmod_poly_divrem_xnmc_precomp(qr, qr, length, n, c, c_precomp, modn);
         prof_stop();
-    }
 
-    _nmod_vec_clear(poly);
+        _nmod_vec_clear(qr);
+    }
 
     FLINT_TEST_CLEAR(state);
 }
 
 void sample_precomp_lazy(void * arg, ulong count)
 {
-    ulong n;
-    nmod_t mod;
+    ulong modn;
     info_t * info = (info_t *) arg;
     flint_bitcnt_t bits = info->bits;
     slong length = info->length;
+    ulong n = info->n;
+    ulong c;
+
     slong i, j;
 
-    ulong * poly;
-    ulong pt;
+    nn_ptr qr;
 
     FLINT_TEST_INIT(state);
 
-    poly = _nmod_vec_init(length);
-
-    for (i = 0; i < count; i++)
+    for (i = 0; i < (slong)count; i++)
     {
-        n = n_randbits(state, bits);
-        if (n == UWORD(0)) n++;
+        modn = n_randprime(state, bits, 1);
 
-        nmod_init(&mod, n);
-
+        qr = _nmod_vec_init(length);
         for (j = 0; j < length; j++)
-            poly[j] = n_randint(state, n);
+            qr[j] = n_randint(state, modn);
 
-        pt = n_randint(state, n);
+        c = _get_c_from_ctype(info->ctype, state, modn);
 
         prof_start();
-        for (j = 0; j < 100; j++)
-        {
-            const ulong pt_precomp = n_mulmod_precomp_shoup(pt, n);
-            ulong val = _nmod_poly_evaluate_nmod_precomp_lazy(poly, length, pt, pt_precomp, n);
-            if (val >= 2*n)
-                val -= 2*n;
-            else if (val >= n)
-                val -= n;
-        }
+        const ulong c_precomp = n_mulmod_precomp_shoup(c, modn);
+        _nmod_poly_divrem_xnmc_precomp_lazy(qr, qr, length, n, c, c_precomp, modn);
         prof_stop();
-    }
 
-    _nmod_vec_clear(poly);
+        _nmod_vec_clear(qr);
+    }
 
     FLINT_TEST_CLEAR(state);
 }
 
 int main(void)
 {
-    const int nblengths = 17;
-    slong lengths[17] = {5, 10, 20, 40, 80,
-                        160, 320, 640, 1280, 2560,
-                        5120, 10240, 20480, 40960, 81920,
-                        163840, 327680};
+    const int nblengths = 15;
+    slong lengths[15] = {5, 40, 80, 160, 320,
+                        640, 1280, 2560, 5120, 10240,
+                        20480, 40960, 81920, 163840, 327680};
 
     double min, max;
-    double mins_divrem[nblengths];
-    double mins_interface[nblengths];
-    double mins_generic[nblengths];
-    double mins_precomp[nblengths];
-    double mins_precomp_lazy[nblengths];
+    double mins_divrem;
+    double mins_interface;
+    double mins_generic;
+    double mins_precomp;
+    double mins_precomp_lazy;
     info_t info;
     flint_bitcnt_t i;
 
-    flint_printf("profiled: divrem | interface | generic | precomp | precomp_lazy\n");
+    flint_printf("profiled:\n");
+    flint_printf("\t1. divrem\n\t2. poly interface\n\t3. vec general\n");
+    flint_printf("\t4. vec precomp\n\t5. precomp_lazy (no excess correction)\n");
+    flint_printf("column c: 0 -> c == -1, 1 -> c == 1, 2 -> c general\n");
+    flint_printf("note: variant 2. expected to be faster than variants 3.4.5. when c == 1 or c == -1\n\n");
+    flint_printf("bit c len    n      1.      2.      3.      4.      5.\n");
 
     for (i = 62; i <= FLINT_BITS; i++)
     {
@@ -278,66 +264,50 @@ int main(void)
         for (int ct = 0; ct <= 2; ct++)
         {
             info.ctype = ct;
-            printf("nbits = %ld, c type = %d\n", i, ct);
             for (int len = 0; len < nblengths; ++len)
             {
                 info.length = lengths[len];
-                info.n = FLINT_MAX(1, lengths[len] / 5);
 
-                prof_repeat(&min, &max, sample_divrem, (void *) &info);
-                mins_divrem[len-1] = min;
-                prof_repeat(&min, &max, sample_interface, (void *) &info);
-                mins_interface[len-1] = min;
-                /* prof_repeat(&min, &max, sample_generic, (void *) &info); */
-                mins_generic[len-1] = min;
-                /* prof_repeat(&min, &max, sample_precomp, (void *) &info); */
-                mins_precomp[len-1] = min;
-                /* prof_repeat(&min, &max, sample_precomp_lazy, (void *) &info); */
-                mins_precomp_lazy[len-1] = min;
-            }
+                info.n = 1;
+                while (info.n < (ulong)lengths[len])
+                {
+                    prof_repeat(&min, &max, sample_divrem, (void *) &info);
+                    mins_divrem = min/(double)FLINT_CLOCK_SCALE_FACTOR;
+                    prof_repeat(&min, &max, sample_interface, (void *) &info);
+                    mins_interface = min/(double)FLINT_CLOCK_SCALE_FACTOR;
+                    prof_repeat(&min, &max, sample_generic, (void *) &info);
+                    mins_generic = min/(double)FLINT_CLOCK_SCALE_FACTOR;
+                    prof_repeat(&min, &max, sample_precomp, (void *) &info);
+                    mins_precomp = min/(double)FLINT_CLOCK_SCALE_FACTOR;
+                    prof_repeat(&min, &max, sample_precomp_lazy, (void *) &info);
+                    mins_precomp_lazy = min/(double)FLINT_CLOCK_SCALE_FACTOR;
 
-            if (i < FLINT_BITS-1)
-            {
-                for (int len = 0; len < nblengths; ++len)
-                {
-                    flint_printf("   len %ld\t%.1e\t%.1e\t%.1e\t%.1e\t%.1e",
-                                 lengths[len],
-                                 mins_divrem[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_interface[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_generic[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_precomp[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_precomp_lazy[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR);
-                    flint_printf("\n");
-                }
-            }
-            else if (i < FLINT_BITS)
-            {
-                for (int len = 0; len < nblengths; ++len)
-                {
-                    flint_printf("   len %ld\t%.1e\t%.1e\t%.1e\t%.1e\t na",
-                                 lengths[len],
-                                 mins_divrem[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_interface[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_generic[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_precomp[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR);
-                    flint_printf("\n");
-                }
-            }
-            else  // i == FLINT_BITS
-            {
-                for (int len = 0; len < nblengths; ++len)
-                {
-                    flint_printf("   len %ld\t%.1e\t%.1e\t%.1e\t na \t na",
-                                 lengths[len],
-                                 mins_divrem[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_interface[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR,
-                                 mins_generic[len-1]/(double)FLINT_CLOCK_SCALE_FACTOR);
-                    flint_printf("\n");
+                    if (i < FLINT_BITS-1)
+                    {
+                        flint_printf("%-4d%-2d%-7d%-7d%.1e %.1e %.1e %.1e %.1e",
+                                    info.bits, info.ctype, info.length, info.n,
+                                    mins_divrem, mins_interface, mins_generic, mins_precomp, mins_precomp_lazy);
+                        flint_printf("\n");
+                    }
+                    else if (i < FLINT_BITS)
+                    {
+                        flint_printf("%-4d%-2d%-7d%-7d%.1e %.1e %.1e %.1e  na",
+                                    info.bits, info.ctype, info.length, info.n,
+                                    mins_divrem, mins_interface, mins_generic, mins_precomp);
+                        flint_printf("\n");
+                    }
+                    else  /* i == FLINT_BITS */
+                    {
+                        flint_printf("%-4d%-2d%-7d%-7d%.1e %.1e %.1e  na     na",
+                                    info.bits, info.ctype, info.length, info.n,
+                                    mins_divrem, mins_interface, mins_generic);
+                        flint_printf("\n");
+                    }
+
+                    info.n = 5 * info.n;
                 }
             }
         }
-
-        flint_printf("\n");
     }
 
     return 0;
