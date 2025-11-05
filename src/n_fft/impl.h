29041af4e7a1ef2c3e886a3e92345a6756a8fa6b
Vincent Neiger <vneiger@users.noreply.github.com>
2025-11-06 00:26:01 +0100

    add impl.h

---
+++ src/n_fft/impl.h
@@ -0,0 +1,25 @@
+/*
+    Copyright (C) 2025 Vincent Neiger
+
+    This file is part of FLINT.
+
+    FLINT is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.  See <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef N_FFT_IMPL_H
+#define N_FFT_IMPL_H
+
+#include "n_fft.h"
+
+void dft_node_lazy_4_4(nn_ptr p, ulong depth, ulong node, n_fft_args_t F);
+void dft_lazy_2_4(nn_ptr p, ulong depth, n_fft_args_t F);
+void dft_lazy_1_4(nn_ptr p, ulong depth, n_fft_args_t F);
+
+void idft_node_lazy_1_2(nn_ptr p, ulong depth, ulong node, n_fft_args_t F);
+void idft_lazy_1_4(nn_ptr p, ulong depth, n_fft_args_t F);
+
+#endif  /* N_FFT_IMPL_H */
+
