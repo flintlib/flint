bbc2c953ae028d7c8dbfa74e057bd52397648a5d
Fredrik Johansson <fredrik.johansson@gmail.com>
2026-02-19 22:35:15 +0100

    Improve radix_mul_1

--- src/radix/mul_1.c
+++ src/radix/mul_1.c
@@ -38,54 +38,20 @@ radix_mul_1(nn_ptr z, nn_srcptr a, slong n, ulong c, const radix_t radix)
         FLINT_ASSERT(cy < mod.n);
         return cy;
     }
-    else if (sbits <= 2 * FLINT_BITS)
-    {
-        ulong cy[2] = { 0, 0, };
-
-        if (mod.norm == 0)
-        {
-            for (i = 0; i < n; i++)
-            {
-                umul_ppmm(hi, lo, a[i], c);
-                add_ssaaaa(cy[1], cy[0], cy[1], cy[0], hi, lo);
-                z[i] = flint_mpn_divrem_2_1_preinv_norm(cy, cy, mod.n, mod.ninv);
-            }
-        }
-        else
-        {
-            for (i = 0; i < n; i++)
-            {
-                umul_ppmm(hi, lo, a[i], c);
-                add_ssaaaa(cy[1], cy[0], cy[1], cy[0], hi, lo);
-                z[i] = flint_mpn_divrem_2_1_preinv_unnorm(cy, cy, mod.n, mod.ninv, mod.norm);
-            }
-        }
-
-        FLINT_ASSERT(cy[0] < mod.n);
-        FLINT_ASSERT(cy[1] == 0);
-        return cy[0];
-    }
     else
     {
         if (mod.norm == 0)
         {
-            ulong r;
             ulong cy0 = 0;
             ulong cy1 = 0;
-            ulong cy2 = 0;
+            ulong r;
 
-            umul_ppmm(hi, lo, a[0], c);
-            udiv_qrnnd_preinv(cy0, r, hi, lo, mod.n, mod.ninv);
-            z[0] = r;
-
-            for (i = 1; i < n; i++)
+            for (i = 0; i < n; i++)
             {
                 umul_ppmm(hi, lo, a[i], c);
-                add_sssaaaaaa(cy2, cy1, cy0, cy2, cy1, cy0, 0, hi, lo);
-                /* cy2 is certainly already reduced */
-                udiv_qrnnd_preinv(cy1, r, cy2, cy1, mod.n, mod.ninv);
+                add_ssaaaa(cy1, cy0, cy1, cy0, hi, lo);
+                r = n_divrem_norm(&cy1, cy1, mod.n);
                 udiv_qrnnd_preinv(cy0, r, r, cy0, mod.n, mod.ninv);
-                cy2 = 0;
                 z[i] = r;
             }
 
@@ -95,18 +61,17 @@ radix_mul_1(nn_ptr z, nn_srcptr a, slong n, ulong c, const radix_t radix)
         }
         else
         {
-            ulong cy[3] = { 0, 0, 0 };
+            ulong cy[2] = { 0, 0, };
 
             for (i = 0; i < n; i++)
             {
                 umul_ppmm(hi, lo, a[i], c);
-                add_sssaaaaaa(cy[2], cy[1], cy[0], cy[2], cy[1], cy[0], 0, hi, lo);
-                z[i] = flint_mpn_divrem_3_1_preinv_unnorm(cy, cy, mod.n, mod.ninv, mod.norm);
+                add_ssaaaa(cy[1], cy[0], cy[1], cy[0], hi, lo);
+                z[i] = flint_mpn_divrem_2_1_preinv_unnorm(cy, cy, mod.n, mod.ninv, mod.norm);
             }
 
             FLINT_ASSERT(cy[0] < mod.n);
             FLINT_ASSERT(cy[1] == 0);
-            FLINT_ASSERT(cy[2] == 0);
             return cy[0];
         }
     }
